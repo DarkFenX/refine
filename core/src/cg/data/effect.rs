48a39315c10c6c1aef1b02e1ade7be38f140ce7e
DarkPhoenix <phoenix@mail.ru>
2020-05-28 20:20:28 +0300

    Implement FKs for effects

--- core/src/cg/data/effect.rs
+++ core/src/cg/data/effect.rs
@@ -1,4 +1,4 @@
-use crate::{defines::ReeInt, dh};
+use crate::{defines::ReeInt, dh, util};
 
 use super::{Data, Fk, Pk};
 
@@ -10,21 +10,45 @@ impl Pk for dh::Effect {
 
 impl Fk for dh::Effect {
     fn get_item_fks(&self, _: &Data) -> Vec<ReeInt> {
-        Vec::new()
+        self.get_fks_from_mod_args("skillTypeID")
     }
     fn get_item_group_fks(&self, _: &Data) -> Vec<ReeInt> {
-        Vec::new()
+        self.get_fks_from_mod_args("groupID")
     }
     fn get_attr_fks(&self, _: &Data) -> Vec<ReeInt> {
-        Vec::new()
+        let mut vec = Vec::new();
+        util::vec_push_opt(&mut vec, self.discharge_attr_id);
+        util::vec_push_opt(&mut vec, self.duration_attr_id);
+        util::vec_push_opt(&mut vec, self.range_attr_id);
+        util::vec_push_opt(&mut vec, self.falloff_attr_id);
+        util::vec_push_opt(&mut vec, self.tracking_attr_id);
+        util::vec_push_opt(&mut vec, self.usage_chance_attr_id);
+        util::vec_push_opt(&mut vec, self.resist_attr_id);
+        vec.extend(self.get_fks_from_mod_args("modifyingAttributeID"));
+        vec.extend(self.get_fks_from_mod_args("modifiedAttributeID"));
+        vec
     }
     fn get_effect_fks(&self, _: &Data) -> Vec<ReeInt> {
-        Vec::new()
+        // EffectStopper modifier type uses this argument
+        self.get_fks_from_mod_args("effectID")
     }
     fn get_fighter_abil_fks(&self, _: &Data) -> Vec<ReeInt> {
         Vec::new()
     }
     fn get_buff_fks(&self, _: &Data) -> Vec<ReeInt> {
         Vec::new()
+    }
+}
+impl dh::Effect {
+    fn get_fks_from_mod_args(&self, field: &'static str) -> Vec<ReeInt> {
+        let mut vec = Vec::new();
+        for modifier in self.mods.iter() {
+            for (k, v) in modifier.args.iter() {
+                if let (true, &dh::Primitive::Int(fk)) = (k == field, v) {
+                    vec.push(fk);
+                }
+            }
+        }
+        vec
     }
 }
