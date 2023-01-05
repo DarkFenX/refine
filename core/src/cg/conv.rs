508ec2481eb2e1fbf947b8b8159219aa1b5d38f8
DarkPhoenix <phoenix@mail.ru>
2023-01-05 03:37:15 +0300

    Convert items

--- core/src/cg/conv.rs
+++ core/src/cg/conv.rs
@@ -3,23 +3,82 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 use crate::{
-    consts::{ModAfeeFilter, ModAggrMode, ModDomain, ModOp},
+    consts::{ItemType, ModAfeeFilter, ModAggrMode, ModDomain, ModOp},
     ct,
     defines::ReeInt,
     util::{Error, Result},
 };
 
-use super::Data;
+use super::{data::Support, Data};
 
 // Convert data handler-provided entities into cacheable types.
-pub(super) fn convert(data: &Data, warns: &mut Vec<String>) {
+pub(super) fn convert(data: &Data, supp: &Support, warns: &mut Vec<String>) {
+    let items = conv_items(data, supp, warns);
     let attrs = conv_attrs(data);
     let mutas = conv_mutas(data);
     let buffs = conv_buffs(data, warns);
 }
 
-// fn conv_items(data: &Data) -> Vec<ct::Item> {
-// }
+fn conv_items(data: &Data, supp: &Support, warns: &mut Vec<String>) -> Vec<ct::Item> {
+    // Auxiliary maps
+    let defeff_map = data
+        .item_effects
+        .iter()
+        .filter(|v| v.is_default)
+        .map(|v| (v.item_id, v.effect_id))
+        .collect::<HashMap<ReeInt, ReeInt>>();
+    let mut item_map = HashMap::new();
+    for item_data in data.items.iter() {
+        // Item category ID
+        let cat_id = match supp.grp_cat_map.get(&item_data.group_id) {
+            Some(&cid) => cid,
+            None => {
+                let msg = format!("unable to find category ID for item {}", item_data.id);
+                log::warn!("{}", &msg);
+                warns.push(msg);
+                continue;
+            }
+        };
+        // Item default effect
+        let defeff_id = match defeff_map.get(&item_data.id) {
+            Some(&deff) => Some(deff),
+            None => None,
+        };
+        // Item construction
+        let item = ct::Item::new(
+            item_data.id,
+            // TODO: needs proper processing
+            ItemType::ModHigh,
+            item_data.group_id,
+            cat_id,
+            HashMap::new(),
+            HashMap::new(),
+            defeff_id,
+            HashMap::new(),
+        );
+        item_map.insert(item.id, item);
+    }
+    // Item attributes
+    for item_attr in data.item_attrs.iter() {
+        item_map
+            .get_mut(&item_attr.item_id)
+            .and_then(|v| v.attr_vals.insert(item_attr.attr_id, item_attr.value));
+    }
+    // Item effects
+    for item_effect in data.item_effects.iter() {
+        item_map.get_mut(&item_effect.item_id).and_then(|v| {
+            v.effect_datas
+                .insert(item_effect.effect_id, ct::ItemEffData::new(None, None, None))
+        });
+    }
+    // Item skill requirements
+    for item_srq in data.item_srqs.iter() {
+        item_map
+            .get_mut(&item_srq.item_id)
+            .and_then(|v| v.srqs.insert(item_srq.skill_id, item_srq.level));
+    }
+    item_map.into_iter().map(|(_, v)| v).sorted_by_key(|v| v.id).collect()
+}
 
 fn conv_attrs(data: &Data) -> Vec<ct::Attr> {
     data.attrs
