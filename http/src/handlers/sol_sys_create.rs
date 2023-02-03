72c5435661ea9e3966b6be41137c7fec8e4ef00c
DarkPhoenix <phoenix@mail.ru>
2023-02-03 07:19:54 +0600

    Return body with solar system ID or error, if anything terrible happened

--- http/src/handlers/sol_sys_create.rs
+++ http/src/handlers/sol_sys_create.rs
@@ -5,13 +5,30 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use crate::state::AppState;
 
 #[derive(serde::Deserialize)]
-pub(crate) struct CreateSolSys {
+pub(crate) struct CreateSolSysReq {
     src_alias: Option<String>,
+}
+
+#[derive(serde::Serialize)]
+pub(crate) struct CreateSolSysResp {
+    id: String,
+}
+
+#[derive(serde::Serialize)]
+pub(crate) struct CreateSolSysErr {
+    error: String,
+}
+impl CreateSolSysErr {
+    fn new_from_err(error: reefast::Error) -> CreateSolSysErr {
+        CreateSolSysErr {
+            error: format!("{}", error.msg),
+        }
+    }
 }
 
 pub(crate) async fn create_sol_sys(
     State(state): State<Arc<AppState>>,
-    Json(payload): Json<CreateSolSys>,
+    Json(payload): Json<CreateSolSysReq>,
 ) -> impl IntoResponse {
     let src_mgr = state.src_mgr.clone();
     let r = tokio_rayon::spawn_fifo(move || match payload.src_alias {
@@ -22,9 +39,15 @@ pub(crate) async fn create_sol_sys(
     match r {
         Ok(s) => {
             let id = state.sol_sys_mgr.add_sol_sys(s).await;
-            StatusCode::CREATED
+            (StatusCode::CREATED, Json(CreateSolSysResp { id })).into_response()
         }
-        Err(e) if matches!(e.kind, reefast::ErrorKind::SrcNotFound) => StatusCode::UNPROCESSABLE_ENTITY,
-        _ => StatusCode::INTERNAL_SERVER_ERROR,
+        Err(e) if matches!(e.kind, reefast::ErrorKind::SrcNotFound) => {
+            (StatusCode::UNPROCESSABLE_ENTITY, Json(CreateSolSysErr::new_from_err(e))).into_response()
+        }
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(CreateSolSysErr::new_from_err(e)),
+        )
+            .into_response(),
     }
 }
