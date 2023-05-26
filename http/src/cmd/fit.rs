181c665019bbb96ca37db331007f142a3457762d
DarkPhoenix <phoenix@mail.ru>
2023-05-27 04:14:21 +0600

    Add methods to convert fit commands into solar system commands

--- http/src/cmd/fit.rs
+++ http/src/cmd/fit.rs
@@ -1,23 +1,57 @@
-use crate::cmd::shared::{AddMode, State};
+use crate::cmd::{
+    shared::{AddMode, State},
+    ss,
+};
+
+pub(crate) trait FillFitCmd<T> {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> T;
+}
 
 #[derive(serde::Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub(crate) enum FitCommand {
     SetShip(SetShipCmd),
-    AddModuleHigh(AddModule),
-    AddModuleMid(AddModule),
-    AddModuleLow(AddModule),
+    AddModuleHigh(AddModuleCmd),
+    AddModuleMid(AddModuleCmd),
+    AddModuleLow(AddModuleCmd),
+}
+impl FillFitCmd<ss::SsCommand> for FitCommand {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> ss::SsCommand {
+        match self {
+            FitCommand::SetShip(cmd) => ss::SsCommand::SetShip(cmd.fill_fit(fit_id)),
+            FitCommand::AddModuleHigh(cmd) => ss::SsCommand::AddModuleHigh(cmd.fill_fit(fit_id)),
+            FitCommand::AddModuleMid(cmd) => ss::SsCommand::AddModuleMid(cmd.fill_fit(fit_id)),
+            FitCommand::AddModuleLow(cmd) => ss::SsCommand::AddModuleLow(cmd.fill_fit(fit_id)),
+        }
+    }
 }
 
 #[derive(serde::Deserialize)]
 pub(crate) struct SetShipCmd {
     pub(crate) ship_type_id: reefast::ReeInt,
+    pub(crate) state: Option<bool>,
+}
+impl FillFitCmd<ss::SetShipCmd> for SetShipCmd {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> ss::SetShipCmd {
+        ss::SetShipCmd::new(fit_id, self.ship_type_id, self.state)
+    }
 }
 
 #[derive(serde::Deserialize)]
-pub(crate) struct AddModule {
+pub(crate) struct AddModuleCmd {
     pub(crate) add_mode: AddMode,
     pub(crate) module_type_id: reefast::ReeInt,
-    pub(crate) state: State,
     pub(crate) charge_type_id: Option<reefast::ReeInt>,
+    pub(crate) state: State,
+}
+impl FillFitCmd<ss::AddModuleCmd> for AddModuleCmd {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> ss::AddModuleCmd {
+        ss::AddModuleCmd::new(
+            fit_id,
+            self.add_mode,
+            self.module_type_id,
+            self.charge_type_id,
+            self.state,
+        )
+    }
 }
--- http/src/cmd/ss.rs
+++ http/src/cmd/ss.rs
@@ -4,9 +4,9 @@ use crate::cmd::shared::{AddMode, State};
 #[serde(tag = "type", rename_all = "snake_case")]
 pub(crate) enum SsCommand {
     SetShip(SetShipCmd),
-    AddModuleHigh(AddModule),
-    AddModuleMid(AddModule),
-    AddModuleLow(AddModule),
+    AddModuleHigh(AddModuleCmd),
+    AddModuleMid(AddModuleCmd),
+    AddModuleLow(AddModuleCmd),
 }
 
 #[derive(serde::Deserialize)]
@@ -15,12 +15,38 @@ pub(crate) struct SetShipCmd {
     pub(crate) ship_type_id: reefast::ReeInt,
     pub(crate) state: Option<bool>,
 }
+impl SetShipCmd {
+    pub(crate) fn new(fit_id: reefast::ReeId, ship_type_id: reefast::ReeInt, state: Option<bool>) -> Self {
+        Self {
+            fit_id,
+            ship_type_id,
+            state,
+        }
+    }
+}
 
 #[derive(serde::Deserialize)]
-pub(crate) struct AddModule {
+pub(crate) struct AddModuleCmd {
     pub(crate) fit_id: reefast::ReeId,
     pub(crate) add_mode: AddMode,
     pub(crate) module_type_id: reefast::ReeInt,
     pub(crate) charge_type_id: Option<reefast::ReeInt>,
     pub(crate) state: State,
+}
+impl AddModuleCmd {
+    pub(crate) fn new(
+        fit_id: reefast::ReeId,
+        add_mode: AddMode,
+        module_type_id: reefast::ReeInt,
+        charge_type_id: Option<reefast::ReeInt>,
+        state: State,
+    ) -> Self {
+        Self {
+            fit_id,
+            add_mode,
+            module_type_id,
+            charge_type_id,
+            state,
+        }
+    }
 }
