1e48f786a5d936d7f8d44e3a5dc3128f1751e2f2
DarkPhoenix <phoenix@mail.ru>
2023-05-29 16:40:24 +0600

    Instrument assertions in all support modules which have them

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,10 @@ import os
 
 import pytest
 
-pytest.register_assert_rewrite('tests.support.client.client')
+pytest.register_assert_rewrite(
+    'tests.support.client.client',
+    'tests.support.api_data.types.ss',
+    'tests.support.api_data.types.fit',
+    'tests.support.api_data.types.item')
 
 TEST_FOLDER_SPLIT = os.path.dirname(os.path.normpath(os.path.realpath(__file__))).split(os.sep)
