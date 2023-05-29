23b391660f05cd33f4144beeb7972a81a41434eb
DarkPhoenix <phoenix@mail.ru>
2023-05-30 00:02:52 +0600

    Implement new commands on fit level

--- http/src/cmd/fit.rs
+++ http/src/cmd/fit.rs
@@ -6,19 +6,34 @@ use crate::cmd::{
 #[derive(serde::Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub(crate) enum FitCommand {
+    AddImplant(AddImplantCmd),
     SetShip(SetShipCmd),
     AddModuleHigh(AddModuleCmd),
     AddModuleMid(AddModuleCmd),
     AddModuleLow(AddModuleCmd),
+    AddRig(AddRigCmd),
 }
 impl FitCommand {
     pub(crate) fn fill_fit(self, fit_id: reefast::ReeId) -> ss::SsCommand {
         match self {
+            FitCommand::AddImplant(cmd) => ss::SsCommand::AddImplant(cmd.fill_fit(fit_id)),
             FitCommand::SetShip(cmd) => ss::SsCommand::SetShip(cmd.fill_fit(fit_id)),
             FitCommand::AddModuleHigh(cmd) => ss::SsCommand::AddModuleHigh(cmd.fill_fit(fit_id)),
             FitCommand::AddModuleMid(cmd) => ss::SsCommand::AddModuleMid(cmd.fill_fit(fit_id)),
             FitCommand::AddModuleLow(cmd) => ss::SsCommand::AddModuleLow(cmd.fill_fit(fit_id)),
+            FitCommand::AddRig(cmd) => ss::SsCommand::AddRig(cmd.fill_fit(fit_id)),
         }
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub(crate) struct AddImplantCmd {
+    pub(crate) type_id: reefast::ReeInt,
+    pub(crate) state: Option<bool>,
+}
+impl AddImplantCmd {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> ss::AddImplantCmd {
+        ss::AddImplantCmd::new(fit_id, self.type_id, self.state)
     }
 }
 
@@ -49,5 +64,16 @@ impl AddModuleCmd {
             self.charge_type_id,
             self.state,
         )
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub(crate) struct AddRigCmd {
+    pub(crate) type_id: reefast::ReeInt,
+    pub(crate) state: Option<bool>,
+}
+impl AddRigCmd {
+    fn fill_fit(self, fit_id: reefast::ReeId) -> ss::AddRigCmd {
+        ss::AddRigCmd::new(fit_id, self.type_id, self.state)
     }
 }
