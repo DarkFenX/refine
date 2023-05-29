9442a3612264730be5a41a93d7b2e4ab67852b70
DarkPhoenix <phoenix@mail.ru>
2023-05-29 15:27:37 +0600

    Add utility to choose next free port

--- tests/support/util/__init__.py
+++ tests/support/util/__init__.py
@@ -1,3 +1,4 @@
 from .attr_dict import AttrDict
+from .port import next_free_port
 from .singletons import Absent, Default
 from .stack_key import get_stack_key
---
+++ tests/support/util/port.py
@@ -0,0 +1,19 @@
+import contextlib
+import socket
+
+
+def next_free_port(start_port):
+    for port in range(start_port, 65536):
+        if check_port_free(port=port):
+            return port
+    raise RuntimeError('unable to find free port')
+
+
+def check_port_free(port):
+    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        try:
+            s.bind(('0.0.0.0', port))
+        except OSError:
+            return False
+        else:
+            return True
