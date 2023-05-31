f1ed4b8e89b60c4fe630672280bed24c971fe9ae
DarkPhoenix <phoenix@mail.ru>
2023-06-01 04:29:34 +0600

    Expose HTTP and file handlers based on features, and use HTTP feature in our HTTP lib

--- edh-phobos/src/lib.rs
+++ edh-phobos/src/lib.rs
@@ -1,12 +1,16 @@
 //! EVE data handlers which use JSON produced by
 //! [Phobos](https://github.com/pyfa-org/Phobos) as a data source.
 
+#[cfg(feature = "file")]
 pub use handler_file::PhbFileDHandler;
+#[cfg(feature = "http")]
 pub use handler_http::PhbHttpDHandler;
 pub use util::{Error, ErrorKind, Result};
 
 mod data;
 mod fsd;
+#[cfg(feature = "file")]
 mod handler_file;
+#[cfg(feature = "http")]
 mod handler_http;
 mod util;
