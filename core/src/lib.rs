92feb1cebaaa2479ea0f7a193471532a190cc68f
DarkPhoenix <phoenix@mail.ru>
2023-06-07 06:16:37 +0600

    Reword core lib crate docs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,13 +8,14 @@
 //! setup consists of the following steps:
 //!
 //! - you feed an [`ed::EveDataHandler`](crate::ed::EveDataHandler) implementation and an
-//! [`ad::AdaptedDataHandler`](crate::ad::AdaptedDataHandler) implementation to the library
-//! - you create [`Src`](crate::Src). During its initialization, the library attempts to load cached
-//! adapted data. If the cached data is loaded successfully, the library compares its fingerprint
-//! (data version + library version at the time of cache generation) and current fingerprint
-//! (version of currently provided data + current library version). If cache couldn't be loaded or
-//! fingerprints mismatch, EVE data is fetched and converted into adapted data (this process is
-//! relatively heavy on both IO and CPU), which is then fed to adapted data handler implementation.
+//! [`ad::AdaptedDataHandler`](crate::ad::AdaptedDataHandler) implementation to the
+//! [`Src`](crate::Src) constructor
+//! - during [`Src`](crate::Src) initialization, the library attempts to load cached adapted data.
+//! If the cached data is loaded successfully, the library compares its fingerprint (data version +
+//! library version at the time of cache generation) and current fingerprint (version of currently
+//! provided data + current library version). If cache couldn't be loaded or fingerprints mismatch,
+//! EVE data is fetched and converted into adapted data (this process is relatively heavy on both IO
+//! and CPU), which is then fed to adapted data handler implementation.
 //! - you create [`SolarSystem`](crate::SolarSystem), and manipulate it to create fits with ships
 //! and items, and fetch data and stats
 
