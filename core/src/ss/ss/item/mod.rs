3b3deb1a9a2bf2674d95cac4e045713d91d50588
DarkPhoenix <phoenix@mail.ru>
2023-06-12 02:17:23 +0600

    Move item-specific solar system code to separate module

---
+++ core/src/ss/ss/item/mod.rs
@@ -0,0 +1,87 @@
+mod booster;
+mod character;
+mod charge;
+mod drone;
+mod fighter;
+mod implant;
+mod module;
+mod rig;
+mod ship;
+mod skill;
+mod stance;
+mod subsystem;
+mod sw_effect;
+
+use crate::{
+    defs::ReeId,
+    ss::{helpers, SolarSystem, SsInnerData},
+    ssi, ssn,
+    util::{Error, ErrorKind, Result},
+};
+
+impl SolarSystem {
+    // Public
+    pub fn get_item_info(&self, item_id: &ReeId) -> Result<ssn::SsItemInfo> {
+        self.get_item(item_id).map(|v| ssn::SsItemInfo::from_ss_item(v, self))
+    }
+    pub fn remove_item(&mut self, item_id: &ReeId) -> Result<()> {
+        let main = match self.items.get(item_id) {
+            Some(item) => item,
+            None => return Err(Error::new(ErrorKind::ItemIdNotFound(*item_id))),
+        };
+        helpers::remove_item(&main, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc));
+        match main {
+            // Remove reference to charge if it's charge which we're removing
+            // Item::Charge(c) => match self.items.get_mut(&c.cont_id) {
+            //     None => {
+            //         self.items.remove(item_id);
+            //         return Ok(())
+            //     },
+            //     Some(other) => match other {
+            //         Item::Module(m) => m.charge_id = None,
+            //         _ => (),
+            //     },
+            // },
+            // Remove charge if we're removing a module, charges cannot exist without their carrier
+            ssi::SsItem::Module(m) => match m.charge_a_item_id {
+                Some(other_id) => match self.items.remove(&other_id) {
+                    Some(charge) => {
+                        helpers::remove_item(&charge, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc))
+                    }
+                    _ => (),
+                },
+                _ => (),
+            },
+            _ => (),
+        };
+        self.items.remove(item_id);
+        Ok(())
+    }
+    // Non-public
+    fn alloc_item_id(&mut self) -> Result<ReeId> {
+        let start = self.item_cnt;
+        while self.items.contains_key(&self.item_cnt.0) {
+            self.item_cnt += 1;
+            if start == self.item_cnt {
+                return Err(Error::new(ErrorKind::ItemIdAllocFailed));
+            }
+        }
+        Ok(self.item_cnt.0)
+    }
+    fn add_item(&mut self, item: ssi::SsItem) {
+        let item_id = item.get_id();
+        self.items.insert(item_id, item);
+        let item = self.items.get(&item_id).unwrap();
+        helpers::add_item(item, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc));
+    }
+    fn get_item(&self, item_id: &ReeId) -> Result<&ssi::SsItem> {
+        self.items
+            .get(item_id)
+            .ok_or_else(|| Error::new(ErrorKind::ItemIdNotFound(*item_id)))
+    }
+    fn get_item_mut(&mut self, item_id: &ReeId) -> Result<&mut ssi::SsItem> {
+        self.items
+            .get_mut(item_id)
+            .ok_or_else(|| Error::new(ErrorKind::ItemIdNotFound(*item_id)))
+    }
+}
--- core/src/ss/ss/mod.rs
+++ core/src/ss/ss/mod.rs
@@ -6,28 +6,13 @@ use std::{
 use crate::{
     defs::{ReeId, ReeInt},
     src::Src,
-    ss::{
-        calc::{CalcSvc, SsAttrVal},
-        helpers,
-    },
-    ssi, ssn,
-    util::{Error, ErrorKind, Result},
+    ss::calc::{CalcSvc, SsAttrVal},
+    ssi,
+    util::Result,
 };
 
-mod booster;
-mod character;
-mod charge;
-mod drone;
-mod fighter;
 mod fit;
-mod implant;
-mod module;
-mod rig;
-mod ship;
-mod skill;
-mod stance;
-mod subsystem;
-mod sw_effect;
+mod item;
 
 pub(in crate::ss) struct SsInnerData<'a> {
     pub(in crate::ss) src: &'a Src,
@@ -66,68 +51,6 @@ impl SolarSystem {
             item.reload_a_item(&src)
         }
         self.src = src;
-    }
-    fn alloc_item_id(&mut self) -> Result<ReeId> {
-        let start = self.item_cnt;
-        while self.items.contains_key(&self.item_cnt.0) {
-            self.item_cnt += 1;
-            if start == self.item_cnt {
-                return Err(Error::new(ErrorKind::ItemIdAllocFailed));
-            }
-        }
-        Ok(self.item_cnt.0)
-    }
-    fn add_item(&mut self, item: ssi::SsItem) {
-        let item_id = item.get_id();
-        self.items.insert(item_id, item);
-        let item = self.items.get(&item_id).unwrap();
-        helpers::add_item(item, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc));
-    }
-    fn get_item(&self, item_id: &ReeId) -> Result<&ssi::SsItem> {
-        self.items
-            .get(item_id)
-            .ok_or_else(|| Error::new(ErrorKind::ItemIdNotFound(*item_id)))
-    }
-    fn get_item_mut(&mut self, item_id: &ReeId) -> Result<&mut ssi::SsItem> {
-        self.items
-            .get_mut(item_id)
-            .ok_or_else(|| Error::new(ErrorKind::ItemIdNotFound(*item_id)))
-    }
-    pub fn get_item_info(&self, item_id: &ReeId) -> Result<ssn::SsItemInfo> {
-        self.get_item(item_id).map(|v| ssn::SsItemInfo::from_ss_item(v, self))
-    }
-    pub fn remove_item(&mut self, item_id: &ReeId) -> Result<()> {
-        let main = match self.items.get(item_id) {
-            Some(item) => item,
-            None => return Err(Error::new(ErrorKind::ItemIdNotFound(*item_id))),
-        };
-        helpers::remove_item(&main, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc));
-        match main {
-            // Remove reference to charge if it's charge which we're removing
-            // Item::Charge(c) => match self.items.get_mut(&c.cont_id) {
-            //     None => {
-            //         self.items.remove(item_id);
-            //         return Ok(())
-            //     },
-            //     Some(other) => match other {
-            //         Item::Module(m) => m.charge_id = None,
-            //         _ => (),
-            //     },
-            // },
-            // Remove charge if we're removing a module, charges cannot exist without their carrier
-            ssi::SsItem::Module(m) => match m.charge_a_item_id {
-                Some(other_id) => match self.items.remove(&other_id) {
-                    Some(charge) => {
-                        helpers::remove_item(&charge, &mut SsInnerData::new(&self.src, &self.items, &mut self.calc))
-                    }
-                    _ => (),
-                },
-                _ => (),
-            },
-            _ => (),
-        };
-        self.items.remove(item_id);
-        Ok(())
     }
     // Attribute calculator
     pub fn get_item_attr(&mut self, item_id: &ReeId, attr_id: &ReeInt) -> Result<SsAttrVal> {
