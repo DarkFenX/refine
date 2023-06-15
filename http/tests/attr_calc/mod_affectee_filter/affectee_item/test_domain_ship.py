73e40a53e2a7baca5c84d1be0ffd8b0d74d8f674
DarkPhoenix <phoenix@mail.ru>
2023-06-16 05:15:01 +0600

    Add first tests for direct modifications

--- http/tests/attr_calc/mod_affectee_filter/affectee_item/test_domain_ship.py
+++ http/tests/attr_calc/mod_affectee_filter/affectee_item/test_domain_ship.py
@@ -5,7 +5,7 @@ def test_affected(client, consts):
     eve_src_attr = client.mk_eve_attr()
     eve_tgt_attr = client.mk_eve_attr()
     eve_mod = client.mk_eve_mod(
-        func=consts.ModFunc.loc,
+        func=consts.ModFunc.item,
         dom=consts.ModDom.ship,
         op=consts.ModOp.post_percent,
         src_attr_id=eve_src_attr.id,
@@ -16,8 +16,8 @@ def test_affected(client, consts):
     client.create_sources()
     api_ss = client.create_ss()
     api_fit = api_ss.create_fit()
-    api_src_item = api_fit.add_implant(type_id=eve_src_item.id)
-    api_tgt_item = api_fit.add_rig(type_id=eve_tgt_item.id)
+    api_src_item = api_fit.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.set_ship(type_id=eve_tgt_item.id)
     value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
     assert value == approx(120)
     api_src_item.remove()
@@ -25,11 +25,11 @@ def test_affected(client, consts):
     assert value == approx(100)
 
 
-def test_other_location(client, consts):
+def test_other_domain(client, consts):
     eve_src_attr = client.mk_eve_attr()
     eve_tgt_attr = client.mk_eve_attr()
     eve_mod = client.mk_eve_mod(
-        func=consts.ModFunc.loc,
+        func=consts.ModFunc.item,
         dom=consts.ModDom.ship,
         op=consts.ModOp.post_percent,
         src_attr_id=eve_src_attr.id,
@@ -40,7 +40,28 @@ def test_other_location(client, consts):
     client.create_sources()
     api_ss = client.create_ss()
     api_fit = api_ss.create_fit()
-    api_fit.add_implant(type_id=eve_src_item.id)
-    api_tgt_item = api_fit.add_implant(type_id=eve_tgt_item.id)
+    api_fit.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.set_char(type_id=eve_tgt_item.id)
+    value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
+    assert value == approx(100)
+
+
+def test_child_item(client, consts):
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_mod(
+        func=consts.ModFunc.item,
+        dom=consts.ModDom.ship,
+        op=consts.ModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_fit.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.add_rig(type_id=eve_tgt_item.id)
     value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
     assert value == approx(100)
