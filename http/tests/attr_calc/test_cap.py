c5f6b6b83f4c3d153bf0236f60b49c4054bd90b8
DarkPhoenix <phoenix@mail.ru>
2023-06-15 20:21:18 +0600

    Add another (for now failing) test

--- http/tests/attr_calc/test_cap.py
+++ http/tests/attr_calc/test_cap.py
@@ -53,7 +53,7 @@ def test_unmodified(client, consts):
 def test_modified(client, consts):
     # Check that cap is applied properly when item defines its value,
     # and it's modified further
-    eve_capping_attr = client.mk_eve_attr(def_val=5)
+    eve_capping_attr = client.mk_eve_attr()
     eve_capped_attr = client.mk_eve_attr(max_attr_id=eve_capping_attr.id)
     eve_src_attr = client.mk_eve_attr()
     eve_mod1 = client.mk_eve_mod(
@@ -79,3 +79,42 @@ def test_modified(client, consts):
     value = api_item.update().attr_vals[eve_capped_attr.id].dogma
     # Should be 3 * 6 = 18 without cap, but 0.1 * 6 = 0.6 with cap
     assert value == approx(0.6)
+
+
+def test_update(client, consts):
+    # Make sure that when value of capping attribute changes, values which
+    # depend on it are updated
+    eve_capping_attr = client.mk_eve_attr()
+    eve_capped_attr = client.mk_eve_attr(max_attr_id=eve_capping_attr.id)
+    eve_src_attr = client.mk_eve_attr()
+    eve_capped_mod = client.mk_eve_mod(
+        func=consts.ModFunc.item,
+        dom=consts.ModDom.item,
+        op=consts.ModOp.post_mul,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_capped_attr.id)
+    eve_capped_effect = client.mk_eve_effect(mod_info=[eve_capped_mod])
+    eve_capped_item = client.mk_eve_item(
+        attrs={eve_capping_attr.id: 2, eve_capped_attr.id: 3, eve_src_attr.id: 6},
+        eff_ids=[eve_capped_effect.id])
+    eve_capping_mod = client.mk_eve_mod(
+        func=consts.ModFunc.loc,
+        dom=consts.ModDom.ship,
+        op=consts.ModOp.post_mul,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_capping_attr.id)
+    eve_capping_effect = client.mk_eve_effect(mod_info=[eve_capping_mod])
+    eve_capping_item = client.mk_eve_item(attrs={eve_src_attr.id: 3.5}, eff_ids=[eve_capping_effect.id])
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_capped_item = api_fit.add_rig(type_id=eve_capped_item.id)
+    # Request capped attribute value before adding capping item, to make sure
+    # capping attribute value is calculated
+    value = api_capped_item.update().attr_vals[eve_capped_attr.id].dogma
+    assert value == approx(2)
+    api_fit.add_implant(type_id=eve_capping_item.id)
+    # Here, capping attribute should be multiplied by 3.5 (2 * 3.5 = 7), which
+    # is still below uncapped value of capped attribute (18)
+    value = api_capped_item.update().attr_vals[eve_capped_attr.id].dogma
+    assert value == approx(7)
