a88c40006a2a0445222e0340e121d2b30f33aba2
DarkPhoenix <phoenix@mail.ru>
2023-06-16 13:37:47 +0600

    Add awaiting tests

--- http/tests/attr_calc/mod_affectee_filter/affectee_item/test_domain_ship.py
+++ http/tests/attr_calc/mod_affectee_filter/affectee_item/test_domain_ship.py
@@ -16,6 +16,33 @@ def test_affected(client, consts):
     client.create_sources()
     api_ss = client.create_ss()
     api_fit = api_ss.create_fit()
+    api_tgt_item = api_fit.set_ship(type_id=eve_tgt_item.id)
+    api_src_item = api_fit.add_rig(type_id=eve_src_item.id)
+    value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
+    assert value == approx(120)
+    api_src_item.remove()
+    value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
+    assert value == approx(100)
+
+
+def test_awaiting(client, consts):
+    # Here we add modification before target, and make sure target is still
+    # modified by it. This should work with no specific code in reefast, but
+    # used to need extra processing in eos, so we test it just in case
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_mod(
+        func=consts.ModFunc.item,
+        dom=consts.ModDom.ship,
+        op=consts.ModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
     api_src_item = api_fit.add_rig(type_id=eve_src_item.id)
     api_tgt_item = api_fit.set_ship(type_id=eve_tgt_item.id)
     value = api_tgt_item.update().attr_vals[eve_tgt_attr.id].dogma
