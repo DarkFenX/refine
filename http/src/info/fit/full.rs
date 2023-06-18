26a2f516cdc75d618279c7c9dc440096ea147a34
DarkPhoenix <phoenix@mail.ru>
2023-06-18 08:25:20 +0600

    Add all supported item types to full fit info

--- http/src/info/fit/full.rs
+++ http/src/info/fit/full.rs
@@ -4,9 +4,17 @@ use crate::info::{HItemInfo, HItemInfoMode};
 pub(crate) struct HFitInfoFull {
     pub(crate) id: String,
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) character: Option<HItemInfo>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub(crate) implants: Vec<HItemInfo>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) ship: Option<HItemInfo>,
     #[serde(skip_serializing_if = "HModuleRacks::is_empty")]
     pub(crate) modules: HModuleRacks,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub(crate) rigs: Vec<HItemInfo>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub(crate) drones: Vec<HItemInfo>,
 }
 impl HFitInfoFull {
     pub(in crate::info::fit) fn mk_info(
@@ -14,6 +22,15 @@ impl HFitInfoFull {
         fit_id: &rc::ReeId,
         item_mode: HItemInfoMode,
     ) -> Self {
+        let character = core_ss
+            .get_fit_character_info(&fit_id)
+            .ok()
+            .map(|v| HItemInfo::mk_from_core_character(core_ss, &v, item_mode));
+        let implants = core_ss
+            .get_fit_implant_infos(&fit_id)
+            .iter()
+            .map(|v| HItemInfo::mk_from_core_implant(core_ss, v, item_mode))
+            .collect();
         let ship = core_ss
             .get_fit_ship_info(&fit_id)
             .ok()
@@ -33,10 +50,24 @@ impl HFitInfoFull {
             .iter()
             .map(|v| HItemInfo::mk_from_core_module(core_ss, v, item_mode))
             .collect();
+        let rigs = core_ss
+            .get_fit_rig_infos(&fit_id)
+            .iter()
+            .map(|v| HItemInfo::mk_from_core_rig(core_ss, v, item_mode))
+            .collect();
+        let drones = core_ss
+            .get_fit_drone_infos(&fit_id)
+            .iter()
+            .map(|v| HItemInfo::mk_from_core_drone(core_ss, v, item_mode))
+            .collect();
         Self {
             id: fit_id.to_string(),
+            character,
+            implants,
             ship,
             modules: HModuleRacks::from_infos(modules_high, modules_mid, modules_low),
+            rigs,
+            drones,
         }
     }
 }
