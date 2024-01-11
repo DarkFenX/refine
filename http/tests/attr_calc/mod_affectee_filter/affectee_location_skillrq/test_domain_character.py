b8330bc4f32dfccb3637d8295d63268dc999dc2a
DarkPhoenix <phoenix@mail.ru>
2024-01-11 22:36:59 +0600

    Add tests which check what happens when domain/location owner is set/unset

--- http/tests/attr_calc/mod_affectee_filter/affectee_location_skillrq/test_domain_character.py
+++ http/tests/attr_calc/mod_affectee_filter/affectee_location_skillrq/test_domain_character.py
@@ -78,3 +78,31 @@ def test_other_skillreq(client, consts):
     api_tgt_item = api_fit.add_implant(type_id=eve_tgt_item.id)
     value = api_tgt_item.update().attrs[eve_tgt_attr.id].dogma
     assert value == approx(100)
+
+
+def test_char_toggle(client, consts):
+    # Modifiers which target items on ship location shouldn't apply when ship isn't set
+    eve_skill = client.mk_eve_item()
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_mod(
+        func=consts.ModFunc.loc_srq,
+        dom=consts.ModDom.char,
+        srq=eve_skill.id,
+        op=consts.ModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100}, srqs={eve_skill.id: 1})
+    eve_char_item = client.mk_eve_item()
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_fit.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.add_implant(type_id=eve_tgt_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+    api_char_item = api_fit.set_char(type_id=eve_char_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(120)
+    api_char_item.remove()
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
