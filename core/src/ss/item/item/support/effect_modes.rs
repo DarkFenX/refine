922ceea9336c38df1c776b42f14559521a4b5621
DarkPhoenix <phoenix@mail.ru>
2024-02-29 01:30:56 +0600

    Get rid of OptMap

--- core/src/ss/item/item/support/effect_modes.rs
+++ core/src/ss/item/item/support/effect_modes.rs
@@ -1,11 +1,13 @@
-use crate::{config::DEFAULT_EFFECT_MODE, defs::EEffectId, ss::EffectMode, util::OptMap};
+use std::collections::HashMap;
+
+use crate::{config::DEFAULT_EFFECT_MODE, defs::EEffectId, ss::EffectMode};
 
 pub(in crate::ss) struct EffectModes {
-    data: OptMap<EEffectId, EffectMode>,
+    data: HashMap<EEffectId, EffectMode>,
 }
 impl EffectModes {
     pub(in crate::ss::item::item) fn new() -> Self {
-        Self { data: OptMap::new() }
+        Self { data: HashMap::new() }
     }
     // Query methods
     pub(in crate::ss) fn get(&self, effect_id: &EEffectId) -> &EffectMode {
@@ -14,9 +16,14 @@ impl EffectModes {
     // Modification methods
     pub(in crate::ss) fn set(&mut self, effect_id: EEffectId, mode: EffectMode) {
         if mode == DEFAULT_EFFECT_MODE {
-            self.data.remove(&effect_id)
+            self.data.remove(&effect_id);
+            // This struct is not intended to be used and re-used often,
+            // free up memory when we don't have elements
+            if self.data.is_empty() {
+                self.data = HashMap::new();
+            }
         } else {
-            self.data.insert(effect_id, mode)
+            self.data.insert(effect_id, mode);
         }
     }
 }
