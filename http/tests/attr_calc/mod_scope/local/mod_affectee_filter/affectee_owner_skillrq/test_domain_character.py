5ddd9cf11c74bb569eca478ee8f155e17f29e1f0
DarkPhoenix <phoenix@mail.ru>
2024-04-01 17:36:35 +0600

    Add other-fit test to local owner-skillreq tests

--- http/tests/attr_calc/mod_scope/local/mod_affectee_filter/affectee_owner_skillrq/test_domain_character.py
+++ http/tests/attr_calc/mod_scope/local/mod_affectee_filter/affectee_owner_skillrq/test_domain_character.py
@@ -26,6 +26,7 @@ def test_affected(client, consts):
 
 
 def test_non_owner_modifiable(client, consts):
+    # Check that items which are not marked as owner-modifiable do not receive modification
     eve_skill = client.mk_eve_item()
     eve_src_attr = client.mk_eve_attr()
     eve_tgt_attr = client.mk_eve_attr()
@@ -48,6 +49,7 @@ def test_non_owner_modifiable(client, consts):
 
 
 def test_other_skillreq(client, consts):
+    # Check that entities which don't have needed skill requirement are not affected
     eve_skill1 = client.mk_eve_item()
     eve_skill2 = client.mk_eve_item()
     eve_src_attr = client.mk_eve_attr()
@@ -67,4 +69,28 @@ def test_other_skillreq(client, consts):
     api_fit = api_ss.create_fit()
     api_fit.add_rig(type_id=eve_src_item.id)
     api_tgt_item = api_fit.add_drone(type_id=eve_tgt_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_other_fit(client, consts):
+    # Check that local modifications are not carried over to another fit
+    eve_skill = client.mk_eve_item()
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.own_srq,
+        dom=consts.EveModDom.char,
+        srq=eve_skill.id,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100}, srqs={eve_skill.id: 1})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit1 = api_ss.create_fit()
+    api_fit2 = api_ss.create_fit()
+    api_fit1.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit2.add_drone(type_id=eve_tgt_item.id)
     assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
