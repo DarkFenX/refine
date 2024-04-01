b4a53cfb6a6177535cbe955563b09d002266684e
DarkPhoenix <phoenix@mail.ru>
2024-04-01 22:23:32 +0600

    Finalize system-wide/system-effect location-filtered modification tests

---
+++ http/tests/attr_calc/mod_scope/system_wide_system/mod_affectee_filter/affectee_location/test_domain_other.py
@@ -0,0 +1,26 @@
+from pytest import approx
+
+
+def test_unaffected(client, consts):
+    # There isn't anything which can belong to a system-wide effect, so just check that ship and an
+    # item on it are not affected
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc,
+        dom=consts.EveModDom.other,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_parent_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    eve_child_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_src_item.id)
+    api_fit = api_ss.create_fit()
+    api_parent_tgt_item = api_fit.set_ship(type_id=eve_parent_tgt_item.id)
+    api_child_tgt_item = api_fit.add_rig(type_id=eve_child_tgt_item.id)
+    assert api_parent_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+    assert api_child_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
