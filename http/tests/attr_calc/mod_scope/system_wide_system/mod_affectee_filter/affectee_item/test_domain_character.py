479ba00a28578a03d280b76635f42adca077a60e
DarkPhoenix <phoenix@mail.ru>
2024-04-01 19:43:37 +0600

    Complete first test file for system-wide/system-effect modification

--- http/tests/attr_calc/mod_scope/system_wide_system/mod_affectee_filter/affectee_item/test_domain_character.py
+++ http/tests/attr_calc/mod_scope/system_wide_system/mod_affectee_filter/affectee_item/test_domain_character.py
@@ -16,8 +16,102 @@ def test_affected(client, consts):
     client.create_sources()
     api_ss = client.create_ss()
     api_fit = api_ss.create_fit()
+    api_tgt_item = api_fit.set_char(type_id=eve_tgt_item.id)
+    api_src_item = api_ss.add_sw_effect(type_id=eve_src_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(120)
+    api_src_item.remove()
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_awaiting(client, consts):
+    # Here we add modification before target, and make sure target is still modified by it. This
+    # should work with no specific code in reefast, but used to need extra processing in eos, so we
+    # test it just in case
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
     api_src_item = api_ss.add_sw_effect(type_id=eve_src_item.id)
     api_tgt_item = api_fit.set_char(type_id=eve_tgt_item.id)
     assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(120)
     api_src_item.remove()
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_multiple_fits(client, consts):
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit1 = api_ss.create_fit()
+    api_fit2 = api_ss.create_fit()
+    api_tgt_item1 = api_fit1.set_char(type_id=eve_tgt_item.id)
+    api_tgt_item2 = api_fit2.set_char(type_id=eve_tgt_item.id)
+    api_src_item = api_ss.add_sw_effect(type_id=eve_src_item.id)
+    assert api_tgt_item1.update().attrs[eve_tgt_attr.id].dogma == approx(120)
+    assert api_tgt_item2.update().attrs[eve_tgt_attr.id].dogma == approx(120)
+    api_src_item.remove()
+    assert api_tgt_item1.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+    assert api_tgt_item2.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_other_domain(client, consts):
+    # Make sure "top" entities described by other domains are not affected
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_ss.add_sw_effect(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.set_ship(type_id=eve_tgt_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_child_item(client, consts):
+    # Check that items (in this case implant) are not affected if they belong to location even if
+    # its "owner" (in this case character) is affected
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_ss.add_sw_effect(type_id=eve_src_item.id)
+    api_tgt_item = api_fit.add_implant(type_id=eve_tgt_item.id)
     assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
