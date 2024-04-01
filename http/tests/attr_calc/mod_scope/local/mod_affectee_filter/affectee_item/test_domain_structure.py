512e28596e476248a4e261d74530f44d917a574e
DarkPhoenix <phoenix@mail.ru>
2024-04-01 15:32:11 +0600

    Add a few more tests for scope local direct item modifications

--- http/tests/attr_calc/mod_scope/local/mod_affectee_filter/affectee_item/test_domain_structure.py
+++ http/tests/attr_calc/mod_scope/local/mod_affectee_filter/affectee_item/test_domain_structure.py
@@ -49,6 +49,7 @@ def test_awaiting(client, consts):
 
 
 def test_other_domain(client, consts):
+    # Make sure "top" entities described by other domains are not affected
     eve_src_attr = client.mk_eve_attr()
     eve_tgt_attr = client.mk_eve_attr()
     eve_mod = client.mk_eve_effect_mod(
@@ -69,6 +70,8 @@ def test_other_domain(client, consts):
 
 
 def test_child_item(client, consts):
+    # Check that items (in this case rig) are not affected if they belong to location even if its
+    # "owner" (in this case structure) is affected
     eve_src_attr = client.mk_eve_attr()
     eve_tgt_attr = client.mk_eve_attr()
     eve_mod = client.mk_eve_effect_mod(
@@ -85,4 +88,26 @@ def test_child_item(client, consts):
     api_fit = api_ss.create_fit()
     api_fit.add_rig(type_id=eve_src_item.id)
     api_tgt_item = api_fit.add_rig(type_id=eve_tgt_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_other_fit(client, consts):
+    # Check that local modifications are not carried over to another fit
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.struct,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(mod_info=[eve_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit1 = api_ss.create_fit()
+    api_fit2 = api_ss.create_fit()
+    api_fit1.add_rig(type_id=eve_src_item.id)
+    api_tgt_item = api_fit2.set_struct(type_id=eve_tgt_item.id)
     assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(100)
