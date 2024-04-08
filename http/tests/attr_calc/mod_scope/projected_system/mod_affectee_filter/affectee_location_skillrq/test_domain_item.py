5b947610b8c50a9c44d5c36b3c6f7b008d4bb0db
DarkPhoenix <phoenix@mail.ru>
2024-04-09 03:47:07 +0600

    Add projected effect + system effect affectee location-skillreq tests

---
+++ http/tests/attr_calc/mod_scope/projected_system/mod_affectee_filter/affectee_location_skillrq/test_domain_item.py
@@ -0,0 +1,29 @@
+from pytest import approx
+
+
+def test_unaffected(client, consts):
+    # There isn't anything which can belong to a projected effect, so just check that ship and an
+    # item on it are not affected
+    eve_skill = client.mk_eve_item()
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc_srq,
+        dom=consts.EveModDom.item,
+        srq=eve_skill.id,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_proj_effect = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 100}, srqs={eve_skill.id: 1})
+    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 100}, srqs={eve_skill.id: 1})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_rig = api_fit.add_rig(type_id=eve_rig.id)
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+    assert api_rig.update().attrs[eve_tgt_attr.id].dogma == approx(100)
