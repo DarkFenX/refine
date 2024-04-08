2745dd12ae30adb32bcbb27e56e90a5d80cf67a1
DarkPhoenix <phoenix@mail.ru>
2024-04-08 16:35:15 +0600

    Add first set of projected effect + system effect tests

---
+++ http/tests/attr_calc/mod_scope/projected_system/mod_affectee_filter/affectee_item/test_domain_character.py
@@ -0,0 +1,99 @@
+from pytest import approx
+
+
+def test_affected(client, consts):
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_proj_effect = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_char = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    eve_ship = client.mk_eve_item()
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_char = api_fit.set_char(type_id=eve_char.id)
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_char.update().attrs[eve_tgt_attr.id].dogma == approx(120)
+    api_proj_effect.remove()
+    assert api_char.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_other_domain(client, consts):
+    # Make sure "top" entities described by other domains are not affected
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_proj_effect = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_child_item(client, consts):
+    # Check that items (in this case implant) are not affected if they belong to location even if
+    # its "owner" (in this case character) is affected
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_proj_effect = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_implant = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    eve_ship = client.mk_eve_item()
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_implant = api_fit.add_implant(type_id=eve_implant.id)
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_implant.update().attrs[eve_tgt_attr.id].dogma == approx(100)
+
+
+def test_unaffected_other_fit(client, consts):
+    # Check that projected modifications are not carried over to another fit
+    eve_src_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.char,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_proj_effect = client.mk_eve_item(attrs={eve_src_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_char = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    eve_ship = client.mk_eve_item()
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit1 = api_ss.create_fit()
+    api_fit2 = api_ss.create_fit()
+    api_ship = api_fit1.set_ship(type_id=eve_ship.id)
+    api_char = api_fit2.set_char(type_id=eve_char.id)
+    api_fit2.set_ship(type_id=eve_ship.id)
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_char.update().attrs[eve_tgt_attr.id].dogma == approx(100)
