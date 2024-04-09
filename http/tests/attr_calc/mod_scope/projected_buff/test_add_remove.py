3f1b422d186e1ba369e54a61c2b4c7a1707f123a
DarkPhoenix <phoenix@mail.ru>
2024-04-09 22:43:47 +0600

    Add first tests for projected buffs

---
+++ http/tests/attr_calc/mod_scope/projected_buff/test_add_remove.py
@@ -0,0 +1,56 @@
+from pytest import approx
+
+
+def test_proj_unproj(client, consts):
+    # Check that effects are applied/removed when projected effect is applied/unapplied
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_proj_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    client.create_sources()
+    api_ss = client.create_ss()
+    eve_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+    eve_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
+    eve_proj_effect.change_proj_effect(rm_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+
+
+def test_remove(client, consts):
+    # Check that effects are removed when projected effect is removed
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_proj_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    eve_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    eve_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
+    eve_proj_effect.remove()
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
