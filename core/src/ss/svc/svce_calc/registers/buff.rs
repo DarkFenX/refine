25ed55f161f0bf3c1cc0a0cd390f019454d61faa
DarkPhoenix <phoenix@mail.ru>
2024-04-16 00:10:20 +0600

    Add method to register dependencies between attributes which define buff type IDs and modifiers

--- core/src/ss/svc/svce_calc/registers/buff.rs
+++ core/src/ss/svc/svce_calc/registers/buff.rs
@@ -2,17 +2,21 @@ use std::collections::HashSet;
 
 use crate::{
     ad,
-    defs::{EEffectId, SsItemId},
+    defs::{EAttrId, EEffectId, SsItemId},
+    ss::svc::svce_calc::modifier::SsAttrMod,
+    util::KeyedStorage1L,
 };
 
 // Intended to hold data about buffs which use on-item attributes which define buff type
 pub(in crate::ss::svc::svce_calc) struct BuffRegister {
     effects: HashSet<(SsItemId, EEffectId)>,
+    modifiers: KeyedStorage1L<(SsItemId, EAttrId), SsAttrMod>,
 }
 impl BuffRegister {
     pub(in crate::ss::svc::svce_calc) fn new() -> Self {
         Self {
             effects: HashSet::new(),
+            modifiers: KeyedStorage1L::new(),
         }
     }
     // Modification methods
@@ -25,6 +29,14 @@ impl BuffRegister {
         if uses_default_attrs(effect) {
             self.effects.remove(&(item_id, effect.id));
         }
+    }
+    pub(in crate::ss::svc::svce_calc) fn reg_mod_attr_dep(
+        &mut self,
+        item_id: SsItemId,
+        attr_id: EAttrId,
+        ss_mod: SsAttrMod,
+    ) {
+        self.modifiers.add_entry((item_id, attr_id), ss_mod)
     }
 }
 
