9aa75b7d9e864e6bb8a5ae616d2e1deb4b45358d
DarkPhoenix <phoenix@mail.ru>
2024-04-15 06:45:53 +0600

    Finalize fleet scope tests

--- http/tests/attr_calc/mod_scope/fleet/mod_affectee_filter/test_affectee_location_group.py
+++ http/tests/attr_calc/mod_scope/fleet/mod_affectee_filter/test_affectee_location_group.py
@@ -3,20 +3,21 @@ from pytest import approx
 
 def test_affected_self_child_ship(client, consts):
     # Make sure ship items (such as rigs) are affected by fleet buffs
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -29,20 +30,21 @@ def test_affected_self_child_ship(client, consts):
 
 def test_affected_fleeted_child_ship(client, consts):
     # Make sure ship items (such as rigs) are affected by fleet buffs
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -56,22 +58,51 @@ def test_affected_fleeted_child_ship(client, consts):
     assert api_rig.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
 
 
-def test_unaffected_self_child_struct(client, consts):
-    # Make sure structures are not affected by fleet buffs
+def test_affected_other_group(client, consts):
+    # Check that entities belonging to other item groups are not affected
+    eve_grp1 = client.mk_eve_item_group()
+    eve_grp2 = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp1.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp2.id, attrs={eve_tgt_attr.id: 7.5})
+    eve_ship = client.mk_eve_item()
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_fit.set_ship(type_id=eve_ship.id)
+    api_fit.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_rig = api_fit.add_rig(type_id=eve_rig.id)
+    assert api_rig.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+
+
+def test_unaffected_self_child_struct(client, consts):
+    # Make sure structures are not affected by fleet buffs
+    eve_grp = client.mk_eve_item_group()
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.mod_bonus_warfare_link_armor,
+        cat_id=consts.EveEffCat.active)
+    eve_module = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_struct = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -84,20 +115,21 @@ def test_unaffected_self_child_struct(client, consts):
 
 def test_unaffected_fleeted_child_struct(client, consts):
     # Make sure structures are not affected by fleet buffs
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_struct = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -113,20 +145,21 @@ def test_unaffected_fleeted_child_struct(client, consts):
 
 def test_unaffected_other_fit(client, consts):
     # Check that fits outside of fleet are not affected
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -140,20 +173,21 @@ def test_unaffected_other_fit(client, consts):
 
 def test_unaffected_other_fleet(client, consts):
     # Check that fits outside of fleet are not affected
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -171,20 +205,21 @@ def test_unaffected_other_fleet(client, consts):
 
 def test_self_replace_parent(client, consts):
     # Modifiers which target items on ship location shouldn't apply when ship isn't set
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
@@ -201,20 +236,21 @@ def test_self_replace_parent(client, consts):
 
 def test_fleeted_replace_parent(client, consts):
     # Modifiers which target items on ship location shouldn't apply when ship isn't set
+    eve_grp = client.mk_eve_item_group()
     eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
     eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
     eve_tgt_attr = client.mk_eve_attr()
     eve_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
-        loc_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+        loc_grp_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, group_id=eve_grp.id)])
     eve_effect = client.mk_eve_effect(
         id_=consts.EveEffect.mod_bonus_warfare_link_armor,
         cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_rig = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    eve_rig = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_tgt_attr.id: 7.5})
     eve_ship = client.mk_eve_item()
     client.create_sources()
     api_ss = client.create_ss()
