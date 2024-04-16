4f4a2dca6a6264c4baaea97c6759dda7c6fcc29b
DarkPhoenix <phoenix@mail.ru>
2024-04-17 02:18:24 +0600

    Add tests which verify unknown buff IDs

---
+++ http/tests/attr_calc/mod_scope/fit_wide_buff/test_unknown_buff.py
@@ -0,0 +1,24 @@
+from pytest import approx
+
+
+def test_static(client, consts):
+    # Check that there is no strange side effects when buff isn't defined
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_fw_effect = client.mk_eve_item(
+        # Buff ID which we didn't create
+        attrs={eve_buff_type_attr.id: 7, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_fit = api_ss.create_fit()
+    api_fw_effect = api_fit.add_fw_effect(type_id=eve_fw_effect.id)
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+    api_fw_effect.remove()
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
---
+++ http/tests/attr_calc/mod_scope/projected_buff/test_unknown_buff.py
@@ -0,0 +1,25 @@
+from pytest import approx
+
+
+def test_proj_unproj_ship(client, consts):
+    # Check that there is no strange side effects when buff isn't defined
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_proj_effect = client.mk_eve_item(
+        # Buff ID which we didn't create
+        attrs={eve_buff_type_attr.id: 7, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_proj_effect = api_ss.add_proj_effect(type_id=eve_proj_effect.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+    api_proj_effect.remove()
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
