c2063cc456e50710ab6cb1b63ec1e7d62d3bc6a3
DarkPhoenix <phoenix@mail.ru>
2024-04-18 06:44:23 +0600

    Add more optimizations to release profile, and add new test profile which uses old release settings

--- http/tests/support/server.py
+++ http/tests/support/server.py
@@ -12,7 +12,7 @@ def build_server(proj_root: str):
     http_path = os.path.join(proj_root, 'http')
     os.chdir(http_path)
     subprocess.run(
-        ['cargo', 'build', '--package=reefast-http', '--profile=release'],
+        ['cargo', 'build', '--package=reefast-http', '--profile=test'],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
         check=True)
@@ -34,7 +34,7 @@ def build_config(config_path: str, port: int, log_folder: str) -> ConfigInfo:
 
 
 def run_server(proj_root: str, config_path: str) -> ServerInfo:
-    binary_path = os.path.join(proj_root, 'target', 'release', 'reefast-http')
+    binary_path = os.path.join(proj_root, 'target', 'test', 'reefast-http')
     return ServerInfo(pid=subprocess.Popen(
         [binary_path, config_path],
         stdout=subprocess.DEVNULL,
