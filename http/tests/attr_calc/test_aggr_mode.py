2436a419fbf0eebcb1743cfa8946342df52bcd2b
DarkPhoenix <phoenix@mail.ru>
2024-04-18 07:27:50 +0600

    Revert test profile changes (use more optimized release and regular release names instead) and add aggregation mode tests

--- http/tests/attr_calc/test_aggr_mode.py
+++ http/tests/attr_calc/test_aggr_mode.py
@@ -1,4 +1,114 @@
+# We test just 4 operation types, since CCP doesn't use any other in buffs at the present time
+
 from pytest import approx
+
+
+def test_add_max(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.mod_add,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: -40},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(180)
+
+
+def test_add_min(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.min,
+        op=consts.EveBuffOp.mod_add,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: -40},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(110)
+
+
+def test_postmul_max(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 0.6},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 1.3},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(195)
+
+
+def test_postmul_min(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.min,
+        op=consts.EveBuffOp.post_mul,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 0.6},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 1.3},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(90)
 
 
 def test_postperc_max(client, consts):
@@ -18,11 +128,177 @@ def test_postperc_max(client, consts):
     eve_sw_effect2 = client.mk_eve_item(
         attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
         eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 100})
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
     client.create_sources()
     api_ss = client.create_ss()
     api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
     api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
     api_fit = api_ss.create_fit()
     api_ship = api_fit.set_ship(type_id=eve_ship.id)
-    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(130)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(195)
+
+
+def test_postperc_min(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.min,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: -40},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(90)
+
+
+def test_postassign_max(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_assign,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: -40},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(30)
+
+
+def test_postassign_min(client, consts):
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.min,
+        op=consts.EveBuffOp.post_assign,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect1 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: -40},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_sw_effect2 = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect1.id)
+    api_ss.add_sw_effect(type_id=eve_sw_effect2.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(-40)
+
+
+def test_different_buffs(client, consts):
+    # Different buffs should stack instead of overriding each other
+    eve_buff_type_attr1 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr1 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_buff_type_attr2 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_2_id)
+    eve_buff_val_attr2 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_2_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff1 = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_buff2 = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect = client.mk_eve_item(
+        attrs={
+            eve_buff_type_attr1.id: eve_buff1.id, eve_buff_val_attr1.id: -40,
+            eve_buff_type_attr2.id: eve_buff2.id, eve_buff_val_attr2.id: 30},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(117)
+
+
+def test_different_sources(client, consts):
+    # Same buff type from different sources shouldn't stack
+    eve_buff_val_mult_attr = client.mk_eve_attr()
+    eve_buff_type_attr1 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr1 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_buff_type_attr2 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_2_id)
+    eve_buff_val_attr2 = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_2_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_sw_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr1.id: eve_buff.id, eve_buff_val_attr1.id: 4.7},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_module_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.mod_bonus_warfare_link_armor,
+        cat_id=consts.EveEffCat.active)
+    eve_module = client.mk_eve_item(
+        attrs={eve_buff_val_attr2.id: 1.25},
+        eff_ids=[eve_module_effect.id],
+        defeff_id=eve_module_effect.id)
+    eve_charge_mod1 = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.other,
+        op=consts.EveModOp.post_assign,
+        src_attr_id=eve_buff_type_attr2.id,
+        tgt_attr_id=eve_buff_type_attr2.id)
+    eve_charge_mod2 = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.other,
+        op=consts.EveModOp.post_mul,
+        src_attr_id=eve_buff_val_mult_attr.id,
+        tgt_attr_id=eve_buff_val_attr2.id)
+    eve_charge_effect = client.mk_eve_effect(mod_info=[eve_charge_mod1, eve_charge_mod2])
+    eve_charge = client.mk_eve_item(
+        attrs={eve_buff_type_attr2.id: eve_buff.id, eve_buff_val_mult_attr.id: 4},
+        eff_ids=[eve_charge_effect.id])
+    eve_ship = client.mk_eve_item(attrs={eve_tgt_attr.id: 150})
+    client.create_sources()
+    api_ss = client.create_ss()
+    api_ss.add_sw_effect(type_id=eve_sw_effect.id)
+    api_fit = api_ss.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_fit.add_mod(type_id=eve_module.id, charge_type_id=eve_charge.id, state=consts.ApiState.active)
+    # Aggregation mode is set to max, and fleet buff value is higher (1.25*4 = 5 vs 4.7), so only
+    # fleet buff is applied
+    assert api_ship.update().attrs[eve_tgt_attr.id].dogma == approx(750)
