8e8d79471a3b2b43dfc64fda5b7f082448ebb4bc
DarkPhoenix <phoenix@mail.ru>
2024-04-23 16:23:03 +0600

    Finalize first test module for targeted items

--- http/tests/attr_calc/mod_scope/targeted_regular/mod_affectee_filter/test_affectee_item.py
+++ http/tests/attr_calc/mod_scope/targeted_regular/mod_affectee_filter/test_affectee_item.py
@@ -74,3 +74,140 @@ def test_affected_child(client, consts):
     assert api_drone.update().attrs[eve_attr2.id].dogma == approx(1)
     api_module.remove()
     assert api_drone.update().attrs[eve_attr2.id].dogma == approx(-2)
+
+
+def test_affected_propagation(client, consts):
+    # Check that changes to attribute value which is source of modification are propagated to target
+    eve_src_attr = client.mk_eve_attr()
+    eve_mid_attr = client.mk_eve_attr()
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_src_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc,
+        dom=consts.EveModDom.ship,
+        op=consts.EveModOp.post_percent,
+        src_attr_id=eve_src_attr.id,
+        tgt_attr_id=eve_mid_attr.id)
+    eve_src_effect = client.mk_eve_effect(mod_info=[eve_src_mod])
+    eve_src_item = client.mk_eve_item(attrs={eve_src_attr.id: 50}, eff_ids=[eve_src_effect.id])
+    eve_mid_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.mod_add,
+        src_attr_id=eve_mid_attr.id,
+        tgt_attr_id=eve_tgt_attr.id)
+    eve_mid_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.target, mod_info=[eve_mid_mod])
+    eve_mid_item = client.mk_eve_item(
+        attrs={eve_mid_attr.id: 3},
+        eff_ids=[eve_mid_effect.id],
+        defeff_id=eve_mid_effect.id)
+    eve_tgt_item = client.mk_eve_item(attrs={eve_tgt_attr.id: -2})
+    eve_ship_item = client.mk_eve_item()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit1 = api_sol.create_fit()
+    api_fit2 = api_sol.create_fit()
+    api_fit1.set_ship(type_id=eve_ship_item.id)
+    api_tgt_item = api_fit2.set_ship(type_id=eve_tgt_item.id)
+    api_mid_item = api_fit1.add_mod(type_id=eve_mid_item.id, state=consts.ApiState.active)
+    api_mid_item.change_mod(add_tgts=[(api_tgt_item.id, None)])
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(1)
+    api_src_item = api_fit1.add_rig(type_id=eve_src_item.id)
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(2.5)
+    api_src_item.remove()
+    assert api_tgt_item.update().attrs[eve_tgt_attr.id].dogma == approx(1)
+
+
+def test_unaffected_child_via_root(client, consts):
+    eve_attr1 = client.mk_eve_attr()
+    eve_attr2 = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.mod_add,
+        src_attr_id=eve_attr1.id,
+        tgt_attr_id=eve_attr2.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.target, mod_info=[eve_mod])
+    eve_module = client.mk_eve_item(attrs={eve_attr1.id: 3}, eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_drone = client.mk_eve_item(attrs={eve_attr2.id: -2})
+    eve_ship = client.mk_eve_item()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit1 = api_sol.create_fit()
+    api_fit2 = api_sol.create_fit()
+    api_ship = api_fit2.set_ship(type_id=eve_ship.id)
+    api_drone = api_fit2.add_drone(type_id=eve_drone.id)
+    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_module.change_mod(add_tgts=[(api_ship.id, None)])
+    assert api_drone.update().attrs[eve_attr2.id].dogma == approx(-2)
+
+
+def test_unaffected_root_via_child(client, consts):
+    eve_attr1 = client.mk_eve_attr()
+    eve_attr2 = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.mod_add,
+        src_attr_id=eve_attr1.id,
+        tgt_attr_id=eve_attr2.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.target, mod_info=[eve_mod])
+    eve_module = client.mk_eve_item(attrs={eve_attr1.id: 3}, eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_attr2.id: -2})
+    eve_drone = client.mk_eve_item()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit1 = api_sol.create_fit()
+    api_fit2 = api_sol.create_fit()
+    api_ship = api_fit2.set_ship(type_id=eve_ship.id)
+    api_drone = api_fit2.add_drone(type_id=eve_drone.id)
+    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_module.change_mod(add_tgts=[(api_drone.id, None)])
+    assert api_ship.update().attrs[eve_attr2.id].dogma == approx(-2)
+
+
+def test_unaffected_root_other_fit(client, consts):
+    eve_attr1 = client.mk_eve_attr()
+    eve_attr2 = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.mod_add,
+        src_attr_id=eve_attr1.id,
+        tgt_attr_id=eve_attr2.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.target, mod_info=[eve_mod])
+    eve_module = client.mk_eve_item(attrs={eve_attr1.id: 3}, eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item(attrs={eve_attr2.id: -2})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit1 = api_sol.create_fit()
+    api_fit2 = api_sol.create_fit()
+    api_fit3 = api_sol.create_fit()
+    api_ship2 = api_fit2.set_ship(type_id=eve_ship.id)
+    api_ship3 = api_fit3.set_ship(type_id=eve_ship.id)
+    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_module.change_mod(add_tgts=[(api_ship2.id, None)])
+    assert api_ship3.update().attrs[eve_attr2.id].dogma == approx(-2)
+
+
+def test_child_other_fit(client, consts):
+    eve_attr1 = client.mk_eve_attr()
+    eve_attr2 = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.mod_add,
+        src_attr_id=eve_attr1.id,
+        tgt_attr_id=eve_attr2.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.target, mod_info=[eve_mod])
+    eve_module = client.mk_eve_item(attrs={eve_attr1.id: 3}, eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_drone = client.mk_eve_item(attrs={eve_attr2.id: -2})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit1 = api_sol.create_fit()
+    api_fit2 = api_sol.create_fit()
+    api_fit3 = api_sol.create_fit()
+    api_drone2 = api_fit2.add_drone(type_id=eve_drone.id)
+    api_drone3 = api_fit3.add_drone(type_id=eve_drone.id)
+    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_module.change_mod(add_tgts=[(api_drone2.id, None)])
+    assert api_drone3.update().attrs[eve_attr2.id].dogma == approx(-2)
