75089ec493c1b98800baaa9406bb52a40fa65112
DarkPhoenix <phoenix@mail.ru>
2024-04-25 01:02:41 +0600

    Expand projected+buff suite's basic tests to be state change + target change

--- http/tests/attr_calc/mod_scope/projected_buff/mod_affectee_filter/test_affectee_location_skillrq.py
+++ http/tests/attr_calc/mod_scope/projected_buff/mod_affectee_filter/test_affectee_location_skillrq.py
@@ -1,7 +1,73 @@
 from pytest import approx
 
 
-def test_affected_child_ship(client, consts):
+def test_affected_state_change_child_ship(client, consts):
+    # Make sure ship items (such as modules) are affected by location-skillreq-filtered buff
+    # modification
+    eve_skill = client.mk_eve_item()
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        loc_srq_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, skill_id=eve_skill.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_proj_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_ship = client.mk_eve_item()
+    eve_module = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5}, srqs={eve_skill.id: 1})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
+    api_module = api_fit.add_mod(type_id=eve_module.id)
+    api_proj_effect = api_sol.add_proj_effect(type_id=eve_proj_effect.id, state=False)
+    api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+    api_proj_effect.change_proj_effect(state=True)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
+    api_proj_effect.change_proj_effect(state=False)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+
+
+def test_affected_state_change_child_struct(client, consts):
+    # Make sure structure items (such as modules) are affected by location-skillreq-filtered buff
+    # modification
+    eve_skill = client.mk_eve_item()
+    eve_buff_type_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr = client.mk_eve_attr(id_=consts.EveAttr.warfare_buff_1_value)
+    eve_tgt_attr = client.mk_eve_attr()
+    eve_buff = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_mul,
+        loc_srq_mods=[client.mk_eve_buff_mod(attr_id=eve_tgt_attr.id, skill_id=eve_skill.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.weather_darkness,
+        cat_id=consts.EveEffCat.active)
+    eve_proj_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
+    eve_struct = client.mk_eve_item()
+    eve_module = client.mk_eve_item(attrs={eve_tgt_attr.id: 7.5}, srqs={eve_skill.id: 1})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_struct = api_fit.set_struct(type_id=eve_struct.id)
+    api_module = api_fit.add_mod(type_id=eve_module.id)
+    api_proj_effect = api_sol.add_proj_effect(type_id=eve_proj_effect.id, state=False)
+    api_proj_effect.change_proj_effect(add_tgts=[api_struct.id])
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+    api_proj_effect.change_proj_effect(state=True)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
+    api_proj_effect.change_proj_effect(state=False)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
+
+
+def test_affected_targeting_child_ship(client, consts):
     # Make sure ship items (such as modules) are affected by location-skillreq-filtered buff
     # modification
     eve_skill = client.mk_eve_item()
@@ -26,13 +92,14 @@ def test_affected_child_ship(client, consts):
     api_ship = api_fit.set_ship(type_id=eve_ship.id)
     api_module = api_fit.add_mod(type_id=eve_module.id)
     api_proj_effect = api_sol.add_proj_effect(type_id=eve_proj_effect.id)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
     api_proj_effect.change_proj_effect(add_tgts=[api_ship.id])
     assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
-    api_proj_effect.remove()
+    api_proj_effect.change_proj_effect(rm_tgts=[api_ship.id])
     assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
 
 
-def test_affected_child_struct(client, consts):
+def test_affected_targeting_child_struct(client, consts):
     # Make sure structure items (such as modules) are affected by location-skillreq-filtered buff
     # modification
     eve_skill = client.mk_eve_item()
@@ -57,9 +124,10 @@ def test_affected_child_struct(client, consts):
     api_struct = api_fit.set_struct(type_id=eve_struct.id)
     api_module = api_fit.add_mod(type_id=eve_module.id)
     api_proj_effect = api_sol.add_proj_effect(type_id=eve_proj_effect.id)
+    assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
     api_proj_effect.change_proj_effect(add_tgts=[api_struct.id])
     assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(37.5)
-    api_proj_effect.remove()
+    api_proj_effect.change_proj_effect(rm_tgts=[api_struct.id])
     assert api_module.update().attrs[eve_tgt_attr.id].dogma == approx(7.5)
 
 
