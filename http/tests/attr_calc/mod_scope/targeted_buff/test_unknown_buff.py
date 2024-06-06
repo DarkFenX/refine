2c399d36390ecb792cd76e066325cca55ddfa5fb
DarkPhoenix <phoenix@mail.ru>
2024-06-06 02:06:29 +0200

    Fix targeted buff tests

--- http/tests/attr_calc/mod_scope/targeted_buff/test_unknown_buff.py
+++ http/tests/attr_calc/mod_scope/targeted_buff/test_unknown_buff.py
@@ -7,7 +7,7 @@ def test_target_untarget(client, consts):
     eve_affectee_attr = client.mk_eve_attr()
     eve_effect = client.mk_eve_effect(id_=consts.EveEffect.doomsday_aoe_web, cat_id=consts.EveEffCat.active)
     eve_module = client.mk_eve_item(attrs={eve_affector_attr.id: -55}, eff_ids=[eve_effect.id], defeff_id=eve_effect.id)
-    eve_ship = client.mk_eve_item(attrs={eve_affectee_attr.id: 200})
+    eve_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 200})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit1 = api_sol.create_fit()
