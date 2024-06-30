5e63cb5581ca51b128ee4d48c34397a66fd78177
DarkPhoenix <phoenix@mail.ru>
2024-06-30 15:39:48 +0200

    Move add/remove tests to filter-specific modules for fit-wide system modifications, and expand them

---
+++ http/tests/attr_calc/mod_scope/fit_wide_system/mod_affectee_filter/affectee_location_group/test_add_remove.py
@@ -0,0 +1,89 @@
+from pytest import approx
+
+
+def test_add_fw_item_remove_fw_item(client, consts):
+    eve_grp = client.mk_eve_item_group()
+    eve_affector_attr = client.mk_eve_attr()
+    eve_affectee_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc_grp,
+        dom=consts.EveModDom.struct,
+        grp=eve_grp.id,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr.id,
+        affectee_attr_id=eve_affectee_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_fw_effect = client.mk_eve_item(attrs={eve_affector_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_module = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_affectee_attr.id: 100})
+    eve_struct = client.mk_eve_struct()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fw_effect = api_fit.add_fw_effect(type_id=eve_fw_effect.id)
+    api_fit.set_ship(type_id=eve_struct.id)
+    api_module = api_fit.add_mod(type_id=eve_module.id)
+    assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(120)
+    api_fw_effect.remove()
+    assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(100)
+    api_module.remove()
+    api_module.update(status_code=404)
+    api_fit.remove()
+
+
+def test_add_item_fw_remove_state_item_fw(client, consts):
+    eve_grp = client.mk_eve_item_group()
+    eve_affector_attr = client.mk_eve_attr()
+    eve_affectee_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc_grp,
+        dom=consts.EveModDom.char,
+        grp=eve_grp.id,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr.id,
+        affectee_attr_id=eve_affectee_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_fw_effect = client.mk_eve_item(attrs={eve_affector_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_implant = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_affectee_attr.id: 100})
+    eve_char = client.mk_eve_item()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.set_char(type_id=eve_char.id)
+    api_implant = api_fit.add_implant(type_id=eve_implant.id)
+    assert api_implant.update().attrs[eve_affectee_attr.id].dogma == approx(100)
+    api_fw_effect = api_fit.add_fw_effect(type_id=eve_fw_effect.id)
+    assert api_implant.update().attrs[eve_affectee_attr.id].dogma == approx(120)
+    api_fw_effect.change_fw_effect(state=False)
+    assert api_implant.update().attrs[eve_affectee_attr.id].dogma == approx(100)
+    api_implant.remove()
+    api_implant.update(status_code=404)
+    api_fw_effect.remove()
+    api_fit.remove()
+
+
+def test_add_fw_item_state_remove_fit(client, consts):
+    eve_grp = client.mk_eve_item_group()
+    eve_affector_attr = client.mk_eve_attr()
+    eve_affectee_attr = client.mk_eve_attr()
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc_grp,
+        dom=consts.EveModDom.ship,
+        grp=eve_grp.id,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr.id,
+        affectee_attr_id=eve_affectee_attr.id)
+    eve_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.system, mod_info=[eve_mod])
+    eve_fw_effect = client.mk_eve_item(attrs={eve_affector_attr.id: 20}, eff_ids=[eve_effect.id])
+    eve_module = client.mk_eve_item(grp_id=eve_grp.id, attrs={eve_affectee_attr.id: 100})
+    eve_ship = client.mk_eve_ship()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fw_effect = api_fit.add_fw_effect(type_id=eve_fw_effect.id, state=False)
+    api_fit.set_ship(type_id=eve_ship.id)
+    api_module = api_fit.add_mod(type_id=eve_module.id)
+    assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(100)
+    api_fw_effect.change_fw_effect(state=True)
+    assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(120)
+    api_fit.remove()
+    api_module.update(status_code=404)
