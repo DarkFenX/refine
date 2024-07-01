9cbefd7ddbfb5f9b3b48629f8e4849bce8194d0d
DarkPhoenix <phoenix@mail.ru>
2024-07-01 16:02:19 +0200

    Add new indirect projection test

--- http/tests/attr_calc/projection/test_range.py
+++ http/tests/attr_calc/projection/test_range.py
@@ -17,23 +17,23 @@ def test_falloff_state_change_direct(client, consts):
         range_attr_id=eve_optimal_attr.id,
         falloff_attr_id=eve_falloff_attr.id,
         mod_info=[eve_mod])
-    eve_module = client.mk_eve_item(
+    eve_affector_module = client.mk_eve_item(
         attrs={eve_affector_attr.id: -60, eve_optimal_attr.id: 10000, eve_falloff_attr.id: 5000},
         eff_ids=[eve_effect.id],
         defeff_id=eve_effect.id)
-    eve_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
+    eve_affectee_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
     client.create_sources()
     api_sol = client.create_sol()
-    api_fit1 = api_sol.create_fit()
-    api_fit2 = api_sol.create_fit()
-    api_ship = api_fit2.set_ship(type_id=eve_ship.id)
-    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.online)
-    api_module.change_mod(add_tgts=[(api_ship.id, 15000)])
-    assert api_ship.update().attrs[eve_affectee_attr.id].dogma == approx(500)
-    api_module.change_mod(state=consts.ApiState.active)
-    assert api_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
-    api_module.change_mod(state=consts.ApiState.online)
-    assert api_ship.update().attrs[eve_affectee_attr.id].dogma == approx(500)
+    api_affector_fit = api_sol.create_fit()
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.online)
+    api_affector_module.change_mod(add_tgts=[(api_affectee_ship.id, 15000)])
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(500)
+    api_affector_module.change_mod(state=consts.ApiState.active)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_module.change_mod(state=consts.ApiState.online)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(500)
 
 
 def test_falloff_state_change_indirect(client, consts):
@@ -52,70 +52,25 @@ def test_falloff_state_change_indirect(client, consts):
         range_attr_id=eve_optimal_attr.id,
         falloff_attr_id=eve_falloff_attr.id,
         mod_info=[eve_mod])
-    eve_module = client.mk_eve_item(
+    eve_affector_module = client.mk_eve_item(
         attrs={eve_affector_attr.id: -60, eve_optimal_attr.id: 10000, eve_falloff_attr.id: 5000},
         eff_ids=[eve_effect.id],
         defeff_id=eve_effect.id)
-    eve_ship = client.mk_eve_ship()
-    eve_rig = client.mk_eve_item(attrs={eve_affectee_attr.id: 500})
+    eve_affectee_ship = client.mk_eve_ship()
+    eve_affectee_rig = client.mk_eve_item(attrs={eve_affectee_attr.id: 500})
     client.create_sources()
     api_sol = client.create_sol()
-    api_fit1 = api_sol.create_fit()
-    api_fit2 = api_sol.create_fit()
-    api_ship = api_fit2.set_ship(type_id=eve_ship.id)
-    api_rig = api_fit2.add_rig(type_id=eve_rig.id)
-    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.online)
-    api_module.change_mod(add_tgts=[(api_ship.id, 15000)])
-    assert api_rig.update().attrs[eve_affectee_attr.id].dogma == approx(500)
-    api_module.change_mod(state=consts.ApiState.active)
-    assert api_rig.update().attrs[eve_affectee_attr.id].dogma == approx(350)
-    api_module.change_mod(state=consts.ApiState.online)
-    assert api_rig.update().attrs[eve_affectee_attr.id].dogma == approx(500)
-
-
-def test_falloff_value_change_direct(client, consts):
-    eve_affector_attr = client.mk_eve_attr()
-    eve_affectee_attr = client.mk_eve_attr()
-    eve_optimal_attr = client.mk_eve_attr()
-    eve_falloff_attr = client.mk_eve_attr()
-    eve_boost_attr = client.mk_eve_attr()
-    eve_module_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.item,
-        dom=consts.EveModDom.tgt,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_affector_attr.id,
-        affectee_attr_id=eve_affectee_attr.id)
-    eve_module_effect = client.mk_eve_effect(
-        cat_id=consts.EveEffCat.target,
-        range_attr_id=eve_optimal_attr.id,
-        falloff_attr_id=eve_falloff_attr.id,
-        mod_info=[eve_module_mod])
-    eve_module = client.mk_eve_item(
-        attrs={eve_affector_attr.id: -60, eve_optimal_attr.id: 10000, eve_falloff_attr.id: 5000},
-        eff_ids=[eve_module_effect.id],
-        defeff_id=eve_module_effect.id)
-    eve_rig_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.loc,
-        dom=consts.EveModDom.ship,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_boost_attr.id,
-        affectee_attr_id=eve_falloff_attr.id)
-    eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
-    eve_rig = client.mk_eve_item(attrs={eve_boost_attr.id: 50}, eff_ids=[eve_rig_effect.id])
-    eve_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit1 = api_sol.create_fit()
-    api_fit1.set_ship(type_id=eve_ship.id)
-    api_fit2 = api_sol.create_fit()
-    api_ship2 = api_fit2.set_ship(type_id=eve_ship.id)
-    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
-    api_module.change_mod(add_tgts=[(api_ship2.id, 15000)])
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(350)
-    api_rig = api_fit1.add_rig(type_id=eve_rig.id)
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(279.53983)
-    api_rig.remove()
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_fit = api_sol.create_fit()
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
+    api_affectee_rig = api_affectee_fit.add_rig(type_id=eve_affectee_rig.id)
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.online)
+    api_affector_module.change_mod(add_tgts=[(api_affectee_ship.id, 15000)])
+    assert api_affectee_rig.update().attrs[eve_affectee_attr.id].dogma == approx(500)
+    api_affector_module.change_mod(state=consts.ApiState.active)
+    assert api_affectee_rig.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_module.change_mod(state=consts.ApiState.online)
+    assert api_affectee_rig.update().attrs[eve_affectee_attr.id].dogma == approx(500)
 
 
 def test_optimal_value_change_direct(client, consts):
@@ -146,18 +101,112 @@ def test_optimal_value_change_direct(client, consts):
         affector_attr_id=eve_boost_attr.id,
         affectee_attr_id=eve_optimal_attr.id)
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
-    eve_rig = client.mk_eve_item(attrs={eve_boost_attr.id: 25}, eff_ids=[eve_rig_effect.id])
-    eve_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
+    eve_affector_rig = client.mk_eve_item(attrs={eve_boost_attr.id: 25}, eff_ids=[eve_rig_effect.id])
+    eve_affector_ship = client.mk_eve_ship()
+    eve_affectee_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
     client.create_sources()
     api_sol = client.create_sol()
-    api_fit1 = api_sol.create_fit()
-    api_fit1.set_ship(type_id=eve_ship.id)
-    api_fit2 = api_sol.create_fit()
-    api_ship2 = api_fit2.set_ship(type_id=eve_ship.id)
-    api_module = api_fit1.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
-    api_module.change_mod(add_tgts=[(api_ship2.id, 15000)])
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(350)
-    api_rig = api_fit1.add_rig(type_id=eve_rig.id)
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(247.73108)
-    api_rig.remove()
-    assert api_ship2.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_fit = api_sol.create_fit()
+    api_affector_fit.set_ship(type_id=eve_affector_ship.id)
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_affector_module.change_mod(add_tgts=[(api_affectee_ship.id, 15000)])
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_rig = api_affector_fit.add_rig(type_id=eve_affector_rig.id)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(247.73108)
+    api_affector_rig.remove()
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+
+
+def test_optimal_value_change_indirect(client, consts):
+    eve_affector_attr = client.mk_eve_attr()
+    eve_affectee_attr = client.mk_eve_attr()
+    eve_optimal_attr = client.mk_eve_attr()
+    eve_falloff_attr = client.mk_eve_attr()
+    eve_boost_attr = client.mk_eve_attr()
+    eve_module_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr.id,
+        affectee_attr_id=eve_affectee_attr.id)
+    eve_module_effect = client.mk_eve_effect(
+        cat_id=consts.EveEffCat.target,
+        range_attr_id=eve_optimal_attr.id,
+        falloff_attr_id=eve_falloff_attr.id,
+        mod_info=[eve_module_mod])
+    eve_module = client.mk_eve_item(
+        attrs={eve_affector_attr.id: -60, eve_optimal_attr.id: 10000, eve_falloff_attr.id: 5000},
+        eff_ids=[eve_module_effect.id],
+        defeff_id=eve_module_effect.id)
+    eve_rig_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc,
+        dom=consts.EveModDom.ship,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_boost_attr.id,
+        affectee_attr_id=eve_optimal_attr.id)
+    eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
+    eve_affector_rig = client.mk_eve_item(attrs={eve_boost_attr.id: 25}, eff_ids=[eve_rig_effect.id])
+    eve_ship = client.mk_eve_ship()
+    eve_affectee_module = client.mk_eve_item(attrs={eve_affectee_attr.id: 500})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_affector_fit = api_sol.create_fit()
+    api_affector_fit.set_ship(type_id=eve_ship.id)
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_ship.id)
+    api_affectee_module = api_affectee_fit.add_mod(type_id=eve_affectee_module.id)
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_module.id, state=consts.ApiState.active)
+    api_affector_module.change_mod(add_tgts=[(api_affectee_ship.id, 15000)])
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_rig = api_affector_fit.add_rig(type_id=eve_affector_rig.id)
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(247.73108)
+    api_affector_rig.remove()
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+
+
+def test_falloff_value_change_direct(client, consts):
+    eve_affector_attr = client.mk_eve_attr()
+    eve_affectee_attr = client.mk_eve_attr()
+    eve_optimal_attr = client.mk_eve_attr()
+    eve_falloff_attr = client.mk_eve_attr()
+    eve_boost_attr = client.mk_eve_attr()
+    eve_module_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        dom=consts.EveModDom.tgt,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr.id,
+        affectee_attr_id=eve_affectee_attr.id)
+    eve_module_effect = client.mk_eve_effect(
+        cat_id=consts.EveEffCat.target,
+        range_attr_id=eve_optimal_attr.id,
+        falloff_attr_id=eve_falloff_attr.id,
+        mod_info=[eve_module_mod])
+    eve_affector_module = client.mk_eve_item(
+        attrs={eve_affector_attr.id: -60, eve_optimal_attr.id: 10000, eve_falloff_attr.id: 5000},
+        eff_ids=[eve_module_effect.id],
+        defeff_id=eve_module_effect.id)
+    eve_rig_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.loc,
+        dom=consts.EveModDom.ship,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_boost_attr.id,
+        affectee_attr_id=eve_falloff_attr.id)
+    eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
+    eve_affector_rig = client.mk_eve_item(attrs={eve_boost_attr.id: 50}, eff_ids=[eve_rig_effect.id])
+    eve_affector_ship = client.mk_eve_ship()
+    eve_affectee_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_affector_fit = api_sol.create_fit()
+    api_affector_fit.set_ship(type_id=eve_affector_ship.id)
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
+    api_affector_module.change_mod(add_tgts=[(api_affectee_ship.id, 15000)])
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
+    api_affector_rig = api_affector_fit.add_rig(type_id=eve_affector_rig.id)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(279.53983)
+    api_affector_rig.remove()
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(350)
