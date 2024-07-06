c331eaf492c2dcfa09861eefa6421652b80fe2bc
DarkPhoenix <phoenix@mail.ru>
2024-07-07 01:33:07 +0200

    Add resist tests for projected effects

--- http/tests/attr_calc/mod_scope/projected_buff/mod_affectee_filter/affectee_location/test_resist.py
+++ http/tests/attr_calc/mod_scope/projected_buff/mod_affectee_filter/affectee_location/test_resist.py
@@ -7,33 +7,34 @@ def test_resisted_value_change(client, consts):
     eve_affectee_attr = client.mk_eve_attr()
     eve_resist_attr = client.mk_eve_attr()
     eve_boost_attr = client.mk_eve_attr()
-    eve_sw_effect_buff = client.mk_eve_buff(
+    eve_proj_effect_buff = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_mul,
         loc_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr.id)])
-    eve_sw_effect_effect = client.mk_eve_effect(
+    eve_proj_effect_effect = client.mk_eve_effect(
         id_=consts.EveEffect.weather_darkness,
         cat_id=consts.EveEffCat.active,
         resist_attr_id=eve_resist_attr.id)
-    eve_sw_effect = client.mk_eve_item(
-        attrs={eve_buff_type_attr.id: eve_sw_effect_buff.id, eve_buff_val_attr.id: 5},
-        eff_ids=[eve_sw_effect_effect.id], defeff_id=eve_sw_effect_effect.id)
+    eve_proj_effect = client.mk_eve_item(
+        attrs={eve_buff_type_attr.id: eve_proj_effect_buff.id, eve_buff_val_attr.id: 5},
+        eff_ids=[eve_proj_effect_effect.id], defeff_id=eve_proj_effect_effect.id)
     eve_rig_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.item,
-        dom=consts.EveModDom.struct,
+        dom=consts.EveModDom.ship,
         op=consts.EveModOp.post_percent,
         affector_attr_id=eve_boost_attr.id,
         affectee_attr_id=eve_resist_attr.id)
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
     eve_rig = client.mk_eve_item(attrs={eve_boost_attr.id: -25}, eff_ids=[eve_rig_effect.id])
-    eve_struct = client.mk_eve_struct(attrs={eve_resist_attr.id: 0.4})
+    eve_ship = client.mk_eve_ship(attrs={eve_resist_attr.id: 0.4})
     eve_module = client.mk_eve_item(attrs={eve_affectee_attr.id: 7.5})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_struct.id)
+    api_ship = api_fit.set_ship(type_id=eve_ship.id)
     api_module = api_fit.add_mod(type_id=eve_module.id)
-    api_sol.add_sw_effect(type_id=eve_sw_effect.id)
+    api_proj_effect = api_sol.add_proj_effect(type_id=eve_proj_effect.id)
+    api_proj_effect.change_proj_effect(add_projs=[api_ship.id])
     assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(19.5)
     api_rig = api_fit.add_rig(type_id=eve_rig.id)
     assert api_module.update().attrs[eve_affectee_attr.id].dogma == approx(16.5)
