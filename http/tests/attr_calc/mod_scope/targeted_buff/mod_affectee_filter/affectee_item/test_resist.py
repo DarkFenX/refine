72e1ab552429d19534ceffef8ab7597d638e37a0
DarkPhoenix <phoenix@mail.ru>
2024-07-07 00:44:31 +0200

    Add resist tests for targeted modules which project buffs

--- http/tests/attr_calc/mod_scope/targeted_buff/mod_affectee_filter/affectee_item/test_resist.py
+++ http/tests/attr_calc/mod_scope/targeted_buff/mod_affectee_filter/affectee_item/test_resist.py
@@ -2,24 +2,23 @@ from pytest import approx
 
 
 def test_value_change_root_ship(client, consts):
-    eve_affector_attr = client.mk_eve_attr()
+    eve_affector_attr = client.mk_eve_attr(id_=consts.EveAttr.speed_factor)
     eve_affectee_attr = client.mk_eve_attr()
     eve_resist_attr = client.mk_eve_attr()
     eve_boost_attr = client.mk_eve_attr()
-    eve_module_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.item,
-        dom=consts.EveModDom.tgt,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_affector_attr.id,
-        affectee_attr_id=eve_affectee_attr.id)
-    eve_module_effect = client.mk_eve_effect(
-        cat_id=consts.EveEffCat.target,
-        resist_attr_id=eve_resist_attr.id,
-        mod_info=[eve_module_mod])
+    client.mk_eve_buff(
+        id_=consts.EveBuff.stasis_webification_burst,
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.doomsday_aoe_web,
+        cat_id=consts.EveEffCat.active,
+        resist_attr_id=eve_resist_attr.id)
     eve_affector_module = client.mk_eve_item(
-        attrs={eve_affector_attr.id: -60},
-        eff_ids=[eve_module_effect.id],
-        defeff_id=eve_module_effect.id)
+        attrs={eve_affector_attr.id: -55},
+        eff_ids=[eve_effect.id],
+        defeff_id=eve_effect.id)
     eve_rig_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.item,
         dom=consts.EveModDom.ship,
@@ -28,7 +27,7 @@ def test_value_change_root_ship(client, consts):
         affectee_attr_id=eve_resist_attr.id)
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
     eve_affectee_rig = client.mk_eve_item(attrs={eve_boost_attr.id: -25}, eff_ids=[eve_rig_effect.id])
-    eve_affectee_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 500, eve_resist_attr.id: 0.4})
+    eve_affectee_ship = client.mk_eve_ship(attrs={eve_affectee_attr.id: 200, eve_resist_attr.id: 0.4})
     client.create_sources()
     api_sol = client.create_sol()
     api_affector_fit = api_sol.create_fit()
@@ -36,41 +35,40 @@ def test_value_change_root_ship(client, consts):
     api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
     api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
     api_affector_module.change_mod(add_projs=[api_affectee_ship.id])
-    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(156)
     api_affectee_rig = api_affectee_fit.add_rig(type_id=eve_affectee_rig.id)
-    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(410)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(167)
     api_affectee_rig.remove()
-    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_ship.update().attrs[eve_affectee_attr.id].dogma == approx(156)
 
 
 def test_value_change_root_struct(client, consts):
-    eve_affector_attr = client.mk_eve_attr()
+    eve_affector_attr = client.mk_eve_attr(id_=consts.EveAttr.speed_factor)
     eve_affectee_attr = client.mk_eve_attr()
     eve_resist_attr = client.mk_eve_attr()
     eve_boost_attr = client.mk_eve_attr()
-    eve_module_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.item,
-        dom=consts.EveModDom.tgt,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_affector_attr.id,
-        affectee_attr_id=eve_affectee_attr.id)
-    eve_module_effect = client.mk_eve_effect(
-        cat_id=consts.EveEffCat.target,
-        resist_attr_id=eve_resist_attr.id,
-        mod_info=[eve_module_mod])
+    client.mk_eve_buff(
+        id_=consts.EveBuff.stasis_webification_burst,
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.doomsday_aoe_web,
+        cat_id=consts.EveEffCat.active,
+        resist_attr_id=eve_resist_attr.id)
     eve_affector_module = client.mk_eve_item(
-        attrs={eve_affector_attr.id: -60},
-        eff_ids=[eve_module_effect.id],
-        defeff_id=eve_module_effect.id)
+        attrs={eve_affector_attr.id: -55},
+        eff_ids=[eve_effect.id],
+        defeff_id=eve_effect.id)
     eve_rig_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.item,
-        dom=consts.EveModDom.struct,
+        dom=consts.EveModDom.ship,
         op=consts.EveModOp.post_percent,
         affector_attr_id=eve_boost_attr.id,
         affectee_attr_id=eve_resist_attr.id)
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
     eve_affectee_rig = client.mk_eve_item(attrs={eve_boost_attr.id: -25}, eff_ids=[eve_rig_effect.id])
-    eve_affectee_struct = client.mk_eve_struct(attrs={eve_affectee_attr.id: 500, eve_resist_attr.id: 0.4})
+    eve_affectee_struct = client.mk_eve_struct(attrs={eve_affectee_attr.id: 200, eve_resist_attr.id: 0.4})
     client.create_sources()
     api_sol = client.create_sol()
     api_affector_fit = api_sol.create_fit()
@@ -78,33 +76,32 @@ def test_value_change_root_struct(client, consts):
     api_affectee_struct = api_affectee_fit.set_ship(type_id=eve_affectee_struct.id)
     api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
     api_affector_module.change_mod(add_projs=[api_affectee_struct.id])
-    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(156)
     api_affectee_rig = api_affectee_fit.add_rig(type_id=eve_affectee_rig.id)
-    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(410)
+    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(167)
     api_affectee_rig.remove()
-    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_struct.update().attrs[eve_affectee_attr.id].dogma == approx(156)
 
 
 def test_value_change_child(client, consts):
     eve_skill = client.mk_eve_item()
-    eve_affector_attr = client.mk_eve_attr()
+    eve_affector_attr = client.mk_eve_attr(id_=consts.EveAttr.speed_factor)
     eve_affectee_attr = client.mk_eve_attr()
     eve_resist_attr = client.mk_eve_attr()
     eve_boost_attr = client.mk_eve_attr()
-    eve_module_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.item,
-        dom=consts.EveModDom.tgt,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_affector_attr.id,
-        affectee_attr_id=eve_affectee_attr.id)
-    eve_module_effect = client.mk_eve_effect(
-        cat_id=consts.EveEffCat.target,
-        resist_attr_id=eve_resist_attr.id,
-        mod_info=[eve_module_mod])
+    client.mk_eve_buff(
+        id_=consts.EveBuff.stasis_webification_burst,
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr.id)])
+    eve_effect = client.mk_eve_effect(
+        id_=consts.EveEffect.doomsday_aoe_web,
+        cat_id=consts.EveEffCat.active,
+        resist_attr_id=eve_resist_attr.id)
     eve_affector_module = client.mk_eve_item(
-        attrs={eve_affector_attr.id: -60},
-        eff_ids=[eve_module_effect.id],
-        defeff_id=eve_module_effect.id)
+        attrs={eve_affector_attr.id: -55},
+        eff_ids=[eve_effect.id],
+        defeff_id=eve_effect.id)
     eve_rig_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.own_srq,
         dom=consts.EveModDom.char,
@@ -115,19 +112,17 @@ def test_value_change_child(client, consts):
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
     eve_affectee_rig = client.mk_eve_item(attrs={eve_boost_attr.id: -25}, eff_ids=[eve_rig_effect.id])
     eve_affectee_drone = client.mk_eve_item(
-        attrs={eve_affectee_attr.id: 500, eve_resist_attr.id: 0.4},
+        attrs={eve_affectee_attr.id: 200, eve_resist_attr.id: 0.4},
         srqs={eve_skill.id: 1})
-    eve_affectee_ship = client.mk_eve_ship()
     client.create_sources()
     api_sol = client.create_sol()
     api_affector_fit = api_sol.create_fit()
     api_affectee_fit = api_sol.create_fit()
-    api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
     api_affectee_drone = api_affectee_fit.add_drone(type_id=eve_affectee_drone.id)
     api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
     api_affector_module.change_mod(add_projs=[api_affectee_drone.id])
-    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(156)
     api_affectee_rig = api_affectee_fit.add_rig(type_id=eve_affectee_rig.id)
-    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(410)
+    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(167)
     api_affectee_rig.remove()
-    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_drone.update().attrs[eve_affectee_attr.id].dogma == approx(156)
--- http/tests/attr_calc/mod_scope/targeted_buff/mod_affectee_filter/affectee_location_skillrq/test_resist.py
+++ http/tests/attr_calc/mod_scope/targeted_buff/mod_affectee_filter/affectee_location_skillrq/test_resist.py
@@ -3,25 +3,24 @@ from pytest import approx
 
 def test_value_change(client, consts):
     eve_skill = client.mk_eve_item()
-    eve_affector_attr = client.mk_eve_attr()
+    eve_affector_attr = client.mk_eve_attr(id_=consts.EveAttr.speed_factor)
     eve_affectee_attr = client.mk_eve_attr()
     eve_resist_attr = client.mk_eve_attr()
     eve_boost_attr = client.mk_eve_attr()
-    eve_module_mod = client.mk_eve_effect_mod(
-        func=consts.EveModFunc.loc_srq,
-        dom=consts.EveModDom.tgt,
-        srq=eve_skill.id,
-        op=consts.EveModOp.post_percent,
-        affector_attr_id=eve_affector_attr.id,
-        affectee_attr_id=eve_affectee_attr.id)
+    client.mk_eve_buff(
+        id_=consts.EveBuff.stasis_webification_burst,
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        loc_srq_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr.id, skill_id=eve_skill.id)])
     eve_module_effect = client.mk_eve_effect(
-        cat_id=consts.EveEffCat.target,
-        resist_attr_id=eve_resist_attr.id,
-        mod_info=[eve_module_mod])
+        id_=consts.EveEffect.doomsday_aoe_web,
+        cat_id=consts.EveEffCat.active,
+        resist_attr_id=eve_resist_attr.id)
     eve_affector_module = client.mk_eve_item(
-        attrs={eve_affector_attr.id: -60},
+        attrs={eve_affector_attr.id: -55},
         eff_ids=[eve_module_effect.id],
         defeff_id=eve_module_effect.id)
+    eve_affectee_module = client.mk_eve_item(attrs={eve_affectee_attr.id: 200}, srqs={eve_skill.id: 1})
     eve_rig_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.item,
         dom=consts.EveModDom.ship,
@@ -31,17 +30,16 @@ def test_value_change(client, consts):
     eve_rig_effect = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=[eve_rig_mod])
     eve_affectee_rig = client.mk_eve_item(attrs={eve_boost_attr.id: -25}, eff_ids=[eve_rig_effect.id])
     eve_affectee_ship = client.mk_eve_ship(attrs={eve_resist_attr.id: 0.4})
-    eve_affectee_module = client.mk_eve_item(attrs={eve_affectee_attr.id: 500}, srqs={eve_skill.id: 1})
     client.create_sources()
     api_sol = client.create_sol()
     api_affector_fit = api_sol.create_fit()
     api_affectee_fit = api_sol.create_fit()
+    api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
     api_affectee_ship = api_affectee_fit.set_ship(type_id=eve_affectee_ship.id)
     api_affectee_module = api_affectee_fit.add_mod(type_id=eve_affectee_module.id)
-    api_affector_module = api_affector_fit.add_mod(type_id=eve_affector_module.id, state=consts.ApiState.active)
     api_affector_module.change_mod(add_projs=[api_affectee_ship.id])
-    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(156)
     api_affectee_rig = api_affectee_fit.add_rig(type_id=eve_affectee_rig.id)
-    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(410)
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(167)
     api_affectee_rig.remove()
-    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(380)
+    assert api_affectee_module.update().attrs[eve_affectee_attr.id].dogma == approx(156)
