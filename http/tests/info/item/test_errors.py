4572ee60c1ab18f12fc6d87987dd872f547f755f
DarkPhoenix <phoenix@mail.ru>
2024-07-21 13:14:52 +0200

    Do not pass info mode where we do not care about it

--- http/tests/info/item/test_errors.py
+++ http/tests/info/item/test_errors.py
@@ -1,4 +1,7 @@
 
+from tests.support.util import Absent
+
+
 def test_error_no_item_full(client, consts):
     # Send ID in correct format, but there is no fleet with such ID
     client.create_sources()
@@ -21,6 +24,6 @@ def test_error_no_item_malformed(client, consts):
     # Send ID in incorrect format
     client.create_sources()
     api_sol = client.create_sol()
-    resp = api_sol.get_item_request(item_id='abc', item_info_mode=consts.ApiItemInfoMode.id).send()
+    resp = api_sol.get_item_request(item_id='abc', item_info_mode=Absent).send()
     resp.check(status_code=404, json_predicate={'code': 'IDC-003', 'message': 'unable to cast string "abc" to id'})
     api_sol.check()
