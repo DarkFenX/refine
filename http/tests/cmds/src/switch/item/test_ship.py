5811afaabea7ee39657466de419ac1dca55ea280
DarkPhoenix <phoenix@mail.ru>
2024-07-27 17:04:41 +0200

    Extend ship switch test

--- http/tests/cmds/src/switch/item/test_ship.py
+++ http/tests/cmds/src/switch/item/test_ship.py
@@ -20,7 +20,7 @@ def test_loaded_to_loaded(client):
     assert api_ship.attrs[eve_d2_attr2.id].dogma == approx(85)
 
 
-def test_loaded_to_unloaded(client):
+def test_loaded_to_unloaded_to_loaded(client):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_d1_attr1 = eve_d1.mk_attr()
@@ -29,7 +29,13 @@ def test_loaded_to_unloaded(client):
     api_sol = client.create_sol(data=eve_d1)
     api_fit = api_sol.create_fit()
     api_ship = api_fit.set_ship(type_id=eve_d1_ship.id)
+    # Verification
     assert api_ship.update().attrs[eve_d1_attr1.id].dogma == approx(50)
+    # Action
     api_sol.change_src(src_alias=eve_d2.alias)
-    api_ship.update()
-    assert len(api_ship.attrs) == 0
+    # Verification
+    assert len(api_ship.update().attrs) == 0
+    # Action
+    api_sol.change_src(src_alias=eve_d1.alias)
+    # Verification
+    assert api_ship.update().attrs[eve_d1_attr1.id].dogma == approx(50)
