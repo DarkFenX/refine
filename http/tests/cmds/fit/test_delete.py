a710b2a557bcf35e012d68cd21f057f12b337540
DarkPhoenix <phoenix@mail.ru>
2024-08-05 22:27:37 +0200

    Move autocharge tests to separate feature suite

--- http/tests/cmds/fit/test_delete.py
+++ http/tests/cmds/fit/test_delete.py
@@ -9,16 +9,3 @@ def test_charge(client):
     api_fit.add_mod(type_id=eve_module.id, charge_type_id=eve_charge.id)
     # Check via consistency check if item with charge is properly removed when fit is removed
     api_fit.remove()
-
-
-def test_autocharge(client, consts):
-    eve_attr = client.mk_eve_attr(id_=consts.EveAttr.fighter_ability_launch_bomb_type)
-    eve_effect = client.mk_eve_effect(id_=consts.EveEffect.fighter_ability_launch_bomb, cat_id=consts.EveEffCat.active)
-    eve_charge = client.mk_eve_item()
-    eve_fighter = client.mk_eve_item(attrs={eve_attr.id: eve_charge.id}, eff_ids=[eve_effect.id])
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_fighter(type_id=eve_fighter.id)
-    # Check via consistency check if item with autocharge is properly removed when fit is removed
-    api_fit.remove()
--- http/tests/features/autocharges/test_remove.py
+++ http/tests/features/autocharges/test_remove.py
@@ -1,5 +1,5 @@
 
-def test_remove(client, consts):
+def test_remove_item(client, consts):
     eve_attr = client.mk_eve_attr(id_=consts.EveAttr.fighter_ability_launch_bomb_type)
     eve_effect = client.mk_eve_effect(id_=consts.EveEffect.fighter_ability_launch_bomb, cat_id=consts.EveEffCat.active)
     eve_charge = client.mk_eve_item()
@@ -22,3 +22,16 @@ def test_remove(client, consts):
     api_fighter.remove()
     # Try removing autocharge again
     api_autocharge.remove(status_code=404)
+
+
+def test_remove_fit(client, consts):
+    eve_attr = client.mk_eve_attr(id_=consts.EveAttr.fighter_ability_launch_bomb_type)
+    eve_effect = client.mk_eve_effect(id_=consts.EveEffect.fighter_ability_launch_bomb, cat_id=consts.EveEffCat.active)
+    eve_charge = client.mk_eve_item()
+    eve_fighter = client.mk_eve_item(attrs={eve_attr.id: eve_charge.id}, eff_ids=[eve_effect.id])
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_fighter(type_id=eve_fighter.id)
+    # Check via consistency check if item with autocharge is properly removed when fit is removed
+    api_fit.remove()
