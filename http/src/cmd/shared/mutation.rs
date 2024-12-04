1aa53cdaa74ad5ca245b65c10994a4931ac39c34
DarkPhoenix <phoenix@mail.ru>
2024-12-04 22:59:08 +0100

    Make mutation command request more flexible

--- http/src/cmd/shared/mutation.rs
+++ http/src/cmd/shared/mutation.rs
@@ -1,18 +1,36 @@
 use std::collections::HashMap;
 
-#[serde_with::serde_as]
 #[derive(serde::Deserialize)]
-pub(in crate::cmd) struct HItemMutation {
-    pub(in crate::cmd) mutator_id: rc::EItemId,
-    // Workaround for https://github.com/serde-rs/serde/issues/1183
-    #[serde_as(as = "std::collections::HashMap<serde_with::DisplayFromStr, _>")]
-    pub(in crate::cmd) attrs: HashMap<rc::EAttrId, HItemAttrMutation>,
+#[serde(untagged)]
+pub(in crate::cmd) enum HItemMutation {
+    Short(rc::EItemId),
+    Full(HItemMutationFull),
 }
 impl Into<rc::SolItemMutation> for &HItemMutation {
     fn into(self) -> rc::SolItemMutation {
+        match self {
+            HItemMutation::Short(mutator_id) => rc::SolItemMutation::new(*mutator_id),
+            HItemMutation::Full(full_mutation) => full_mutation.into(),
+        }
+    }
+}
+
+#[serde_with::serde_as]
+#[derive(serde_tuple::Deserialize_tuple)]
+pub(in crate::cmd) struct HItemMutationFull {
+    pub(in crate::cmd) mutator_id: rc::EItemId,
+    // Workaround for https://github.com/serde-rs/serde/issues/1183
+    #[serde_as(as = "Option<std::collections::HashMap<serde_with::DisplayFromStr, _>>")]
+    pub(in crate::cmd) attrs: Option<HashMap<rc::EAttrId, HItemAttrMutation>>,
+}
+impl Into<rc::SolItemMutation> for &HItemMutationFull {
+    fn into(self) -> rc::SolItemMutation {
         rc::SolItemMutation::new_with_attrs(
             self.mutator_id,
-            self.attrs.iter().map(|(k, v)| (*k, v.into())).collect(),
+            self.attrs
+                .as_ref()
+                .map(|v| v.iter().map(|(k, v)| (*k, v.into())).collect())
+                .unwrap_or_else(|| rc::util::StMap::new()),
         )
     }
 }
