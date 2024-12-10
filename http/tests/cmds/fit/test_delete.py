f086c933aa4aebcefc28c75bfb579e6405ed5acb
DarkPhoenix <phoenix@mail.ru>
2024-12-10 10:09:10 +0100

    Fix tests in cmds suite

--- http/tests/cmds/fit/test_delete.py
+++ http/tests/cmds/fit/test_delete.py
@@ -1,11 +1,11 @@
 
 
 def test_charge(client):
-    eve_module = client.mk_eve_item()
-    eve_charge = client.mk_eve_item()
+    eve_module_id = client.mk_eve_item()
+    eve_charge_id = client.mk_eve_item()
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.add_mod(type_id=eve_module.id, charge_type_id=eve_charge.id)
+    api_fit.add_mod(type_id=eve_module_id, charge_type_id=eve_charge_id)
     # Check via consistency check if item with charge is properly removed when fit is removed
     api_fit.remove()
--- http/tests/cmds/src/switch/item/test_ship.py
+++ http/tests/cmds/src/switch/item/test_ship.py
@@ -4,33 +4,37 @@ from tests import approx, check_no_field
 def test_loaded_to_loaded(client):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
-    eve_d1_attr1 = eve_d1.mk_attr()
-    eve_d2_attr1 = eve_d2.mk_attr()
-    eve_d2_attr2 = eve_d2.mk_attr()
-    eve_d1_ship = eve_d1.mk_ship(attrs={eve_d1_attr1.id: 50})
-    eve_d2.mk_ship(id_=eve_d1_ship.id, attrs={eve_d2_attr1.id: 30, eve_d2_attr2.id: 85})
+    eve_d1_attr1_id = client.alloc_attr_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_attr(datas=[eve_d1], id_=eve_d1_attr1_id)
+    eve_d2_attr1_id = client.alloc_attr_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_attr(datas=[eve_d2], id_=eve_d2_attr1_id)
+    eve_d2_attr2_id = client.alloc_attr_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_attr(datas=[eve_d2], id_=eve_d2_attr2_id)
+    eve_ship_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_ship(datas=[eve_d1], id_=eve_ship_id, attrs={eve_d1_attr1_id: 50})
+    client.mk_eve_ship(datas=[eve_d2], id_=eve_ship_id, attrs={eve_d2_attr1_id: 30, eve_d2_attr2_id: 85})
     client.create_sources()
     api_sol = client.create_sol(data=eve_d1)
     api_fit = api_sol.create_fit()
-    api_ship = api_fit.set_ship(type_id=eve_d1_ship.id)
-    assert api_ship.update().attrs[eve_d1_attr1.id].dogma == approx(50)
+    api_ship = api_fit.set_ship(type_id=eve_ship_id)
+    assert api_ship.update().attrs[eve_d1_attr1_id].dogma == approx(50)
     api_sol.change_src(data=eve_d2)
     api_ship.update()
-    assert api_ship.attrs[eve_d2_attr1.id].dogma == approx(30)
-    assert api_ship.attrs[eve_d2_attr2.id].dogma == approx(85)
+    assert api_ship.attrs[eve_d2_attr1_id].dogma == approx(30)
+    assert api_ship.attrs[eve_d2_attr2_id].dogma == approx(85)
 
 
 def test_loaded_to_unloaded_to_loaded(client):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
-    eve_d1_attr1 = eve_d1.mk_attr()
-    eve_d1_ship = eve_d1.mk_ship(attrs={eve_d1_attr1.id: 50})
+    eve_d1_attr1_id = client.mk_eve_attr(datas=[eve_d1])
+    eve_d1_ship_ip = client.mk_eve_ship(datas=[eve_d1], attrs={eve_d1_attr1_id: 50})
     client.create_sources()
     api_sol = client.create_sol(data=eve_d1)
     api_fit = api_sol.create_fit()
-    api_ship = api_fit.set_ship(type_id=eve_d1_ship.id)
+    api_ship = api_fit.set_ship(type_id=eve_d1_ship_ip)
     # Verification
-    assert api_ship.update().attrs[eve_d1_attr1.id].dogma == approx(50)
+    assert api_ship.update().attrs[eve_d1_attr1_id].dogma == approx(50)
     # Action
     api_sol.change_src(data=eve_d2)
     # Verification
@@ -40,4 +44,4 @@ def test_loaded_to_unloaded_to_loaded(client):
     # Action
     api_sol.change_src(data=eve_d1)
     # Verification
-    assert api_ship.update().attrs[eve_d1_attr1.id].dogma == approx(50)
+    assert api_ship.update().attrs[eve_d1_attr1_id].dogma == approx(50)
