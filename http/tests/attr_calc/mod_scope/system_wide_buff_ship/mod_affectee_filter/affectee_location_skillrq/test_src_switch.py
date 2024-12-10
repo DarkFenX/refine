7d5ac73b938616219ed0afc029676c4addf7e279
DarkPhoenix <phoenix@mail.ru>
2024-12-10 15:35:24 +0100

    Fix tests in system-* suites

--- http/tests/attr_calc/mod_scope/system_wide_buff_ship/mod_affectee_filter/affectee_location_skillrq/test_src_switch.py
+++ http/tests/attr_calc/mod_scope/system_wide_buff_ship/mod_affectee_filter/affectee_location_skillrq/test_src_switch.py
@@ -4,42 +4,28 @@ from tests import approx
 def test_to_struct(client, consts):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
-    eve_skill_id = eve_d1.mk_item().id
-    eve_d2.mk_item(id_=eve_skill_id)
-    eve_d1.mk_attr(id_=consts.EveAttr.warfare_buff_1_id)
-    eve_d2.mk_attr(id_=consts.EveAttr.warfare_buff_1_id)
-    eve_d1.mk_attr(id_=consts.EveAttr.warfare_buff_1_value)
-    eve_d2.mk_attr(id_=consts.EveAttr.warfare_buff_1_value)
-    eve_affectee_attr_id = eve_d1.mk_attr().id
-    eve_d2.mk_attr(id_=eve_affectee_attr_id)
-    eve_buff_id = eve_d1.mk_buff(
-        aggr_mode=consts.EveBuffAggrMode.max,
-        op=consts.EveBuffOp.post_percent,
-        loc_srq_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr_id, skill_id=eve_skill_id)]).id
-    eve_d2.mk_buff(
-        id_=eve_buff_id,
+    eve_skill_id = client.mk_eve_item(datas=[eve_d1, eve_d2])
+    eve_buff_type_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.warfare_buff_1_id)
+    eve_buff_val_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.warfare_buff_1_value)
+    eve_affectee_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2])
+    eve_buff_id = client.mk_eve_buff(
+        datas=[eve_d1, eve_d2],
         aggr_mode=consts.EveBuffAggrMode.max,
         op=consts.EveBuffOp.post_percent,
         loc_srq_mods=[client.mk_eve_buff_mod(attr_id=eve_affectee_attr_id, skill_id=eve_skill_id)])
-    eve_d1.mk_effect(
+    eve_effect_id = client.mk_eve_effect(
+        datas=[eve_d1, eve_d2],
         id_=consts.EveEffect.mod_titan_effect_generator,
         cat_id=consts.EveEffCat.active)
-    eve_d2.mk_effect(
-        id_=consts.EveEffect.mod_titan_effect_generator,
-        cat_id=consts.EveEffCat.active)
-    eve_sw_effect_id = eve_d1.mk_item(
-        attrs={consts.EveAttr.warfare_buff_1_id: eve_buff_id, consts.EveAttr.warfare_buff_1_value: 30},
-        eff_ids=[consts.EveEffect.mod_titan_effect_generator],
-        defeff_id=consts.EveEffect.mod_titan_effect_generator).id
-    eve_d2.mk_item(
-        id_=eve_sw_effect_id,
-        attrs={consts.EveAttr.warfare_buff_1_id: eve_buff_id, consts.EveAttr.warfare_buff_1_value: 30},
-        eff_ids=[consts.EveEffect.mod_titan_effect_generator],
-        defeff_id=consts.EveEffect.mod_titan_effect_generator)
-    eve_rig_id = eve_d1.mk_item(attrs={eve_affectee_attr_id: 200}, srqs={eve_skill_id: 1}).id
-    eve_d2.mk_item(id_=eve_rig_id, attrs={eve_affectee_attr_id: 200}, srqs={eve_skill_id: 1})
-    eve_root_id = eve_d1.mk_ship().id
-    eve_d2.mk_struct(id_=eve_root_id)
+    eve_sw_effect_id = client.mk_eve_item(
+        datas=[eve_d1, eve_d2],
+        attrs={eve_buff_type_attr_id: eve_buff_id, eve_buff_val_attr_id: 30},
+        eff_ids=[eve_effect_id],
+        defeff_id=eve_effect_id)
+    eve_rig_id = client.mk_eve_item(datas=[eve_d1, eve_d2], attrs={eve_affectee_attr_id: 200}, srqs={eve_skill_id: 1})
+    eve_root_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_ship(datas=[eve_d1], id_=eve_root_id)
+    client.mk_eve_struct(datas=[eve_d2], id_=eve_root_id)
     client.create_sources()
     api_sol = client.create_sol(data=eve_d1)
     api_sol.add_sw_effect(type_id=eve_sw_effect_id)
