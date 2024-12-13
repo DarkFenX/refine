b512ed152e4a1920faa6488ae71564d92a3c4a10
DarkPhoenix <phoenix@mail.ru>
2024-12-13 09:07:24 +0100

    Repurpose existing source switching tests

--- http/tests/features/mutated_items/test_src_switch.py
+++ http/tests/features/mutated_items/test_src_switch.py
@@ -1,8 +1,8 @@
 from tests import approx
 
 
-def test_complete_to_complete_same(client, consts):
-    # Source switch to slightly different mutated item with the same ID
+def test_complete_to_complete_different_base_value(client, consts):
+    # Check how mutation values are transferred upon new base attribute value on new source
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_lower_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2])
@@ -31,7 +31,6 @@ def test_complete_to_complete_same(client, consts):
         eve_higher_attr_id: {consts.ApiAttrMutation.absolute: 120}}))
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_mutated_item_id
     assert api_item.mutation.attrs[eve_lower_attr_id].roll == approx(0)
     assert api_item.mutation.attrs[eve_lower_attr_id].absolute == approx(80)
     assert api_item.mutation.attrs[eve_within_attr_id].roll == approx(0.5)
@@ -45,7 +44,6 @@ def test_complete_to_complete_same(client, consts):
     api_sol.change_src(data=eve_d2)
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_mutated_item_id
     assert api_item.mutation.attrs[eve_lower_attr_id].roll == approx(0)
     assert api_item.mutation.attrs[eve_lower_attr_id].absolute == approx(64)
     assert api_item.mutation.attrs[eve_within_attr_id].roll == approx(0.5)
@@ -57,9 +55,8 @@ def test_complete_to_complete_same(client, consts):
     assert api_item.attrs[eve_higher_attr_id].base == approx(144)
 
 
-def test_complete_to_complete_different(client, consts):
-    # Source switch to mutated item with different type ID (i.e. check that mutated item is defined
-    # by base item ID and mutator ID)
+def test_complete_to_complete_different_ranges(client, consts):
+    # Check how mutation values are transferred upon new mutation ranges on new source
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_lower_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2])
@@ -68,20 +65,17 @@ def test_complete_to_complete_different(client, consts):
     eve_base_item_id = client.mk_eve_item(
         datas=[eve_d1, eve_d2],
         attrs={eve_lower_attr_id: 100, eve_within_attr_id: 100, eve_higher_attr_id: 100})
-    eve_d1_mutated_item_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
-    client.mk_eve_item(datas=[eve_d1], id_=eve_d1_mutated_item_id)
-    eve_d2_mutated_item_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
-    client.mk_eve_item(datas=[eve_d2], id_=eve_d2_mutated_item_id)
+    eve_mutated_item_id = client.mk_eve_item(datas=[eve_d1, eve_d2])
     eve_mutator_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
     client.mk_eve_mutator(
         datas=[eve_d1],
         id_=eve_mutator_id,
-        items=[([eve_base_item_id], eve_d1_mutated_item_id)],
+        items=[([eve_base_item_id], eve_mutated_item_id)],
         attributes={eve_lower_attr_id: (0.8, 1.2), eve_within_attr_id: (0.8, 1.2), eve_higher_attr_id: (0.8, 1.2)})
     client.mk_eve_mutator(
         datas=[eve_d2],
         id_=eve_mutator_id,
-        items=[([eve_base_item_id], eve_d2_mutated_item_id)],
+        items=[([eve_base_item_id], eve_mutated_item_id)],
         attributes={eve_lower_attr_id: (0.7, 1.1), eve_within_attr_id: (0.8, 1.2), eve_higher_attr_id: (0.9, 1.3)})
     client.create_sources()
     api_sol = client.create_sol(data=eve_d1)
@@ -92,7 +86,6 @@ def test_complete_to_complete_different(client, consts):
         eve_higher_attr_id: {consts.ApiAttrMutation.absolute: 100}}))
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_d1_mutated_item_id
     assert api_item.mutation.attrs[eve_lower_attr_id].roll == approx(0.5)
     assert api_item.mutation.attrs[eve_lower_attr_id].absolute == approx(100)
     assert api_item.mutation.attrs[eve_within_attr_id].roll == approx(0.5)
@@ -106,7 +99,6 @@ def test_complete_to_complete_different(client, consts):
     api_sol.change_src(data=eve_d2)
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_d2_mutated_item_id
     assert api_item.mutation.attrs[eve_lower_attr_id].roll == approx(0.5)
     assert api_item.mutation.attrs[eve_lower_attr_id].absolute == approx(90)
     assert api_item.mutation.attrs[eve_within_attr_id].roll == approx(0.5)
