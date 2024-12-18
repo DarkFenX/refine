e2e434e6a034ce5770533b2ba387cb262df975a3
DarkPhoenix <phoenix@mail.ru>
2024-12-18 14:17:15 +0100

    Reorder tests

--- http/tests/features/mutated_items/test_unmutate.py
+++ http/tests/features/mutated_items/test_unmutate.py
@@ -9,7 +9,7 @@ Stages is just a short description of how much data was available for the mutati
 from tests import approx, check_no_field
 
 
-def test_from_unmutated(client):
+def test_from_stage4(client, consts):
     eve_attr_id = client.mk_eve_attr()
     eve_base_item_id = client.mk_eve_item(attrs={eve_attr_id: 100})
     eve_mutated_item_id = client.mk_eve_item()
@@ -19,12 +19,17 @@ def test_from_unmutated(client):
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_item = api_fit.add_mod(type_id=eve_base_item_id)
+    api_item = api_fit.add_mod(
+        type_id=eve_base_item_id,
+        mutation=(eve_mutator_id, {eve_attr_id: {consts.ApiAttrMutation.roll: 0.6}}))
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_base_item_id
-    with check_no_field():
-        api_item.mutation  # pylint: disable=W0104
+    assert api_item.type_id == eve_mutated_item_id
+    assert api_item.mutation.base_type_id == eve_base_item_id
+    assert len(api_item.mutation.attrs) == 1
+    assert api_item.mutation.attrs[eve_attr_id].roll == approx(0.6)
+    assert api_item.mutation.attrs[eve_attr_id].absolute == approx(104)
+    assert api_item.attrs[eve_attr_id].base == approx(104)
     # Action
     api_item.change_mod(mutation=None)
     # Verification
@@ -34,7 +39,7 @@ def test_from_unmutated(client):
         api_item.mutation  # pylint: disable=W0104
     # Action
     api_item.change_mod(mutation=eve_mutator_id)
-    # Verification
+    # Verification - after mutating item again, all the old mutations should be gone
     api_item.update()
     assert api_item.type_id == eve_mutated_item_id
     assert api_item.mutation.base_type_id == eve_base_item_id
@@ -44,10 +49,13 @@ def test_from_unmutated(client):
     assert api_item.attrs[eve_attr_id].base == approx(100)
 
 
-def test_from_stage1(client, consts):
+def test_from_stage3(client, consts):
     eve_attr_id = client.mk_eve_attr()
     eve_base_item_id = client.mk_eve_item(attrs={eve_attr_id: 100})
-    eve_mutator_id = client.alloc_item_id()
+    eve_mutated_item_id = client.alloc_item_id()
+    eve_mutator_id = client.mk_eve_mutator(
+        items=[([eve_base_item_id], eve_mutated_item_id)],
+        attributes={eve_attr_id: (0.8, 1.2)})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
@@ -109,13 +117,10 @@ def test_from_stage2(client, consts):
         api_item.mutation  # pylint: disable=W0104
 
 
-def test_from_stage3(client, consts):
+def test_from_stage1(client, consts):
     eve_attr_id = client.mk_eve_attr()
     eve_base_item_id = client.mk_eve_item(attrs={eve_attr_id: 100})
-    eve_mutated_item_id = client.alloc_item_id()
-    eve_mutator_id = client.mk_eve_mutator(
-        items=[([eve_base_item_id], eve_mutated_item_id)],
-        attributes={eve_attr_id: (0.8, 1.2)})
+    eve_mutator_id = client.alloc_item_id()
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
@@ -143,7 +148,7 @@ def test_from_stage3(client, consts):
         api_item.mutation  # pylint: disable=W0104
 
 
-def test_from_stage4(client, consts):
+def test_from_unmutated(client):
     eve_attr_id = client.mk_eve_attr()
     eve_base_item_id = client.mk_eve_item(attrs={eve_attr_id: 100})
     eve_mutated_item_id = client.mk_eve_item()
@@ -153,17 +158,12 @@ def test_from_stage4(client, consts):
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_item = api_fit.add_mod(
-        type_id=eve_base_item_id,
-        mutation=(eve_mutator_id, {eve_attr_id: {consts.ApiAttrMutation.roll: 0.6}}))
+    api_item = api_fit.add_mod(type_id=eve_base_item_id)
     # Verification
     api_item.update()
-    assert api_item.type_id == eve_mutated_item_id
-    assert api_item.mutation.base_type_id == eve_base_item_id
-    assert len(api_item.mutation.attrs) == 1
-    assert api_item.mutation.attrs[eve_attr_id].roll == approx(0.6)
-    assert api_item.mutation.attrs[eve_attr_id].absolute == approx(104)
-    assert api_item.attrs[eve_attr_id].base == approx(104)
+    assert api_item.type_id == eve_base_item_id
+    with check_no_field():
+        api_item.mutation  # pylint: disable=W0104
     # Action
     api_item.change_mod(mutation=None)
     # Verification
@@ -173,7 +173,7 @@ def test_from_stage4(client, consts):
         api_item.mutation  # pylint: disable=W0104
     # Action
     api_item.change_mod(mutation=eve_mutator_id)
-    # Verification - after mutating item again, all the old mutations should be gone
+    # Verification
     api_item.update()
     assert api_item.type_id == eve_mutated_item_id
     assert api_item.mutation.base_type_id == eve_base_item_id
