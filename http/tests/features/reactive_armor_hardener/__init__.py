56bf46b806c838b8af1f6699531302c060273f75
DarkPhoenix <phoenix@mail.ru>
2025-01-02 22:38:16 +0100

    Add first proper RAH tests

--- http/tests/features/reactive_armor_hardener/__init__.py
+++ http/tests/features/reactive_armor_hardener/__init__.py
@@ -18,23 +18,40 @@ class RahEveInfo:
     ship_id: int
 
 
-def setup_rah(*, client, consts, datas=Default) -> RahEveInfo:
-    eve_res_max_attr_id = client.mk_eve_attr(datas=datas, id_=consts.EveAttr.armor_max_dmg_resonance, def_val=1)
+def setup_rah(
+        *,
+        client,
+        consts,
+        datas=Default,
+        ship_resos=(0.5, 0.65, 0.75, 0.9),
+        rah_resos=(0.85, 0.85, 0.85, 0.85),
+        rah_shift_amount=6,
+        cycle_time=10000,
+        heat_cycle_mod=-15,
+) -> RahEveInfo:
+    eve_res_max_attr_id = client.mk_eve_attr(
+        datas=datas,
+        id_=consts.EveAttr.armor_max_dmg_resonance,
+        def_val=1)
     eve_res_em_attr_id = client.mk_eve_attr(
         datas=datas,
         id_=consts.EveAttr.armor_em_dmg_resonance,
+        stackable=False,
         max_attr_id=eve_res_max_attr_id)
     eve_res_therm_attr_id = client.mk_eve_attr(
         datas=datas,
         id_=consts.EveAttr.armor_therm_dmg_resonance,
+        stackable=False,
         max_attr_id=eve_res_max_attr_id)
     eve_res_kin_attr_id = client.mk_eve_attr(
         datas=datas,
         id_=consts.EveAttr.armor_kin_dmg_resonance,
+        stackable=False,
         max_attr_id=eve_res_max_attr_id)
     eve_res_expl_attr_id = client.mk_eve_attr(
         datas=datas,
         id_=consts.EveAttr.armor_expl_dmg_resonance,
+        stackable=False,
         max_attr_id=eve_res_max_attr_id)
     eve_cycle_time_attr_id = client.mk_eve_attr(datas=datas, id_=consts.EveAttr.duration)
     eve_cycle_time_bonus_attr_id = client.mk_eve_attr(datas=datas, id_=consts.EveAttr.overload_self_duration_bonus)
@@ -57,21 +74,23 @@ def setup_rah(*, client, consts, datas=Default) -> RahEveInfo:
         mod_info=[eve_heat_mod])
     eve_rah_id = client.mk_eve_item(
         datas=datas,
-        attrs={
-            eve_res_em_attr_id: 0.85,
-            eve_res_therm_attr_id: 0.85,
-            eve_res_kin_attr_id: 0.85,
-            eve_res_expl_attr_id: 0.85,
-            eve_cycle_time_attr_id: 10000,
-            eve_cycle_time_bonus_attr_id: -15,
-            eve_res_shift_attr_id: 6},
+        attrs=dict(zip(
+            (eve_res_em_attr_id,
+             eve_res_therm_attr_id,
+             eve_res_kin_attr_id,
+             eve_res_expl_attr_id,
+             eve_res_shift_attr_id,
+             eve_cycle_time_attr_id,
+             eve_cycle_time_bonus_attr_id),
+            (*rah_resos,
+             rah_shift_amount,
+             cycle_time,
+             heat_cycle_mod))),
         eff_ids=[eve_rah_effect_id, eve_heat_effect_id],
         defeff_id=eve_rah_effect_id)
-    eve_ship_id = client.mk_eve_ship(datas=datas, attrs={
-        eve_res_em_attr_id: 0.5,
-        eve_res_therm_attr_id: 0.65,
-        eve_res_kin_attr_id: 0.75,
-        eve_res_expl_attr_id: 0.9})
+    eve_ship_id = client.mk_eve_ship(datas=datas, attrs=dict(zip(
+        (eve_res_em_attr_id, eve_res_therm_attr_id, eve_res_kin_attr_id, eve_res_expl_attr_id),
+        ship_resos)))
     return RahEveInfo(
         res_max_attr_id=eve_res_max_attr_id,
         res_em_attr_id=eve_res_em_attr_id,
