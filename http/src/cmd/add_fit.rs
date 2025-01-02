35f379bf18ac77fb3a6e663e5a988b51c8dcb6af
DarkPhoenix <phoenix@mail.ru>
2025-01-02 10:25:05 +0100

    Change flow of add fit command

--- http/src/cmd/add_fit.rs
+++ http/src/cmd/add_fit.rs
@@ -6,32 +6,34 @@ pub(crate) struct HAddFitCmd {
 }
 impl HAddFitCmd {
     pub(crate) fn execute(&self, core_sol: &mut rc::SolarSystem) -> Result<rc::SolFitInfo, HExecError> {
-        let fit_id = core_sol.add_fit().id;
-        if let Some(rah_incoming_dmg) = &self.rah_incoming_dmg {
-            if let Err(error) = core_sol.set_fit_rah_incoming_dmg(&fit_id, rah_incoming_dmg.into()) {
-                match error {
-                    rc::err::SetFitRahIncomingDmgError::EmDmgNegative(e) => {
-                        core_sol.remove_fit(&fit_id).unwrap();
-                        return Err(HExecError::InvalidDmgProfileEm(e));
+        let fit_info = core_sol.add_fit();
+        match &self.rah_incoming_dmg {
+            Some(rah_incoming_dmg) => {
+                if let Err(error) = core_sol.set_fit_rah_incoming_dmg(&fit_info.id, rah_incoming_dmg.into()) {
+                    match error {
+                        rc::err::SetFitRahIncomingDmgError::EmDmgNegative(e) => {
+                            core_sol.remove_fit(&fit_info.id).unwrap();
+                            return Err(HExecError::InvalidDmgProfileEm(e));
+                        }
+                        rc::err::SetFitRahIncomingDmgError::ThermDmgNegative(e) => {
+                            core_sol.remove_fit(&fit_info.id).unwrap();
+                            return Err(HExecError::InvalidDmgProfileTherm(e));
+                        }
+                        rc::err::SetFitRahIncomingDmgError::KinDmgNegative(e) => {
+                            core_sol.remove_fit(&fit_info.id).unwrap();
+                            return Err(HExecError::InvalidDmgProfileKin(e));
+                        }
+                        rc::err::SetFitRahIncomingDmgError::ExplDmgNegative(e) => {
+                            core_sol.remove_fit(&fit_info.id).unwrap();
+                            return Err(HExecError::InvalidDmgProfileExpl(e));
+                        }
+                        rc::err::SetFitRahIncomingDmgError::FitNotFound(_) => panic!(),
                     }
-                    rc::err::SetFitRahIncomingDmgError::ThermDmgNegative(e) => {
-                        core_sol.remove_fit(&fit_id).unwrap();
-                        return Err(HExecError::InvalidDmgProfileTherm(e));
-                    }
-                    rc::err::SetFitRahIncomingDmgError::KinDmgNegative(e) => {
-                        core_sol.remove_fit(&fit_id).unwrap();
-                        return Err(HExecError::InvalidDmgProfileKin(e));
-                    }
-                    rc::err::SetFitRahIncomingDmgError::ExplDmgNegative(e) => {
-                        core_sol.remove_fit(&fit_id).unwrap();
-                        return Err(HExecError::InvalidDmgProfileExpl(e));
-                    }
-                    rc::err::SetFitRahIncomingDmgError::FitNotFound(_) => panic!(),
                 }
-            };
-        };
-        let core_fit_info = core_sol.get_fit(&fit_id).unwrap();
-        Ok(core_fit_info)
+            }
+            None => return Ok(fit_info),
+        }
+        Ok(core_sol.get_fit(&fit_info.id).unwrap())
     }
 }
 impl Default for HAddFitCmd {
