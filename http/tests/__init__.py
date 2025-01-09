8a5803547c63547779da318dbc8f2423c06f568b
DarkPhoenix <phoenix@mail.ru>
2025-01-09 10:11:53 +0100

    Change approx function, and add new RAH test

--- http/tests/__init__.py
+++ http/tests/__init__.py
@@ -16,14 +16,18 @@ TEST_FOLDER_SPLIT = os.path.dirname(os.path.normpath(os.path.realpath(__file__))
 
 
 # Wrapper around pytest approx function, to override default parameters
-def approx(expected):
+def approx(expected, accuracy=7):
+    # 6 digits after dot for numbers more than 1 and less than -1
     if abs(expected) >= 1:
-        return pytest.approx(expected=expected, abs=1e-6)
+        tolerance = 10 ** -(accuracy - 1)
+        return pytest.approx(expected=expected, abs=tolerance)
+    # 7 digits after dot when we expect 0
     if expected == 0:
-        return pytest.approx(expected=expected, abs=1e-7)
+        tolerance = 10 ** -accuracy
+        return pytest.approx(expected=expected, abs=tolerance)
     # 7 significant digits for numbers between 0 and 1/-1
     highest_magnitude = math.floor(math.log10(abs(expected)))
-    tolerance = 10 ** (highest_magnitude - 6)
+    tolerance = 10 ** (highest_magnitude - accuracy + 1)
     return pytest.approx(expected=expected, abs=tolerance)
 
 
