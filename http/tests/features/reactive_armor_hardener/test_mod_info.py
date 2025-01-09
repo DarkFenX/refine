5e6ce847ce45dbf9455e57c41cbdc4abe16c54b9
DarkPhoenix <phoenix@mail.ru>
2025-01-09 17:18:52 +0100

    Add test which checks incoming modification info propagation

--- http/tests/features/reactive_armor_hardener/test_mod_info.py
+++ http/tests/features/reactive_armor_hardener/test_mod_info.py
@@ -1,4 +1,4 @@
-from tests import approx
+from tests import approx, check_no_field
 from tests.features.reactive_armor_hardener import make_eve_rah, make_eve_ship, setup_rah_basics
 
 
@@ -117,3 +117,69 @@ def test_normal(client, consts):
     assert api_ship_mod_expl.applied_val == approx(0.795)
     assert api_ship_mod_expl.affectors.one().item_id == api_rah.id
     assert api_ship_mod_expl.affectors.one().attr_id == eve_basic_info.res_expl_attr_id
+
+
+def test_incoming(client, consts):
+    # Check that incoming modifications are not hidden by RAH sim
+    eve_basic_info = setup_rah_basics(client=client, consts=consts)
+    eve_rah_id = make_eve_rah(
+        client=client,
+        basic_info=eve_basic_info,
+        resos=(0.85, 0.85, 0.85, 0.85),
+        shift_amount=6)
+    eve_ship_id = make_eve_ship(client=client, basic_info=eve_basic_info, resos=(0.5, 0.65, 0.59, 0.51))
+    eve_implant_attr_id = client.mk_eve_attr()
+    eve_implant_mods = [
+        client.mk_eve_effect_mod(
+            func=consts.EveModFunc.loc,
+            dom=consts.EveModDom.ship,
+            op=consts.EveModOp.post_percent,
+            affector_attr_id=eve_implant_attr_id,
+            affectee_attr_id=attr_id)
+        for attr_id in (
+            eve_basic_info.res_em_attr_id,
+            eve_basic_info.res_therm_attr_id,
+            eve_basic_info.res_kin_attr_id,
+            eve_basic_info.res_expl_attr_id)]
+    eve_implant_effect_id = client.mk_eve_effect(cat_id=consts.EveEffCat.passive, mod_info=eve_implant_mods)
+    eve_implant_id = client.mk_eve_item(attrs={eve_implant_attr_id: -10}, eff_ids=[eve_implant_effect_id])
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit(rah_incoming_dmg=(1, 1, 1, 1))
+    api_implant = api_fit.add_implant(type_id=eve_implant_id)
+    api_fit.set_ship(type_id=eve_ship_id)
+    api_rah = api_fit.add_mod(type_id=eve_rah_id, state=consts.ApiState.active)
+    # Verification
+    api_rah.update()
+    assert api_rah.attrs[eve_basic_info.res_em_attr_id].dogma == approx(0.855)
+    assert api_rah.attrs[eve_basic_info.res_therm_attr_id].dogma == approx(0.675)
+    assert api_rah.attrs[eve_basic_info.res_kin_attr_id].dogma == approx(0.735)
+    assert api_rah.attrs[eve_basic_info.res_expl_attr_id].dogma == approx(0.795)
+    api_em_mod = api_rah.mods[eve_basic_info.res_em_attr_id].one()
+    assert api_em_mod.op == consts.ApiModOp.post_percent
+    assert api_em_mod.initial_val == approx(-10)
+    assert api_em_mod.stacking_mult == consts.PenaltyStr.p1
+    assert api_em_mod.applied_val == approx(-10)
+    assert api_em_mod.affectors.one().item_id == api_implant.id
+    assert api_em_mod.affectors.one().attr_id == eve_implant_attr_id
+    api_therm_mod = api_rah.mods[eve_basic_info.res_therm_attr_id].one()
+    assert api_therm_mod.op == consts.ApiModOp.post_percent
+    assert api_therm_mod.initial_val == approx(-10)
+    assert api_therm_mod.stacking_mult == consts.PenaltyStr.p1
+    assert api_therm_mod.applied_val == approx(-10)
+    assert api_therm_mod.affectors.one().item_id == api_implant.id
+    assert api_therm_mod.affectors.one().attr_id == eve_implant_attr_id
+    api_kin_mod = api_rah.mods[eve_basic_info.res_kin_attr_id].one()
+    assert api_kin_mod.op == consts.ApiModOp.post_percent
+    assert api_kin_mod.initial_val == approx(-10)
+    assert api_kin_mod.stacking_mult == consts.PenaltyStr.p1
+    assert api_kin_mod.applied_val == approx(-10)
+    assert api_kin_mod.affectors.one().item_id == api_implant.id
+    assert api_kin_mod.affectors.one().attr_id == eve_implant_attr_id
+    api_expl_mod = api_rah.mods[eve_basic_info.res_expl_attr_id].one()
+    assert api_expl_mod.op == consts.ApiModOp.post_percent
+    assert api_expl_mod.initial_val == approx(-10)
+    assert api_expl_mod.stacking_mult == consts.PenaltyStr.p1
+    assert api_expl_mod.applied_val == approx(-10)
+    assert api_expl_mod.affectors.one().item_id == api_implant.id
+    assert api_expl_mod.affectors.one().attr_id == eve_implant_attr_id
