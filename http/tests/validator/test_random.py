ae8730056b1632c85f7f967673f6a4c6a2dbdc12
DarkPhoenix <phoenix@mail.ru>
2025-01-12 16:41:08 +0100

    Fix test

--- http/tests/validator/test_random.py
+++ http/tests/validator/test_random.py
@@ -1,11 +1,12 @@
 
 
-def test_bundled(client, consts):
+def test_random(client, consts):
     eve_use_attr_id = client.mk_eve_attr(id_=consts.EveAttr.cpu)
     eve_output_attr_id = client.mk_eve_attr(id_=consts.EveAttr.cpu_output)
-    eve_module1_id = client.mk_eve_item(attrs={eve_use_attr_id: 50})
-    eve_module2_id = client.mk_eve_item(attrs={eve_use_attr_id: 100})
-    eve_ship_id = client.mk_eve_ship(attrs={eve_output_attr_id: 125})
+    eve_effect_id = client.mk_eve_online_effect()
+    eve_module1_id = client.mk_eve_item(attrs={eve_use_attr_id: 50}, eff_ids=[eve_effect_id])
+    eve_module2_id = client.mk_eve_item(attrs={eve_use_attr_id: 100}, eff_ids=[eve_effect_id])
+    eve_ship_id = client.mk_eve_ship(attrs={eve_output_attr_id: 150})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
@@ -13,3 +14,4 @@ def test_bundled(client, consts):
     api_fit.add_mod(type_id=eve_module1_id, state=consts.ApiState.online)
     api_fit.add_mod(type_id=eve_module2_id, state=consts.ApiState.online)
     api_fit.validate()
+    # api_fit.update(fit_info_mode=consts.ApiFitInfoMode.full, item_info_mode=consts.ApiItemInfoMode.full)
