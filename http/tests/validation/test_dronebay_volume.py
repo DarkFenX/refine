b6ebd6b839565041f28e9fc9c4d9abe88c8760a4
DarkPhoenix <phoenix@mail.ru>
2025-01-15 17:54:32 +0100

    Add mutation test to dronebay volume

--- http/tests/validation/test_dronebay_volume.py
+++ http/tests/validation/test_dronebay_volume.py
@@ -155,6 +155,47 @@ def test_modified_output(client, consts):
         api_val.details  # pylint: disable=W0104
 
 
+def test_mutation_use(client, consts):
+    eve_use_attr_id = client.mk_eve_attr(id_=consts.EveAttr.volume)
+    eve_output_attr_id = client.mk_eve_attr(id_=consts.EveAttr.drone_capacity)
+    eve_base_drone_id = client.mk_eve_item(attrs={eve_use_attr_id: 120})
+    eve_mutated_drone_id = client.mk_eve_item(attrs={eve_use_attr_id: 130})
+    eve_mutator_id = client.mk_eve_mutator(
+        items=[([eve_base_drone_id], eve_mutated_drone_id)],
+        attrs={eve_use_attr_id: (0.8, 1.2)})
+    eve_ship_id = client.mk_eve_ship(attrs={eve_output_attr_id: 125})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.set_ship(type_id=eve_ship_id)
+    api_drone = api_fit.add_drone(type_id=eve_base_drone_id)
+    # Verification
+    api_val = api_fit.validate(include=[consts.ApiValType.dronebay_volume])
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # pylint: disable=W0104
+    # Action
+    api_drone.change_drone(mutation=eve_mutator_id)
+    # Verification - unrealistic scenario, but testing here detail of implementation: mutated drone
+    # has different volume, it should be used instead
+    api_val = api_fit.validate(include=[consts.ApiValType.dronebay_volume])
+    assert api_val.passed is False
+    assert api_val.details.dronebay_volume.used == approx(130)
+    assert api_val.details.dronebay_volume.output == approx(125)
+    assert len(api_val.details.dronebay_volume.users) == 1
+    assert api_val.details.dronebay_volume.users[api_drone.id] == approx(130)
+    # Action
+    api_drone.change_drone(mutation={eve_use_attr_id: {consts.ApiAttrMutation.roll: 0.8}})
+    # Verification - unrealistic scenario, but testing here detail of implementation: mutated volume
+    # value does not change anything for the validation
+    api_val = api_fit.validate(include=[consts.ApiValType.dronebay_volume])
+    assert api_val.passed is False
+    assert api_val.details.dronebay_volume.used == approx(130)
+    assert api_val.details.dronebay_volume.output == approx(125)
+    assert len(api_val.details.dronebay_volume.users) == 1
+    assert api_val.details.dronebay_volume.users[api_drone.id] == approx(130)
+
+
 def test_rounding(client, consts):
     # Calibration shouldn't have its sum or individual values rounded
     eve_use_attr_id = client.mk_eve_attr(id_=consts.EveAttr.volume)
