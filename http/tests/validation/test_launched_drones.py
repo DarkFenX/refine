d2b654e70a2f29240abcb904856efc231e3b1be8
DarkPhoenix <phoenix@mail.ru>
2025-01-18 02:29:19 +0100

    Fix tests

--- http/tests/validation/test_launched_drones.py
+++ http/tests/validation/test_launched_drones.py
@@ -4,11 +4,11 @@ from tests import approx, check_no_field
 def test_fail_single(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -19,34 +19,14 @@ def test_fail_single(client, consts):
     assert api_drone.id in api_val.details.launched_drones.users
 
 
-def test_fail_multiple_ship(client, consts):
+def test_fail_multiple(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 1})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 1})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
-    api_drone1 = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
-    api_drone2 = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
-    # Verification
-    api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
-    assert api_val.passed is False
-    assert api_val.details.launched_drones.used == 2
-    assert api_val.details.launched_drones.total == 1
-    assert len(api_val.details.launched_drones.users) == 2
-    assert api_drone1.id in api_val.details.launched_drones.users
-    assert api_drone2.id in api_val.details.launched_drones.users
-
-
-def test_fail_multiple_struct(client, consts):
-    eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
-    eve_drone_id = client.mk_eve_item()
-    eve_struct_id = client.mk_eve_struct(attrs={eve_total_attr_id: 1})
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_struct_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone1 = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     api_drone2 = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
@@ -62,11 +42,11 @@ def test_fail_multiple_struct(client, consts):
 def test_equal(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 1})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 1})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -81,21 +61,21 @@ def test_modified_total(client, consts):
     eve_mod_attr_id = client.mk_eve_attr()
     eve_mod = client.mk_eve_effect_mod(
         func=consts.EveModFunc.item,
-        dom=consts.EveModDom.ship,
+        dom=consts.EveModDom.char,
         op=consts.EveModOp.mod_add,
         affector_attr_id=eve_mod_attr_id,
         affectee_attr_id=eve_total_attr_id)
     eve_effect_id = client.mk_eve_effect(mod_info=[eve_mod])
     eve_implant_id = client.mk_eve_item(attrs={eve_mod_attr_id: 1}, eff_ids=[eve_effect_id])
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_ship = api_fit.set_ship(type_id=eve_ship_id)
+    api_char = api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
-    assert api_ship.update().attrs[eve_total_attr_id].extra == approx(0)
+    assert api_char.update().attrs[eve_total_attr_id].extra == approx(0)
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
     assert api_val.passed is False
     assert api_val.details.launched_drones.used == 1
@@ -105,7 +85,7 @@ def test_modified_total(client, consts):
     # Action
     api_fit.add_implant(type_id=eve_implant_id)
     # Verification
-    assert api_ship.update().attrs[eve_total_attr_id].extra == approx(1)
+    assert api_char.update().attrs[eve_total_attr_id].extra == approx(1)
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
     assert api_val.passed is True
     with check_no_field():
@@ -115,12 +95,12 @@ def test_modified_total(client, consts):
 def test_fractional_total(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship1_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0.4})
-    eve_ship2_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0.6})
+    eve_char1_id = client.mk_eve_item(attrs={eve_total_attr_id: 0.4})
+    eve_char2_id = client.mk_eve_item(attrs={eve_total_attr_id: 0.6})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship1_id)
+    api_fit.set_char(type_id=eve_char1_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -130,7 +110,7 @@ def test_fractional_total(client, consts):
     assert len(api_val.details.launched_drones.users) == 1
     assert api_drone.id in api_val.details.launched_drones.users
     # Action
-    api_fit.set_ship(type_id=eve_ship2_id)
+    api_fit.set_char(type_id=eve_char2_id)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
     assert api_val.passed is True
@@ -138,7 +118,7 @@ def test_fractional_total(client, consts):
         api_val.details  # pylint: disable=W0104
 
 
-def test_no_ship(client, consts):
+def test_no_char(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
     # Create an item which has the attribute, just to prevent the attribute from being cleaned up
@@ -159,12 +139,12 @@ def test_no_ship(client, consts):
 def test_unloaded_user(client, consts):
     # Unloaded drones still take slot
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     eve_drone_id = client.alloc_item_id()
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -175,16 +155,16 @@ def test_unloaded_user(client, consts):
     assert api_drone.id in api_val.details.launched_drones.users
 
 
-def test_unloaded_ship(client, consts):
+def test_unloaded_char(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
     # Create an item which has the attribute, just to prevent the attribute from being cleaned up
     client.mk_eve_item(attrs={eve_total_attr_id: 5})
-    eve_ship_id = client.alloc_item_id()
+    eve_char_id = client.alloc_item_id()
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -198,13 +178,13 @@ def test_unloaded_ship(client, consts):
 def test_no_value_total(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship()
+    eve_char_id = client.mk_eve_item()
     # Make an item to ensure that total attribute is not cleaned up
     client.mk_eve_item(attrs={eve_total_attr_id: 50})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -220,11 +200,11 @@ def test_no_attr_total(client, consts):
     # irrelevant
     eve_total_attr_id = consts.EveAttr.max_active_drones
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -238,11 +218,11 @@ def test_no_attr_total(client, consts):
 def test_criterion_state(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_drone_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_drone = api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiState.offline)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
@@ -270,11 +250,11 @@ def test_criterion_state(client, consts):
 def test_criterion_drone(client, consts):
     eve_total_attr_id = client.mk_eve_attr(id_=consts.EveAttr.max_active_drones)
     eve_fighter_id = client.mk_eve_item()
-    eve_ship_id = client.mk_eve_ship(attrs={eve_total_attr_id: 0})
+    eve_char_id = client.mk_eve_item(attrs={eve_total_attr_id: 0})
     client.create_sources()
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_ship_id)
+    api_fit.set_char(type_id=eve_char_id)
     api_fit.add_fighter(type_id=eve_fighter_id, state=consts.ApiState.online)
     # Verification
     api_val = api_fit.validate(include=[consts.ApiValType.launched_drones])
