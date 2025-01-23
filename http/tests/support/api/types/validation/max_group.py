2d4b931677b5091599481b8e6e323f7760dab91d
DarkPhoenix <phoenix@mail.ru>
2025-01-23 17:03:03 +0100

    Change max group validation so that some child entities are accessible via names

--- http/tests/support/api/types/validation/max_group.py
+++ http/tests/support/api/types/validation/max_group.py
@@ -1,4 +1,21 @@
+from dataclasses import dataclass
+
+
 class ValMaxGroupDetails(dict):
 
     def __init__(self, *, data: dict):
-        super().__init__({int(k): v for k, v in data.items()})
+        super().__init__({int(k): ValMaxGroupGroup(data=v) for k, v in data.items()})
+
+
+@dataclass
+class ValMaxGroupGroup:
+
+    count: int
+    items: dict[str, int]
+
+    def __init__(self, *, data):
+        self.count = data[0]
+        self.items = dict(data[1])
+
+    def __eq__(self, other):
+        return (self.count, self.items) == (other[0], other[1])
