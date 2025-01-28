31fbc557051d1ab4bf6b717f810b3e9c540dcbe7
DarkPhoenix <phoenix@mail.ru>
2025-01-28 10:25:26 +0100

    Add another skill requirement test

--- http/tests/validation/test_skill_reqs.py
+++ http/tests/validation/test_skill_reqs.py
@@ -303,3 +303,38 @@ def test_not_loaded_skill(client, consts):
     api_val = api_fit.validate(include=[consts.ApiValType.skill_reqs])
     assert api_val.passed is False
     assert api_val.details.skill_reqs == {api_module.id: {eve_skill_id: (None, 3)}}
+
+
+def test_failed_replacement(client, consts):
+    # Check that failed attempt to replace skill doesn't affect validation
+    eve_skill_id = client.mk_eve_item()
+    eve_module_id = client.mk_eve_item(srqs={eve_skill_id: 3})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_skill = api_fit.add_skill(type_id=eve_skill_id, level=3)
+    api_module = api_fit.add_mod(type_id=eve_module_id)
+    # Verification
+    api_val = api_fit.validate(include=[consts.ApiValType.skill_reqs])
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # pylint: disable=W0104
+    # Action
+    api_fit.add_skill(type_id=eve_skill_id, level=2, status_code=409)
+    # Verification
+    api_val = api_fit.validate(include=[consts.ApiValType.skill_reqs])
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # pylint: disable=W0104
+    # Action
+    api_skill.change_skill(level=2)
+    # Verification
+    api_val = api_fit.validate(include=[consts.ApiValType.skill_reqs])
+    assert api_val.passed is False
+    assert api_val.details.skill_reqs == {api_module.id: {eve_skill_id: (2, 3)}}
+    # Action
+    api_fit.add_skill(type_id=eve_skill_id, level=4, status_code=409)
+    # Verification
+    api_val = api_fit.validate(include=[consts.ApiValType.skill_reqs])
+    assert api_val.passed is False
+    assert api_val.details.skill_reqs == {api_module.id: {eve_skill_id: (2, 3)}}
