33173ed472ce0c8c3e392c29aba94b9fa52bb397
DarkPhoenix <phoenix@mail.ru>
2025-01-30 11:58:28 +0100

    Add autocharge test

--- http/tests/validation/test_charge_group.py
+++ http/tests/validation/test_charge_group.py
@@ -336,3 +336,28 @@ def test_state(client, consts):
     api_val = api_fit.validate(include=[consts.ApiValType.charge_group])
     assert api_val.passed is False
     assert api_val.details.charge_group == {api_module.charge.id: (api_module.id, eve_grp1_id, [eve_grp2_id])}
+
+
+def test_autocharge_fighter(client, consts):
+    # Autocharges / fighters are not subject for the validation
+    eve_grp1_id = client.mk_eve_item_group()
+    eve_grp2_id = client.mk_eve_item_group()
+    eve_group_attr_id = client.mk_eve_attr(id_=consts.EveAttr.charge_group1, unit_id=consts.EveAttrUnit.group_id)
+    eve_autocharge_attr_id = client.mk_eve_attr(id_=consts.EveAttr.ftr_abil_launch_bomb_type)
+    eve_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.fighter_ability_launch_bomb,
+        cat_id=consts.EveEffCat.active)
+    eve_charge_id = client.mk_eve_item(grp_id=eve_grp1_id)
+    eve_fighter_id = client.mk_eve_item(
+        attrs={eve_autocharge_attr_id: eve_charge_id, eve_group_attr_id: eve_grp2_id},
+        eff_ids=[eve_effect_id])
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fighter = api_fit.add_fighter(type_id=eve_fighter_id)
+    # Verification
+    assert len(api_fighter.update().autocharges) == 1
+    api_val = api_fit.validate(include=[consts.ApiValType.charge_group])
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # pylint: disable=W0104
