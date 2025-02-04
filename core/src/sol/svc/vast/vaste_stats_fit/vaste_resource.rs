332e51df2b16db870664b03233c00e2254582166
DarkPhoenix <phoenix@mail.ru>
2025-02-04 19:27:18 +0100

    Move static methods out of structure implementation

--- core/src/sol/svc/vast/vaste_stats_fit/vaste_resource.rs
+++ core/src/sol/svc/vast/vaste_stats_fit/vaste_resource.rs
@@ -26,7 +26,7 @@ impl SolVastFitData {
         calc: &mut SolCalc,
         fit: &SolFit,
     ) -> SolStatRes {
-        self.get_resource_stats_fitting(
+        get_resource_stats_fitting(
             uad,
             calc,
             fit,
@@ -41,7 +41,7 @@ impl SolVastFitData {
         calc: &mut SolCalc,
         fit: &SolFit,
     ) -> SolStatRes {
-        self.get_resource_stats_fitting(
+        get_resource_stats_fitting(
             uad,
             calc,
             fit,
@@ -56,7 +56,7 @@ impl SolVastFitData {
         calc: &mut SolCalc,
         fit: &SolFit,
     ) -> SolStatRes {
-        self.get_resource_stats_other(
+        get_resource_stats_other(
             uad,
             calc,
             fit,
@@ -70,7 +70,7 @@ impl SolVastFitData {
         calc: &mut SolCalc,
         fit: &SolFit,
     ) -> SolStatRes {
-        self.get_resource_stats_other(uad, calc, fit, self.drones_volume.values(), &ec::attrs::DRONE_CAPACITY)
+        get_resource_stats_other(uad, calc, fit, self.drones_volume.values(), &ec::attrs::DRONE_CAPACITY)
     }
     pub(in crate::sol::svc::vast) fn get_stats_drone_bandwidth(
         &self,
@@ -78,7 +78,7 @@ impl SolVastFitData {
         calc: &mut SolCalc,
         fit: &SolFit,
     ) -> SolStatRes {
-        self.get_resource_stats_other(
+        get_resource_stats_other(
             uad,
             calc,
             fit,
@@ -86,45 +86,43 @@ impl SolVastFitData {
             &ec::attrs::DRONE_BANDWIDTH,
         )
     }
-    // Private methods
-    fn get_resource_stats_fitting<'a>(
-        &self,
-        uad: &SolUad,
-        calc: &mut SolCalc,
-        fit: &SolFit,
-        items: impl Iterator<Item = &'a SolItemId>,
-        use_attr_id: &EAttrId,
-        output_attr_id: &EAttrId,
-    ) -> SolStatRes {
-        let output = match fit.ship {
-            Some(ship_id) => match calc.get_item_attr_val(uad, &ship_id, output_attr_id) {
-                Ok(attr_val) => Some(attr_val.extra),
-                Err(_) => None,
-            },
-            None => None,
-        };
-        let used = items
-            .filter_map(|i| calc.get_item_attr_val(uad, i, use_attr_id).ok().map(|v| v.extra))
-            .sum();
-        // Round possible float errors despite individual use values being rounded
-        SolStatRes::new(round(used, 2), output)
-    }
-    fn get_resource_stats_other<'a>(
-        &self,
-        uad: &SolUad,
-        calc: &mut SolCalc,
-        fit: &SolFit,
-        items_use: impl Iterator<Item = &'a AttrVal>,
-        output_attr_id: &EAttrId,
-    ) -> SolStatRes {
-        let output = match fit.ship {
-            Some(ship_id) => match calc.get_item_attr_val(uad, &ship_id, output_attr_id) {
-                Ok(attr_val) => Some(attr_val.extra),
-                Err(_) => None,
-            },
-            None => None,
-        };
-        let used = items_use.sum();
-        SolStatRes::new(used, output)
-    }
+}
+
+fn get_resource_stats_fitting<'a>(
+    uad: &SolUad,
+    calc: &mut SolCalc,
+    fit: &SolFit,
+    items: impl Iterator<Item = &'a SolItemId>,
+    use_attr_id: &EAttrId,
+    output_attr_id: &EAttrId,
+) -> SolStatRes {
+    let output = match fit.ship {
+        Some(ship_id) => match calc.get_item_attr_val(uad, &ship_id, output_attr_id) {
+            Ok(attr_val) => Some(attr_val.extra),
+            Err(_) => None,
+        },
+        None => None,
+    };
+    let used = items
+        .filter_map(|i| calc.get_item_attr_val(uad, i, use_attr_id).ok().map(|v| v.extra))
+        .sum();
+    // Round possible float errors despite individual use values being rounded
+    SolStatRes::new(round(used, 2), output)
+}
+fn get_resource_stats_other<'a>(
+    uad: &SolUad,
+    calc: &mut SolCalc,
+    fit: &SolFit,
+    items_use: impl Iterator<Item = &'a AttrVal>,
+    output_attr_id: &EAttrId,
+) -> SolStatRes {
+    let output = match fit.ship {
+        Some(ship_id) => match calc.get_item_attr_val(uad, &ship_id, output_attr_id) {
+            Ok(attr_val) => Some(attr_val.extra),
+            Err(_) => None,
+        },
+        None => None,
+    };
+    let used = items_use.sum();
+    SolStatRes::new(used, output)
 }
