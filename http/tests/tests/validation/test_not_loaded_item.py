3ce13db53fe2ff6719a10b025710406a5583a223
DarkPhoenix <phoenix@mail.ru>
2025-03-18 11:06:35 +0100

    Test services in  "not loaded" validator

--- http/tests/tests/validation/test_not_loaded_item.py
+++ http/tests/tests/validation/test_not_loaded_item.py
@@ -2,6 +2,20 @@ from tests import check_no_field
 from tests.fw.api import ValOptions
 
 
+def test_booster(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_booster(type_id=eve_loaded_id)
+    api_not_loaded = api_fit.add_booster(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
 def test_character(client):
     eve_loaded_id = client.mk_eve_item()
     eve_not_loaded_id = client.alloc_item_id()
@@ -22,144 +36,6 @@ def test_character(client):
     assert api_val.details.not_loaded_item == [api_char.id]
 
 
-def test_skill(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_skill(type_id=eve_loaded_id, level=1)
-    api_not_loaded = api_fit.add_skill(type_id=eve_not_loaded_id, level=1)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_implant(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_implant(type_id=eve_loaded_id)
-    api_not_loaded = api_fit.add_implant(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_booster(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_booster(type_id=eve_loaded_id)
-    api_not_loaded = api_fit.add_booster(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_ship(client):
-    eve_loaded_id = client.mk_eve_ship()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.set_ship(type_id=eve_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is True
-    with check_no_field():
-        api_val.details  # noqa: B018
-    # Action
-    api_ship = api_fit.set_ship(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_ship.id]
-
-
-def test_stance(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.set_stance(type_id=eve_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is True
-    with check_no_field():
-        api_val.details  # noqa: B018
-    # Action
-    api_stance = api_fit.set_stance(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_stance.id]
-
-
-def test_subsystem(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_subsystem(type_id=eve_loaded_id)
-    api_not_loaded = api_fit.add_subsystem(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_module_high(client, consts):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.high)
-    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.high)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_module_mid(client, consts):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.mid)
-    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.mid)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
-def test_module_low(client, consts):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.low)
-    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.low)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
-
-
 def test_charge(client):
     eve_module_id = client.mk_eve_item()
     eve_loaded_id = client.mk_eve_item()
@@ -173,20 +49,6 @@ def test_charge(client):
     api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
     assert api_val.passed is False
     assert api_val.details.not_loaded_item == [api_not_loaded.charge.id]
-
-
-def test_rig(client):
-    eve_loaded_id = client.mk_eve_item()
-    eve_not_loaded_id = client.alloc_item_id()
-    client.create_sources()
-    api_sol = client.create_sol()
-    api_fit = api_sol.create_fit()
-    api_fit.add_rig(type_id=eve_loaded_id)
-    api_not_loaded = api_fit.add_rig(type_id=eve_not_loaded_id)
-    # Verification
-    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
-    assert api_val.passed is False
-    assert api_val.details.not_loaded_item == [api_not_loaded.id]
 
 
 def test_drone(client):
@@ -225,6 +87,158 @@ def test_fw_effect(client):
     api_fit = api_sol.create_fit()
     api_fit.add_fw_effect(type_id=eve_loaded_id)
     api_not_loaded = api_fit.add_fw_effect(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_implant(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_implant(type_id=eve_loaded_id)
+    api_not_loaded = api_fit.add_implant(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_module_high(client, consts):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.high)
+    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.high)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_module_low(client, consts):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.low)
+    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.low)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_module_mid(client, consts):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_mod(type_id=eve_loaded_id, rack=consts.ApiRack.mid)
+    api_not_loaded = api_fit.add_mod(type_id=eve_not_loaded_id, rack=consts.ApiRack.mid)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_rig(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_rig(type_id=eve_loaded_id)
+    api_not_loaded = api_fit.add_rig(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_service(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_service(type_id=eve_loaded_id)
+    api_not_loaded = api_fit.add_service(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_ship(client):
+    eve_loaded_id = client.mk_eve_ship()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.set_ship(type_id=eve_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # noqa: B018
+    # Action
+    api_ship = api_fit.set_ship(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_ship.id]
+
+
+def test_skill(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_skill(type_id=eve_loaded_id, level=1)
+    api_not_loaded = api_fit.add_skill(type_id=eve_not_loaded_id, level=1)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_not_loaded.id]
+
+
+def test_stance(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.set_stance(type_id=eve_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is True
+    with check_no_field():
+        api_val.details  # noqa: B018
+    # Action
+    api_stance = api_fit.set_stance(type_id=eve_not_loaded_id)
+    # Verification
+    api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_stance.id]
+
+
+def test_subsystem(client):
+    eve_loaded_id = client.mk_eve_item()
+    eve_not_loaded_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_subsystem(type_id=eve_loaded_id)
+    api_not_loaded = api_fit.add_subsystem(type_id=eve_not_loaded_id)
     # Verification
     api_val = api_fit.validate(options=ValOptions(not_loaded_item=True))
     assert api_val.passed is False
