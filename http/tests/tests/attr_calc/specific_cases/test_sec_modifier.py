3980c29751adb69334e6d6cf7780196717761ff7
DarkPhoenix <phoenix@mail.ru>
2025-04-01 18:10:28 +0200

    Modify test to check multiplication in hisec case

--- http/tests/tests/attr_calc/specific_cases/test_sec_modifier.py
+++ http/tests/tests/attr_calc/specific_cases/test_sec_modifier.py
@@ -10,8 +10,8 @@ from tests import approx
 
 
 def test_sec_zones(client, consts):
-    # Realistic case, pretty much a t2 ME rig, except for general attr modifier value being 1.1
-    # instead of 1
+    # Realistic case, pretty much an engineering ME rig, except for general attr modifier value
+    # being 1.1 instead of 1 and hisec modifier value being 1.2 instead of 1
     eve_sec_attr_id = client.mk_eve_attr(id_=consts.EveAttr.security_modifier)
     eve_hisec_attr_id = client.mk_eve_attr(id_=consts.EveAttr.hisec_modifier)
     eve_lowsec_attr_id = client.mk_eve_attr(id_=consts.EveAttr.lowsec_modifier)
@@ -27,7 +27,7 @@ def test_sec_zones(client, consts):
     eve_rig_id = client.mk_eve_item(
         attrs={
             eve_sec_attr_id: 1.1,
-            eve_hisec_attr_id: 1,
+            eve_hisec_attr_id: 1.2,
             eve_lowsec_attr_id: 1.9,
             eve_nullsec_attr_id: 2.1,
             eve_affectee_attr_id: -2.4},
@@ -38,32 +38,42 @@ def test_sec_zones(client, consts):
     api_rig = api_fit.add_rig(type_id=eve_rig_id)
     # Verification
     api_rig.update()
-    assert api_rig.attrs[eve_sec_attr_id].dogma == approx(1)
-    assert api_rig.attrs[eve_affectee_attr_id].dogma == approx(-2.4)
+    assert api_rig.attrs[eve_sec_attr_id].dogma == approx(1.2)
+    assert api_rig.attrs[eve_affectee_attr_id].dogma == approx(-2.88)
     api_sec_mod = api_rig.mods[eve_sec_attr_id].one()
     assert api_sec_mod.op == consts.ApiModOp.pre_assign
-    assert api_sec_mod.initial_val == approx(1)
+    assert api_sec_mod.initial_val == approx(1.2)
     assert api_sec_mod.stacking_mult is None
-    assert api_sec_mod.applied_val == approx(1)
+    assert api_sec_mod.applied_val == approx(1.2)
     assert api_sec_mod.affectors.one().item_id == api_rig.id
     assert api_sec_mod.affectors.one().attr_id == eve_hisec_attr_id
-    # Multiplication by 1 is insignificant and is not exposed
-    assert eve_affectee_attr_id not in api_rig.mods
+    api_affectee_mod = api_rig.mods[eve_affectee_attr_id].one()
+    assert api_affectee_mod.op == consts.ApiModOp.post_mul
+    assert api_affectee_mod.initial_val == approx(1.2)
+    assert api_affectee_mod.stacking_mult is None
+    assert api_affectee_mod.applied_val == approx(1.2)
+    assert api_affectee_mod.affectors.one().item_id == api_rig.id
+    assert api_affectee_mod.affectors.one().attr_id == eve_sec_attr_id
     # Action
     api_sol.change(sec_zone=consts.ApiSecZone.hisec_c5)
     # Verification
     api_rig.update()
-    assert api_rig.attrs[eve_sec_attr_id].dogma == approx(1)
-    assert api_rig.attrs[eve_affectee_attr_id].dogma == approx(-2.4)
+    assert api_rig.attrs[eve_sec_attr_id].dogma == approx(1.2)
+    assert api_rig.attrs[eve_affectee_attr_id].dogma == approx(-2.88)
     api_sec_mod = api_rig.mods[eve_sec_attr_id].one()
     assert api_sec_mod.op == consts.ApiModOp.pre_assign
-    assert api_sec_mod.initial_val == approx(1)
+    assert api_sec_mod.initial_val == approx(1.2)
     assert api_sec_mod.stacking_mult is None
-    assert api_sec_mod.applied_val == approx(1)
+    assert api_sec_mod.applied_val == approx(1.2)
     assert api_sec_mod.affectors.one().item_id == api_rig.id
     assert api_sec_mod.affectors.one().attr_id == eve_hisec_attr_id
-    # Multiplication by 1 is insignificant and is not exposed
-    assert eve_affectee_attr_id not in api_rig.mods
+    api_affectee_mod = api_rig.mods[eve_affectee_attr_id].one()
+    assert api_affectee_mod.op == consts.ApiModOp.post_mul
+    assert api_affectee_mod.initial_val == approx(1.2)
+    assert api_affectee_mod.stacking_mult is None
+    assert api_affectee_mod.applied_val == approx(1.2)
+    assert api_affectee_mod.affectors.one().item_id == api_rig.id
+    assert api_affectee_mod.affectors.one().attr_id == eve_sec_attr_id
     # Action
     api_sol.change(sec_zone=consts.ApiSecZone.lowsec)
     # Verification
