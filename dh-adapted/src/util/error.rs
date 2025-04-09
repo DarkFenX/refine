b5a2e52eb403b8aec9c21564ab3bb53ab38fbb53
DarkPhoenix <phoenix@mail.ru>
2025-04-09 21:01:27 +0200

    Migrate adapted data handler errors to thiserror

--- dh-adapted/src/util/error.rs
+++ dh-adapted/src/util/error.rs
@@ -1,24 +1,14 @@
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error("handler does not support cache")]
     NoCacheSupport,
     #[cfg(feature = "json")]
+    #[error("unable to open cache for reading: {0}")]
     RamJsonReadFailed(String),
     #[cfg(feature = "json")]
+    #[error("unable to decompress cache: {0}")]
     RamJsonDecompFailed(String),
     #[cfg(feature = "json")]
+    #[error("unable to parse cache data: {0}")]
     RamJsonParseFailed(String),
-}
-impl std::error::Error for Error {}
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Self::NoCacheSupport => write!(f, "handler does not support cache"),
-            #[cfg(feature = "json")]
-            Self::RamJsonReadFailed(msg) => write!(f, "unable to open cache for reading: {msg}"),
-            #[cfg(feature = "json")]
-            Self::RamJsonDecompFailed(msg) => write!(f, "unable to decompress cache: {msg}"),
-            #[cfg(feature = "json")]
-            Self::RamJsonParseFailed(msg) => write!(f, "unable to parse cache data: {msg}"),
-        }
-    }
 }
