c34e5ec9b7bb68e70ae06e4741d3b49c7156d9f3
DarkPhoenix <phoenix@mail.ru>
2025-04-10 18:29:44 +0200

    Migrate eve data handler errors to thiserror

--- dh-eve/src/util/err_res.rs
+++ dh-eve/src/util/err_res.rs
@@ -1,71 +1,49 @@
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[cfg(any(feature = "phb-http", feature = "phb-file"))]
     /// Unable to parse data due to it being in unexpected format.
     ///
     /// Includes suffix of requested entity.
+    #[cfg(any(feature = "phb-http", feature = "phb-file"))]
+    #[error("{0} FSD decomposition failed: highest-level entity is not a map")]
     PhbUnexpectedFsdTopEntity(String),
-    #[cfg(feature = "phb-http")]
     /// HTTP handler cannot use passed URL as base.
     ///
     /// Includes passed URL and text description of failure.
-    PhbHttpInvalidBaseUrl(String, String),
     #[cfg(feature = "phb-http")]
+    #[error("invalid base URL \"{0}\": {1}")]
+    PhbHttpInvalidBaseUrl(String, String),
     /// HTTP handler is unable to join base URL and suffix.
     ///
     /// Includes suffix and text description of failure.
-    PhbHttpSuffixJoinFailed(String, String),
     #[cfg(feature = "phb-http")]
+    #[error("{0} is failed to be joined to base URL: {1}")]
+    PhbHttpSuffixJoinFailed(String, String),
     /// HTTP handler is unable to fetch data.
     ///
     /// Includes suffix and text description of failure.
-    PhbHttpSuffixFetchFailed(String, String),
     #[cfg(feature = "phb-http")]
+    #[error("{0} fetching failed: {1}")]
+    PhbHttpSuffixFetchFailed(String, String),
     /// HTTP handler is unable to parse data.
     ///
     /// Includes suffix and text description of failure.
+    #[cfg(feature = "phb-http")]
+    #[error("{0} parsing failed: {1}")]
     PhbHttpSuffixParseFailed(String, String),
-    #[cfg(feature = "phb-file")]
     /// File handler is unable to read data.
     ///
     /// Includes suffix and text description of failure.
-    PhbFileSuffixReadFailed(String, String),
     #[cfg(feature = "phb-file")]
+    #[error("{0} reading failed: {1}")]
+    PhbFileSuffixReadFailed(String, String),
     /// File handler is unable to parse data.
     ///
     /// Includes suffix and text description of failure.
-    PhbFileSuffixParseFailed(String, String),
     #[cfg(feature = "phb-file")]
+    #[error("{0} parsing failed: {1}")]
+    PhbFileSuffixParseFailed(String, String),
     /// File handler is unable to find client version in metadata.
+    #[cfg(feature = "phb-file")]
+    #[error("unable to find client build field")]
     PhbFileNoClientBuild,
-}
-impl std::error::Error for Error {}
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            #[cfg(any(feature = "phb-http", feature = "phb-file"))]
-            Self::PhbUnexpectedFsdTopEntity(suffix) => {
-                write!(
-                    f,
-                    "{suffix} FSD decomposition failed: highest-level entity is not a map"
-                )
-            }
-            #[cfg(feature = "phb-http")]
-            Self::PhbHttpInvalidBaseUrl(url, msg) => write!(f, "invalid base URL \"{url}\": {msg}"),
-            #[cfg(feature = "phb-http")]
-            Self::PhbHttpSuffixJoinFailed(suffix, msg) => {
-                write!(f, "{suffix} is failed to be joined to base URL: {msg}")
-            }
-            #[cfg(feature = "phb-http")]
-            Self::PhbHttpSuffixFetchFailed(suffix, msg) => write!(f, "{suffix} fetching failed: {msg}"),
-            #[cfg(feature = "phb-http")]
-            Self::PhbHttpSuffixParseFailed(suffix, msg) => write!(f, "{suffix} parsing failed: {msg}"),
-            #[cfg(feature = "phb-file")]
-            Self::PhbFileSuffixReadFailed(suffix, msg) => write!(f, "{suffix} reading failed: {msg}"),
-            #[cfg(feature = "phb-file")]
-            Self::PhbFileSuffixParseFailed(suffix, msg) => write!(f, "{suffix} parsing failed: {msg}"),
-            #[cfg(feature = "phb-file")]
-            Self::PhbFileNoClientBuild => write!(f, "unable to find client build field"),
-        }
-    }
 }
