3d6c72d6f8f8e474acaae6a56e140cf27c86d3d8
DarkPhoenix <phoenix@mail.ru>
2025-04-25 19:38:02 +0200

    Rework fit/fleet getters a bit

--- core/src/sol/api/fleet/get.rs
+++ core/src/sol/api/fleet/get.rs
@@ -1,7 +1,7 @@
 use crate::{
     err::basic::FleetFoundError,
     sol::{
-        FleetId, FleetKey, SolarSystem,
+        FleetId, SolarSystem,
         api::{Fleet, FleetMut},
     },
 };
@@ -9,17 +9,11 @@ use crate::{
 impl SolarSystem {
     pub fn get_fleet(&self, fleet_id: &FleetId) -> Result<Fleet, GetFleetError> {
         let fleet_key = self.uad.fleets.key_by_id_err(fleet_id)?;
-        Ok(self.internal_get_fleet(fleet_key))
+        Ok(Fleet::new(self, fleet_key))
     }
     pub fn get_fleet_mut(&mut self, fleet_id: &FleetId) -> Result<FleetMut, GetFleetError> {
         let fleet_key = self.uad.fleets.key_by_id_err(fleet_id)?;
-        Ok(self.internal_get_fleet_mut(fleet_key))
-    }
-    pub(in crate::sol) fn internal_get_fleet(&self, fleet_key: FleetKey) -> Fleet {
-        Fleet::new(self, fleet_key)
-    }
-    pub(in crate::sol) fn internal_get_fleet_mut(&mut self, fleet_key: FleetKey) -> FleetMut {
-        FleetMut::new(self, fleet_key)
+        Ok(FleetMut::new(self, fleet_key))
     }
 }
 
