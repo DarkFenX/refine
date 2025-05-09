19150ea8b010ac47cf49833aa95b9fababcf438e
DarkPhoenix <phoenix@mail.ru>
2025-05-09 15:46:00 +0200

    Add test which checks setting of count override of not loaded fighter

---
+++ http/tests/tests/features/fighters/count_override.py
@@ -0,0 +1,31 @@
+from tests import check_no_field
+
+
+def test_set_not_loaded(client, consts):
+    eve_d1 = client.mk_eve_data()
+    eve_d2 = client.mk_eve_data()
+    eve_max_count_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.ftr_sq_max_size)
+    eve_fighter_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_item(datas=[eve_d1], id_=eve_fighter_id, attrs={eve_max_count_attr_id: 12})
+    client.create_sources()
+    api_sol = client.create_sol(data=eve_d1)
+    api_fit = api_sol.create_fit()
+    api_fighter = api_fit.add_fighter(type_id=eve_fighter_id)
+    # Verification
+    assert api_fighter.update().count == [12, 12]
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification
+    api_fighter.update()
+    with check_no_field():
+        api_fighter.count  # noqa: B018
+    # Action
+    api_fighter.change_fighter(count=3)
+    # Verification
+    api_fighter.update()
+    with check_no_field():
+        api_fighter.count  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d1)
+    # Verification
+    assert api_fighter.update().count == [3, 12]
