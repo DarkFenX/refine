a2b4b871682a1f978e4400759deb2e7d308132d1
DarkPhoenix <phoenix@mail.ru>
2025-05-18 10:34:05 +0200

    Redo charge remove processing

--- core/src/sol/api/item/item_charge/charge_remove.rs
+++ core/src/sol/api/item/item_charge/charge_remove.rs
@@ -1,23 +1,36 @@
+use itertools::Itertools;
+
 use crate::sol::{ItemKey, SolarSystem, api::ChargeMut};
 
 impl SolarSystem {
     pub(in crate::sol::api) fn internal_remove_charge(&mut self, item_key: ItemKey) {
         let uad_item = self.uad.items.get(item_key);
         let uad_charge = uad_item.get_charge().unwrap();
+        let module_item_key = uad_charge.get_cont_item_key();
+        let projectee_item_keys = uad_charge.get_projs().iter_projectee_item_keys().copied().collect_vec();
         // Remove outgoing projections
-        for &projectee_item_key in uad_charge.get_projs().iter_projectee_item_keys() {
-            // Update services for charge
-            let projectee_uad_item = self.uad.items.get(projectee_item_key);
-            self.svc
-                .remove_item_projection(&self.uad, item_key, uad_item, projectee_item_key, projectee_uad_item);
-            // Update user data for charge - do not touch projections container on charge itself,
-            // because we're removing it anyway
-            self.proj_tracker.unreg_projectee(&item_key, &projectee_item_key);
+        if !projectee_item_keys.is_empty() {
+            for projectee_item_key in projectee_item_keys.into_iter() {
+                // Update services
+                let projectee_uad_item = self.uad.items.get(projectee_item_key);
+                self.svc
+                    .remove_item_projection(&self.uad, item_key, uad_item, projectee_item_key, projectee_uad_item);
+                // Projection tracker
+                self.proj_tracker.unreg_projectee(&item_key, &projectee_item_key);
+            }
+            // Clear on-charge projections, so that they don't get processed 2nd time on charge
+            // removal from services
+            self.uad
+                .items
+                .get_mut(item_key)
+                .get_charge_mut()
+                .unwrap()
+                .get_projs_mut()
+                .clear();
         }
         // Update services
-        self.svc.remove_item(&self.uad, item_key, uad_item);
+        self.internal_remove_item_key_from_svc(item_key);
         // Update user data
-        let module_item_key = uad_charge.get_cont_item_key();
         let uad_module = self.uad.items.get_mut(module_item_key).get_module_mut().unwrap();
         uad_module.set_charge_item_key(None);
         self.uad.items.remove(item_key);
