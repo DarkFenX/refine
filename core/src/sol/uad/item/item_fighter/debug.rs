6ea5ba670461de17a0b44bd108533d788e3834cc
DarkPhoenix <phoenix@mail.ru>
2025-05-18 17:51:49 +0200

    Check parent-child projection match for (auto)charges

--- core/src/sol/uad/item/item_fighter/debug.rs
+++ core/src/sol/uad/item/item_fighter/debug.rs
@@ -1,6 +1,8 @@
+use itertools::Itertools;
+
 use super::UadFighter;
 use crate::sol::{
-    debug::{DebugResult, check_fit_key},
+    debug::{DebugError, DebugResult, check_fit_key},
     uad::Uad,
 };
 
@@ -9,6 +11,25 @@ impl UadFighter {
         check_fit_key(uad, self.get_fit_key())?;
         self.get_autocharges().consistency_check(uad)?;
         self.get_projs().consistency_check(uad)?;
+        // If fighter has autocharges, make sure projections on them match
+        if !self.get_autocharges().is_empty() {
+            let fighter_projs = self.get_projs().iter().map(|(k, v)| (*k, *v)).sorted().collect_vec();
+            for &autocharge_key in self.get_autocharges().values() {
+                let autocharge_projs = uad
+                    .items
+                    .get(autocharge_key)
+                    .get_autocharge()
+                    .unwrap()
+                    .get_projs()
+                    .iter()
+                    .map(|(k, v)| (*k, *v))
+                    .sorted()
+                    .collect_vec();
+                if fighter_projs != autocharge_projs {
+                    return Err(DebugError {});
+                }
+            }
+        }
         Ok(())
     }
 }
--- core/src/sol/uad/item/item_module/debug.rs
+++ core/src/sol/uad/item/item_module/debug.rs
@@ -1,6 +1,8 @@
+use itertools::Itertools;
+
 use super::UadModule;
 use crate::sol::{
-    debug::{DebugResult, check_fit_key, check_item_key},
+    debug::{DebugError, DebugResult, check_fit_key, check_item_key},
     uad::Uad,
 };
 
@@ -11,6 +13,23 @@ impl UadModule {
             check_item_key(uad, charge_key, false)?;
         }
         self.get_projs().consistency_check(uad)?;
+        // If module has a charge, make sure projections on them match
+        if let Some(charge_key) = self.get_charge_item_key() {
+            let module_projs = self.get_projs().iter().map(|(k, v)| (*k, *v)).sorted().collect_vec();
+            let charge_projs = uad
+                .items
+                .get(charge_key)
+                .get_charge()
+                .unwrap()
+                .get_projs()
+                .iter()
+                .map(|(k, v)| (*k, *v))
+                .sorted()
+                .collect_vec();
+            if module_projs != charge_projs {
+                return Err(DebugError {});
+            }
+        }
         Ok(())
     }
 }
