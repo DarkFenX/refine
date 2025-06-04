c2002f98cf79474d7ff7259e1390b42e57ae7ae2
DarkPhoenix <phoenix@mail.ru>
2025-06-04 02:26:16 +0200

    Add validation methods for non-fit non-loaded items

--- core/src/sol/svc/vast/vaste_vals_fit/vaste_not_loaded_item.rs
+++ core/src/sol/svc/vast/vaste_vals_fit/vaste_not_loaded_item.rs
@@ -1,5 +1,9 @@
 use crate::{
-    sol::{ItemId, ItemKey, svc::vast::VastFitData, uad::Uad},
+    sol::{
+        ItemId, ItemKey,
+        svc::vast::{Vast, VastFitData},
+        uad::Uad,
+    },
     util::RSet,
 };
 
@@ -8,29 +12,47 @@ pub struct ValNotLoadedItemFail {
     pub item_ids: Vec<ItemId>,
 }
 
-impl VastFitData {
-    // Fast validations
+impl Vast {
     pub(in crate::sol::svc::vast) fn validate_not_loaded_item_fast(&self, kfs: &RSet<ItemKey>) -> bool {
-        match kfs.is_empty() {
-            true => self.not_loaded.is_empty(),
-            false => self.not_loaded.difference(kfs).next().is_none(),
-        }
+        validate_fast(kfs, &self.not_loaded)
     }
-    // Verbose validations
     pub(in crate::sol::svc::vast) fn validate_not_loaded_item_verbose(
         &self,
         kfs: &RSet<ItemKey>,
         uad: &Uad,
     ) -> Option<ValNotLoadedItemFail> {
-        let item_ids: Vec<_> = self
-            .not_loaded
-            .iter()
-            .filter(|item_key| !kfs.contains(item_key))
-            .map(|item_key| uad.items.id_by_key(*item_key))
-            .collect();
-        match item_ids.is_empty() {
-            true => None,
-            false => Some(ValNotLoadedItemFail { item_ids }),
-        }
+        validate_verbose(kfs, &self.not_loaded, uad)
+    }
+}
+
+impl VastFitData {
+    pub(in crate::sol::svc::vast) fn validate_not_loaded_item_fast(&self, kfs: &RSet<ItemKey>) -> bool {
+        validate_fast(kfs, &self.not_loaded)
+    }
+    pub(in crate::sol::svc::vast) fn validate_not_loaded_item_verbose(
+        &self,
+        kfs: &RSet<ItemKey>,
+        uad: &Uad,
+    ) -> Option<ValNotLoadedItemFail> {
+        validate_verbose(kfs, &self.not_loaded, uad)
+    }
+}
+
+fn validate_fast(kfs: &RSet<ItemKey>, not_loaded: &RSet<ItemKey>) -> bool {
+    match kfs.is_empty() {
+        true => not_loaded.is_empty(),
+        false => not_loaded.difference(kfs).next().is_none(),
+    }
+}
+
+fn validate_verbose(kfs: &RSet<ItemKey>, not_loaded: &RSet<ItemKey>, uad: &Uad) -> Option<ValNotLoadedItemFail> {
+    let item_ids: Vec<_> = not_loaded
+        .iter()
+        .filter(|item_key| !kfs.contains(item_key))
+        .map(|item_key| uad.items.id_by_key(*item_key))
+        .collect();
+    match item_ids.is_empty() {
+        true => None,
+        false => Some(ValNotLoadedItemFail { item_ids }),
     }
 }
