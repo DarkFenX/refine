707fdf727925f0e55be8033859c32a3eb9601f70
DarkPhoenix <phoenix@mail.ru>
2025-06-11 11:15:17 +0200

    Remove layer-specific methods from public API

--- core/src/sol/api/fit/stats/tank.rs
+++ core/src/sol/api/fit/stats/tank.rs
@@ -7,13 +7,4 @@ impl<'a> FitMut<'a> {
     pub fn get_resists(&mut self) -> Option<StatTank<DmgKinds<AttrVal>>> {
         self.get_ship_mut().and_then(|mut v| v.get_resists())
     }
-    pub fn get_shield_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        self.get_ship_mut().and_then(|mut v| v.get_shield_resists())
-    }
-    pub fn get_armor_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        self.get_ship_mut().and_then(|mut v| v.get_armor_resists())
-    }
-    pub fn get_structure_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        self.get_ship_mut().and_then(|mut v| v.get_structure_resists())
-    }
 }
--- core/src/sol/api/item/item_ship/stats/tank.rs
+++ core/src/sol/api/item/item_ship/stats/tank.rs
@@ -11,13 +11,4 @@ impl<'a> ShipMut<'a> {
     pub fn get_resists(&mut self) -> Option<StatTank<DmgKinds<AttrVal>>> {
         Vast::get_item_resists(&self.sol.uad, &mut self.sol.svc.calc, self.key)
     }
-    pub fn get_shield_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        Vast::get_item_shield_resists(&self.sol.uad, &mut self.sol.svc.calc, self.key)
-    }
-    pub fn get_armor_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        Vast::get_item_armor_resists(&self.sol.uad, &mut self.sol.svc.calc, self.key)
-    }
-    pub fn get_structure_resists(&mut self) -> Option<DmgKinds<AttrVal>> {
-        Vast::get_item_structure_resists(&self.sol.uad, &mut self.sol.svc.calc, self.key)
-    }
 }
