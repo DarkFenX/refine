be879cef0386e28a270c89f4cf1c8d6a679bafcf
DarkPhoenix <phoenix@mail.ru>
2025-06-21 18:55:28 +0200

    Fix an omission

--- core/src/sol/svc/vast/vaste_maintain/vaste_effects_started_stopped.rs
+++ core/src/sol/svc/vast/vaste_maintain/vaste_effects_started_stopped.rs
@@ -43,13 +43,13 @@ impl Vast {
                 ac::effects::FUELED_SHIELD_BOOSTING => {
                     if let Some(fit_id) = item.get_fit_key() {
                         let fit_data = self.get_fit_data_mut(&fit_id);
-                        fit_data.limitable_sb.insert(EffectSpec::new(item_key, a_effect.id));
+                        fit_data.limitable_sb.remove(&EffectSpec::new(item_key, a_effect.id));
                     }
                 }
                 ac::effects::FUELED_ARMOR_REPAIR => {
                     if let Some(fit_id) = item.get_fit_key() {
                         let fit_data = self.get_fit_data_mut(&fit_id);
-                        fit_data.limitable_ar.insert(EffectSpec::new(item_key, a_effect.id));
+                        fit_data.limitable_ar.remove(&EffectSpec::new(item_key, a_effect.id));
                     }
                 }
                 _ => (),
