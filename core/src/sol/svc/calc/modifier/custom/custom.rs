401abbaca238875ba7092c625d50f00f5c9b4a24
DarkPhoenix <phoenix@mail.ru>
2025-06-28 08:59:24 +0200

    Add struct which is supposed to house all runtime modifier customizations

---
+++ core/src/sol/svc/calc/modifier/custom/custom.rs
@@ -0,0 +1,28 @@
+use smallvec::SmallVec;
+
+use crate::{
+    ad,
+    sol::{
+        ItemKey,
+        svc::{SvcCtx, calc::AffectorInfo},
+        uad::item::UadItem,
+    },
+};
+
+pub(crate) struct CustomAffectorValue {
+    // Modifiers have two ways to define affector attribute:
+    // - cheap way is via this field, with limitation that value of the attribute has to be on the
+    //   same item as the effect modifier is created from. All the regular modifiers use this
+    //   approach;
+    // - more expensive and flexible way via registering arbitrary attribute dependencies in the
+    //  dependency register during attribute value calculation.
+    // Use this field over the dependency approach whenever possible.
+    pub(crate) affector_a_attr_id: Option<ad::AAttrId>,
+    // Should return all the affecting attributes. Can be slow, used only when fetching modification
+    // info
+    pub(crate) affector_info_getter: fn(&SvcCtx, ItemKey) -> SmallVec<AffectorInfo, 1>,
+    // Reviser functions are triggered upon certain events; if they return true, affected attribute
+    // values are marked for recalculation.
+    pub(crate) item_add_reviser: Option<fn(&SvcCtx, ItemKey, ItemKey, &UadItem) -> bool> = None,
+    pub(crate) item_remove_reviser: Option<fn(&SvcCtx, ItemKey, ItemKey, &UadItem) -> bool> = None,
+}
--- core/src/sol/svc/calc/modifier/custom/mod.rs
+++ core/src/sol/svc/calc/modifier/custom/mod.rs
@@ -1,6 +1,7 @@
 pub(in crate::sol::svc::calc) use extend::extend_with_custom_mods;
 
 pub(super) mod aar_rep_amount;
+mod custom;
 mod extend;
 pub(super) mod missile_flight_time;
 pub(super) mod prop_speed_boost;
