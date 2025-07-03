009ac1bfdc1318893475cf53501ba68f28f55504
DarkPhoenix <phoenix@mail.ru>
2025-07-04 00:43:36 +0200

    Reorganize projection/mutation modules in HTTP cmd module

---
+++ http/src/cmd/shared/mutation/mod.rs
@@ -0,0 +1,3 @@
+pub(in crate::cmd) use def::{HMutationOnAdd, HMutationOnChange, apply_mattrs_on_add, apply_mattrs_on_change};
+
+mod def;
---
+++ http/src/cmd/shared/proj/mod.rs
@@ -0,0 +1,5 @@
+pub(in crate::cmd) use def::{HProjDef, HProjDefFull};
+pub(in crate::cmd) use range::HProjRange;
+
+mod def;
+mod range;
