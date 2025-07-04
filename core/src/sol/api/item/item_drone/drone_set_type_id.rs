3eec2ae2bd4c67ed5ed481fb99f6ec2ff8ab1796
DarkPhoenix <phoenix@mail.ru>
2025-07-04 19:38:44 +0200

    Avoid allocations when working on incoming projections

--- core/src/sol/api/item/item_drone/drone_set_type_id.rs
+++ core/src/sol/api/item/item_drone/drone_set_type_id.rs
@@ -20,27 +20,25 @@ impl SolarSystem {
             uad_prange.update_src_rad(drone_radius);
         }
         // Update drone radius for incoming projections
-        let mut updated_projectors = Vec::new();
         for &projector_item_key in self.rprojs.iter_projectors(&item_key) {
             let projector_uad_item = self.uad.items.get_mut(projector_item_key);
             if let Some(uad_prange) = projector_uad_item.get_projs_mut().unwrap().get_mut_range(&item_key) {
                 if uad_prange.update_tgt_rad(drone_radius) {
-                    updated_projectors.push((projector_item_key, *uad_prange));
+                    let uad_prange = Some(*uad_prange);
+                    let uad_item = self.uad.items.get(item_key);
+                    SolarSystem::util_change_item_proj_range(
+                        &self.uad,
+                        &mut self.svc,
+                        &self.reffs,
+                        projector_item_key,
+                        item_key,
+                        uad_item,
+                        uad_prange,
+                    );
                 }
             }
         }
         let uad_item = self.uad.items.get(item_key);
-        for (projector_item_key, uad_prange) in updated_projectors {
-            SolarSystem::util_change_item_proj_range(
-                &self.uad,
-                &mut self.svc,
-                &self.reffs,
-                projector_item_key,
-                item_key,
-                uad_item,
-                Some(uad_prange),
-            );
-        }
         SolarSystem::util_add_drone_with_projs(&self.uad, &mut self.svc, &mut self.reffs, item_key, uad_item);
     }
 }
