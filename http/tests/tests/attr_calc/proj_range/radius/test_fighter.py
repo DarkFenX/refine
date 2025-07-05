cf83a45f804b7bc2e42f0164fc933fbe2e0fb939
DarkPhoenix <phoenix@mail.ru>
2025-07-05 14:43:19 +0200

    Add incoming projection + fighter radius change test

--- http/tests/tests/attr_calc/proj_range/radius/test_fighter.py
+++ http/tests/tests/attr_calc/proj_range/radius/test_fighter.py
@@ -404,3 +404,74 @@ def test_outgoing_switch_src(client, consts):
     api_affectee_ship.update()
     assert api_affectee_ship.attrs[eve_affectee_attr1_id].dogma == approx(287.5)
     assert api_affectee_ship.attrs[eve_affectee_attr2_id].dogma == approx(287.5)
+
+
+def test_incoming_switch_src(client, consts):
+    eve_d1 = client.mk_eve_data()
+    eve_d2 = client.mk_eve_data()
+    eve_d3 = client.mk_eve_data()
+    eve_d4 = client.mk_eve_data()
+    eve_radius_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2, eve_d3, eve_d4], id_=consts.EveAttr.radius)
+    eve_affector_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2, eve_d3, eve_d4])
+    eve_affectee_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2, eve_d3])
+    eve_optimal_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2, eve_d3, eve_d4])
+    eve_falloff_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2, eve_d3, eve_d4])
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        loc=consts.EveModLoc.tgt,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr_id,
+        affectee_attr_id=eve_affectee_attr_id)
+    eve_effect_id = client.mk_eve_effect(
+        datas=[eve_d1, eve_d2, eve_d3, eve_d4],
+        cat_id=consts.EveEffCat.target,
+        range_attr_id=eve_optimal_attr_id,
+        falloff_attr_id=eve_falloff_attr_id,
+        mod_info=[eve_mod])
+    eve_affector_drone_id = client.mk_eve_item(
+        datas=[eve_d1, eve_d2, eve_d3, eve_d4],
+        attrs={eve_affector_attr_id: -85, eve_optimal_attr_id: 1000, eve_falloff_attr_id: 10000},
+        eff_ids=[eve_effect_id],
+        defeff_id=eve_effect_id)
+    eve_affectee_fighter_id = client.alloc_item_id(datas=[eve_d1, eve_d2, eve_d3, eve_d4])
+    client.mk_eve_item(
+        datas=[eve_d1],
+        id_=eve_affectee_fighter_id,
+        attrs={eve_radius_attr_id: 25, eve_affectee_attr_id: 1000})
+    client.mk_eve_item(
+        datas=[eve_d2],
+        id_=eve_affectee_fighter_id,
+        attrs={eve_radius_attr_id: 500, eve_affectee_attr_id: 1000})
+    client.mk_eve_item(datas=[eve_d3], id_=eve_affectee_fighter_id, attrs={eve_affectee_attr_id: 1000})
+    client.create_sources()
+    api_sol = client.create_sol(data=eve_d1)
+    api_affector_fit = api_sol.create_fit()
+    api_affector_drone = api_affector_fit.add_drone(type_id=eve_affector_drone_id, state=consts.ApiMinionState.engaging)
+    api_affectee_fit = api_sol.create_fit()
+    api_affectee_fighter = api_affectee_fit.add_fighter(type_id=eve_affectee_fighter_id)
+    api_affector_drone.change_drone(add_projs=[(api_affectee_fighter.id, range_s2s_to_api(val=11000))])
+    # Verification
+    assert api_affector_drone.update().projs[api_affectee_fighter.id] == (11025, 11000)
+    assert api_affectee_fighter.update().attrs[eve_affectee_attr_id].dogma == approx(575)
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification
+    assert api_affector_drone.update().projs[api_affectee_fighter.id] == (11025, 10525)
+    assert api_affectee_fighter.update().attrs[eve_affectee_attr_id].dogma == approx(546.78155)
+    # Action
+    api_sol.change_src(data=eve_d3)
+    # Verification
+    assert api_affector_drone.update().projs[api_affectee_fighter.id] == (11025, 11025)
+    assert api_affectee_fighter.update().attrs[eve_affectee_attr_id].dogma == approx(576.472223)
+    # Action
+    api_sol.change_src(data=eve_d4)
+    # Verification
+    assert api_affector_drone.update().projs[api_affectee_fighter.id] == (11025, 11025)
+    api_affectee_fighter.update()
+    with check_no_field():
+        api_affectee_fighter.attrs  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d1)
+    # Verification
+    assert api_affector_drone.update().projs[api_affectee_fighter.id] == (11025, 11000)
+    assert api_affectee_fighter.update().attrs[eve_affectee_attr_id].dogma == approx(575)
