222c95cc8deef8d3906532d054f2a27079c521da
DarkPhoenix <phoenix@mail.ru>
2025-07-10 23:03:32 +0200

    Add no cycle time tests for RR stats

--- http/tests/tests/stats/rr/test_armor.py
+++ http/tests/tests/stats/rr/test_armor.py
@@ -120,3 +120,51 @@ def test_zero_cycle_time(client, consts):
     # Verification
     api_stats = api_fit.get_stats(options=StatsOptions(rr_armor=True))
     assert api_stats.rr_armor == approx(0)
+
+
+def test_no_cycle_time(client, consts):
+    eve_rep_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.armor_dmg_amount)
+    eve_ancil_mult_attr_id = client.mk_eve_attr(id_=consts.EveAttr.charged_armor_dmg_mult)
+    eve_cycle_time_attr_id = client.mk_eve_attr()
+    eve_module_normal_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.ship_mod_remote_armor_repairer,
+        cat_id=consts.EveEffCat.target)
+    eve_module_ancil_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.ship_mod_ancillary_remote_armor_repairer,
+        cat_id=consts.EveEffCat.target)
+    eve_module_spool_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.ship_mod_remote_armor_mutadaptive_repairer,
+        cat_id=consts.EveEffCat.target)
+    eve_drone_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.npc_entity_remote_armor_repairer,
+        cat_id=consts.EveEffCat.target)
+    eve_module_normal_id = client.mk_eve_item(
+        attrs={eve_rep_amount_attr_id: 376, eve_cycle_time_attr_id: 6000},
+        eff_ids=[eve_module_normal_effect_id],
+        defeff_id=eve_module_normal_effect_id)
+    eve_module_ancil_id = client.mk_eve_item(
+        attrs={eve_rep_amount_attr_id: 145, eve_cycle_time_attr_id: 6000, eve_ancil_mult_attr_id: 3},
+        eff_ids=[eve_module_ancil_effect_id],
+        defeff_id=eve_module_ancil_effect_id)
+    eve_module_spool_id = client.mk_eve_item(
+        attrs={eve_rep_amount_attr_id: 512, eve_cycle_time_attr_id: 6000},
+        eff_ids=[eve_module_spool_effect_id],
+        defeff_id=eve_module_spool_effect_id)
+    eve_drone_id = client.mk_eve_item(
+        attrs={eve_rep_amount_attr_id: 72, eve_cycle_time_attr_id: 5000},
+        eff_ids=[eve_drone_effect_id],
+        defeff_id=eve_drone_effect_id)
+    eve_paste_id = client.mk_eve_item(id_=consts.EveItem.nanite_repair_paste)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_fit.add_module(type_id=eve_module_normal_id, state=consts.ApiModuleState.active)
+    api_fit.add_module(
+        type_id=eve_module_ancil_id,
+        state=consts.ApiModuleState.active,
+        charge_type_id=eve_paste_id)
+    api_fit.add_module(type_id=eve_module_spool_id, state=consts.ApiModuleState.active)
+    api_fit.add_drone(type_id=eve_drone_id, state=consts.ApiMinionState.engaging)
+    # Verification
+    api_stats = api_fit.get_stats(options=StatsOptions(rr_armor=True))
+    assert api_stats.rr_armor == approx(0)
