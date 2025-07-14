d68f2ea01255ded7bfd6904d7959fafdcaedf0d4
DarkPhoenix <phoenix@mail.ru>
2025-07-14 13:08:26 +0200

    Add more shield active tank tests

--- http/tests/tests/stats/tank/active/test_shield.py
+++ http/tests/tests/stats/tank/active/test_shield.py
@@ -1,4 +1,4 @@
-from tests import ANY_VALUE, approx
+from tests import ANY_VALUE, Range, approx
 from tests.fw.api import FitStatsOptions, ItemStatsOptions
 from tests.tests.stats.tank import (
     make_eve_drone_shield,
@@ -108,6 +108,94 @@ def test_drone(client, consts):
     assert api_tgt_drone_rps.hull == [0, 0, ANY_VALUE]
 
 
+def test_hp_limit_and_resist(client, consts):
+    eve_basic_info = setup_tank_basics(client=client, consts=consts)
+    eve_ship_id = make_eve_tankable(client=client, basic_info=eve_basic_info, hps=(50, 1000, 1000), rr_resist=0.1)
+    eve_module_lsb_id = make_eve_local_sb(client=client, basic_info=eve_basic_info, rep_amount=228, cycle_time=3000)
+    eve_module_lasb_id = make_eve_local_asb(client=client, basic_info=eve_basic_info, rep_amount=146, cycle_time=3000)
+    eve_module_rsb_id = make_eve_remote_sb(client=client, basic_info=eve_basic_info, rep_amount=508, cycle_time=8000)
+    eve_module_rasb_id = make_eve_remote_asb(client=client, basic_info=eve_basic_info, rep_amount=950, cycle_time=8000)
+    eve_drone_id = make_eve_drone_shield(client=client, basic_info=eve_basic_info, rep_amount=72, cycle_time=5000)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_module_rsb = api_src_fit.add_module(type_id=eve_module_rsb_id, state=consts.ApiModuleState.active)
+    api_module_rasb = api_src_fit.add_module(type_id=eve_module_rasb_id, state=consts.ApiModuleState.active)
+    api_drone = api_src_fit.add_drone(type_id=eve_drone_id, state=consts.ApiMinionState.engaging)
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_ship_id)
+    api_tgt_fit.add_module(type_id=eve_module_lsb_id, state=consts.ApiModuleState.active)
+    api_tgt_fit.add_module(type_id=eve_module_lasb_id, state=consts.ApiModuleState.active)
+    api_module_rsb.change_module(add_projs=[api_tgt_ship.id])
+    api_module_rasb.change_module(add_projs=[api_tgt_ship.id])
+    api_drone.change_drone(add_projs=[api_tgt_ship.id])
+    # Verification - local reps are not resisted but limited, remote reps resisted and limited,
+    # except for drone which has low enough reps to be below limit
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(rps=True))
+    api_tgt_fit_rps, = api_tgt_fit_stats.rps
+    assert api_tgt_fit_rps.shield == [approx(33.333333), approx(13.94), ANY_VALUE]
+    assert api_tgt_fit_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_fit_rps.hull == [0, 0, ANY_VALUE]
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(rps=True))
+    api_tgt_ship_rps, = api_tgt_ship_stats.rps
+    assert api_tgt_ship_rps.shield == [approx(33.333333), approx(13.94), ANY_VALUE]
+    assert api_tgt_ship_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_ship_rps.hull == [0, 0, ANY_VALUE]
+
+
+def test_hp_limit_and_range(client, consts):
+    eve_basic_info = setup_tank_basics(client=client, consts=consts)
+    eve_ship_id = make_eve_tankable(client=client, basic_info=eve_basic_info, hps=(300, 1000, 1000))
+    eve_module_lsb_id = make_eve_local_sb(client=client, basic_info=eve_basic_info, rep_amount=228, cycle_time=3000)
+    eve_module_lasb_id = make_eve_local_asb(client=client, basic_info=eve_basic_info, rep_amount=146, cycle_time=3000)
+    eve_module_rsb_id = make_eve_remote_sb(
+        client=client,
+        basic_info=eve_basic_info,
+        rep_amount=508,
+        cycle_time=8000,
+        optimal_range=10000,
+        falloff_range=5000)
+    eve_module_rasb_id = make_eve_remote_asb(
+        client=client,
+        basic_info=eve_basic_info,
+        rep_amount=950,
+        cycle_time=8000,
+        optimal_range=10000,
+        falloff_range=5000)
+    eve_drone_id = make_eve_drone_shield(
+        client=client,
+        basic_info=eve_basic_info,
+        rep_amount=72,
+        cycle_time=5000,
+        optimal_range=10000)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_module_rsb = api_src_fit.add_module(type_id=eve_module_rsb_id, state=consts.ApiModuleState.active)
+    api_module_rasb = api_src_fit.add_module(type_id=eve_module_rasb_id, state=consts.ApiModuleState.active)
+    api_drone = api_src_fit.add_drone(type_id=eve_drone_id, state=consts.ApiMinionState.engaging)
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_ship_id)
+    api_tgt_fit.add_module(type_id=eve_module_lsb_id, state=consts.ApiModuleState.active)
+    api_tgt_fit.add_module(type_id=eve_module_lasb_id, state=consts.ApiModuleState.active)
+    api_module_rsb.change_module(add_projs=[(api_tgt_ship.id, Range.s2s_to_api(val=15000))])
+    api_module_rasb.change_module(add_projs=[(api_tgt_ship.id, Range.s2s_to_api(val=15000))])
+    api_drone.change_drone(add_projs=[(api_tgt_ship.id, Range.s2s_to_api(val=10001))])
+    # Verification - effect of local reps is not reduced altogether, drone effect is removed since
+    # it's out of range, regular RR effect is reduced but not limited, ancillary RR effect is
+    # reduced and limited (since its reduced RR amount is still more than target HP)
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(rps=True))
+    api_tgt_fit_rps, = api_tgt_fit_stats.rps
+    assert api_tgt_fit_rps.shield == [approx(124.666667), approx(69.25), ANY_VALUE]
+    assert api_tgt_fit_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_fit_rps.hull == [0, 0, ANY_VALUE]
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(rps=True))
+    api_tgt_ship_rps, = api_tgt_ship_stats.rps
+    assert api_tgt_ship_rps.shield == [approx(124.666667), approx(69.25), ANY_VALUE]
+    assert api_tgt_ship_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_ship_rps.hull == [0, 0, ANY_VALUE]
+
+
 def test_zero_cycle_time(client, consts):
     eve_basic_info = setup_tank_basics(client=client, consts=consts)
     eve_ship_id = make_eve_tankable(client=client, basic_info=eve_basic_info, hps=(3000, 1000, 1000))
@@ -140,3 +228,71 @@ def test_zero_cycle_time(client, consts):
     assert api_tgt_ship_rps.shield == [0, 0, ANY_VALUE]
     assert api_tgt_ship_rps.armor == [0, 0, ANY_VALUE]
     assert api_tgt_ship_rps.hull == [0, 0, ANY_VALUE]
+
+
+def test_no_cycle_time(client, consts):
+    eve_basic_info = setup_tank_basics(client=client, consts=consts, effect_duration=False)
+    eve_ship_id = make_eve_tankable(client=client, basic_info=eve_basic_info, hps=(3000, 1000, 1000))
+    eve_module_lsb_id = make_eve_local_sb(client=client, basic_info=eve_basic_info, rep_amount=228, cycle_time=3000)
+    eve_module_lasb_id = make_eve_local_asb(client=client, basic_info=eve_basic_info, rep_amount=146, cycle_time=3000)
+    eve_module_rsb_id = make_eve_remote_sb(client=client, basic_info=eve_basic_info, rep_amount=508, cycle_time=8000)
+    eve_module_rasb_id = make_eve_remote_asb(client=client, basic_info=eve_basic_info, rep_amount=950, cycle_time=8000)
+    eve_drone_id = make_eve_drone_shield(client=client, basic_info=eve_basic_info, rep_amount=72, cycle_time=5000)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_module_rsb = api_src_fit.add_module(type_id=eve_module_rsb_id, state=consts.ApiModuleState.active)
+    api_module_rasb = api_src_fit.add_module(type_id=eve_module_rasb_id, state=consts.ApiModuleState.active)
+    api_drone = api_src_fit.add_drone(type_id=eve_drone_id, state=consts.ApiMinionState.engaging)
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_ship_id)
+    api_tgt_fit.add_module(type_id=eve_module_lsb_id, state=consts.ApiModuleState.active)
+    api_tgt_fit.add_module(type_id=eve_module_lasb_id, state=consts.ApiModuleState.active)
+    api_module_rsb.change_module(add_projs=[api_tgt_ship.id])
+    api_module_rasb.change_module(add_projs=[api_tgt_ship.id])
+    api_drone.change_drone(add_projs=[api_tgt_ship.id])
+    # Verification
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(rps=True))
+    api_tgt_fit_rps, = api_tgt_fit_stats.rps
+    assert api_tgt_fit_rps.shield == [0, 0, ANY_VALUE]
+    assert api_tgt_fit_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_fit_rps.hull == [0, 0, ANY_VALUE]
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(rps=True))
+    api_tgt_ship_rps, = api_tgt_ship_stats.rps
+    assert api_tgt_ship_rps.shield == [0, 0, ANY_VALUE]
+    assert api_tgt_ship_rps.armor == [0, 0, ANY_VALUE]
+    assert api_tgt_ship_rps.hull == [0, 0, ANY_VALUE]
+
+
+def test_item_not_loaded(client, consts):
+    eve_basic_info = setup_tank_basics(client=client, consts=consts)
+    eve_item_id = client.alloc_item_id()
+    eve_module_lsb_id = make_eve_local_sb(client=client, basic_info=eve_basic_info, rep_amount=228, cycle_time=3000)
+    eve_module_lasb_id = make_eve_local_asb(client=client, basic_info=eve_basic_info, rep_amount=146, cycle_time=3000)
+    eve_module_rsb_id = make_eve_remote_sb(client=client, basic_info=eve_basic_info, rep_amount=508, cycle_time=8000)
+    eve_module_rasb_id = make_eve_remote_asb(client=client, basic_info=eve_basic_info, rep_amount=950, cycle_time=8000)
+    eve_drone_id = make_eve_drone_shield(client=client, basic_info=eve_basic_info, rep_amount=72, cycle_time=5000)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_module_rsb = api_src_fit.add_module(type_id=eve_module_rsb_id, state=consts.ApiModuleState.active)
+    api_module_rasb = api_src_fit.add_module(type_id=eve_module_rasb_id, state=consts.ApiModuleState.active)
+    api_src_drone = api_src_fit.add_drone(type_id=eve_drone_id, state=consts.ApiMinionState.engaging)
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_item_id)
+    api_tgt_drone = api_tgt_fit.add_drone(type_id=eve_item_id)
+    api_tgt_fighter = api_tgt_fit.add_fighter(type_id=eve_item_id)
+    api_tgt_fit.add_module(type_id=eve_module_lsb_id, state=consts.ApiModuleState.active)
+    api_tgt_fit.add_module(type_id=eve_module_lasb_id, state=consts.ApiModuleState.active)
+    api_module_rsb.change_module(add_projs=[api_tgt_ship.id, api_tgt_drone.id, api_tgt_fighter.id])
+    api_module_rasb.change_module(add_projs=[api_tgt_ship.id, api_tgt_drone.id, api_tgt_fighter.id])
+    api_src_drone.change_drone(add_projs=[api_tgt_ship.id, api_tgt_drone.id, api_tgt_fighter.id])
+    # Verification
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(rps=True))
+    assert api_tgt_fit_stats.rps is None
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(rps=True))
+    assert api_tgt_ship_stats.rps is None
+    api_tgt_drone_stats = api_tgt_drone.get_stats(options=ItemStatsOptions(rps=True))
+    assert api_tgt_drone_stats.rps is None
+    api_tgt_fighter_stats = api_tgt_fighter.get_stats(options=ItemStatsOptions(rps=True))
+    assert api_tgt_fighter_stats.rps is None
