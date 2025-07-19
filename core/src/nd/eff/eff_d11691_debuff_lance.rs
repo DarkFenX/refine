78ed78d53fe7e5c8bdad7c9f7a2cdde8a1cf4e1c
DarkPhoenix <phoenix@mail.ru>
2025-07-19 19:34:31 +0200

    Implement debuff lance damage part of the effect

--- core/src/nd/eff/eff_d11691_debuff_lance.rs
+++ core/src/nd/eff/eff_d11691_debuff_lance.rs
@@ -1,11 +1,18 @@
 use crate::{
     ac, ad,
-    def::OF,
+    def::{AttrVal, Count, ItemKey, OF},
     ec,
+    misc::{DmgKinds, Spool},
     nd::{
         NEffect, NEffectHc,
         eff::shared::proj_mult::{get_proj_attrs_simple, get_proj_mult_simple_s2s},
     },
+    svc::{
+        SvcCtx,
+        calc::Calc,
+        output::{Output, OutputComplex, OutputSimple},
+    },
+    util::floor_unerr,
 };
 
 pub(super) fn mk_n_effect() -> NEffect {
@@ -27,8 +34,55 @@ pub(super) fn mk_n_effect() -> NEffect {
         xt_get_proj_attrs: Some(get_proj_attrs_simple),
         hc: NEffectHc {
             get_proj_mult: Some(get_proj_mult_simple_s2s),
+            get_normal_dmg_opc: Some(get_dmg_opc),
             ..
         },
         ..
     }
+}
+
+fn get_dmg_opc(
+    ctx: SvcCtx,
+    calc: &mut Calc,
+    projector_key: ItemKey,
+    _projector_a_effect: &ad::AEffectRt,
+    _spool: Option<Spool>,
+    _projectee_key: Option<ItemKey>,
+) -> Option<Output<DmgKinds<AttrVal>>> {
+    let dmg_em = calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::EM_DMG)?;
+    let dmg_therm = calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::THERM_DMG)?;
+    let dmg_kin = calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::KIN_DMG)?;
+    let dmg_expl = calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::EXPL_DMG)?;
+    let delay =
+        calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::DOOMSDAY_WARNING_DURATION)? / OF(1000.0);
+    Some(
+        match calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::DOOMSDAY_DAMAGE_CYCLE_TIME)? {
+            interval if interval > OF(0.0) => {
+                let duration =
+                    calc.get_item_attr_val_extra_opt(ctx, projector_key, &ac::attrs::DOOMSDAY_DAMAGE_DURATION)?
+                        / OF(1000.0);
+                let repeats = floor_unerr(duration / interval) as Count;
+                Output::Complex(OutputComplex {
+                    amount: DmgKinds {
+                        em: dmg_em,
+                        thermal: dmg_therm,
+                        kinetic: dmg_kin,
+                        explosive: dmg_expl,
+                    },
+                    delay,
+                    repeats,
+                    interval,
+                })
+            }
+            _ => Output::Simple(OutputSimple {
+                amount: DmgKinds {
+                    em: dmg_em,
+                    thermal: dmg_therm,
+                    kinetic: dmg_kin,
+                    explosive: dmg_expl,
+                },
+                delay,
+            }),
+        },
+    )
 }
