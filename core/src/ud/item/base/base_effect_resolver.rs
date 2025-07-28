c83284ada1555081b80056924c520a727804321d
DarkPhoenix <phoenix@mail.ru>
2025-07-28 13:32:38 +0200

    Add info about charge/autocharge activation status to effect updates struct

--- core/src/ud/item/base/base_effect_resolver.rs
+++ core/src/ud/item/base/base_effect_resolver.rs
@@ -1,39 +1,60 @@
-use crate::{ac, ad, misc::EffectMode, rd, src::Src, ud::item::misc::EffectModes, util::RSet};
+use crate::{
+    ac,
+    ad::{AEffectId, AState},
+    misc::EffectMode,
+    nd::NEffectChargeLoc,
+    rd::{REffect, REffectKey, RItem, RcEffect},
+    src::Src,
+    ud::item::misc::EffectModes,
+    util::RSet,
+};
 
-const ONLINE_EFFECT_ID: ad::AEffectId = ac::effects::ONLINE;
+const ONLINE_EFFECT_ID: AEffectId = ac::effects::ONLINE;
+
+pub(crate) struct UAutochargeActivation {
+    pub(crate) effect_key: REffectKey,
+    pub(crate) active: bool,
+}
 
 pub(crate) struct UEffectUpdates {
-    pub(crate) to_start: Vec<rd::RcEffect>,
-    pub(crate) to_stop: Vec<rd::RcEffect>,
+    pub(crate) to_start: Vec<RcEffect>,
+    pub(crate) to_stop: Vec<RcEffect>,
+    // Fields which contain info about (auto)charge activation/deactivation
+    pub(crate) charge: Option<bool>,
+    pub(crate) autocharges: Vec<UAutochargeActivation>,
 }
 impl UEffectUpdates {
     pub(crate) fn new() -> Self {
         Self {
             to_start: Vec::new(),
             to_stop: Vec::new(),
+            charge: None,
+            autocharges: Vec::new(),
         }
     }
     pub(in crate::ud::item) fn clear(&mut self) {
         self.to_start.clear();
         self.to_stop.clear();
+        self.charge = None;
+        self.autocharges.clear();
     }
 }
 
 pub(super) fn process_effects(
     reuse_eupdates: &mut UEffectUpdates,
-    reffs: &mut RSet<rd::REffectKey>,
+    reffs: &mut RSet<REffectKey>,
     src: &Src,
-    r_item: &rd::RItem,
-    item_a_state: ad::AState,
+    item: &RItem,
+    item_state: AState,
     item_effect_modes: &EffectModes,
 ) {
-    match item_a_state {
-        ad::AState::Ghost => stop_all_effects(reuse_eupdates, reffs, src),
-        _ => update_running_effects(reuse_eupdates, reffs, src, r_item, item_a_state, item_effect_modes),
+    match item_state {
+        AState::Ghost => stop_all_effects(reuse_eupdates, reffs, src),
+        _ => update_running_effects(reuse_eupdates, reffs, src, item, item_state, item_effect_modes),
     }
 }
 
-fn stop_all_effects(reuse_eupdates: &mut UEffectUpdates, reffs: &mut RSet<rd::REffectKey>, src: &Src) {
+fn stop_all_effects(reuse_eupdates: &mut UEffectUpdates, reffs: &mut RSet<REffectKey>, src: &Src) {
     // We don't want waste time resolving effects when we want them to just stop (which happens
     // before e.g. item removal)
     reuse_eupdates
@@ -43,14 +64,14 @@ fn stop_all_effects(reuse_eupdates: &mut UEffectUpdates, reffs: &mut RSet<rd::RE
 
 fn update_running_effects(
     reuse_eupdates: &mut UEffectUpdates,
-    reffs: &mut RSet<rd::REffectKey>,
+    reffs: &mut RSet<REffectKey>,
     src: &Src,
-    r_item: &rd::RItem,
-    item_a_state: ad::AState,
+    item: &RItem,
+    item_state: AState,
     item_effect_modes: &EffectModes,
 ) {
     // Separate handling for the online effect
-    let online_should_run = resolve_online_effect_status(r_item, item_effect_modes, item_a_state);
+    let online_should_run = resolve_online_effect_status(item, item_effect_modes, item_state);
     let online_running = match src.get_online_effect_key() {
         Some(online_effect_key) => reffs.contains(&online_effect_key),
         None => false,
@@ -62,90 +83,138 @@ fn update_running_effects(
     } else if !online_running && online_should_run {
         reuse_eupdates.to_start.push(src.get_online_effect().unwrap().clone());
     }
-    for &effect_key in r_item.get_effect_datas().keys() {
+    for &effect_key in item.get_effect_datas().keys() {
         // Online effect has already been handled
         if Some(effect_key) == src.get_online_effect_key() {
             continue;
         }
-        let r_effect = src.get_effect(effect_key);
+        let effect = src.get_effect(effect_key);
         let should_run = resolve_regular_effect_status(
             item_effect_modes,
-            r_item.get_defeff_key(),
-            item_a_state,
+            item.get_defeff_key(),
+            item_state,
             online_should_run,
-            r_effect,
+            effect,
         );
         let running = reffs.contains(&effect_key);
         if running && !should_run {
-            reuse_eupdates.to_stop.push(r_effect.clone());
+            reuse_eupdates.to_stop.push(effect.clone());
+            if activates_charge(item, effect) {
+                reuse_eupdates.charge = Some(false);
+            }
+            if activates_autocharge(effect) {
+                reuse_eupdates.autocharges.push(UAutochargeActivation {
+                    effect_key,
+                    active: false,
+                });
+            }
         } else if !running && should_run {
-            reuse_eupdates.to_start.push(r_effect.clone());
+            reuse_eupdates.to_start.push(effect.clone());
+            if activates_charge(item, effect) {
+                reuse_eupdates.charge = Some(true);
+            }
+            if activates_autocharge(effect) {
+                reuse_eupdates.autocharges.push(UAutochargeActivation {
+                    effect_key,
+                    active: true,
+                });
+            }
         };
     }
-    reffs.extend(reuse_eupdates.to_start.iter().map(|a_effect| a_effect.get_key()));
-    for a_effect in reuse_eupdates.to_stop.iter() {
-        reffs.remove(&a_effect.get_key());
+    reffs.extend(reuse_eupdates.to_start.iter().map(|effect| effect.get_key()));
+    for effect in reuse_eupdates.to_stop.iter() {
+        reffs.remove(&effect.get_key());
     }
 }
 
-fn resolve_online_effect_status(r_item: &rd::RItem, item_effect_modes: &EffectModes, item_a_state: ad::AState) -> bool {
-    if !r_item.has_online_effect() {
+fn resolve_online_effect_status(item: &RItem, item_effect_modes: &EffectModes, item_state: AState) -> bool {
+    if !item.has_online_effect() {
         return false;
     }
     match item_effect_modes.get_by_id(&ONLINE_EFFECT_ID) {
         // Since other effects from online category depend on the online effect in full compliance
         // mode, use simplified resolution for the online effect itself
-        EffectMode::FullCompliance | EffectMode::StateCompliance => item_a_state >= ad::AState::Online,
+        EffectMode::FullCompliance | EffectMode::StateCompliance => item_state >= AState::Online,
         // Shouldn't run anything in ghost state even with force run mode
-        EffectMode::ForceRun => item_a_state != ad::AState::Ghost,
+        EffectMode::ForceRun => item_state != AState::Ghost,
         EffectMode::ForceStop => false,
     }
 }
 
 fn resolve_regular_effect_status(
     item_effect_modes: &EffectModes,
-    item_defeff_key: Option<rd::REffectKey>,
-    item_a_state: ad::AState,
+    item_defeff_key: Option<REffectKey>,
+    item_state: AState,
     online_running: bool,
-    r_effect: &rd::REffect,
+    effect: &REffect,
 ) -> bool {
     // Ghosted items should never affect anything regardless of effect mode, so check it first
     // wherever applicable
-    match item_effect_modes.get_by_key(&r_effect.get_key()) {
+    match item_effect_modes.get_by_key(&effect.get_key()) {
         EffectMode::FullCompliance => {
-            item_a_state != ad::AState::Ghost
-                && resolve_regular_effect_status_full(item_defeff_key, item_a_state, r_effect, online_running)
+            item_state != AState::Ghost
+                && resolve_regular_effect_status_full(item_defeff_key, item_state, effect, online_running)
         }
-        EffectMode::StateCompliance => item_a_state != ad::AState::Ghost && item_a_state >= r_effect.get_state(),
-        EffectMode::ForceRun => item_a_state != ad::AState::Ghost,
+        EffectMode::StateCompliance => item_state != AState::Ghost && item_state >= effect.get_state(),
+        EffectMode::ForceRun => item_state != AState::Ghost,
         EffectMode::ForceStop => false,
     }
 }
 
 fn resolve_regular_effect_status_full(
-    item_defeff_key: Option<rd::REffectKey>,
-    item_a_state: ad::AState,
-    r_effect: &rd::REffect,
+    item_defeff_key: Option<REffectKey>,
+    item_state: AState,
+    effect: &REffect,
     online_running: bool,
 ) -> bool {
-    match r_effect.get_state() {
-        ad::AState::Ghost => unreachable!("ghost state should never reach full resolver"),
+    match effect.get_state() {
+        AState::Ghost => unreachable!("ghost state should never reach full resolver"),
         // Offline effects require item in offline+ state, and no fitting usage chance attribute
         // (not to run booster side effects by default)
-        ad::AState::Offline => item_a_state >= r_effect.get_state() && r_effect.get_chance_attr_id().is_none(),
+        AState::Offline => item_state >= effect.get_state() && effect.get_chance_attr_id().is_none(),
         // Online effects depend on 'online' effect, ignoring everything else
-        ad::AState::Online => online_running,
+        AState::Online => online_running,
         // Only default active effect is run, and only if item is in active+ state
-        ad::AState::Active => {
-            if r_effect.get_state() > item_a_state {
+        AState::Active => {
+            if effect.get_state() > item_state {
                 return false;
             };
             match item_defeff_key {
-                Some(defeff_key) => defeff_key == r_effect.get_key(),
+                Some(defeff_key) => defeff_key == effect.get_key(),
                 _ => false,
             }
         }
         // No additional restrictions for overload effects except for item being overloaded
-        ad::AState::Overload => item_a_state >= r_effect.get_state(),
+        AState::Overload => item_state >= effect.get_state(),
     }
+}
+
+fn activates_charge(item: &RItem, effect: &REffect) -> bool {
+    let charge_info = match effect.get_charge_info() {
+        Some(charge_info) => charge_info,
+        None => return false,
+    };
+    if !charge_info.activates_charge {
+        return false;
+    }
+    if !matches!(charge_info.location, NEffectChargeLoc::Loaded(_)) {
+        return false;
+    }
+    // Only default effects can activate regular charge
+    let defeff_key = match item.get_defeff_key() {
+        Some(defeff_key) => defeff_key,
+        None => return false,
+    };
+    defeff_key == effect.get_key()
+}
+
+fn activates_autocharge(effect: &REffect) -> bool {
+    let charge_info = match effect.get_charge_info() {
+        Some(charge_info) => charge_info,
+        None => return false,
+    };
+    if !charge_info.activates_charge {
+        return false;
+    }
+    matches!(charge_info.location, NEffectChargeLoc::Autocharge(_))
 }
