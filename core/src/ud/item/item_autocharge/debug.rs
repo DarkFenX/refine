3de68895fb93a01f2a6fc348c86ef0802aa0ec63
DarkPhoenix <phoenix@mail.ru>
2025-07-30 17:02:32 +0200

    Add extra autocharge consistency check

--- core/src/ud/item/item_autocharge/debug.rs
+++ core/src/ud/item/item_autocharge/debug.rs
@@ -1,5 +1,5 @@
 use crate::{
-    dbg::{DebugResult, check_fit_key, check_item_key},
+    dbg::{DebugResult, check_effect_key, check_fit_key, check_item_key},
     ud::{UAutocharge, UData},
 };
 
@@ -9,6 +9,8 @@ impl UAutocharge {
         check_fit_key(u_data, self.get_fit_key())?;
         // All autocharges are supposed to be loaded
         check_item_key(u_data, self.get_cont_item_key(), true)?;
+        // Autocharges should exist only for effects available on current source
+        check_effect_key(u_data, self.get_cont_effect_key())?;
         self.get_projs().consistency_check(u_data)?;
         Ok(())
     }
