0fc1bacb72bce827364506b411a33a1a72d9ef54
DarkPhoenix <phoenix@mail.ru>
2025-07-31 10:47:04 +0200

    Expand debug checks a little

--- core/src/ud/item/item_autocharge/debug.rs
+++ core/src/ud/item/item_autocharge/debug.rs
@@ -1,5 +1,5 @@
 use crate::{
-    dbg::{DebugResult, check_effect_key, check_fit_key, check_item_key},
+    dbg::{DebugError, DebugResult, check_effect_key, check_fit_key, check_item_key},
     ud::{UAutocharge, UData},
 };
 
@@ -11,6 +11,16 @@ impl UAutocharge {
         check_item_key(u_data, self.get_cont_item_key(), true)?;
         // Autocharges should exist only for effects available on current source
         check_effect_key(u_data, self.get_cont_effect_key())?;
+        // Effect key should be available on containing item
+        if !u_data
+            .items
+            .get(self.get_cont_item_key())
+            .get_effect_datas()
+            .unwrap()
+            .contains_key(&self.get_cont_effect_key())
+        {
+            return Err(DebugError {});
+        }
         self.get_projs().consistency_check(u_data)?;
         Ok(())
     }
