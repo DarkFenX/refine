696b2c2738b69288e791b73db75f38dbdd23c51c
DarkPhoenix <phoenix@mail.ru>
2025-08-05 13:17:31 +0200

    Expand existing dps checks with absent/non-loaded item tests

---
+++ http/tests/tests/stats/dmg/test_misc.py
@@ -0,0 +1,23 @@
+from tests.fw.api import FitStatsOptions, ItemStatsOptions
+
+
+def test_not_loaded(client, consts):
+    eve_item_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_module = api_fit.add_module(type_id=eve_item_id, state=consts.ApiModuleState.active)
+    api_drone = api_fit.add_drone(type_id=eve_item_id, state=consts.ApiMinionState.engaging)
+    api_fighter = api_fit.add_fighter(type_id=eve_item_id, state=consts.ApiMinionState.engaging)
+    api_fit_stats = api_fit.get_stats(options=FitStatsOptions(dps=True, volley=True))
+    assert api_fit_stats.dps.one() == [0, 0, 0, 0]
+    assert api_fit_stats.volley.one() == [0, 0, 0, 0]
+    api_module_stats = api_module.get_stats(options=ItemStatsOptions(dps=True, volley=True))
+    assert api_module_stats.dps is None
+    assert api_module_stats.volley is None
+    api_drone_stats = api_drone.get_stats(options=ItemStatsOptions(dps=True, volley=True))
+    assert api_drone_stats.dps is None
+    assert api_drone_stats.volley is None
+    api_fighter_stats = api_fighter.get_stats(options=ItemStatsOptions(dps=True, volley=True))
+    assert api_fighter_stats.dps is None
+    assert api_fighter_stats.volley is None
--- http/tests/tests/stats/dmg/test_turret_projectile.py
+++ http/tests/tests/stats/dmg/test_turret_projectile.py
@@ -73,3 +73,41 @@ def test_reload(client, consts):
     api_module_dps_burst, api_module_dps_reload = api_module_stats.dps
     assert api_module_dps_burst == [approx(116.4375), 0, approx(12.9375), approx(25.875)]
     assert api_module_dps_reload == [approx(109.588235), 0, approx(12.176471), approx(24.352941)]
+
+
+def test_charge_absent(client, consts):
+    eve_basic_info = setup_dmg_basics(client=client, consts=consts)
+    eve_module_id = make_eve_turret_proj(
+        client=client, basic_info=eve_basic_info, dmg_mult=45, capacity=0.25, cycle_time=8000, reload_time=10000)
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_module = api_fit.add_module(type_id=eve_module_id, state=consts.ApiModuleState.active)
+    # Verification
+    api_fit_stats = api_fit.get_stats(options=FitStatsOptions(dps=True, volley=True))
+    assert api_fit_stats.dps.one() == [0, 0, 0, 0]
+    assert api_fit_stats.volley.one() == [0, 0, 0, 0]
+    api_module_stats = api_module.get_stats(options=ItemStatsOptions(dps=True, volley=True))
+    assert api_module_stats.dps.one() == [0, 0, 0, 0]
+    assert api_module_stats.volley.one() == [0, 0, 0, 0]
+
+
+def test_charge_not_loaded(client, consts):
+    eve_basic_info = setup_dmg_basics(client=client, consts=consts)
+    eve_module_id = make_eve_turret_proj(
+        client=client, basic_info=eve_basic_info, dmg_mult=45, capacity=0.25, cycle_time=8000, reload_time=10000)
+    eve_charge_id = client.alloc_item_id()
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_module = api_fit.add_module(
+        type_id=eve_module_id,
+        state=consts.ApiModuleState.active,
+        charge_type_id=eve_charge_id)
+    # Verification
+    api_fit_stats = api_fit.get_stats(options=FitStatsOptions(dps=True, volley=True))
+    assert api_fit_stats.dps.one() == [0, 0, 0, 0]
+    assert api_fit_stats.volley.one() == [0, 0, 0, 0]
+    api_module_stats = api_module.get_stats(options=ItemStatsOptions(dps=True, volley=True))
+    assert api_module_stats.dps.one() == [0, 0, 0, 0]
+    assert api_module_stats.volley.one() == [0, 0, 0, 0]
