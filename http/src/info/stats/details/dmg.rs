25f744a5d562182409dcf2f6777461a739ce238e
DarkPhoenix <phoenix@mail.ru>
2025-08-09 22:45:49 +0200

    Expose breacher stats in HTTP and process them in test framework

--- http/src/info/stats/details/dmg.rs
+++ http/src/info/stats/details/dmg.rs
@@ -4,6 +4,7 @@ pub(crate) struct HStatDmg {
     thermal: rc::AttrVal,
     kinetic: rc::AttrVal,
     explosive: rc::AttrVal,
+    breacher: Option<HStatDmgBreacher>,
 }
 impl From<rc::stats::StatDmg> for HStatDmg {
     fn from(core_stat: rc::stats::StatDmg) -> Self {
@@ -12,6 +13,21 @@ impl From<rc::stats::StatDmg> for HStatDmg {
             thermal: core_stat.thermal,
             kinetic: core_stat.kinetic,
             explosive: core_stat.explosive,
+            breacher: core_stat.breacher.map(|v| v.into()),
+        }
+    }
+}
+
+#[derive(serde_tuple::Serialize_tuple)]
+pub(crate) struct HStatDmgBreacher {
+    absolute_max: rc::AttrVal,
+    relative_max: rc::AttrVal,
+}
+impl From<rc::stats::StatDmgBreacher> for HStatDmgBreacher {
+    fn from(core_stat: rc::stats::StatDmgBreacher) -> Self {
+        Self {
+            absolute_max: core_stat.absolute_max,
+            relative_max: core_stat.relative_max,
         }
     }
 }
--- http/tests/fw/api/types/stats/stat_dmg.py
+++ http/tests/fw/api/types/stats/stat_dmg.py
@@ -11,9 +11,11 @@ class StatDmg:
     thermal: float
     kinetic: float
     explosive: float
+    breacher: StatDmgBreacher | None
 
     def __init__(self, *, data: list | tuple) -> None:
-        self.em, self.thermal, self.kinetic, self.explosive = data
+        self.em, self.thermal, self.kinetic, self.explosive, breacher = data
+        self.breacher = StatDmgBreacher(data=breacher) if breacher is not None else None
 
     def __getitem__(self, item: int) -> typing.Any:
         field = dataclasses.fields(self)[item]
@@ -22,4 +24,26 @@ class StatDmg:
     def __eq__(self, other: list | tuple) -> bool:
         if isinstance(other, tuple):
             other = list(other)
-        return [self.em, self.thermal, self.kinetic, self.explosive] == other
+        # Assume breacher is None if no 5th element is specified
+        if isinstance(other, list) and len(other) == 4:
+            other = other + [None]
+        return [self.em, self.thermal, self.kinetic, self.explosive, self.breacher] == other
+
+
+@dataclasses.dataclass
+class StatDmgBreacher:
+
+    abs_max: float
+    rel_max: float
+
+    def __init__(self, *, data: list | tuple) -> None:
+        self.abs_max, self.rel_max = data
+
+    def __getitem__(self, item: int) -> typing.Any:
+        field = dataclasses.fields(self)[item]
+        return getattr(self, field.name)
+
+    def __eq__(self, other: list | tuple) -> bool:
+        if isinstance(other, tuple):
+            other = list(other)
+        return [self.abs_max, self.rel_max] == other
