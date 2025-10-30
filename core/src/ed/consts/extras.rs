dfea9be52f6d97f169c34321ae8f8903c20f01a0
DarkPhoenix <phoenix@mail.ru>
2025-10-30 14:21:09 +0100

    Add AU/LY constants

--- core/src/ed/consts/extras.rs
+++ core/src/ed/consts/extras.rs
@@ -1,4 +1,4 @@
-use crate::{ec::attrs, ed::EAttrId};
+use crate::{ec::attrs, ed::EAttrId, def::AttrVal, def::OF};
 
 pub(crate) const BUFF_STDATTR_IDS: [EAttrId; 4] = [
     attrs::WARFARE_BUFF1_ID,
@@ -6,3 +6,6 @@ pub(crate) const BUFF_STDATTR_IDS: [EAttrId; 4] = [
     attrs::WARFARE_BUFF3_ID,
     attrs::WARFARE_BUFF4_ID,
 ];
+
+pub(crate) const AU: AttrVal = OF(149_597_870_700.0);
+pub(crate) const LY: AttrVal = OF(9_460_000_000_000_000.0);
