e2510e93cbad8213a0756010baad06eb49d66bda
DarkPhoenix <phoenix@mail.ru>
2025-10-31 15:31:46 +0100

    Fill data containers for incoming ET/neut effects

--- core/src/svc/vast/vaste_maintain/vaste_effect_projected_unprojected.rs
+++ core/src/svc/vast/vaste_maintain/vaste_effect_projected_unprojected.rs
@@ -11,35 +11,35 @@ impl Vast {
         &mut self,
         projector_key: UItemKey,
         projector_item: &UItem,
-        r_effect: &REffect,
+        effect: &REffect,
         projectee_key: UItemKey,
         projectee_item: &UItem,
     ) {
-        if r_effect.get_category() == ac::effcats::TARGET {
-            if !r_effect.get_stopped_effect_keys().is_empty()
+        if effect.get_category() == ac::effcats::TARGET {
+            if !effect.get_stopped_effect_keys().is_empty()
                 && let Some(projectee_fit_key) = projectee_item.get_fit_key()
             {
                 let projectee_fit_data = self.fit_datas.get_mut(&projectee_fit_key).unwrap();
-                let stopper = EffectSpec::new(projector_key, r_effect.get_key());
-                for stop_a_effect_id in r_effect.get_stopped_effect_keys().iter() {
+                let stopper = EffectSpec::new(projector_key, effect.get_key());
+                for stop_a_effect_id in effect.get_stopped_effect_keys().iter() {
                     let stopped = EffectSpec::new(projectee_key, *stop_a_effect_id);
                     projectee_fit_data.stopped_effects.add_entry(stopped, stopper);
                 }
             }
             if let Some(projector_fit_key) = projector_item.get_fit_key() {
                 let projector_fit_data = self.fit_datas.get_mut(&projector_fit_key).unwrap();
-                let projector_espec = EffectSpec::new(projector_key, r_effect.get_key());
-                if r_effect.is_assist() {
+                let projector_espec = EffectSpec::new(projector_key, effect.get_key());
+                if effect.is_assist() {
                     projector_fit_data
                         .blockable_assistance
                         .add_entry(projectee_key, projector_espec);
                 }
-                if is_offense_blockable(projector_item, r_effect) {
+                if is_offense_blockable(projector_item, effect) {
                     projector_fit_data
                         .blockable_offense
                         .add_entry(projectee_key, projector_espec);
                 }
-                if let Some(resist_a_attr_id) = eff_funcs::get_resist_a_attr_id(projector_item, r_effect) {
+                if let Some(resist_a_attr_id) = eff_funcs::get_resist_a_attr_id(projector_item, effect) {
                     let projectee_aspec = AttrSpec::new(projectee_key, resist_a_attr_id);
                     projector_fit_data
                         .resist_immunity
@@ -47,64 +47,82 @@ impl Vast {
                 }
             }
         }
-        if let Some(rep_getter) = r_effect.get_remote_shield_rep_opc_getter() {
-            self.irr_shield
-                .add_entry(projectee_key, projector_key, r_effect.get_key(), rep_getter);
-            if r_effect.get_charge_info().is_some() {
+        if let Some(rep_getter) = effect.get_remote_shield_rep_opc_getter() {
+            if effect.is_active_with_duration() {
+                self.irr_shield
+                    .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
+            }
+            if effect.get_charge_info().is_some() {
                 self.irr_shield_limitable
-                    .add_entry(projectee_key, projector_key, r_effect.get_key(), rep_getter);
+                    .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
             }
         }
-        if let Some(rep_getter) = r_effect.get_remote_armor_rep_opc_getter() {
-            self.irr_armor
-                .add_entry(projectee_key, projector_key, r_effect.get_key(), rep_getter);
-            if r_effect.get_charge_info().is_some() {
+        if let Some(rep_getter) = effect.get_remote_armor_rep_opc_getter() {
+            if effect.is_active_with_duration() {
+                self.irr_armor
+                    .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
+            }
+            if effect.get_charge_info().is_some() {
                 self.irr_armor_limitable
-                    .add_entry(projectee_key, projector_key, r_effect.get_key(), rep_getter);
+                    .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
             }
         }
-        if let Some(rep_getter) = r_effect.get_remote_hull_rep_opc_getter() {
+        if let Some(rep_getter) = effect.get_remote_hull_rep_opc_getter()
+            && effect.is_active_with_duration()
+        {
             self.irr_hull
-                .add_entry(projectee_key, projector_key, r_effect.get_key(), rep_getter);
+                .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
         }
-        if let Some(ecm_getter) = r_effect.get_ecm_opc_getter() {
+        if let Some(rep_getter) = effect.get_remote_cap_rep_opc_getter()
+            && effect.is_active_with_duration()
+        {
+            self.in_cap
+                .add_entry(projectee_key, projector_key, effect.get_key(), rep_getter);
+        }
+        if let Some(neut_getter) = effect.get_neut_opc_getter()
+            && effect.is_active_with_duration()
+        {
+            self.in_neuts
+                .add_entry(projectee_key, projector_key, effect.get_key(), neut_getter);
+        }
+        if let Some(ecm_getter) = effect.get_ecm_opc_getter() {
             self.in_ecm
-                .add_entry(projectee_key, projector_key, r_effect.get_key(), ecm_getter);
+                .add_entry(projectee_key, projector_key, effect.get_key(), ecm_getter);
         }
     }
     pub(in crate::svc) fn effect_unprojected(
         &mut self,
         projector_key: UItemKey,
         projector_item: &UItem,
-        r_effect: &REffect,
+        effect: &REffect,
         projectee_key: UItemKey,
         projectee_item: &UItem,
     ) {
-        if r_effect.get_category() == ac::effcats::TARGET {
-            if !r_effect.get_stopped_effect_keys().is_empty()
+        if effect.get_category() == ac::effcats::TARGET {
+            if !effect.get_stopped_effect_keys().is_empty()
                 && let Some(projectee_fit_key) = projectee_item.get_fit_key()
             {
                 let projectee_fit_data = self.fit_datas.get_mut(&projectee_fit_key).unwrap();
-                let stopper = EffectSpec::new(projector_key, r_effect.get_key());
-                for stop_a_effect_id in r_effect.get_stopped_effect_keys().iter() {
+                let stopper = EffectSpec::new(projector_key, effect.get_key());
+                for stop_a_effect_id in effect.get_stopped_effect_keys().iter() {
                     let stopped = EffectSpec::new(projectee_key, *stop_a_effect_id);
                     projectee_fit_data.stopped_effects.remove_entry(&stopped, &stopper);
                 }
             }
             if let Some(projector_fit_key) = projector_item.get_fit_key() {
                 let projector_fit_data = self.fit_datas.get_mut(&projector_fit_key).unwrap();
-                let projector_espec = EffectSpec::new(projector_key, r_effect.get_key());
-                if r_effect.is_assist() {
+                let projector_espec = EffectSpec::new(projector_key, effect.get_key());
+                if effect.is_assist() {
                     projector_fit_data
                         .blockable_assistance
                         .remove_entry(&projectee_key, &projector_espec);
                 }
-                if is_offense_blockable(projector_item, r_effect) {
+                if is_offense_blockable(projector_item, effect) {
                     projector_fit_data
                         .blockable_offense
                         .remove_entry(&projectee_key, &projector_espec);
                 }
-                if let Some(resist_a_attr_id) = eff_funcs::get_resist_a_attr_id(projector_item, r_effect) {
+                if let Some(resist_a_attr_id) = eff_funcs::get_resist_a_attr_id(projector_item, effect) {
                     let projectee_aspec = AttrSpec::new(projectee_key, resist_a_attr_id);
                     projector_fit_data
                         .resist_immunity
@@ -112,29 +130,39 @@ impl Vast {
                 }
             }
         }
-        if r_effect.get_remote_shield_rep_opc_getter().is_some() {
-            self.irr_shield
-                .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
-            if r_effect.get_charge_info().is_some() {
+        if effect.get_remote_shield_rep_opc_getter().is_some() {
+            if effect.is_active_with_duration() {
+                self.irr_shield
+                    .remove_l3(&projectee_key, &projector_key, &effect.get_key());
+            }
+            if effect.get_charge_info().is_some() {
                 self.irr_shield_limitable
-                    .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
+                    .remove_l3(&projectee_key, &projector_key, &effect.get_key());
             }
         }
-        if r_effect.get_remote_armor_rep_opc_getter().is_some() {
-            self.irr_armor
-                .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
-            if r_effect.get_charge_info().is_some() {
+        if effect.get_remote_armor_rep_opc_getter().is_some() {
+            if effect.is_active_with_duration() {
+                self.irr_armor
+                    .remove_l3(&projectee_key, &projector_key, &effect.get_key());
+            }
+            if effect.get_charge_info().is_some() {
                 self.irr_armor_limitable
-                    .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
+                    .remove_l3(&projectee_key, &projector_key, &effect.get_key());
             }
         }
-        if r_effect.get_remote_hull_rep_opc_getter().is_some() {
+        if effect.get_remote_hull_rep_opc_getter().is_some() && effect.is_active_with_duration() {
             self.irr_hull
-                .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
+                .remove_l3(&projectee_key, &projector_key, &effect.get_key());
         }
-        if r_effect.get_ecm_opc_getter().is_some() {
-            self.in_ecm
-                .remove_l3(&projectee_key, &projector_key, &r_effect.get_key());
+        if effect.get_remote_cap_rep_opc_getter().is_some() && effect.is_active_with_duration() {
+            self.in_cap.remove_l3(&projectee_key, &projector_key, &effect.get_key());
+        }
+        if effect.get_neut_opc_getter().is_some() && effect.is_active_with_duration() {
+            self.in_neuts
+                .remove_l3(&projectee_key, &projector_key, &effect.get_key());
+        }
+        if effect.get_ecm_opc_getter().is_some() {
+            self.in_ecm.remove_l3(&projectee_key, &projector_key, &effect.get_key());
         }
     }
 }
