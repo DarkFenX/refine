7286ed27e7ced8d1616018477b4f53755212cc0e
DarkPhoenix <phoenix@mail.ru>
2025-11-01 22:50:57 +0100

    Implement incoming cap transfers and neuts in cap balance stats

--- core/src/svc/vast/vaste_stats_item/cap/vaste_cap_balance.rs
+++ core/src/svc/vast/vaste_stats_item/cap/vaste_cap_balance.rs
@@ -56,16 +56,6 @@ pub struct StatCapConsumerOptions {
     pub reload: bool = false,
 }
 
-const CYCLE_OPTIONS_SIM: CycleOptions = CycleOptions {
-    reload_mode: CycleOptionReload::Sim,
-    reload_optionals: true,
-};
-
-const CYCLE_OPTIONS_BURST: CycleOptions = CycleOptions {
-    reload_mode: CycleOptionReload::Burst,
-    reload_optionals: false,
-};
-
 impl Vast {
     pub(in crate::svc) fn get_stat_item_cap_balance(
         &self,
@@ -79,90 +69,148 @@ impl Vast {
         let fit_data = self.fit_datas.get(&item.get_ship().unwrap().get_fit_key()).unwrap();
         let mut balance = OF(0.0);
         if src_kinds.regen.enabled {
-            balance += Vast::internal_get_stat_item_cap_regen_unchecked(ctx, calc, item_key, src_kinds.regen.cap_perc);
+            balance += get_cap_regen(ctx, calc, item_key, src_kinds.regen.cap_perc);
         }
         if src_kinds.cap_boosters {
-            balance += Vast::internal_get_stat_item_cap_boosts_unchecked(ctx, calc, &fit_data);
+            balance += get_cap_boosts(ctx, calc, &fit_data);
         }
         if src_kinds.consumers.enabled {
-            balance -=
-                Vast::internal_get_stat_item_cap_consumed_unchecked(ctx, calc, src_kinds.consumers.reload, &fit_data);
+            balance -= get_cap_consumed(ctx, calc, src_kinds.consumers.reload, &fit_data);
+        }
+        if src_kinds.incoming_transfers {
+            balance += get_cap_transfers(ctx, calc, item_key, self);
+        }
+        if src_kinds.incoming_neuts {
+            balance -= get_neuts(ctx, calc, item_key, self);
         }
         Ok(balance)
     }
-    fn internal_get_stat_item_cap_regen_unchecked(
-        ctx: SvcCtx,
-        calc: &mut Calc,
-        item_key: UItemKey,
-        cap_perc: Option<AttrVal>,
-    ) -> AttrVal {
-        let max_amount = Vast::internal_get_stat_item_cap_unchecked(ctx, calc, item_key);
-        let cap_regen_time = calc
-            .get_item_attr_val_extra(ctx, item_key, &ac::attrs::RECHARGE_RATE)
-            .unwrap()
-            / OF(1000.0);
-        let cap_perc = match cap_perc {
-            Some(cap_perc) => cap_perc.clamp(OF(0.0), OF(1.0)),
-            None => OF(0.25),
-        };
-        let result = OF(10.0) * max_amount / cap_regen_time * (OF(cap_perc.sqrt()) - cap_perc);
-        match result.is_finite() {
-            true => result,
-            false => OF(0.0),
-        }
+}
+
+const CYCLE_OPTIONS_SIM: CycleOptions = CycleOptions {
+    reload_mode: CycleOptionReload::Sim,
+    reload_optionals: true,
+};
+const CYCLE_OPTIONS_BURST: CycleOptions = CycleOptions {
+    reload_mode: CycleOptionReload::Burst,
+    reload_optionals: false,
+};
+
+fn get_cap_regen(ctx: SvcCtx, calc: &mut Calc, item_key: UItemKey, cap_perc: Option<AttrVal>) -> AttrVal {
+    let max_amount = Vast::internal_get_stat_item_cap_unchecked(ctx, calc, item_key);
+    let cap_regen_time = calc
+        .get_item_attr_val_extra(ctx, item_key, &ac::attrs::RECHARGE_RATE)
+        .unwrap()
+        / OF(1000.0);
+    let cap_perc = match cap_perc {
+        Some(cap_perc) => cap_perc.clamp(OF(0.0), OF(1.0)),
+        None => OF(0.25),
+    };
+    let result = OF(10.0) * max_amount / cap_regen_time * (OF(cap_perc.sqrt()) - cap_perc);
+    match result.is_finite() {
+        true => result,
+        false => OF(0.0),
     }
-    fn internal_get_stat_item_cap_boosts_unchecked(ctx: SvcCtx, calc: &mut Calc, fit_data: &VastFitData) -> AttrVal {
-        let mut cps = OF(0.0);
-        for (&item_key, item_data) in fit_data.cap_boosts.iter() {
-            let cycle_map = match get_item_cycle_info(ctx, calc, item_key, CYCLE_OPTIONS_SIM, false) {
-                Some(cycle_map) => cycle_map,
+}
+
+fn get_cap_boosts(ctx: SvcCtx, calc: &mut Calc, fit_data: &VastFitData) -> AttrVal {
+    let mut cps = OF(0.0);
+    for (&item_key, item_data) in fit_data.cap_boosts.iter() {
+        let cycle_map = match get_item_cycle_info(ctx, calc, item_key, CYCLE_OPTIONS_SIM, false) {
+            Some(cycle_map) => cycle_map,
+            None => continue,
+        };
+        for (&effect_key, cap_getter) in item_data.iter() {
+            let output_per_cycle = match cap_getter(ctx, calc, item_key) {
+                Some(output_per_cycle) => output_per_cycle,
                 None => continue,
             };
-            for (&effect_key, rep_getter) in item_data.iter() {
-                let output_per_cycle = match rep_getter(ctx, calc, item_key) {
-                    Some(output_per_cycle) => output_per_cycle,
-                    None => continue,
-                };
-                let effect_cycles = match cycle_map.get(&effect_key) {
-                    Some(effect_cycles) => effect_cycles,
-                    None => continue,
-                };
-                if !effect_cycles.is_infinite() {
-                    continue;
-                }
-                cps += output_per_cycle.get_total() / effect_cycles.get_average_cycle_time();
-            }
-        }
-        cps
-    }
-    fn internal_get_stat_item_cap_consumed_unchecked(
-        ctx: SvcCtx,
-        calc: &mut Calc,
-        reload: bool,
-        fit_data: &VastFitData,
-    ) -> AttrVal {
-        let mut cps = OF(0.0);
-        let cycle_options = match reload {
-            true => CYCLE_OPTIONS_SIM,
-            false => CYCLE_OPTIONS_BURST,
-        };
-        for (&item_key, item_data) in fit_data.cap_consumers.iter() {
-            let cycle_map = match get_item_cycle_info(ctx, calc, item_key, cycle_options, false) {
-                Some(cycle_map) => cycle_map,
+            let effect_cycles = match cycle_map.get(&effect_key) {
+                Some(effect_cycles) => effect_cycles,
                 None => continue,
             };
-            for (&effect_key, attr_id) in item_data.iter() {
-                let cap_used = match calc.get_item_attr_val_extra(ctx, item_key, attr_id) {
-                    Ok(cap_used) => cap_used,
-                    Err(_) => continue,
-                };
-                let effect_cycles = match cycle_map.get(&effect_key) {
-                    Some(effect_cycles) => effect_cycles,
-                    None => continue,
-                };
-                cps += cap_used / effect_cycles.get_average_cycle_time();
-            }
+            cps += output_per_cycle.get_total() / effect_cycles.get_average_cycle_time();
         }
-        cps
     }
+    cps
+}
+
+fn get_cap_consumed(ctx: SvcCtx, calc: &mut Calc, reload: bool, fit_data: &VastFitData) -> AttrVal {
+    let mut cps = OF(0.0);
+    let cycle_options = match reload {
+        true => CYCLE_OPTIONS_SIM,
+        false => CYCLE_OPTIONS_BURST,
+    };
+    for (&item_key, item_data) in fit_data.cap_consumers.iter() {
+        let cycle_map = match get_item_cycle_info(ctx, calc, item_key, cycle_options, false) {
+            Some(cycle_map) => cycle_map,
+            None => continue,
+        };
+        for (&effect_key, attr_id) in item_data.iter() {
+            let cap_used = match calc.get_item_attr_val_extra(ctx, item_key, attr_id) {
+                Ok(cap_used) => cap_used,
+                Err(_) => continue,
+            };
+            let effect_cycles = match cycle_map.get(&effect_key) {
+                Some(effect_cycles) => effect_cycles,
+                None => continue,
+            };
+            cps += cap_used / effect_cycles.get_average_cycle_time();
+        }
+    }
+    cps
+}
+
+fn get_cap_transfers(ctx: SvcCtx, calc: &mut Calc, cap_item_key: UItemKey, vast: &Vast) -> AttrVal {
+    let mut cps = OF(0.0);
+    let transfer_data = match vast.in_cap.get_l1(&cap_item_key) {
+        Some(transfer_data) => transfer_data,
+        None => return cps,
+    };
+    for (&transfer_item_key, item_data) in transfer_data.iter() {
+        let cycle_map = match get_item_cycle_info(ctx, calc, transfer_item_key, CYCLE_OPTIONS_BURST, false) {
+            Some(cycle_map) => cycle_map,
+            None => continue,
+        };
+        for (&effect_key, cap_getter) in item_data.iter() {
+            let effect = ctx.u_data.src.get_effect(effect_key);
+            let output_per_cycle = match cap_getter(ctx, calc, transfer_item_key, effect, None, Some(cap_item_key)) {
+                Some(output_per_cycle) => output_per_cycle,
+                None => continue,
+            };
+            let effect_cycles = match cycle_map.get(&effect_key) {
+                Some(effect_cycles) => effect_cycles,
+                None => continue,
+            };
+            cps += output_per_cycle.get_total() / effect_cycles.get_average_cycle_time();
+        }
+    }
+    cps
+}
+
+fn get_neuts(ctx: SvcCtx, calc: &mut Calc, cap_item_key: UItemKey, vast: &Vast) -> AttrVal {
+    let mut nps = OF(0.0);
+    let transfer_data = match vast.in_neuts.get_l1(&cap_item_key) {
+        Some(transfer_data) => transfer_data,
+        None => return nps,
+    };
+    for (&transfer_item_key, item_data) in transfer_data.iter() {
+        let cycle_map = match get_item_cycle_info(ctx, calc, transfer_item_key, CYCLE_OPTIONS_BURST, false) {
+            Some(cycle_map) => cycle_map,
+            None => continue,
+        };
+        for (&effect_key, cap_getter) in item_data.iter() {
+            let effect = ctx.u_data.src.get_effect(effect_key);
+            let output_per_cycle = match cap_getter(ctx, calc, transfer_item_key, effect, Some(cap_item_key)) {
+                Some(output_per_cycle) => output_per_cycle,
+                None => continue,
+            };
+            let effect_cycles = match cycle_map.get(&effect_key) {
+                Some(effect_cycles) => effect_cycles,
+                None => continue,
+            };
+            nps += output_per_cycle.get_total() / effect_cycles.get_average_cycle_time();
+        }
+    }
+    nps
 }
