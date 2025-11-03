10445a2d877c4839db2103e87a6806abbd89a337
DarkPhoenix <phoenix@mail.ru>
2025-11-03 01:13:04 +0100

    Implement iterator for general cycle enum

--- core/src/svc/cycle/cycle.rs
+++ core/src/svc/cycle/cycle.rs
@@ -29,4 +29,33 @@ impl Cycle {
             Self::Reload2(reload2) => reload2.get_average_cycle_time(),
         }
     }
+    pub(in crate::svc) fn iter_cycles(&self) -> impl Iterator<Item = AttrVal> {
+        match self {
+            Self::Simple(simple) => CycleIter::Simple(simple.iter_cycles()),
+            Self::Reload1(reload1) => CycleIter::Reload1(reload1.iter_cycles()),
+            Self::Reload2(reload2) => CycleIter::Reload2(reload2.iter_cycles()),
+        }
+    }
+}
+
+pub(in crate::svc) enum CycleIter<S, R1, R2> {
+    Simple(S),
+    Reload1(R1),
+    Reload2(R2),
+}
+impl<S, R1, R2> Iterator for CycleIter<S, R1, R2>
+where
+    S: Iterator<Item = AttrVal>,
+    R1: Iterator<Item = AttrVal>,
+    R2: Iterator<Item = AttrVal>,
+{
+    type Item = AttrVal;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::Simple(simple) => simple.next(),
+            Self::Reload1(reload1) => reload1.next(),
+            Self::Reload2(reload2) => reload2.next(),
+        }
+    }
 }
