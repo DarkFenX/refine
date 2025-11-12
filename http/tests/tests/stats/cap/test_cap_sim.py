d883f45588b2fd8174244594115c4f10335d8e86
DarkPhoenix <phoenix@mail.ru>
2025-11-12 19:31:09 +0100

    Add neut staggering test

--- http/tests/tests/stats/cap/test_cap_sim.py
+++ http/tests/tests/stats/cap/test_cap_sim.py
@@ -138,6 +138,47 @@ def test_stability_only_transfers(client, consts):
         {consts.ApiCapSimResult.stable: 1}]
 
 
+def test_stagger_neuts(client, consts):
+    eve_ship_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.capacitor_capacity)
+    eve_regen_attr_id = client.mk_eve_attr(id_=consts.EveAttr.recharge_rate)
+    eve_neut_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.energy_neut_amount)
+    eve_sig_radius_attr_id = client.mk_eve_attr(id_=consts.EveAttr.sig_radius)
+    eve_cycle_time_attr_id = client.mk_eve_attr()
+    eve_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.energy_neut_falloff,
+        cat_id=consts.EveEffCat.target,
+        duration_attr_id=eve_cycle_time_attr_id)
+    eve_module_id = client.mk_eve_item(
+        attrs={eve_neut_amount_attr_id: 120, eve_cycle_time_attr_id: 10000},
+        eff_ids=[eve_effect_id],
+        defeff_id=eve_effect_id)
+    eve_ship_id = client.mk_eve_ship(attrs={
+        eve_ship_amount_attr_id: 1812.5,
+        eve_regen_attr_id: 93750,
+        eve_sig_radius_attr_id: 1})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_ship_id)
+    for _ in range(4):
+        api_src_module = api_src_fit.add_module(type_id=eve_module_id, state=consts.ApiModuleState.active)
+        api_src_module.change_module(add_projs=[api_tgt_ship.id])
+    # Verification - when neuts are applied together, they break through peak regen, but when
+    # staggered, they do not
+    api_options = [StatsOptionCapSim(), StatsOptionCapSim(stagger=True), StatsOptionCapSim(stagger=False)]
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(cap_sim=(True, api_options)))
+    assert api_tgt_fit_stats.cap_sim == [
+        {consts.ApiCapSimResult.time: approx(390)},
+        {consts.ApiCapSimResult.stable: approx(0.2891368)},
+        {consts.ApiCapSimResult.time: approx(390)}]
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(cap_sim=(True, api_options)))
+    assert api_tgt_ship_stats.cap_sim == [
+        {consts.ApiCapSimResult.time: approx(390)},
+        {consts.ApiCapSimResult.stable: approx(0.2891368)},
+        {consts.ApiCapSimResult.time: approx(390)}]
+
+
 def test_injector_emergency(client, consts):
     eve_ship_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.capacitor_capacity)
     eve_boost_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.capacitor_bonus)
