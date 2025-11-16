f76302c7f21feb480c428d4629fb39b0ad0f2b6f
DarkPhoenix <phoenix@mail.ru>
2025-11-16 02:15:55 +0100

    Add weapon disruption modifiers which affect drones and fighters

--- core/src/nd/eff/shared/wd.rs
+++ core/src/nd/eff/shared/wd.rs
@@ -1,5 +1,3 @@
-// TODO: add anti-drone/fighter/npc TD and GD modifiers
-
 use crate::{
     ac,
     ad::{AAttrId, AEffect, AEffectAffecteeFilter, AEffectId, AEffectLocation, AEffectModifier, AModifierSrq, AOp},
@@ -32,22 +30,70 @@ pub(in crate::nd::eff) fn update_effect_wd(a_effect_id: AEffectId, a_effect: &mu
 
 fn add_td_modifiers(mods: &mut Vec<AEffectModifier>) {
     mods.extend([
-        make_td_mod(ac::attrs::MAX_RANGE_BONUS, ac::attrs::MAX_RANGE),
-        make_td_mod(ac::attrs::FALLOFF_BONUS, ac::attrs::FALLOFF),
-        make_td_mod(ac::attrs::TRACKING_SPEED_BONUS, ac::attrs::TRACKING_SPEED),
+        // Modules
+        make_td_loc_mod(ac::attrs::MAX_RANGE_BONUS, ac::attrs::MAX_RANGE),
+        make_td_loc_mod(ac::attrs::FALLOFF_BONUS, ac::attrs::FALLOFF),
+        make_td_loc_mod(ac::attrs::TRACKING_SPEED_BONUS, ac::attrs::TRACKING_SPEED),
+        // Drones & NPCs
+        make_direct_mod(ac::attrs::MAX_RANGE_BONUS, ac::attrs::MAX_RANGE),
+        make_direct_mod(ac::attrs::FALLOFF_BONUS, ac::attrs::FALLOFF),
+        make_direct_mod(ac::attrs::TRACKING_SPEED_BONUS, ac::attrs::TRACKING_SPEED),
+        // Fighters - explosion radius is actually getting improved, which is a bug on CCP side
+        make_direct_mod(
+            ac::attrs::MAX_RANGE_BONUS,
+            ac::attrs::FTR_ABIL_ATK_MISSILE_RANGE_OPTIMAL,
+        ),
+        make_direct_mod(ac::attrs::FALLOFF_BONUS, ac::attrs::FTR_ABIL_ATK_MISSILE_RANGE_FALLOFF),
+        make_direct_mod(
+            ac::attrs::TRACKING_SPEED_BONUS,
+            ac::attrs::FTR_ABIL_ATK_MISSILE_EXPLOSION_RADIUS,
+        ),
+        make_direct_mod(
+            ac::attrs::TRACKING_SPEED_BONUS,
+            ac::attrs::FTR_ABIL_ATK_MISSILE_EXPLOSION_VELOCITY,
+        ),
     ]);
 }
 
 fn add_gd_modifiers(mods: &mut Vec<AEffectModifier>) {
     mods.extend([
-        make_gd_mod(ac::attrs::MISSILE_VELOCITY_BONUS, ac::attrs::MAX_VELOCITY),
-        make_gd_mod(ac::attrs::EXPLOSION_DELAY_BONUS, ac::attrs::EXPLOSION_DELAY),
-        make_gd_mod(ac::attrs::AOE_CLOUD_SIZE_BONUS, ac::attrs::AOE_CLOUD_SIZE),
-        make_gd_mod(ac::attrs::AOE_VELOCITY_BONUS, ac::attrs::AOE_VELOCITY),
+        // Modules
+        make_gd_loc_mod(ac::attrs::MISSILE_VELOCITY_BONUS, ac::attrs::MAX_VELOCITY),
+        make_gd_loc_mod(ac::attrs::EXPLOSION_DELAY_BONUS, ac::attrs::EXPLOSION_DELAY),
+        make_gd_loc_mod(ac::attrs::AOE_CLOUD_SIZE_BONUS, ac::attrs::AOE_CLOUD_SIZE),
+        make_gd_loc_mod(ac::attrs::AOE_VELOCITY_BONUS, ac::attrs::AOE_VELOCITY),
+        // NPCs
+        make_direct_mod(
+            ac::attrs::MISSILE_VELOCITY_BONUS,
+            ac::attrs::MISSILE_ENTITY_VELOCITY_MULTIPLIER,
+        ),
+        make_direct_mod(
+            ac::attrs::EXPLOSION_DELAY_BONUS,
+            ac::attrs::MISSILE_ENTITY_FLIGHT_TIME_MULTIPLIER,
+        ),
+        make_direct_mod(
+            ac::attrs::AOE_CLOUD_SIZE_BONUS,
+            ac::attrs::MISSILE_ENTITY_AOE_CLOUD_SIZE_MULTIPLIER,
+        ),
+        make_direct_mod(
+            ac::attrs::AOE_VELOCITY_BONUS,
+            ac::attrs::MISSILE_ENTITY_AOE_VELOCITY_MULTIPLIER,
+        ),
+        // Fighters
+        make_direct_mod(ac::attrs::MISSILE_VELOCITY_BONUS, ac::attrs::FTR_ABIL_MISSILES_RANGE),
+        make_direct_mod(ac::attrs::EXPLOSION_DELAY_BONUS, ac::attrs::FTR_ABIL_MISSILES_RANGE),
+        make_direct_mod(
+            ac::attrs::AOE_CLOUD_SIZE_BONUS,
+            ac::attrs::FTR_ABIL_MISSILES_EXPLOSION_RADIUS,
+        ),
+        make_direct_mod(
+            ac::attrs::AOE_VELOCITY_BONUS,
+            ac::attrs::FTR_ABIL_MISSILES_EXPLOSION_VELOCITY,
+        ),
     ]);
 }
 
-fn make_td_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectModifier {
+fn make_td_loc_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectModifier {
     AEffectModifier {
         affector_attr_id,
         op: AOp::PostPerc,
@@ -59,11 +105,23 @@ fn make_td_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectM
     }
 }
 
-fn make_gd_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectModifier {
+fn make_gd_loc_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectModifier {
     AEffectModifier {
         affector_attr_id,
         op: AOp::PostPerc,
-        affectee_filter: AEffectAffecteeFilter::OwnSrq(AModifierSrq::ItemId(ac::items::MISSILE_LAUNCHER_OPERATION)),
+        affectee_filter: AEffectAffecteeFilter::LocSrq(
+            AEffectLocation::Target,
+            AModifierSrq::ItemId(ac::items::MISSILE_LAUNCHER_OPERATION),
+        ),
+        affectee_attr_id,
+    }
+}
+
+fn make_direct_mod(affector_attr_id: AAttrId, affectee_attr_id: AAttrId) -> AEffectModifier {
+    AEffectModifier {
+        affector_attr_id,
+        op: AOp::PostPerc,
+        affectee_filter: AEffectAffecteeFilter::Direct(AEffectLocation::Target),
         affectee_attr_id,
     }
 }
