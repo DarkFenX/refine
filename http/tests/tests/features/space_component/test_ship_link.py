9434a4d285511cfd9aa33330766d85be49737fdd
DarkPhoenix <phoenix@mail.ru>
2025-11-24 12:45:32 +0100

    Add space component-specific item list filter tests, and add system-wide effect space component tests

--- http/tests/tests/features/space_component/test_ship_link.py
+++ http/tests/tests/features/space_component/test_ship_link.py
@@ -10,7 +10,35 @@ define if an item can receive ship link effect or not.
 from tests import Effect, approx
 
 
-def test_affectee_filter_item(client, consts):
+def test_affectee_filter_item_list(client, consts):
+    eve_attr_id = client.mk_eve_attr()
+    eve_buff_id = client.mk_eve_buff(
+        aggr_mode=consts.EveBuffAggrMode.max,
+        op=consts.EveBuffOp.post_percent,
+        item_mods=[client.mk_eve_buff_mod(attr_id=eve_attr_id)])
+    eve_beacon_id = client.mk_eve_item()
+    eve_ship_id = client.mk_eve_ship(attrs={eve_attr_id: 200})
+    eve_struct_id = client.mk_eve_struct(attrs={eve_attr_id: 200})
+    eve_drone_id = client.mk_eve_drone(attrs={eve_attr_id: 200})
+    eve_item_list_id = client.mk_eve_item_list(inc_type_ids=[eve_struct_id, eve_drone_id])
+    client.mk_eve_space_comp(type_id=eve_beacon_id, sl_buffs=({eve_buff_id: 10}, eve_item_list_id))
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_sol.add_sw_effect(type_id=eve_beacon_id)
+    api_fit = api_sol.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship_id)
+    api_drone = api_fit.add_drone(type_id=eve_drone_id)
+    # Verification
+    assert api_ship.update().attrs[eve_attr_id].dogma == approx(200)
+    assert api_drone.update().attrs[eve_attr_id].dogma == approx(220)
+    # Action
+    api_struct = api_fit.set_ship(type_id=eve_struct_id)
+    # Verification
+    assert api_struct.update().attrs[eve_attr_id].dogma == approx(220)
+    assert api_drone.update().attrs[eve_attr_id].dogma == approx(220)
+
+
+def test_affectee_filter_unspecified(client, consts):
     eve_attr_id = client.mk_eve_attr()
     eve_buff_id = client.mk_eve_buff(
         aggr_mode=consts.EveBuffAggrMode.max,
