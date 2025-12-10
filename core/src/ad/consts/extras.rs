bd6d06ad07262fa1165c3d840d59cdd3d0d52c4b
DarkPhoenix <phoenix@mail.ru>
2025-12-10 12:22:36 +0100

    Put both buff merge attr sets close to each other

--- core/src/ad/consts/extras.rs
+++ core/src/ad/consts/extras.rs
@@ -1,4 +1,4 @@
-pub(crate) use crate::ec::extras::AU;
+pub(crate) use crate::ec::extras::{AU, BUFF_MERGE_ATTRS};
 use crate::{
     ad::{AAttrId, AAttrVal, consts::attrs},
     def::OF,
@@ -6,12 +6,5 @@ use crate::{
 
 pub(crate) const LIMITED_PRECISION_ATTR_IDS: [AAttrId; 4] =
     [attrs::CPU, attrs::POWER, attrs::CPU_OUTPUT, attrs::POWER_OUTPUT];
-
-pub(crate) const BUFF_MERGE_ATTRS: [(AAttrId, AAttrId); 4] = [
-    (attrs::WARFARE_BUFF1_ID, attrs::WARFARE_BUFF1_VAL),
-    (attrs::WARFARE_BUFF2_ID, attrs::WARFARE_BUFF2_VAL),
-    (attrs::WARFARE_BUFF3_ID, attrs::WARFARE_BUFF3_VAL),
-    (attrs::WARFARE_BUFF4_ID, attrs::WARFARE_BUFF4_VAL),
-];
 
 pub(crate) const MAX_SUBCAP_MODULE_VOLUME: AAttrVal = OF(3500.0);
--- core/src/ed/consts/extras.rs
+++ core/src/ed/consts/extras.rs
@@ -10,5 +10,11 @@ pub(crate) const BUFF_MERGE_ATTR_IDS: [EAttrId; 4] = [
     attrs::WARFARE_BUFF3_ID,
     attrs::WARFARE_BUFF4_ID,
 ];
+pub(crate) const BUFF_MERGE_ATTRS: [(EAttrId, EAttrId); 4] = [
+    (attrs::WARFARE_BUFF1_ID, attrs::WARFARE_BUFF1_VAL),
+    (attrs::WARFARE_BUFF2_ID, attrs::WARFARE_BUFF2_VAL),
+    (attrs::WARFARE_BUFF3_ID, attrs::WARFARE_BUFF3_VAL),
+    (attrs::WARFARE_BUFF4_ID, attrs::WARFARE_BUFF4_VAL),
+];
 
 pub(crate) const AU: AttrVal = OF(149_597_870_700.0);
