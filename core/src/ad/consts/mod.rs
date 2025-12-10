e54fbcd09521019960d1e024cf4b1047c99cc25e
DarkPhoenix <phoenix@mail.ru>
2025-12-10 12:46:21 +0100

    Move all extra constants to eve data module

--- core/src/ad/consts/mod.rs
+++ core/src/ad/consts/mod.rs
@@ -1,7 +1,6 @@
 //! Adapted data constants.
 
-pub(crate) use crate::ed::consts::{attrs, buffs, effcats, itemcats, itemgrps, items};
+pub(crate) use crate::ed::consts::{attrs, buffs, effcats, extras, itemcats, itemgrps, items};
 
 pub(crate) mod effects;
-pub(crate) mod extras;
 pub(crate) mod itemlists;
