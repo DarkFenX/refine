2db44ee204bc1e9077344aeebafa89d4f5d91b55
DarkPhoenix <phoenix@mail.ru>
2025-12-26 21:53:46 +0100

    Finalize implementation of "sustained" aggregator

--- core/src/svc/aggr/local_looped.rs
+++ core/src/svc/aggr/local_looped.rs
@@ -25,29 +25,29 @@ where
 {
     match ospec.charge_mult {
         Some(charge_mult_getter) => {
-            aggr_local_time_chargedness(ctx, calc, item_key, effect, cycle.into(), ospec, charge_mult_getter)
+            aggr_local_time_charge(ctx, calc, item_key, effect, cycle.into(), ospec, charge_mult_getter)
         }
         None => aggr_local_time(ctx, calc, item_key, effect, cycle.into(), ospec),
     }
 }
 
-fn aggr_local_time_chargedness<T>(
+fn aggr_local_time_charge<T>(
     ctx: SvcCtx,
     calc: &mut Calc,
     item_key: UItemKey,
     effect: &REffect,
-    cycle: CycleSeq<CycleDataTimeCharge>,
+    cseq: CycleSeq<CycleDataTimeCharge>,
     ospec: &REffectLocalOpcSpec<T>,
     charge_mult_getter: NChargeMultGetter,
 ) -> Option<T>
 where
     T: Copy + Aggregable,
 {
-    let cycle_loop = cycle.try_loop_cseq()?;
+    let cseq = cseq.try_loop_cseq()?;
     let inv_data = try_make_local_inv_data(ctx, calc, item_key, effect, ospec)?;
     let mut output = T::default();
     let mut time = OF(0.0);
-    for cycle_part in cycle_loop.iter_cseq_parts() {
+    for cycle_part in cseq.iter_cseq_parts() {
         let mut part_output = inv_data.base.instance_sum() * OF::from(cycle_part.repeat_count);
         if let Some(chargedness) = cycle_part.data.chargedness
             && let Some(charge_mult) = charge_mult_getter(ctx, calc, item_key, chargedness)
@@ -57,7 +57,7 @@ where
         output += part_output;
         time += cycle_part.data.time;
     }
-    None
+    Some(output / time)
 }
 
 fn aggr_local_time<T>(
@@ -65,14 +65,19 @@ fn aggr_local_time<T>(
     calc: &mut Calc,
     item_key: UItemKey,
     effect: &REffect,
-    cycle: CycleSeq<CycleDataTime>,
+    cseq: CycleSeq<CycleDataTime>,
     ospec: &REffectLocalOpcSpec<T>,
 ) -> Option<T>
 where
     T: Copy + Aggregable,
 {
-    let cycle_loop = cycle.try_loop_cseq()?;
+    let cseq = cseq.try_loop_cseq()?;
     let inv_data = try_make_local_inv_data(ctx, calc, item_key, effect, ospec)?;
-    for cycle_part in cycle_loop.iter_cseq_parts() {}
-    None
+    let mut output = T::default();
+    let mut time = OF(0.0);
+    for cycle_part in cseq.iter_cseq_parts() {
+        output += inv_data.base.instance_sum() * OF::from(cycle_part.repeat_count);
+        time += cycle_part.data.time;
+    }
+    Some(output / time)
 }
--- core/src/svc/aggr/traits/aggregable.rs
+++ core/src/svc/aggr/traits/aggregable.rs
@@ -1,6 +1,11 @@
 use crate::{def::AttrVal, util::Limit};
 
 pub(in crate::svc::aggr) trait Aggregable:
-    Default + std::ops::AddAssign<Self> + std::ops::Mul<AttrVal, Output = Self> + std::ops::MulAssign<AttrVal> + Limit
+    Default
+    + std::ops::AddAssign<Self>
+    + std::ops::Mul<AttrVal, Output = Self>
+    + std::ops::MulAssign<AttrVal>
+    + std::ops::Div<AttrVal, Output = Self>
+    + Limit
 {
 }
