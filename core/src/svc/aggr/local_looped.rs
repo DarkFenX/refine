c038cb6dcbbc61220560f94eed257ac42cce402e
DarkPhoenix <phoenix@mail.ru>
2025-12-27 23:52:52 +0100

    Merge similar methods back together, not to overduplicate the code

--- core/src/svc/aggr/local_looped.rs
+++ core/src/svc/aggr/local_looped.rs
@@ -1,13 +1,8 @@
 use super::{local_inv_data::LocalInvariantData, traits::Aggregable};
 use crate::{
     def::{AttrVal, OF},
-    nd::NChargeMultGetter,
     rd::{REffect, REffectLocalOpcSpec},
-    svc::{
-        SvcCtx,
-        calc::Calc,
-        cycle::{CycleDataTime, CycleDataTimeCharge, CycleSeq},
-    },
+    svc::{SvcCtx, calc::Calc, cycle::CycleSeq},
     ud::UItemKey,
 };
 
@@ -23,73 +18,26 @@ pub(in crate::svc) fn aggr_local_looped_per_second<T>(
 where
     T: Copy + Aggregable,
 {
-    match ospec.charge_mult {
-        Some(charge_mult_getter) => {
-            aggr_local_time_charge(ctx, calc, item_key, effect, cseq.into(), ospec, charge_mult_getter)
-        }
-        None => aggr_local_time(ctx, calc, item_key, effect, cseq.into(), ospec),
-    }
-}
-
-fn aggr_local_time_charge<T>(
-    ctx: SvcCtx,
-    calc: &mut Calc,
-    item_key: UItemKey,
-    effect: &REffect,
-    cseq: CycleSeq<CycleDataTimeCharge>,
-    ospec: &REffectLocalOpcSpec<T>,
-    charge_mult_getter: NChargeMultGetter,
-) -> Option<T>
-where
-    T: Copy + Aggregable,
-{
     let cseq = cseq.try_loop_cseq()?;
     let inv_local = LocalInvariantData::try_make(ctx, calc, item_key, effect, ospec)?;
     let mut value = T::default();
     let mut time = OF(0.0);
     for cycle_part in cseq.iter_cseq_parts() {
-        let cycle_repeat_count = AttrVal::from(cycle_part.repeat_count);
-        // Value
         let mut part_output = inv_local.output;
-        if let Some(chargedness) = cycle_part.data.chargedness
+        // Chargedness
+        if let Some(charge_mult_getter) = ospec.charge_mult
+            && let Some(chargedness) = cycle_part.data.chargedness
             && let Some(charge_mult) = charge_mult_getter(ctx, calc, item_key, chargedness)
         {
             part_output *= charge_mult;
         }
+        // Limit
         if let Some(limit) = inv_local.amount_limit {
             part_output.limit_amount(limit);
         }
-        value += part_output.instance_sum() * cycle_repeat_count;
-        // Time
-        time += cycle_part.data.time * cycle_repeat_count;
-    }
-    Some(value / time)
-}
-
-fn aggr_local_time<T>(
-    ctx: SvcCtx,
-    calc: &mut Calc,
-    item_key: UItemKey,
-    effect: &REffect,
-    cseq: CycleSeq<CycleDataTime>,
-    ospec: &REffectLocalOpcSpec<T>,
-) -> Option<T>
-where
-    T: Copy + Aggregable,
-{
-    let cseq = cseq.try_loop_cseq()?;
-    let inv_local = LocalInvariantData::try_make(ctx, calc, item_key, effect, ospec)?;
-    let mut value = T::default();
-    let mut time = OF(0.0);
-    for cycle_part in cseq.iter_cseq_parts() {
+        // Update total values
         let cycle_repeat_count = AttrVal::from(cycle_part.repeat_count);
-        // Value
-        let mut part_output = inv_local.output;
-        if let Some(limit) = inv_local.amount_limit {
-            part_output.limit_amount(limit);
-        }
         value += part_output.instance_sum() * cycle_repeat_count;
-        // Time
         time += cycle_part.data.time * cycle_repeat_count;
     }
     Some(value / time)
--- core/src/svc/aggr/proj_looped.rs
+++ core/src/svc/aggr/proj_looped.rs
@@ -4,12 +4,11 @@ use super::{
 };
 use crate::{
     def::{AttrVal, Count, OF},
-    nd::NChargeMultGetter,
     rd::{REffect, REffectProjOpcSpec},
     svc::{
         SvcCtx,
         calc::Calc,
-        cycle::{CycleDataTime, CycleDataTimeCharge, CycleDataTimeInt, CycleSeq, CycleSeqLooped},
+        cycle::{CycleDataFull, CycleDataTimeCharge, CycleSeq, CycleSeqLooped},
     },
     ud::UItemKey,
 };
@@ -27,40 +26,9 @@ pub(in crate::svc) fn aggr_proj_looped_per_second<T>(
 where
     T: Copy + Aggregable,
 {
-    let inv_spool = SpoolInvariantData::try_make(ctx, calc, projector_key, effect, ospec);
-    // Spool needs interruption data, charge multiplier needs chargedness data
-    match (inv_spool, ospec.charge_mult) {
-        (Some(inv_spool), Some(charge_mult_getter)) => aggr_proj_chargedness(
-            ctx,
-            calc,
-            projector_key,
-            effect,
-            cseq.into(),
-            ospec,
-            projectee_key,
-            charge_mult_getter,
-        ),
-        (Some(inv_spool), None) => aggr_proj_spool(
-            ctx,
-            calc,
-            projector_key,
-            effect,
-            cseq.into(),
-            ospec,
-            projectee_key,
-            inv_spool,
-        ),
-        (None, Some(charge_mult_getter)) => aggr_proj_chargedness(
-            ctx,
-            calc,
-            projector_key,
-            effect,
-            cseq.into(),
-            ospec,
-            projectee_key,
-            charge_mult_getter,
-        ),
-        (None, None) => aggr_proj_basic(ctx, calc, projector_key, effect, cseq.into(), ospec, projectee_key),
+    match SpoolInvariantData::try_make(ctx, calc, projector_key, effect, ospec) {
+        Some(inv_spool) => aggr_proj_spool(ctx, calc, projector_key, effect, cseq, ospec, projectee_key, inv_spool),
+        None => aggr_proj_regular(ctx, calc, projector_key, effect, cseq.into(), ospec, projectee_key),
     }
 }
 
@@ -69,7 +37,7 @@ fn aggr_proj_spool<T>(
     calc: &mut Calc,
     projector_key: UItemKey,
     effect: &REffect,
-    cseq: CycleSeq<CycleDataTimeInt>,
+    cseq: &CycleSeq<CycleDataFull>,
     ospec: &REffectProjOpcSpec<T>,
     projectee_key: Option<UItemKey>,
     inv_spool: SpoolInvariantData,
@@ -84,11 +52,23 @@ where
     let mut value = T::default();
     let mut time = OF(0.0);
     'part: for cycle_part in cseq.iter_cseq_parts() {
+        // Calculate chargedness mult once for every part, no need to do it for every cycle
+        let charge_mult = if let Some(charge_mult_getter) = ospec.charge_mult
+            && let Some(chargedness) = cycle_part.data.chargedness
+        {
+            charge_mult_getter(ctx, calc, projector_key, chargedness)
+        } else {
+            None
+        };
         for i in 0..cycle_part.repeat_count {
             let mut part_output = inv_proj.output;
             // Case when the rest of cycle part is at full spool
-            if !cycle_part.data.interrupt && uninterrupted_cycles >= inv_spool.cycles_to_max {
+            if cycle_part.data.interrupt.is_none() && uninterrupted_cycles >= inv_spool.cycles_to_max {
                 let remaining_cycles = cycle_part.repeat_count - i;
+                // Chargedness
+                if let Some(charge_mult) = charge_mult {
+                    part_output *= charge_mult;
+                }
                 // Spool
                 part_output *= OF(1.0) + inv_spool.max;
                 uninterrupted_cycles += remaining_cycles;
@@ -102,11 +82,15 @@ where
                 time += cycle_part.data.time * remaining_cycles;
                 continue 'part;
             }
+            // Chargedness
+            if let Some(charge_mult) = charge_mult {
+                part_output *= charge_mult;
+            }
             // Spool
             part_output *= OF(1.0) + inv_spool.max.min(inv_spool.step * uninterrupted_cycles as f64);
             match cycle_part.data.interrupt {
-                true => uninterrupted_cycles = 0,
-                false => uninterrupted_cycles += 1,
+                Some(_) => uninterrupted_cycles = 0,
+                None => uninterrupted_cycles += 1,
             }
             // Limit
             if let Some(limit) = inv_proj.amount_limit {
@@ -119,16 +103,16 @@ where
     }
     Some(value / time)
 }
-fn get_uninterrupted_cycles(cseq: &CycleSeqLooped<CycleDataTimeInt>, inv_spool: &SpoolInvariantData) -> Count {
+fn get_uninterrupted_cycles(cseq: &CycleSeqLooped<CycleDataFull>, inv_spool: &SpoolInvariantData) -> Count {
     let mut uninterrupted_cycles = 0;
     let mut interruptions = false;
     for cycle_part in cseq.iter_cseq_parts() {
         match cycle_part.data.interrupt {
-            true => {
+            Some(_) => {
                 uninterrupted_cycles = 0;
                 interruptions = true;
             }
-            false => {
+            None => {
                 uninterrupted_cycles += cycle_part.repeat_count;
             }
         }
@@ -140,7 +124,7 @@ fn get_uninterrupted_cycles(cseq: &CycleSeqLooped<CycleDataTimeInt>, inv_spool: 
     uninterrupted_cycles
 }
 
-fn aggr_proj_chargedness<T>(
+fn aggr_proj_regular<T>(
     ctx: SvcCtx,
     calc: &mut Calc,
     projector_key: UItemKey,
@@ -148,7 +132,6 @@ fn aggr_proj_chargedness<T>(
     cseq: CycleSeq<CycleDataTimeCharge>,
     ospec: &REffectProjOpcSpec<T>,
     projectee_key: Option<UItemKey>,
-    charge_mult_getter: NChargeMultGetter,
 ) -> Option<T>
 where
     T: Copy + Aggregable,
@@ -159,48 +142,20 @@ where
     let mut time = OF(0.0);
     for cycle_part in cseq.iter_cseq_parts() {
         let cycle_repeat_count = AttrVal::from(cycle_part.repeat_count);
-        // Value
         let mut part_output = inv_proj.output;
-        if let Some(chargedness) = cycle_part.data.chargedness
+        // Chargedness
+        if let Some(charge_mult_getter) = ospec.charge_mult
+            && let Some(chargedness) = cycle_part.data.chargedness
             && let Some(charge_mult) = charge_mult_getter(ctx, calc, projector_key, chargedness)
         {
             part_output *= charge_mult;
         }
+        // Limit
         if let Some(limit) = inv_proj.amount_limit {
             part_output.limit_amount(limit);
         }
+        // Update total values
         value += part_output.instance_sum() * cycle_repeat_count;
-        // Time
-        time += cycle_part.data.time * cycle_repeat_count;
-    }
-    Some(value / time)
-}
-
-fn aggr_proj_basic<T>(
-    ctx: SvcCtx,
-    calc: &mut Calc,
-    projector_key: UItemKey,
-    effect: &REffect,
-    cseq: CycleSeq<CycleDataTime>,
-    ospec: &REffectProjOpcSpec<T>,
-    projectee_key: Option<UItemKey>,
-) -> Option<T>
-where
-    T: Copy + Aggregable,
-{
-    let cseq = cseq.try_loop_cseq()?;
-    let inv_proj = ProjInvariantData::try_make(ctx, calc, projector_key, effect, ospec, projectee_key)?;
-    let mut value = T::default();
-    let mut time = OF(0.0);
-    for cycle_part in cseq.iter_cseq_parts() {
-        let cycle_repeat_count = AttrVal::from(cycle_part.repeat_count);
-        // Value
-        let mut part_output = inv_proj.output;
-        if let Some(limit) = inv_proj.amount_limit {
-            part_output.limit_amount(limit);
-        }
-        value += part_output.instance_sum() * cycle_repeat_count;
-        // Time
         time += cycle_part.data.time * cycle_repeat_count;
     }
     Some(value / time)
