da211087de0c604bc2c2275f02859048427ba8d8
DarkPhoenix <phoenix@mail.ru>
2025-12-27 01:55:09 +0100

    Add a draft of projected looped aggregator, for now without spool support

--- core/src/svc/aggr/mod.rs
+++ core/src/svc/aggr/mod.rs
@@ -4,11 +4,13 @@
 pub(in crate::svc) use local_first::{aggr_local_first, aggr_local_first_per_second};
 pub(in crate::svc) use local_looped::aggr_local_looped_per_second;
 pub(in crate::svc) use proj_first::{aggr_proj_first, aggr_proj_first_per_second};
+pub(in crate::svc) use proj_looped::aggr_proj_looped_per_second;
 
 mod local_first;
 mod local_inv_data;
 mod local_looped;
 mod proj_first;
 mod proj_inv_data;
+mod proj_looped;
 mod shared;
 mod traits;
---
+++ core/src/svc/aggr/proj_looped.rs
@@ -0,0 +1,102 @@
+use super::{proj_inv_data::try_make_proj_inv_data, traits::Aggregable};
+use crate::{
+    def::OF,
+    nd::NChargeMultGetter,
+    rd::{REffect, REffectProjOpcSpec},
+    svc::{SvcCtx, calc::Calc, cycle::CycleSeq},
+    ud::UItemKey,
+};
+
+// Projected effects, considers only infinite parts of cycles
+pub(in crate::svc) fn aggr_proj_looped_per_second<T>(
+    ctx: SvcCtx,
+    calc: &mut Calc,
+    projector_key: UItemKey,
+    effect: &REffect,
+    cseq: &CycleSeq,
+    ospec: &REffectProjOpcSpec<T>,
+    projectee_key: Option<UItemKey>,
+) -> Option<T>
+where
+    T: Copy + Aggregable,
+{
+    match ospec.charge_mult {
+        Some(charge_mult_getter) => aggr_proj_time_charge(
+            ctx,
+            calc,
+            projector_key,
+            effect,
+            cseq.into(),
+            ospec,
+            projectee_key,
+            charge_mult_getter,
+        ),
+        None => aggr_proj_time(ctx, calc, projector_key, effect, cseq.into(), ospec, projectee_key),
+    }
+}
+
+fn aggr_proj_time_charge<T>(
+    ctx: SvcCtx,
+    calc: &mut Calc,
+    projector_key: UItemKey,
+    effect: &REffect,
+    cseq: &CycleSeq,
+    ospec: &REffectProjOpcSpec<T>,
+    projectee_key: Option<UItemKey>,
+    charge_mult_getter: NChargeMultGetter,
+) -> Option<T>
+where
+    T: Copy + Aggregable,
+{
+    let cseq = cseq.try_loop_cseq()?;
+    let inv_data = try_make_proj_inv_data(ctx, calc, projector_key, effect, ospec, projectee_key)?;
+    let mut value = T::default();
+    let mut time = OF(0.0);
+    for cycle_part in cseq.iter_cseq_parts() {
+        let cycle_repeat_count = OF::from(cycle_part.repeat_count);
+        // Value
+        let mut part_output = inv_data.output;
+        if let Some(chargedness) = cycle_part.data.chargedness
+            && let Some(charge_mult) = charge_mult_getter(ctx, calc, projector_key, chargedness)
+        {
+            part_output *= charge_mult;
+        }
+        if let Some(limit) = inv_data.amount_limit {
+            part_output.limit_amount(limit);
+        }
+        value += part_output.instance_sum() * cycle_repeat_count;
+        // Time
+        time += cycle_part.data.time * cycle_repeat_count;
+    }
+    Some(value / time)
+}
+
+fn aggr_proj_time<T>(
+    ctx: SvcCtx,
+    calc: &mut Calc,
+    projector_key: UItemKey,
+    effect: &REffect,
+    cseq: &CycleSeq,
+    ospec: &REffectProjOpcSpec<T>,
+    projectee_key: Option<UItemKey>,
+) -> Option<T>
+where
+    T: Copy + Aggregable,
+{
+    let cseq = cseq.try_loop_cseq()?;
+    let inv_data = try_make_proj_inv_data(ctx, calc, projector_key, effect, ospec, projectee_key)?;
+    let mut value = T::default();
+    let mut time = OF(0.0);
+    for cycle_part in cseq.iter_cseq_parts() {
+        let cycle_repeat_count = OF::from(cycle_part.repeat_count);
+        // Value
+        let mut part_output = inv_data.output;
+        if let Some(limit) = inv_data.amount_limit {
+            part_output.limit_amount(limit);
+        }
+        value += part_output.instance_sum() * cycle_repeat_count;
+        // Time
+        time += cycle_part.data.time * cycle_repeat_count;
+    }
+    Some(value / time)
+}
