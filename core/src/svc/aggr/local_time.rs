bab3b3feee010bdcf92e3865e9bddf504a2d3eb9
DarkPhoenix <phoenix@mail.ru>
2025-12-30 17:41:19 +0100

    Finalize local time aggregator

--- core/src/svc/aggr/local_time.rs
+++ core/src/svc/aggr/local_time.rs
@@ -91,7 +91,42 @@ where
                 process_infinite(&mut total_amount, &mut time, &inner.p3_data)
             }
         }
-        CycleSeq::LoopLimSin(inner) => {}
+        CycleSeq::LoopLimSin(inner) => {
+            if time > OF(0.0) {
+                // Calculate total "tail time" for whole looped sequence. Data format implies that
+                // output can be different, so theoretically tail from first part can be longer than
+                // second part with its tail
+                let full_tail_time = (inner.p1_data.tail_time - inner.p2_data.time).max(inner.p2_data.tail_time);
+                let full_time = inner.p1_data.time * inner.p1_repeat_count as f64 + inner.p2_data.time;
+                // Process full loop repeats
+                let full_repeats = get_count_full_repeats(time, full_time, full_tail_time);
+                total_amount += inner.p1_data.output.get_amount_sum() * AttrVal::from(inner.p1_repeat_count) * full_repeats;
+                total_amount += inner.p2_data.output.get_amount_sum() * full_repeats;
+                time -= full_time * full_repeats;
+                while time >= OF(0.0) {
+                    let mut p1_remaining_repeats = inner.p1_repeat_count;
+                    // Process as many full part 1 repeats as time can fit
+                    let p1_repeats = inner.p1_repeat_count.min(
+                        get_count_full_repeats(time, inner.p1_data.time, inner.p1_data.tail_time).into_inner() as Count,
+                    );
+                    total_amount += inner.p1_data.output.get_amount_sum() * AttrVal::from(p1_repeats);
+                    time -= inner.p1_data.time * AttrVal::from(p1_repeats);
+                    p1_remaining_repeats -= p1_repeats;
+                    // Process partial part 1 repeats
+                    while time >= OF(0.0) && p1_remaining_repeats > 0 {
+                        total_amount += inner.p1_data.output.get_amount_sum_by_time(time);
+                        time -= inner.p1_data.time;
+                    }
+                    // Process partial part 2
+                    if time >= OF(0.0) {
+                        total_amount += inner.p2_data.output.get_amount_sum_by_time(time);
+                        time -= inner.p2_data.time;
+                    }
+                    // Outer while loop is for cases of really long tails, which never happen in EVE
+                    // but can happen in current data format
+                }
+            }
+        }
     }
     Some(total_amount)
 }
@@ -100,7 +135,7 @@ fn process_single<T>(total_amount: &mut T, time: &mut AttrVal, data: &AggrPartDa
 where
     T: Default + Copy + std::ops::AddAssign<T> + std::ops::Mul<AttrVal, Output = T>,
 {
-    match *time > data.time + data.tail_time {
+    match *time >= data.time + data.tail_time {
         true => *total_amount += data.output.get_amount_sum(),
         false => *total_amount += data.output.get_amount_sum_by_time(*time),
     }
@@ -111,7 +146,7 @@ fn process_limited<T>(total_amount: &mut T, time: &mut AttrVal, data: &AggrPartD
 where
     T: Default + Copy + std::ops::AddAssign<T> + std::ops::Mul<AttrVal, Output = T>,
 {
-    let full_repeats = (get_count_full_repeats(*time, data.time, data.tail_time).trunc() as Count).min(repeat_count);
+    let full_repeats = repeat_count.min(get_count_full_repeats(*time, data.time, data.tail_time).into_inner() as Count);
     *total_amount += data.output.get_amount_sum() * AttrVal::from(full_repeats);
     let mut remaining_repeats = repeat_count - full_repeats;
     while *time >= OF(0.0) && remaining_repeats > 0 {
