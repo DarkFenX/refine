fa94284b1010ba67bb1c9990d858059d7153e7d9
DarkPhoenix <phoenix@mail.ru>
2026-03-02 16:55:02 +0100

    Change how covariance check works to be similar to a lender one

--- core/src/api/util/mut_iter.rs
+++ core/src/api/util/mut_iter.rs
@@ -1,3 +1,12 @@
+//! Generic mutable iterator uses `lender` crate, which requires lends to check covariance. In other
+//! lending iterators, it is checked via a macros it provides. Here, using macros is impossible,
+//! since lends here use trait hierarchy, with some traits having an associated type. Associated
+//! types on traits break covariance checks, so they are pushed further closer to specific types,
+//! and lender itself just assumes covariance.
+//!
+//! tl;dr: a bunch of hacks to make it work. See https://github.com/WanderLanz/Lender/issues/37 for
+//! more info.
+
 use lender::{Lender, Lending, unsafe_assume_covariance};
 
 use crate::{
@@ -52,7 +61,10 @@ where
 
 pub trait RefFamily {
     type Ref<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short>;
+
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>>;
 }
 pub(crate) trait New: RefFamily {
     type UId: Copy;
@@ -62,170 +74,234 @@ pub(crate) trait New: RefFamily {
 // Implementations for non-item entities
 impl RefFamily for FleetMut<'_> {
     type Ref<'a> = FleetMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for FleetMut<'_> {
     type UId = UFleetId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        FleetMut::__check_covariance(CovariantProof::new());
         FleetMut::new(sol, uid)
     }
 }
 impl RefFamily for FitMut<'_> {
     type Ref<'a> = FitMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for FitMut<'_> {
     type UId = UFitId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        FitMut::__check_covariance(CovariantProof::new());
         FitMut::new(sol, uid)
     }
 }
 // Implementations for items
 impl RefFamily for AutochargeMut<'_> {
     type Ref<'a> = AutochargeMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for AutochargeMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        AutochargeMut::__check_covariance(CovariantProof::new());
         AutochargeMut::new(sol, uid)
     }
 }
 impl RefFamily for BoosterMut<'_> {
     type Ref<'a> = BoosterMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for BoosterMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        BoosterMut::__check_covariance(CovariantProof::new());
         BoosterMut::new(sol, uid)
     }
 }
 impl RefFamily for DroneMut<'_> {
     type Ref<'a> = DroneMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for DroneMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        DroneMut::__check_covariance(CovariantProof::new());
         DroneMut::new(sol, uid)
     }
 }
 impl RefFamily for FighterMut<'_> {
     type Ref<'a> = FighterMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for FighterMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        FighterMut::__check_covariance(CovariantProof::new());
         FighterMut::new(sol, uid)
     }
 }
 impl RefFamily for FwEffectMut<'_> {
     type Ref<'a> = FwEffectMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for FwEffectMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        FwEffectMut::__check_covariance(CovariantProof::new());
         FwEffectMut::new(sol, uid)
     }
 }
 impl RefFamily for ImplantMut<'_> {
     type Ref<'a> = ImplantMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for ImplantMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        ImplantMut::__check_covariance(CovariantProof::new());
         ImplantMut::new(sol, uid)
     }
 }
 impl RefFamily for ProjEffectMut<'_> {
     type Ref<'a> = ProjEffectMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for ProjEffectMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        ProjEffectMut::__check_covariance(CovariantProof::new());
         ProjEffectMut::new(sol, uid)
     }
 }
 impl RefFamily for RigMut<'_> {
     type Ref<'a> = RigMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for RigMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        RigMut::__check_covariance(CovariantProof::new());
         RigMut::new(sol, uid)
     }
 }
 impl RefFamily for ServiceMut<'_> {
     type Ref<'a> = ServiceMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for ServiceMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        ServiceMut::__check_covariance(CovariantProof::new());
         ServiceMut::new(sol, uid)
     }
 }
 impl RefFamily for SkillMut<'_> {
     type Ref<'a> = SkillMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for SkillMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        SkillMut::__check_covariance(CovariantProof::new());
         SkillMut::new(sol, uid)
     }
 }
 impl RefFamily for SubsystemMut<'_> {
     type Ref<'a> = SubsystemMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for SubsystemMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        SubsystemMut::__check_covariance(CovariantProof::new());
         SubsystemMut::new(sol, uid)
     }
 }
 impl RefFamily for SwEffectMut<'_> {
     type Ref<'a> = SwEffectMut<'a>;
-    fn __covariance_check<'long: 'short, 'short>(p: *const Self::Ref<'long>) -> *const Self::Ref<'short> {
-        p
+    #[inline(always)]
+    fn __check_covariance<'long: 'short, 'short>(
+        proof: CovariantProof<Self::Ref<'long>>,
+    ) -> CovariantProof<Self::Ref<'short>> {
+        proof
     }
 }
 impl New for SwEffectMut<'_> {
     type UId = UItemId;
     fn new_new(sol: &mut SolarSystem, uid: Self::UId) -> Self::Ref<'_> {
+        SwEffectMut::__check_covariance(CovariantProof::new());
         SwEffectMut::new(sol, uid)
+    }
+}
+
+// Covariance check-specific things
+pub struct CovariantProof<T>(core::marker::PhantomData<fn() -> T>);
+impl<T> CovariantProof<T> {
+    fn new() -> Self {
+        CovariantProof(core::marker::PhantomData)
     }
 }
