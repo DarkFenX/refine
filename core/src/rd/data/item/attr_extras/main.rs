34fbabf81eb216642c40f9f30c1d9751676e2cae
DarkPhoenix <phoenix@mail.ru>
2026-04-01 07:05:33 +0200

    Reorder fields in item AXt for OCD reasons

--- core/src/rd/data/item/attr_extras/main.rs
+++ core/src/rd/data/item/attr_extras/main.rs
@@ -37,80 +37,48 @@ use crate::{
 // during runtime.
 #[derive(Clone, Default)]
 pub(crate) struct RItemAXt {
-    // Item type
-    pub(crate) kind: Option<ItemKind>,
-    // Unmutated and unmodified item volume
+    // Unmutated and unmodified (by dogma modifiers) attribute values, cast to necessary type
     pub(crate) volume: PValue,
-    // Unmutated and unmodified item capacity
     pub(crate) capacity: PValue,
-    // Unmutated and unmodified item radius
     pub(crate) radius: PValue,
-    // If set, item can be fit to a ship which fits into the limit
-    pub(crate) ship_limit: Option<RItemShipLimit>,
-    // If set, item can load only charges which fit into limit
-    pub(crate) charge_limit: Option<RItemChargeLimit>,
-    // If set, item can be loaded as charge into other items which fits this limit
-    pub(crate) cont_limit: Option<RItemContLimit>,
-    // Slot index an implant takes
-    pub(crate) implant_slot: Option<SlotIndex>,
-    // Slot index a booster takes
-    pub(crate) booster_slot: Option<SlotIndex>,
-    // Slot index a subsystem takes
-    pub(crate) subsystem_slot: Option<SlotIndex>,
-    // Defines if a fighter takes a light fighter slot or not
-    pub(crate) is_light_fighter: bool,
-    // Defines if a fighter takes a heavy fighter slot or not
-    pub(crate) is_heavy_fighter: bool,
-    // Defines if a fighter takes a support fighter slot or not
-    pub(crate) is_support_fighter: bool,
-    // Defines if a fighter takes a standup light fighter slot or not
-    pub(crate) is_st_light_fighter: bool,
-    // Defines if a fighter takes a standup heavy fighter slot or not
-    pub(crate) is_st_heavy_fighter: bool,
-    // Defines if a fighter takes a standup support fighter slot or not
-    pub(crate) is_st_support_fighter: bool,
-    // Which ship type this item fits to
-    pub(crate) item_ship_kind: Option<RShipKind>,
-    // If set, ship can use drones which fit into the limit
-    pub(crate) drone_limit: Option<RShipDroneLimit>,
-    // By default, a fighter squad will have this count of fighters
-    pub(crate) max_fighter_count: FighterCount,
-    // Drone bandwidth consumption
-    pub(crate) bandwidth_use: Option<Value>,
-    // Required thermodynamics skill level
-    pub(crate) overload_td_lvl: Option<SkillLevel>,
-    // Max amount of items with this type ID a fit can have
-    pub(crate) max_type_fitted: Option<Count>,
-    // Max security class this module can be online in (2 hisec, 1 lowsec, 0 the rest)
-    pub(crate) online_max_sec_class: Option<Value>,
-    // Can be limited to specific security zones if some of the limit attributes are defined
-    pub(crate) sec_zone_limitable: bool,
-    // True if assistive item projected to targets immune to offensive modifiers should break the
-    // offense immunity validation
-    pub(crate) disallow_vs_ew_immune_tgt: bool,
-    // Attribute ID which defines what attribute on target is used to resist some effects on this
-    // item
-    pub(crate) remote_resist_attr_rid: Option<RAttrId>,
-    // Unmutated and unmodified charge size
-    pub(crate) charge_size: Option<Value>,
-    // Unmutated and unmodified charge rate
-    pub(crate) charge_rate: Count,
-    // True if item has some speed
-    pub(crate) is_mobile: bool,
-    // Rig calibration cost
     pub(crate) calibration_use: Option<Value>,
-    // Can item be limited by "max group fitted" limit
-    pub(crate) max_group_fitted_limited: bool,
-    // Can item be limited by "max group online" limit
-    pub(crate) max_group_online_limited: bool,
-    // Can item be limited by "max group active" limit
-    pub(crate) max_group_active_limited: bool,
-    // Size of a rig, or rig size used by a ship
-    pub(crate) rig_size: Option<Value>,
-    // Base time it takes to refuel a fighter, even if it did not spend any charges
+    pub(crate) bandwidth_use: Option<Value>,
+    pub(crate) rig_size: Option<Value>,    // On-rig and on-ship attribute
+    pub(crate) charge_size: Option<Value>, // On-module and on-charge attribute
+    pub(crate) charge_rate: Count,
+    pub(crate) max_fighter_count: FighterCount,
     pub(crate) fighter_refuel_duration: PValue,
-    // True for items which have both maxVelocity and entityCruiseSpeed
-    pub(crate) entity_mwd: bool,
+    pub(crate) remote_resist_attr_rid: Option<RAttrId>,
+    // Fighter kind flags
+    pub(crate) is_light_fighter: bool,
+    pub(crate) is_heavy_fighter: bool,
+    pub(crate) is_support_fighter: bool,
+    pub(crate) is_st_light_fighter: bool,
+    pub(crate) is_st_heavy_fighter: bool,
+    pub(crate) is_st_support_fighter: bool,
+    // Slot index this item takes
+    pub(crate) implant_slot: Option<SlotIndex>,
+    pub(crate) booster_slot: Option<SlotIndex>,
+    pub(crate) subsystem_slot: Option<SlotIndex>,
+    // Various aggregated limits
+    pub(crate) ship_limit: Option<RItemShipLimit>, // Items can be fit to those ships
+    pub(crate) charge_limit: Option<RItemChargeLimit>, // Items can load those charges
+    pub(crate) cont_limit: Option<RItemContLimit>, // Charges can be loaded into those items
+    pub(crate) drone_limit: Option<RShipDroneLimit>, // Ship can use those drones
+    // Is item limitable by an appropriate "max group" limit, or cannot be affected at all
+    pub(crate) max_group_fitted_limited: bool,
+    pub(crate) max_group_online_limited: bool,
+    pub(crate) max_group_active_limited: bool,
+    // Misc
+    pub(crate) kind: Option<ItemKind>,
+    pub(crate) item_ship_kind: Option<RShipKind>, // Which ship type this item fits to
+    pub(crate) max_type_fitted: Option<Count>,    // Max amount of fit items of this type ID
+    pub(crate) overload_td_lvl: Option<SkillLevel>, // Required thermodynamics level for overheat
+    pub(crate) sec_zone_limitable: bool,          // If item can be sec zone limited altogether
+    pub(crate) online_max_sec_class: Option<Value>, // 2 hisec, 1 lowsec, 0 the rest
+    pub(crate) is_mobile: bool,                   // Used to differentiate between mobile and sentry drones
+    pub(crate) entity_mwd: bool,                  // Used to differentiate between single/dual-prop drones
+    pub(crate) disallow_vs_ew_immune_tgt: bool,
 }
 impl RItemAXt {
     pub(crate) fn fill(
@@ -124,6 +92,39 @@ impl RItemAXt {
         attr_consts: &RAttrConsts,
         effect_consts: &REffectConsts,
     ) {
+        // Unmutated and unmodified (by dogma modifiers) attribute values, cast to necessary type
+        self.volume = get_volume(item_attrs, attr_consts);
+        self.capacity = get_capacity(item_attrs, attr_consts);
+        self.radius = get_radius(item_attrs, attr_consts);
+        self.calibration_use = get_calibration_use(item_attrs, attr_consts);
+        self.bandwidth_use = get_bandwidth_use(item_attrs, attr_consts);
+        self.rig_size = get_rig_size(item_attrs, attr_consts);
+        self.charge_size = get_charge_size(item_attrs, attr_consts);
+        self.charge_rate = get_charge_rate(item_attrs, attr_consts);
+        self.max_fighter_count = get_max_fighter_count(item_attrs, attr_consts);
+        self.fighter_refuel_duration = get_fighter_refuel_duration(item_attrs, attr_consts);
+        self.remote_resist_attr_rid = get_remote_resist_attr_id(item_attrs, attr_consts, attr_aid_rid_map);
+        // Fighter kind flags
+        self.is_light_fighter = get_light_fighter_flag(item_attrs, attr_consts);
+        self.is_heavy_fighter = get_heavy_fighter_flag(item_attrs, attr_consts);
+        self.is_support_fighter = get_support_fighter_flag(item_attrs, attr_consts);
+        self.is_st_light_fighter = get_st_light_fighter_flag(item_attrs, attr_consts);
+        self.is_st_heavy_fighter = get_st_heavy_fighter_flag(item_attrs, attr_consts);
+        self.is_st_support_fighter = get_st_support_fighter_flag(item_attrs, attr_consts);
+        // Slot index this item takes
+        self.implant_slot = get_implant_slot(item_attrs, attr_consts);
+        self.booster_slot = get_booster_slot(item_attrs, attr_consts);
+        self.subsystem_slot = get_subsystem_slot(item_attrs, attr_consts);
+        // Various aggregated limits
+        self.ship_limit = get_item_ship_limit(item_id, item_attrs, attr_consts);
+        self.charge_limit = get_item_charge_limit(item_attrs, attr_consts);
+        self.cont_limit = get_item_container_limit(item_attrs, attr_consts);
+        self.drone_limit = get_ship_drone_limit(item_attrs, attr_consts);
+        // Is item limitable by an appropriate "max group" limit, or cannot be affected at all
+        self.max_group_fitted_limited = get_max_group_fitted_limited(item_attrs, attr_consts);
+        self.max_group_online_limited = get_max_group_online_limited(item_attrs, attr_consts);
+        self.max_group_active_limited = get_max_group_active_limited(item_attrs, attr_consts);
+        // Misc
         self.kind = get_item_kind(
             item_grp_id,
             item_cat_id,
@@ -132,40 +133,13 @@ impl RItemAXt {
             attr_consts,
             effect_consts,
         );
-        self.volume = get_volume(item_attrs, attr_consts);
-        self.capacity = get_capacity(item_attrs, attr_consts);
-        self.radius = get_radius(item_attrs, attr_consts);
-        self.ship_limit = get_item_ship_limit(item_id, item_attrs, attr_consts);
-        self.charge_limit = get_item_charge_limit(item_attrs, attr_consts);
-        self.cont_limit = get_item_container_limit(item_attrs, attr_consts);
-        self.implant_slot = get_implant_slot(item_attrs, attr_consts);
-        self.booster_slot = get_booster_slot(item_attrs, attr_consts);
-        self.subsystem_slot = get_subsystem_slot(item_attrs, attr_consts);
-        self.is_light_fighter = get_light_fighter_flag(item_attrs, attr_consts);
-        self.is_heavy_fighter = get_heavy_fighter_flag(item_attrs, attr_consts);
-        self.is_support_fighter = get_support_fighter_flag(item_attrs, attr_consts);
-        self.is_st_light_fighter = get_st_light_fighter_flag(item_attrs, attr_consts);
-        self.is_st_heavy_fighter = get_st_heavy_fighter_flag(item_attrs, attr_consts);
-        self.is_st_support_fighter = get_st_support_fighter_flag(item_attrs, attr_consts);
         self.item_ship_kind = get_item_ship_kind(item_cat_id, item_attrs, attr_consts);
-        self.drone_limit = get_ship_drone_limit(item_attrs, attr_consts);
-        self.max_fighter_count = get_max_fighter_count(item_attrs, attr_consts);
-        self.bandwidth_use = get_bandwidth_use(item_attrs, attr_consts);
-        self.overload_td_lvl = get_overload_td_lvl(item_attrs, attr_consts);
         self.max_type_fitted = get_max_type_fitted_count(item_attrs, attr_consts);
-        self.online_max_sec_class = get_online_max_sec_class(item_attrs, attr_consts);
+        self.overload_td_lvl = get_overload_td_lvl(item_attrs, attr_consts);
         self.sec_zone_limitable = is_sec_zone_limitable(item_attrs, attr_consts);
-        self.disallow_vs_ew_immune_tgt = get_disallow_vs_ew_immune_tgt(item_attrs, attr_consts);
-        self.remote_resist_attr_rid = get_remote_resist_attr_id(item_attrs, attr_consts, attr_aid_rid_map);
-        self.charge_size = get_charge_size(item_attrs, attr_consts);
-        self.charge_rate = get_charge_rate(item_attrs, attr_consts);
+        self.online_max_sec_class = get_online_max_sec_class(item_attrs, attr_consts);
         self.is_mobile = is_mobile(item_attrs, attr_consts);
-        self.calibration_use = get_calibration_use(item_attrs, attr_consts);
-        self.max_group_fitted_limited = get_max_group_fitted_limited(item_attrs, attr_consts);
-        self.max_group_online_limited = get_max_group_online_limited(item_attrs, attr_consts);
-        self.max_group_active_limited = get_max_group_active_limited(item_attrs, attr_consts);
-        self.rig_size = get_rig_size(item_attrs, attr_consts);
-        self.fighter_refuel_duration = get_fighter_refuel_duration(item_attrs, attr_consts);
         self.entity_mwd = entity_has_mwd(item_attrs, attr_consts);
+        self.disallow_vs_ew_immune_tgt = get_disallow_vs_ew_immune_tgt(item_attrs, attr_consts);
     }
 }
