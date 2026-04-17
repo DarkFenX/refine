a04da8569e98d6ab3d87b6c7a072be40e8c9e032
DarkPhoenix <phoenix@mail.ru>
2026-04-17 13:24:48 +0200

    Extend ability & side effect tests

--- http/tests/tests/features/abilities/test_state.py
+++ http/tests/tests/features/abilities/test_state.py
@@ -1,4 +1,4 @@
-from fw import approx
+from fw import approx, check_no_field
 
 
 def test_switch_state_local(client, consts):
@@ -304,8 +304,7 @@ def test_src_switch_no_abil_data(client, consts):
     assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(200)
     # Action
     api_fighter.change_fighter(abilities={eve_secondary_abil_id: True})
-    # Verification - attempt to change state of ability which is not defined for a fighter did not
-    # affect effect mode, and it is still disabled on 2nd source
+    # Verification
     api_fighter.update()
     assert len(api_fighter.abilities) == 2
     assert api_fighter.abilities[eve_primary_abil_id].state is False
@@ -325,7 +324,8 @@ def test_src_switch_no_abil_data(client, consts):
 
 
 def test_src_switch_no_abil(client, consts):
-    # Case when ability is not defined altogether
+    # Case when ability is not defined altogether, but there is an effect which normally backs this
+    # ability
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_affector_attr1_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.ftr_abil_ab_speed_bonus)
@@ -388,8 +388,7 @@ def test_src_switch_no_abil(client, consts):
     assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(200)
     # Action
     api_fighter.change_fighter(abilities={eve_secondary_abil_id: True})
-    # Verification - attempt to change state of ability which is not defined for a fighter did not
-    # affect effect mode, and it is still disabled on 2nd source
+    # Verification
     api_fighter.update()
     assert len(api_fighter.abilities) == 2
     assert api_fighter.abilities[eve_primary_abil_id].state is False
@@ -406,3 +405,82 @@ def test_src_switch_no_abil(client, consts):
     assert api_fighter.abilities[eve_primary_abil_id].state is False
     assert api_fighter.attrs[eve_affectee_attr1_id].modified == approx(1017.5)
     assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(250)
+
+
+def test_src_switch_not_loaded(client, consts):
+    # Case when fighter is not loaded
+    eve_d1 = client.mk_eve_data()
+    eve_d2 = client.mk_eve_data()
+    eve_affector_attr1_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.ftr_abil_ab_speed_bonus)
+    eve_affectee_attr1_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.max_velocity)
+    eve_affector_attr2_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.ftr_abil_mjd_sig_radius_bonus)
+    eve_affectee_attr2_id = client.mk_eve_attr(datas=[eve_d1, eve_d2], id_=consts.EveAttr.sig_radius)
+    eve_primary_effect_id = client.mk_eve_effect(
+        datas=[eve_d1, eve_d2], id_=consts.EveEffect.ftr_abil_ab, cat_id=consts.EveEffCat.active)
+    eve_secondary_effect_id = client.mk_eve_effect(
+        datas=[eve_d1, eve_d2], id_=consts.EveEffect.ftr_abil_mjd, cat_id=consts.EveEffCat.active)
+    eve_primary_abil_id = client.mk_eve_abil(datas=[eve_d1, eve_d2], id_=consts.EveAbil.ab)
+    eve_secondary_abil_id = client.mk_eve_abil(datas=[eve_d2], id_=consts.EveAbil.mjd)
+    eve_fighter_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_fighter(
+        datas=[eve_d2],
+        id_=eve_fighter_id,
+        attrs={
+            eve_affector_attr1_id: 400, eve_affectee_attr1_id: 2035,
+            eve_affector_attr2_id: 150, eve_affectee_attr2_id: 200},
+        eff_ids=[eve_primary_effect_id, eve_secondary_effect_id],
+        defeff_id=eve_primary_effect_id,
+        abils=[client.mk_eve_item_abil(id_=eve_primary_abil_id), client.mk_eve_item_abil(id_=eve_secondary_abil_id)])
+    # Another fighter, just to keep effects & abilities from being cleaned up
+    client.mk_eve_fighter(
+        datas=[eve_d1],
+        eff_ids=[eve_primary_effect_id, eve_secondary_effect_id],
+        abils=[client.mk_eve_item_abil(id_=eve_primary_abil_id), client.mk_eve_item_abil(id_=eve_secondary_abil_id)])
+    client.create_sources()
+    api_sol = client.create_sol(data=eve_d1)
+    api_fit = api_sol.create_fit()
+    api_fighter = api_fit.add_fighter(type_id=eve_fighter_id, state=consts.ApiMinionState.engaging)
+    # Verification
+    api_fighter.update()
+    with check_no_field():
+        api_fighter.abilities  # noqa: B018
+    # Action
+    api_fighter.change_fighter(abilities={eve_primary_abil_id: False, eve_secondary_abil_id: True})
+    # Verification - nothing is affected
+    api_fighter.update()
+    with check_no_field():
+        api_fighter.abilities  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification - attempt to change state of ability when fighter was not loaded did not change
+    # anything for fighter which is loaded on second source
+    api_fighter.update()
+    assert len(api_fighter.abilities) == 2
+    assert api_fighter.abilities[eve_primary_abil_id].state is True
+    assert api_fighter.abilities[eve_secondary_abil_id].state is False
+    assert api_fighter.attrs[eve_affectee_attr1_id].modified == approx(10175)
+    assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(200)
+    # Action
+    api_fighter.change_fighter(abilities={eve_primary_abil_id: False, eve_secondary_abil_id: True})
+    # Verification
+    api_fighter.update()
+    assert len(api_fighter.abilities) == 2
+    assert api_fighter.abilities[eve_primary_abil_id].state is False
+    assert api_fighter.abilities[eve_secondary_abil_id].state is True
+    assert api_fighter.attrs[eve_affectee_attr1_id].modified == approx(2035)
+    assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(500)
+    # Action
+    api_sol.change_src(data=eve_d1)
+    # Verification
+    api_fighter.update()
+    with check_no_field():
+        api_fighter.abilities  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification - states were kept as they were on 2nd source
+    api_fighter.update()
+    assert len(api_fighter.abilities) == 2
+    assert api_fighter.abilities[eve_primary_abil_id].state is False
+    assert api_fighter.abilities[eve_secondary_abil_id].state is True
+    assert api_fighter.attrs[eve_affectee_attr1_id].modified == approx(2035)
+    assert api_fighter.attrs[eve_affectee_attr2_id].modified == approx(500)
--- http/tests/tests/features/booster_side_effects/test_src_switch.py
+++ http/tests/tests/features/booster_side_effects/test_src_switch.py
@@ -1,7 +1,7 @@
 from fw import approx, check_no_field
 
 
-def test_valid_to_valid_matching(client, consts):
+def test_valid_effect_to_valid_effect_matching(client, consts):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_chance_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2])
@@ -69,7 +69,7 @@ def test_valid_to_valid_matching(client, consts):
     assert api_side.mod.str == approx(25)
 
 
-def test_valid_to_valid_different(client, consts):
+def test_valid_effect_to_valid_effect_different(client, consts):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_d1_chance_attr_id = client.alloc_attr_id(datas=[eve_d1, eve_d2])
@@ -259,7 +259,7 @@ def test_regular_effect_toggle(client, consts):
     assert api_side.mod.str == approx(25)
 
 
-def test_absent_to_valid(client, consts):
+def test_absent_effect_to_valid_effect(client, consts):
     eve_d1 = client.mk_eve_data()
     eve_d2 = client.mk_eve_data()
     eve_chance_attr_id = client.mk_eve_attr(datas=[eve_d2])
@@ -274,6 +274,102 @@ def test_absent_to_valid(client, consts):
     eve_effect_id = client.mk_eve_effect(datas=[eve_d2], chance_attr_id=eve_chance_attr_id, mod_info=[eve_mod])
     eve_booster_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
     client.mk_eve_item(datas=[eve_d1], id_=eve_booster_id)
+    client.mk_eve_item(
+        datas=[eve_d2],
+        id_=eve_booster_id,
+        attrs={eve_chance_attr_id: 0.5, eve_affector_attr_id: 30},
+        eff_ids=[eve_effect_id])
+    eve_ship_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
+    client.mk_eve_ship(datas=[eve_d1], id_=eve_ship_id, attrs={eve_affectee_attr_id: 100})
+    client.mk_eve_ship(datas=[eve_d2], id_=eve_ship_id, attrs={eve_affectee_attr_id: 200})
+    client.create_sources()
+    api_sol = client.create_sol(data=eve_d1)
+    api_fit = api_sol.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_ship_id)
+    api_booster1 = api_fit.add_booster(type_id=eve_booster_id)
+    api_booster2 = api_fit.add_booster(type_id=eve_booster_id)
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(100)
+    api_booster1.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    api_booster2.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    # Action
+    api_booster1.change_booster(side_effects={eve_effect_id: True})
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(100)
+    api_booster1.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    api_booster2.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(260)
+    api_side1 = api_booster1.update().side_effects[eve_effect_id]
+    assert api_side1.chance == approx(0.5)
+    assert api_side1.state is True
+    assert api_side1.mod.op == consts.ApiSideEffectOp.perc
+    assert api_side1.mod.str == approx(30)
+    api_side2 = api_booster2.update().side_effects[eve_effect_id]
+    assert api_side2.chance == approx(0.5)
+    assert api_side2.state is False
+    assert api_side2.mod.op == consts.ApiSideEffectOp.perc
+    assert api_side2.mod.str == approx(30)
+    # Action
+    api_sol.change_src(data=eve_d1)
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(100)
+    api_booster1.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    api_booster2.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    # Action
+    api_booster2.change_booster(side_effects={eve_effect_id: True})
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(100)
+    api_booster1.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    api_booster2.update()
+    with check_no_field():
+        api_booster1.side_effects  # noqa: B018
+    # Action
+    api_sol.change_src(data=eve_d2)
+    # Verification
+    assert api_ship.update().attrs[eve_affectee_attr_id].modified == approx(338)
+    api_side1 = api_booster1.update().side_effects[eve_effect_id]
+    assert api_side1.chance == approx(0.5)
+    assert api_side1.state is True
+    assert api_side1.mod.op == consts.ApiSideEffectOp.perc
+    assert api_side1.mod.str == approx(30)
+    api_side2 = api_booster2.update().side_effects[eve_effect_id]
+    assert api_side2.chance == approx(0.5)
+    assert api_side2.state is True
+    assert api_side2.mod.op == consts.ApiSideEffectOp.perc
+    assert api_side2.mod.str == approx(30)
+
+
+def test_not_loaded_booster_to_valid_effect(client, consts):
+    eve_d1 = client.mk_eve_data()
+    eve_d2 = client.mk_eve_data()
+    eve_chance_attr_id = client.mk_eve_attr(datas=[eve_d2])
+    eve_affector_attr_id = client.mk_eve_attr(datas=[eve_d2])
+    eve_affectee_attr_id = client.mk_eve_attr(datas=[eve_d1, eve_d2])
+    eve_mod = client.mk_eve_effect_mod(
+        func=consts.EveModFunc.item,
+        loc=consts.EveModLoc.ship,
+        op=consts.EveModOp.post_percent,
+        affector_attr_id=eve_affector_attr_id,
+        affectee_attr_id=eve_affectee_attr_id)
+    eve_effect_id = client.mk_eve_effect(datas=[eve_d2], chance_attr_id=eve_chance_attr_id, mod_info=[eve_mod])
+    eve_booster_id = client.alloc_item_id(datas=[eve_d1, eve_d2])
     client.mk_eve_item(
         datas=[eve_d2],
         id_=eve_booster_id,
