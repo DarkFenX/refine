56d40eb0b0e4b4cd1c4256b1f3da1ef35966ab3f
DarkPhoenix <phoenix@mail.ru>
2026-05-17 23:09:11 +0200

    Add cap sim merging + hard downtime test

--- http/tests/tests/stats/cap/cap_sim/test_merge.py
+++ http/tests/tests/stats/cap/cap_sim/test_merge.py
@@ -136,3 +136,60 @@ def test_different_signs(client, consts):
     api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(
         cap_sim=(True, [StatsOptionCapSim(stagger=False)])))
     assert api_tgt_ship_stats.cap_sim.one() == {consts.ApiCapSimResult.stable: approx(0.5)}
+
+
+def test_hard_downtime(client, consts):
+
+    def mk_fighter(refuel_time: float) -> int:
+        # Not realistic fighter - use count on neuting ability is limited
+        return client.mk_eve_fighter(
+            attrs={
+                eve_neut_amount_attr_id: 99,
+                eve_cycle_time_attr_id: 6000,
+                eve_refuel_time_attr_id: refuel_time,
+                eve_max_count_attr_id: 3,
+                eve_radius_attr_id: 35},
+            eff_ids=[eve_effect_id],
+            defeff_id=eve_effect_id,
+            abils=[client.mk_eve_item_abil(id_=eve_abil_id, charge_count=5, charge_rearm_time=1)])
+
+    eve_neut_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.ftr_abil_energy_neut_amount)
+    eve_max_count_attr_id = client.mk_eve_attr(id_=consts.EveAttr.ftr_sq_max_size)
+    eve_cycle_time_attr_id = client.mk_eve_attr()
+    eve_refuel_time_attr_id = client.mk_eve_attr(id_=consts.EveAttr.ftr_refueling_time)
+    eve_radius_attr_id = client.mk_eve_attr(id_=consts.EveAttr.radius)
+    eve_ship_amount_attr_id = client.mk_eve_attr(id_=consts.EveAttr.capacitor_capacity)
+    eve_regen_attr_id = client.mk_eve_attr(id_=consts.EveAttr.recharge_rate)
+    eve_effect_id = client.mk_eve_effect(
+        id_=consts.EveEffect.ftr_abil_energy_neut,
+        cat_id=consts.EveEffCat.target,
+        duration_attr_id=eve_cycle_time_attr_id)
+    eve_abil_id = client.mk_eve_abil(id_=consts.EveAbil.energy_neut)
+    eve_fighter1_id = mk_fighter(refuel_time=5000)
+    eve_fighter2_id = mk_fighter(refuel_time=50000)
+    eve_tgt_ship_id = client.mk_eve_ship(
+        attrs={eve_radius_attr_id: 220, eve_ship_amount_attr_id: 10000, eve_regen_attr_id: 500000})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_src_fit = api_sol.create_fit()
+    api_src_fighter1 = api_src_fit.add_fighter(
+        type_id=eve_fighter1_id,
+        state=consts.ApiMinionState.engaging,
+        rearm_minion=consts.ApiRearmMinion.on_first_empty)
+    api_src_fighter2 = api_src_fit.add_fighter(
+        type_id=eve_fighter2_id,
+        state=consts.ApiMinionState.engaging,
+        rearm_minion=consts.ApiRearmMinion.on_first_empty)
+    api_tgt_fit = api_sol.create_fit()
+    api_tgt_ship = api_tgt_fit.set_ship(type_id=eve_tgt_ship_id)
+    api_src_fighter1.change_fighter(add_projs=[api_tgt_ship.id])
+    api_src_fighter2.change_fighter(add_projs=[api_tgt_ship.id])
+    # Verification - fighters with similar stats but different hard downtime (refuel) duration
+    # shouldn't be merged. If longer duration was taken for both, cap would've been stable; if
+    # shorter - it'd last about 5 minutes
+    api_tgt_fit_stats = api_tgt_fit.get_stats(options=FitStatsOptions(
+        cap_sim=(True, [StatsOptionCapSim(stagger=False)])))
+    assert api_tgt_fit_stats.cap_sim.one() == {consts.ApiCapSimResult.time: approx(1149)}
+    api_tgt_ship_stats = api_tgt_ship.get_stats(options=ItemStatsOptions(
+        cap_sim=(True, [StatsOptionCapSim(stagger=False)])))
+    assert api_tgt_ship_stats.cap_sim.one() == {consts.ApiCapSimResult.time: approx(1149)}
