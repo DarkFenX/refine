b15a4dbaa87da6be27cffd5f7996afaa7f66bef5
DarkPhoenix <phoenix@mail.ru>
2026-05-28 15:38:10 +0200

    Extend LR fighter test

--- http/tests/tests/stats/dmg/fighter/long_range/test_general.py
+++ http/tests/tests/stats/dmg/fighter/long_range/test_general.py
@@ -15,6 +15,7 @@ def test_state(client, consts):
     api_sol = client.create_sol()
     api_fit = api_sol.create_fit()
     api_fighter = api_fit.add_fighter(type_id=eve_fighter_id, state=consts.ApiMinionState.engaging)
+    api_autocharge = api_fighter.autocharges[eve_basic_info.ftr_abil_bomb_effect_id]
     api_fleet = api_sol.create_fleet()
     api_fleet.change(add_fits=[api_fit.id])
     # Verification
@@ -27,6 +28,14 @@ def test_state(client, consts):
     api_fighter_dmg_stats = api_fighter.get_stats(options=ItemStatsOptions(dmg=True)).dmg.one()
     assert api_fighter_dmg_stats.dps == [0, 0, 0, approx(698.730469)]
     assert api_fighter_dmg_stats.volley == [0, 0, 0, approx(5589.84375)]
+    api_autocharge_stats = api_autocharge.get_stats(options=ItemStatsOptions(
+        dmg=(True, [StatsOptionItemDmg(), StatsOptionItemDmg(ignore_state=True)])))
+    api_autocharge_dmg_normal, api_autocharge_dmg_ignored = api_autocharge_stats.dmg
+    assert api_autocharge_dmg_normal.dps == [0, 0, 0, 0]
+    assert api_autocharge_dmg_normal.volley == [0, 0, 0, 0]
+    # TODO: autocharge with ignore state should deal DPS
+    assert api_autocharge_dmg_ignored.dps == [0, 0, 0, 0]
+    assert api_autocharge_dmg_ignored.volley == [0, 0, 0, 0]
     # Action
     api_fighter.change_fighter(state=consts.ApiMinionState.in_space)
     # Verification
@@ -46,12 +55,14 @@ def test_state(client, consts):
     # Action
     api_fighter.change_fighter(state=consts.ApiMinionState.engaging, abilities={eve_basic_info.bomb_abil_id: True})
     # Verification
+    # TODO: autocharge DPS should scale with fighter count
     api_fleet_dmg_stats = api_fleet.get_stats(options=FleetStatsOptions(dmg=True)).dmg.one()
     assert api_fleet_dmg_stats.dps == [0, 0, 0, approx(709.397135)]
     assert api_fleet_dmg_stats.volley == [0, 0, 0, approx(6229.84375)]
     api_fit_dmg_stats = api_fit.get_stats(options=FitStatsOptions(dmg=True)).dmg.one()
     assert api_fit_dmg_stats.dps == [0, 0, 0, approx(709.397135)]
     assert api_fit_dmg_stats.volley == [0, 0, 0, approx(6229.84375)]
-    api_fighter_dmg_stats = api_fighter.get_stats(options=ItemStatsOptions(dmg=True)).dmg.one()
-    assert api_fighter_dmg_stats.dps == [0, 0, 0, approx(698.730469)]
-    assert api_fighter_dmg_stats.volley == [0, 0, 0, approx(5589.84375)]
+    api_fighter_dmg_stats = api_fighter.get_stats(options=ItemStatsOptions(
+        dmg=(True, [StatsOptionItemDmg(include_charges=True)]))).dmg.one()
+    assert api_fighter_dmg_stats.dps == [0, 0, 0, approx(709.397135)]
+    assert api_fighter_dmg_stats.volley == [0, 0, 0, approx(6229.84375)]
