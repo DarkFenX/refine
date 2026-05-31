19322f8bef1460861fa979c42c052f416a4f1ca0
DarkPhoenix <phoenix@mail.ru>
2026-05-31 13:35:53 +0200

    Remove unnecessary derives

--- core/src/svc/calc/info/affector.rs
+++ core/src/svc/calc/info/affector.rs
@@ -1,6 +1,5 @@
 use crate::{api::AttrId, ud::ItemId};
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Affector {
     pub item_id: ItemId,
     pub attr_id: Option<AttrId>,
--- core/src/svc/calc/info/modification.rs
+++ core/src/svc/calc/info/modification.rs
@@ -4,7 +4,6 @@ use crate::{
     svc::calc::Affector,
 };
 
-#[derive(Clone, Eq, PartialEq, Hash)]
 pub struct Modification {
     pub op: Op,
     pub initial_str: Value,
