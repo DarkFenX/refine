500868b0f4cfe48b4c4e253decd7ff69be0e347e
DarkPhoenix <phoenix@mail.ru>
2026-06-04 20:13:44 +0200

    Add new "max" stat accumulator

---
+++ core/src/svc/vast/aggr/accum/max.rs
@@ -0,0 +1,52 @@
+use super::shared::{SeqAccum, SeqInstanceAccum};
+use crate::{
+    num::{Count, PValue},
+    util::{LibDefault, LibMax},
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Top-level accumulator interface
+////////////////////////////////////////////////////////////////////////////////////////////////////
+impl<I> SeqAccum<SeqInstanceAccumMax<I>> {
+    pub(in crate::svc::vast) fn new_max() -> Self
+    where
+        I: LibDefault,
+    {
+        SeqAccum {
+            instances: SeqInstanceAccumMax::new(),
+            time: PValue::ZERO,
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sequence accumulator implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////
+pub(in crate::svc::vast) struct SeqInstanceAccumMax<I> {
+    pub(in crate::svc::vast) max: I,
+}
+impl<I> SeqInstanceAccumMax<I>
+where
+    I: LibDefault,
+{
+    pub(in crate::svc::vast) fn new() -> Self {
+        Self { max: I::lib_default() }
+    }
+}
+impl<I> SeqInstanceAccum<I> for SeqInstanceAccumMax<I>
+where
+    I: Copy + std::ops::MulAssign<PValue> + LibDefault + LibMax,
+{
+    fn add_instance(&mut self, mut instance: I, chance_mult: Option<PValue>, _count: Count) {
+        if let Some(chance_mult) = chance_mult {
+            instance *= chance_mult;
+        }
+        self.max = LibMax::lib_max(self.max, instance);
+    }
+    fn copy_blank(&self) -> Self {
+        Self::new()
+    }
+    fn merge(&mut self, other: &Self, _count: Count) {
+        self.max = LibMax::lib_max(self.max, other.max);
+    }
+}
--- core/src/svc/vast/aggr/accum/mod.rs
+++ core/src/svc/vast/aggr/accum/mod.rs
@@ -1,5 +1,6 @@
 pub(in crate::svc::vast) use shared::{SeqAccum, SeqInstanceAccum};
 
+mod max;
 mod shared;
 mod stack;
 mod stack_max;
