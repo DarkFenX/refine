5a5ada819f3e3127be1e1294f43d60c17d83b6bf
DarkPhoenix <phoenix@mail.ru>
2026-06-18 20:21:47 +0200

    Rewrite secondary arena to use cranelift's secondary map

--- core/src/util/storage/arena/secondary.rs
+++ core/src/util/storage/arena/secondary.rs
@@ -1,10 +1,15 @@
+use cranelift_entity::SecondaryMap;
+
 use super::shared::ArenaId;
 
 // This secondary container assumes that code using it ensures that entities behind passed IDs exist
 #[derive(Clone)]
-pub(crate) struct ArenaSecondary<I, V> {
-    data: Vec<Option<V>>,
-    key: std::marker::PhantomData<I>,
+pub(crate) struct ArenaSecondary<I, V>
+where
+    I: ArenaId,
+    V: Clone,
+{
+    data: SecondaryMap<I, Option<V>>,
 }
 impl<I, V> ArenaSecondary<I, V>
 where
@@ -13,33 +18,24 @@ where
 {
     pub(crate) fn new() -> Self {
         Self {
-            data: Vec::new(),
-            key: std::marker::PhantomData,
+            data: SecondaryMap::new(),
         }
     }
     pub(crate) fn get(&self, id: I) -> &V {
-        self.data.get(id.index()).unwrap().as_ref().unwrap()
+        self.data.get(id).unwrap().as_ref().unwrap()
     }
     pub(crate) fn get_mut(&mut self, id: I) -> &mut V {
-        self.data.get_mut(id.index()).unwrap().as_mut().unwrap()
+        self.data.get_mut(id).unwrap().as_mut().unwrap()
     }
     pub(crate) fn iter(&self) -> impl Iterator<Item = (I, &V)> {
         self.data
             .iter()
-            .enumerate()
-            .filter_map(|(index, value)| value.as_ref().map(|v| (I::new(index), v)))
+            .filter_map(|(id, value)| value.as_ref().map(|v| (id, v)))
     }
     pub(crate) fn insert(&mut self, id: I, value: V) {
-        let index = id.index();
-        if index >= self.data.len() {
-            self.data.resize(index + 1, None);
-        }
-        self.data[index] = Some(value);
+        self.data.insert(id, Some(value));
     }
     pub(crate) fn remove(&mut self, id: I) {
-        let index = id.index();
-        if index < self.data.len() {
-            self.data[index] = None;
-        }
+        self.data.remove(id);
     }
 }
