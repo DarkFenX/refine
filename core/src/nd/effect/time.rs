9d2bd9acf87793131b1a68526a02a77e5d83f1d1
DarkPhoenix <phoenix@mail.ru>
2026-06-20 20:02:21 +0200

    Add more comments

--- core/src/nd/effect/time.rs
+++ core/src/nd/effect/time.rs
@@ -1,15 +1,21 @@
 use crate::num::PValue;
 
-// Time relatively effect cycle start when some event happens
+/// Time relatively effect cycle start when some event happens.
+///
+/// Information specified in this enum is ignored, just its presence is noted. Might change in the
+/// future.
 pub(crate) enum NEffectTime {
-    // Event is triggered at the end of effect cycle
+    /// Event is triggered at the end of effect cycle.
     CycleEnd,
-    // Event is triggered at hardcoded point of time (in seconds)
+    /// Event is triggered at hardcoded point of time (in seconds).
     Hardcoded(PValue),
 }
 
-// Duration of some effect's impact relatively effect cycle start (e.g. disallow-cloak duration)
+/// Duration of some effect's impact relatively effect cycle start (e.g. disallow-cloak duration).
+///
+/// Information specified in this enum is ignored, just its presence is noted. Might change in the
+/// future.
 pub(crate) enum NEffectDuration {
-    // Impactful as long as effect is in active duration of its cycle
+    /// Impactful as long as effect is in active part of its cycle.
     Effect,
 }
