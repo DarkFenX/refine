332923ad3fece43af65884da1f8724dc9938fc65
DarkPhoenix <phoenix@mail.ru>
2026-06-21 12:02:15 +0200

    Add checks for non-ship buff item list length during adapted data generation

--- core/src/ad/generator/flow/s8_conv_post/item_buff_item_lists.rs
+++ core/src/ad/generator/flow/s8_conv_post/item_buff_item_lists.rs
@@ -2,9 +2,11 @@
 // information about item lists used by buffs stored on items belonging to those item lists.
 
 use crate::{
-    ad::{AData, AEffectBuffScope},
+    ad::{AData, AEffectBuffScope, AItemCatId},
     util::RSet,
 };
+
+const LENGTH_THRESHOLD: usize = 5;
 
 pub(in crate::ad::generator::flow::s8_conv_post) fn fill_buff_item_lists(a_data: &mut AData) {
     // Collect item lists which are used in buffs
@@ -25,12 +27,17 @@ pub(in crate::ad::generator::flow::s8_conv_post) fn fill_buff_item_lists(a_data:
             }
         }
     }
+    let mut proj_max_len = 0;
+    let mut fleet_max_len = 0;
     // Put data about buff-involved item lists onto items which belong to those lists
     for item_list_aid in proj_item_list_aids {
         if let Some(a_item_list) = a_data.item_lists.data.get(&item_list_aid) {
             for item_aid in a_item_list.item_ids.iter() {
                 if let Some(a_item) = a_data.items.data.get_mut(item_aid) {
                     a_item.proj_buff_item_list_ids.insert(item_list_aid);
+                    if a_item.cat_id != AItemCatId::SHIP {
+                        proj_max_len = proj_max_len.max(a_item.proj_buff_item_list_ids.len());
+                    }
                 }
             }
         }
@@ -40,8 +47,29 @@ pub(in crate::ad::generator::flow::s8_conv_post) fn fill_buff_item_lists(a_data:
             for item_aid in a_item_list.item_ids.iter() {
                 if let Some(a_item) = a_data.items.data.get_mut(item_aid) {
                     a_item.fleet_buff_item_list_ids.insert(item_list_aid);
+                    if a_item.cat_id != AItemCatId::SHIP {
+                        fleet_max_len = fleet_max_len.max(a_item.fleet_buff_item_list_ids.len());
+                    }
                 }
             }
         }
+    }
+    // Calculator module works with assumption that count of item lists for non-ships won't exceed
+    // certain count. When that limit is broken, it might make sense to:
+    // - switch from using vector to hash set (mostly it concerns rd module items);
+    // - possibly change how calculator processes type list checks when working on buffs.
+    // Ships are excluded mostly because performance when adding/removing ships does not matter
+    // much. 5 is an arbitrary threshold, need to reassess different approaches once it is reached.
+    if proj_max_len >= LENGTH_THRESHOLD {
+        tracing::warn!(
+            "max count of item list IDs involved in projected buffs is {}",
+            proj_max_len
+        );
+    }
+    if fleet_max_len >= LENGTH_THRESHOLD {
+        tracing::warn!(
+            "max count of item list IDs involved in fleet buffs is {}",
+            fleet_max_len
+        );
     }
 }
