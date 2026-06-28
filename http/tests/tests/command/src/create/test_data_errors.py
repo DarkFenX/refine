5765853f7338171a5ec21454d3e67e3402eba67f
DarkPhoenix <phoenix@mail.ru>
2026-06-28 09:38:07 +0200

    Add key parsing failure testing

--- http/tests/tests/command/src/create/test_data_errors.py
+++ http/tests/tests/command/src/create/test_data_errors.py
@@ -1,3 +1,13 @@
+"""
+This module has tests which checks what happens when EVE data handler receives malformed data. Data
+errors can be recoverable (correct JSON, but unexpected data), in which case handler should skip
+malformed entity if possible, and unrecoverable.
+
+Since some phobos files contain data for 2 separate EVE data entities (typedogma,
+dynamicitemattributes), data handler implementations have different functions to read/parse them,
+and they have to be tested separately.
+"""
+
 import typing
 
 from fw import check_no_field
@@ -7,11 +17,36 @@ if typing.TYPE_CHECKING:
     from fw.eve.containers import EvePrimitives
 
 
+def test_types_key(client, log):
+
+    def data_prim_hook(prim_data: EvePrimitives):
+        prim_data.types = {
+            f'str{k}' if k == eve_item1_id else k: v
+            for k, v in prim_data.types.items()}
+
+    # Same group ID just to avoid cleanup
+    eve_group_id = client.mk_eve_item_group()
+    eve_item1_id = client.mk_eve_item(grp_id=eve_group_id)
+    eve_item2_id = client.mk_eve_item(grp_id=eve_group_id)
+    client.create_sources(data_prim_hook=data_prim_hook)
+    log.wait_log_entry(
+        msg='1 warnings encountered during fetching of EItem, showing up to 5:',
+        level='WARN',
+        span='src-new:adg')
+    log.wait_log_entry(
+        msg=f'failed to cast key "str{eve_item1_id}" to integer',
+        level='WARN',
+        span='src-new:adg')
+    api_sol = client.create_sol()
+    api_item1 = api_sol.add_sw_effect(type_id=eve_item1_id)
+    api_sol.add_sw_effect(type_id=eve_item2_id)
+    # Verification - item which was failed is not in EVE item database, so it cannot be loaded
+    api_val = api_sol.validate(options=ValOptions(not_loaded_item=True))
+    assert api_val.passed is False
+    assert api_val.details.not_loaded_item == [api_item1.id]
+
+
 def test_types_value(client, log):
-    # This test checks what happens when handler sees some valid JSON, but not an item object. The
-    # malformed item should be skipped, and the rest of the file processed normally. There are two
-    # versions of parsing function in EVE data handler, this test covers the one which extracts one
-    # entity out of each entry.
 
     def data_prim_hook(prim_data: EvePrimitives):
         prim_data.types[eve_item1_id] = [1, 2, 3]
@@ -38,11 +73,49 @@ def test_types_value(client, log):
     assert api_val.details.not_loaded_item == [api_item1.id]
 
 
+def test_typedogma_key(client, log):
+
+    def data_prim_hook(prim_data: EvePrimitives):
+        prim_data.typedogma = {
+            f'pre{k}' if k == eve_item1_id else k: v
+            for k, v in prim_data.typedogma.items()}
+
+    eve_attr_id = client.mk_eve_attr()
+    eve_effect_id = client.mk_eve_effect()
+    eve_item1_id = client.mk_eve_item(attrs={eve_attr_id: 5}, eff_ids=[eve_effect_id])
+    eve_item2_id = client.mk_eve_item(attrs={eve_attr_id: 7}, eff_ids=[eve_effect_id])
+    client.create_sources(data_prim_hook=data_prim_hook)
+    log.wait_log_entry(
+        msg='1 warnings encountered during fetching of EItemAttr, showing up to 5:',
+        level='WARN',
+        span='src-new:adg')
+    log.wait_log_entry(
+        msg=f'failed to cast key "pre{eve_item1_id}" to integer',
+        level='WARN',
+        span='src-new:adg')
+    log.wait_log_entry(
+        msg='1 warnings encountered during fetching of EItemEffect, showing up to 5:',
+        level='WARN',
+        span='src-new:adg')
+    log.wait_log_entry(
+        msg=f'failed to cast key "pre{eve_item1_id}" to integer',
+        level='WARN',
+        span='src-new:adg')
+    api_sol = client.create_sol()
+    api_item1 = api_sol.add_sw_effect(type_id=eve_item1_id)
+    api_item2 = api_sol.add_sw_effect(type_id=eve_item2_id)
+    # Verification
+    api_item1.update()
+    with check_no_field():
+        api_item1.attrs  # noqa: B018
+    with check_no_field():
+        api_item1.effects  # noqa: B018
+    api_item2.update()
+    assert eve_attr_id in api_item2.attrs
+    assert eve_effect_id in api_item2.effects
+
+
 def test_typedogma_value(client, log):
-    # This test checks what happens when handler sees some valid JSON, but not an item object. The
-    # malformed item should be skipped, and the rest of the file processed normally. There are two
-    # versions of parsing function in EVE data handler, this test covers the one which extracts two
-    # entities out of each entry.
 
     def data_prim_hook(prim_data: EvePrimitives):
         prim_data.typedogma[eve_item1_id] = 'random'
