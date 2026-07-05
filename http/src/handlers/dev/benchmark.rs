3de77bc5f9ddf080a60503487eb6522556a21753
DarkPhoenix <phoenix@mail.ru>
2026-07-05 14:08:46 +0200

    A pass over dev handlers

--- http/src/handlers/dev/benchmark.rs
+++ http/src/handlers/dev/benchmark.rs
@@ -4,25 +4,25 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
 };
+use axum_extra::extract::WithRejection;
 
-use crate::{cmd::HBenchmarkCmd, err::HApiError, handlers::shared::HSingleErr, state::HAppState};
+use crate::{cmd::HBenchmarkCmd, err::HApiError, state::HAppState};
 
 pub(crate) async fn dev_benchmark_sol(
     State(state): State<HAppState>,
     Path(sol_id): Path<String>,
-    Json(payload): Json<HBenchmarkCmd>,
+    WithRejection(Json(payload), _): WithRejection<Json<HBenchmarkCmd>, HApiError>,
 ) -> impl IntoResponse {
     let sol = match state.sol_mgr.get_sol(&sol_id).await {
         Ok(sol) => sol,
         Err(br_err) => return HApiError::from_br_path_sol(br_err).into_response(),
     };
-    let resp = match match payload {
+    match match payload {
         HBenchmarkCmd::AttrCalc(cmd) => sol.lock().await.dev_benchmark_attrs(&state.tpool, cmd).await,
         HBenchmarkCmd::Stats(cmd) => sol.lock().await.dev_benchmark_stats(&state.tpool, cmd).await,
         HBenchmarkCmd::TryFitItems(cmd) => sol.lock().await.dev_benchmark_try_fit_items(&state.tpool, cmd).await,
     } {
         Ok(_) => StatusCode::OK.into_response(),
-        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(HSingleErr::from_bridge(e))).into_response(),
-    };
-    resp
+        Err(br_err) => HApiError::from_br_path_sol(br_err).into_response(),
+    }
 }
--- http/src/handlers/dev/check.rs
+++ http/src/handlers/dev/check.rs
@@ -1,23 +1,21 @@
 use axum::{
-    Json,
     extract::{Path, State},
     http::StatusCode,
     response::IntoResponse,
 };
 
-use crate::{err::HApiError, handlers::HSingleErr, state::HAppState};
+use crate::{err::HApiError, state::HAppState};
 
 pub(crate) async fn dev_check_sol(State(state): State<HAppState>, Path(sol_id): Path<String>) -> impl IntoResponse {
     let sol = match state.sol_mgr.get_sol(&sol_id).await {
         Ok(sol) => sol,
         Err(br_err) => return HApiError::from_br_path_sol(br_err).into_response(),
     };
-    let resp = match sol.lock().await.dev_consistency_check(&state.tpool).await {
+    match sol.lock().await.dev_consistency_check(&state.tpool).await {
         Ok(result) => match result {
             true => StatusCode::OK.into_response(),
             false => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
         },
-        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(HSingleErr::from_bridge(e))).into_response(),
-    };
-    resp
+        Err(br_err) => HApiError::from_br_path_sol(br_err).into_response(),
+    }
 }
