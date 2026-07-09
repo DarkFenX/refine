28dafd7f062ee4a403d67bb35981bb33c1ec49bb
DarkPhoenix <phoenix@mail.ru>
2026-07-09 20:23:37 +0200

    Add fuel use tests for conduit

--- http/tests/tests/stats/mobility/test_jump.py
+++ http/tests/tests/stats/mobility/test_jump.py
@@ -164,6 +164,69 @@ def test_self_struct(client, consts):
         api_struct_jump_stat.self  # noqa: B018
 
 
+def test_conduit_ranges(client, consts):
+    eve_range_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_range)
+    eve_fuel_type_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_consumption_type)
+    eve_conduit_flag_attr_id = client.mk_eve_attr(id_=consts.EveAttr.enable_perform_conduit_jump)
+    eve_conduit_count_attr_id = client.mk_eve_attr(id_=consts.EveAttr.conduit_jump_passenger_count)
+    eve_conduit_fuel_use_attr_id = client.mk_eve_attr(id_=consts.EveAttr.conduit_jump_drive_consumption_amount)
+    eve_fuel_id = client.mk_eve_item()
+    eve_main_ship_id = client.mk_eve_ship(attrs={
+        eve_range_attr_id: 5,
+        eve_fuel_type_attr_id: eve_fuel_id,
+        eve_conduit_flag_attr_id: 1,
+        eve_conduit_fuel_use_attr_id: 3000,
+        eve_conduit_count_attr_id: 30})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit = api_sol.create_fit()
+    api_ship = api_fit.set_ship(type_id=eve_main_ship_id)
+    # Verification
+    api_jump_options = [
+        StatsOptionJump(),
+        StatsOptionJump(range=5.1),
+        StatsOptionJump(range=5),
+        StatsOptionJump(range='max'),
+        StatsOptionJump(range=2),
+        StatsOptionJump(range=0.1),
+        StatsOptionJump(range=0),
+        StatsOptionJump(range=1.00001)]
+    (api_fit_jump_default,
+     api_fit_jump_excessive,
+     api_fit_jump_max_num,
+     api_fit_jump_max_spec,
+     api_fit_jump_med,
+     api_fit_jump_low,
+     api_fit_jump_zero,
+     api_fit_jump_rounding) = api_fit.get_stats(options=FitStatsOptions(jump=(True, api_jump_options))).jump
+    assert api_fit_jump_default.conduit.fuel_use_self == approx(15000)
+    with check_no_field():
+        api_fit_jump_excessive.conduit  # noqa: B018
+    assert api_fit_jump_max_num.conduit.fuel_use_self == approx(15000)
+    assert api_fit_jump_max_spec.conduit.fuel_use_self == approx(15000)
+    assert api_fit_jump_med.conduit.fuel_use_self == approx(6000)
+    assert api_fit_jump_low.conduit.fuel_use_self == approx(300)
+    assert api_fit_jump_zero.conduit.fuel_use_self == approx(0)
+    assert api_fit_jump_rounding.conduit.fuel_use_self == approx(3001)
+    (api_ship_jump_default,
+     api_ship_jump_excessive,
+     api_ship_jump_max_num,
+     api_ship_jump_max_spec,
+     api_ship_jump_med,
+     api_ship_jump_low,
+     api_ship_jump_zero,
+     api_ship_jump_rounding) = api_ship.get_stats(options=ItemStatsOptions(jump=(True, api_jump_options))).jump
+    assert api_ship_jump_default.conduit.fuel_use_self == approx(15000)
+    with check_no_field():
+        api_ship_jump_excessive.conduit  # noqa: B018
+    assert api_ship_jump_max_num.conduit.fuel_use_self == approx(15000)
+    assert api_ship_jump_max_spec.conduit.fuel_use_self == approx(15000)
+    assert api_ship_jump_med.conduit.fuel_use_self == approx(6000)
+    assert api_ship_jump_low.conduit.fuel_use_self == approx(300)
+    assert api_ship_jump_zero.conduit.fuel_use_self == approx(0)
+    assert api_ship_jump_rounding.conduit.fuel_use_self == approx(3001)
+
+
 def test_conduit_passenger_status(client, consts):
     eve_range_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_range)
     eve_fuel_type_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_consumption_type)
@@ -241,6 +304,38 @@ def test_conduit_passenger_status(client, consts):
         jump=(True, [StatsOptionJump(passenger_fit_ids=[api_fit_pass_disabled.id])])))
     api_ship_main_passengers = api_ship_main_stats.jump.one().conduit.fuel_use_passengers
     assert api_ship_main_passengers[api_fit_pass_disabled.id] == 0
+
+
+def test_conduit_attr_fuel_absent(client, consts):
+    eve_range_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_range)
+    eve_fuel_type_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_drive_consumption_type)
+    eve_conduit_flag_attr_id = client.mk_eve_attr(id_=consts.EveAttr.enable_perform_conduit_jump)
+    eve_conduit_count_attr_id = client.mk_eve_attr(id_=consts.EveAttr.conduit_jump_passenger_count)
+    eve_conduit_pass_attr_id = client.mk_eve_attr()
+    eve_conduit_pass_ref_attr_id = client.mk_eve_attr(id_=consts.EveAttr.jump_conduit_passenger_required_attr_id)
+    eve_conduit_fuel_use_attr_id = consts.EveAttr.conduit_jump_drive_consumption_amount
+    eve_fuel_id = client.mk_eve_item()
+    eve_main_ship_id = client.mk_eve_ship(attrs={
+        eve_range_attr_id: 5,
+        eve_fuel_type_attr_id: eve_fuel_id,
+        eve_conduit_flag_attr_id: 1,
+        eve_conduit_pass_ref_attr_id: eve_conduit_pass_attr_id,
+        eve_conduit_fuel_use_attr_id: 3000,
+        eve_conduit_count_attr_id: 30})
+    eve_pass_ship_id = client.mk_eve_ship(attrs={eve_conduit_pass_attr_id: 1})
+    client.create_sources()
+    api_sol = client.create_sol()
+    api_fit_main = api_sol.create_fit()
+    api_ship_main = api_fit_main.set_ship(type_id=eve_main_ship_id)
+    api_fit_pass = api_sol.create_fit()
+    api_fit_pass.set_ship(type_id=eve_pass_ship_id)
+    # Verification
+    api_fit_main_stats = api_fit_main.get_stats(options=FitStatsOptions(
+        jump=(True, [StatsOptionJump(passenger_fit_ids=[api_fit_pass.id])])))
+    assert api_fit_main_stats.jump.one().conduit.fuel_use_self == approx(0)
+    api_ship_main_stats = api_ship_main.get_stats(options=ItemStatsOptions(
+        jump=(True, [StatsOptionJump(passenger_fit_ids=[api_fit_pass.id])])))
+    assert api_ship_main_stats.jump.one().conduit.fuel_use_self == approx(0)
 
 
 def test_conduit_attr_conduit_flag_values_ship(client, consts):
