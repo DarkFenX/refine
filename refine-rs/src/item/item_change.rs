834edf25677e3011a2fd87565dabfdd3cb5fe653
DarkPhoenix <phoenix@mail.ru>
2026-07-16 02:07:01 +0200

    Add hybrid methods which do something and get info about created/changed item

--- refine-rs/src/item/item_change.rs
+++ refine-rs/src/item/item_change.rs
@@ -1,5 +1,6 @@
 use crate::{
     cmd::{ChangeItemEnumCmd, ChangeItemEnumError, ChangedItemIdsResp},
+    info::{ItemInfo, ItemInfoMode},
     item::Item,
 };
 
@@ -12,6 +13,23 @@ impl Item<'_, '_> {
                 // Holding mutex on sol - nothing can remove the item before we get it here
                 let mut core_item = core_sol.get_item_mut(&item_id).unwrap();
                 cmd.execute(&mut core_item)
+            })
+            .await
+    }
+    #[tracing::instrument(name = "itm-chg", level = "trace", skip_all)]
+    pub async fn change_and_get_info(
+        &mut self,
+        cmd: ChangeItemEnumCmd,
+        item_mode: ItemInfoMode,
+    ) -> Result<(ChangedItemIdsResp, ItemInfo), ChangeItemEnumError> {
+        let item_id = self.id;
+        self.sol
+            .exec_standard_fallible(move |core_sol| {
+                // Holding mutex on sol - nothing can remove the item before we get it here
+                let mut core_item = core_sol.get_item_mut(&item_id).unwrap();
+                let resp = cmd.execute(&mut core_item)?;
+                let item_info = ItemInfo::from_core(&mut core_item, item_mode);
+                Ok((resp, item_info))
             })
             .await
     }
--- refine-rs/src/item/sol_add_item.rs
+++ refine-rs/src/item/sol_add_item.rs
@@ -1,5 +1,6 @@
 use crate::{
     cmd::{AddItemEnumCmd, AddItemEnumError},
+    info::{ItemInfo, ItemInfoMode},
     item::Item,
     sol::SolarSystem,
 };
@@ -11,5 +12,22 @@ impl<'r, 's> SolarSystem<'r> {
             .exec_standard_fallible(move |core_sol| cmd.execute(core_sol).map(|cmd_resp| cmd_resp.item_id))
             .await?;
         Ok(Item::new(self, item_id))
+    }
+    #[tracing::instrument(name = "itm-add", level = "trace", skip_all)]
+    pub async fn add_item_and_get_info(
+        &'s mut self,
+        cmd: AddItemEnumCmd,
+        item_mode: ItemInfoMode,
+    ) -> Result<(Item<'r, 's>, ItemInfo), AddItemEnumError> {
+        let (item_id, item_info) = self
+            .exec_standard_fallible(move |core_sol| {
+                let item_id = cmd.execute(core_sol).map(|cmd_resp| cmd_resp.item_id)?;
+                let mut core_item = core_sol.get_item_mut(&item_id).unwrap();
+                let item_info = ItemInfo::from_core(&mut core_item, item_mode);
+                Ok::<_, AddItemEnumError>((item_id, item_info))
+            })
+            .await?;
+        let item = Item::new(self, item_id);
+        Ok((item, item_info))
     }
 }
