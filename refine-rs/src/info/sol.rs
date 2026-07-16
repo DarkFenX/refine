18ce7eca50921e98c53f09da04306c8e7b01e23c
DarkPhoenix <phoenix@mail.ru>
2026-07-16 10:45:38 +0200

    Add a way to create sol + get info

--- refine-rs/src/info/sol.rs
+++ refine-rs/src/info/sol.rs
@@ -36,28 +36,49 @@ impl SolInfo {
             id: sol_id,
             extended: match sol_mode {
                 SolInfoMode::Id => None,
-                SolInfoMode::Full => Some(SolInfoExt {
-                    fleets: core_sol
-                        .iter_fleets_mut()
-                        .map_into_iter(|mut core_fleet| FleetInfo::from_core(&mut core_fleet, fleet_mode))
-                        .collect(),
-                    fits: core_sol
-                        .iter_fits_mut()
-                        .map_into_iter(|mut core_fit| FitInfo::from_core(&mut core_fit, fit_mode, item_mode))
-                        .collect(),
-                    sw_effects: core_sol
-                        .iter_sw_effects_mut()
-                        .map_into_iter(|mut core_sw_effect| SwEffectInfo::from_core(&mut core_sw_effect, item_mode))
-                        .collect(),
-                    proj_effects: core_sol
-                        .iter_proj_effects_mut()
-                        .map_into_iter(|mut proj_effect| ProjEffectInfo::from_core(&mut proj_effect, item_mode))
-                        .collect(),
-                    sec_zone: core_sol.get_sec_zone(),
-                    default_spool: core_sol.get_default_spool(),
-                    default_incoming_dps: core_sol.get_default_incoming_dps(),
-                }),
+                SolInfoMode::Full => SolInfoExt::try_from_core(core_sol, sol_mode, fleet_mode, fit_mode, item_mode),
             },
+        }
+    }
+    pub(crate) fn from_id_and_ext(sol_id: SolarSystemId, sol_info_ext: Option<SolInfoExt>) -> Self {
+        Self {
+            id: sol_id,
+            extended: sol_info_ext,
+        }
+    }
+}
+
+impl SolInfoExt {
+    pub(crate) fn try_from_core(
+        core_sol: &mut rc::SolarSystem,
+        sol_mode: SolInfoMode,
+        fleet_mode: FleetInfoMode,
+        fit_mode: FitInfoMode,
+        item_mode: ItemInfoMode,
+    ) -> Option<Self> {
+        match sol_mode {
+            SolInfoMode::Id => None,
+            SolInfoMode::Full => Some(Self {
+                fleets: core_sol
+                    .iter_fleets_mut()
+                    .map_into_iter(|mut core_fleet| FleetInfo::from_core(&mut core_fleet, fleet_mode))
+                    .collect(),
+                fits: core_sol
+                    .iter_fits_mut()
+                    .map_into_iter(|mut core_fit| FitInfo::from_core(&mut core_fit, fit_mode, item_mode))
+                    .collect(),
+                sw_effects: core_sol
+                    .iter_sw_effects_mut()
+                    .map_into_iter(|mut core_sw_effect| SwEffectInfo::from_core(&mut core_sw_effect, item_mode))
+                    .collect(),
+                proj_effects: core_sol
+                    .iter_proj_effects_mut()
+                    .map_into_iter(|mut proj_effect| ProjEffectInfo::from_core(&mut proj_effect, item_mode))
+                    .collect(),
+                sec_zone: core_sol.get_sec_zone(),
+                default_spool: core_sol.get_default_spool(),
+                default_incoming_dps: core_sol.get_default_incoming_dps(),
+            }),
         }
     }
 }
--- refine-rs/src/sol/refine_add_sol.rs
+++ refine-rs/src/sol/refine_add_sol.rs
@@ -1,5 +1,6 @@
 use crate::{
     cmd::AddSolCmd,
+    info::{FitInfoMode, FleetInfoMode, ItemInfoMode, SolInfo, SolInfoExt, SolInfoMode},
     refine::Refine,
     sol::{SolarSystem, SolarSystemId, SolarSystemInnerGuarded},
     src::{GetSrcError, SrcAlias},
@@ -19,6 +20,31 @@ impl Refine {
         let id = self.store_inner_sol(inner_sol.clone()).await;
         let sol = SolarSystem::new(self, id, inner_sol).await;
         Ok(sol)
+    }
+    #[tracing::instrument(name = "sol-add", level = "trace", skip_all)]
+    pub async fn add_sol_and_get_info(
+        &self,
+        src_alias: Option<&SrcAlias>,
+        cmd: AddSolCmd,
+        sol_mode: SolInfoMode,
+        fleet_mode: FleetInfoMode,
+        fit_mode: FitInfoMode,
+        item_mode: ItemInfoMode,
+    ) -> Result<(SolarSystem<'_>, SolInfo), AddSolError> {
+        let core_src = self.internal_get_src(src_alias).await?.get_core().clone();
+        let (inner_sol, info_ext) = self
+            .tpool
+            .exec_standard(move || {
+                let mut core_sol = cmd.execute(&core_src);
+                let info_ext = SolInfoExt::try_from_core(&mut core_sol, sol_mode, fleet_mode, fit_mode, item_mode);
+                let inner_sol = SolarSystemInnerGuarded::new(core_sol);
+                (inner_sol, info_ext)
+            })
+            .await;
+        let id = self.store_inner_sol(inner_sol.clone()).await;
+        let info = SolInfo::from_id_and_ext(id, info_ext);
+        let sol = SolarSystem::new(self, id, inner_sol).await;
+        Ok((sol, info))
     }
     async fn store_inner_sol(&self, inner_sol: SolarSystemInnerGuarded) -> SolarSystemId {
         let mut id = SolarSystemId::new();
