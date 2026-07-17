5cec4c075d8802f3ae32f17a3644f28448f55720
DarkPhoenix <phoenix@mail.ru>
2026-07-17 16:42:41 +0200

    Add item tank stats

--- refine-rs/src/cmd/stats/item.rs
+++ refine-rs/src/cmd/stats/item.rs
@@ -3,8 +3,9 @@ use rc::ItemMutCommon;
 use crate::{
     PValue,
     stats::{
-        ItemStats, StatDmg, StatMining, StatOptionExt, StatOptionItemDmg, StatOptionItemMining, StatOptionItemOutCps,
-        StatOptionItemOutNps, StatOptionItemOutRps, StatOutReps,
+        ItemStats, StatDmg, StatEhp, StatErps, StatMining, StatOption, StatOptionEhp, StatOptionErps, StatOptionExt,
+        StatOptionItemDmg, StatOptionItemMining, StatOptionItemOutCps, StatOptionItemOutNps, StatOptionItemOutRps,
+        StatOptionRps, StatOutReps, StatRps,
     },
 };
 
@@ -17,6 +18,14 @@ pub struct GetItemStatsCmd {
     pub outgoing_nps: StatOptionExt<StatOptionItemOutNps> = StatOptionExt::Default,
     pub outgoing_rps: StatOptionExt<StatOptionItemOutRps> = StatOptionExt::Default,
     pub outgoing_cps: StatOptionExt<StatOptionItemOutCps> = StatOptionExt::Default,
+    // Tank
+    pub resists: StatOption = StatOption::Default,
+    pub hp: StatOption = StatOption::Default,
+    pub ehp: StatOptionExt<StatOptionEhp> = StatOptionExt::Default,
+    pub wc_ehp: StatOption = StatOption::Default,
+    pub rps: StatOptionExt<StatOptionRps> = StatOptionExt::Default,
+    pub erps: StatOptionExt<StatOptionErps> = StatOptionExt::Default,
+    pub breach_resist: StatOption = StatOption::Default,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,6 +51,30 @@ impl GetItemStatsCmd {
         }
         if let Some(options) = self.outgoing_rps.into_enabled(self.default) {
             stats.outgoing_rps = get_outgoing_rps_stats(core_item, options).into();
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // Tank
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        if self.resists.into_enabled(self.default) {
+            stats.resists = core_item.get_stat_resists().into();
+        }
+        if self.hp.into_enabled(self.default) {
+            stats.hp = core_item.get_stat_hp().into();
+        }
+        if let Some(options) = self.ehp.into_enabled(self.default) {
+            stats.ehp = get_ehp_stats(core_item, options).into()
+        }
+        if self.wc_ehp.into_enabled(self.default) {
+            stats.wc_ehp = core_item.get_stat_wc_ehp().into();
+        }
+        if let Some(options) = self.rps.into_enabled(self.default) {
+            stats.rps = get_rps_stats(core_item, options).into();
+        }
+        if let Some(options) = self.erps.into_enabled(self.default) {
+            stats.erps = get_erps_stats(core_item, options).into();
+        }
+        if self.breach_resist.into_enabled(self.default) {
+            stats.breach_resist = core_item.get_stat_breach_resist().into();
         }
         stats
     }
@@ -178,6 +211,40 @@ fn get_outgoing_cps_stats(
                 Ok(stat) => stats.push(Some(stat)),
                 Err(_) => return None,
             },
+        }
+    }
+    Some(stats)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Tank
+////////////////////////////////////////////////////////////////////////////////////////////////////
+fn get_ehp_stats(core_item: &mut rc::ItemMut, options: Vec<StatOptionEhp>) -> Option<Vec<StatEhp>> {
+    let mut stats = Vec::with_capacity(options.len());
+    for option in options.into_iter() {
+        match core_item.get_stat_ehp(option.incoming_dps) {
+            Ok(stat) => stats.push(stat),
+            Err(_) => return None,
+        }
+    }
+    Some(stats)
+}
+fn get_rps_stats(core_item: &mut rc::ItemMut, options: Vec<StatOptionRps>) -> Option<Vec<StatRps>> {
+    let mut stats = Vec::with_capacity(options.len());
+    for option in options.into_iter() {
+        match core_item.get_stat_rps(option.time_options, option.shield_perc) {
+            Ok(stat) => stats.push(stat),
+            Err(_) => return None,
+        }
+    }
+    Some(stats)
+}
+fn get_erps_stats(core_item: &mut rc::ItemMut, options: Vec<StatOptionErps>) -> Option<Vec<StatErps>> {
+    let mut stats = Vec::with_capacity(options.len());
+    for option in options.into_iter() {
+        match core_item.get_stat_erps(option.incoming_dps, option.time_options, option.shield_perc) {
+            Ok(stat) => stats.push(stat),
+            Err(_) => return None,
         }
     }
     Some(stats)
--- refine-rs/src/info/stats/item.rs
+++ refine-rs/src/info/stats/item.rs
@@ -1,6 +1,6 @@
 pub use crate::{
-    PValue, TriStateField,
-    stats::{StatDmg, StatMining, StatOutReps},
+    PValue, TriStateField, UnitInterval,
+    stats::{StatDmg, StatEhp, StatErps, StatHp, StatMining, StatOutReps, StatResists, StatRps},
 };
 
 pub struct ItemStats {
@@ -10,4 +10,12 @@ pub struct ItemStats {
     pub outgoing_nps: TriStateField<Vec<Option<PValue>>> = TriStateField::Absent,
     pub outgoing_rps: TriStateField<Vec<Option<StatOutReps>>> = TriStateField::Absent,
     pub outgoing_cps: TriStateField<Vec<Option<PValue>>> = TriStateField::Absent,
+    // Tank
+    pub resists: TriStateField<Vec<StatResists>> = TriStateField::Absent,
+    pub hp: TriStateField<Vec<StatHp>> = TriStateField::Absent,
+    pub ehp: TriStateField<Vec<StatEhp>> = TriStateField::Absent,
+    pub wc_ehp: TriStateField<Vec<StatEhp>> = TriStateField::Absent,
+    pub rps: TriStateField<Vec<StatRps>> = TriStateField::Absent,
+    pub erps: TriStateField<Vec<StatErps>> = TriStateField::Absent,
+    pub breach_resist: TriStateField<Vec<UnitInterval>> = TriStateField::Absent,
 }
