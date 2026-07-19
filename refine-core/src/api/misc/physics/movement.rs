2d5352f2992ec9677de5359473c46644c331c4cb
DarkPhoenix <phoenix@mail.ru>
2026-07-19 23:03:31 +0200

    Custom implementation of serialization/deserialization for movement vector

--- refine-core/src/api/misc/physics/movement.rs
+++ refine-core/src/api/misc/physics/movement.rs
@@ -6,10 +6,6 @@ use crate::{
     ud::UPhysics,
 };
 
-#[cfg_attr(
-    feature = "serde",
-    derive(serde_tuple::Serialize_tuple, serde_tuple::Deserialize_tuple)
-)]
 #[derive(Copy, Clone)]
 pub struct Movement {
     /// Movement direction of an item.
@@ -18,10 +14,6 @@ pub struct Movement {
     pub speed: PValue,
 }
 
-#[cfg_attr(
-    feature = "serde",
-    derive(serde_tuple::Serialize_tuple, serde_tuple::Deserialize_tuple)
-)]
 #[derive(Copy, Clone)]
 pub struct Direction {
     /// Degrees relatively X axis counter-clockwise.
@@ -72,6 +64,73 @@ impl Direction {
             x: Value::from_f64(az_cos * el_cos),
             y: Value::from_f64(az_sin * el_cos),
             z: Value::from_f64(el_sin),
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Custom serialization/deserialization
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#[cfg(feature = "serde")]
+mod custom_serde {
+    use serde::{
+        de::{Deserialize, Deserializer, Error, SeqAccess, Visitor},
+        ser::{Serialize, SerializeTuple, Serializer},
+    };
+
+    use super::*;
+
+    impl Serialize for Movement {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let mut tuple = serializer.serialize_tuple(3)?;
+            tuple.serialize_element(&self.direction.azimuth)?;
+            tuple.serialize_element(&self.direction.elevation)?;
+            tuple.serialize_element(&self.speed)?;
+            tuple.end()
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Movement {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            struct VisitorState;
+
+            impl<'de> Visitor<'de> for VisitorState {
+                type Value = Movement;
+
+                fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    formatter.write_str("sequence with 3 elements")
+                }
+
+                fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
+                where
+                    S: SeqAccess<'de>,
+                {
+                    let azimuth = match SeqAccess::next_element(&mut seq)? {
+                        Some(azimuth) => azimuth,
+                        None => return Err(Error::invalid_length(0, &"sequence with 3 elements")),
+                    };
+                    let elevation = match SeqAccess::next_element(&mut seq)? {
+                        Some(elevation) => elevation,
+                        None => return Err(Error::invalid_length(1, &"sequence with 3 elements")),
+                    };
+                    let speed = match SeqAccess::next_element(&mut seq)? {
+                        Some(speed) => speed,
+                        None => return Err(Error::invalid_length(2, &"sequence with 3 elements")),
+                    };
+                    Ok(Movement {
+                        direction: Direction { azimuth, elevation },
+                        speed,
+                    })
+                }
+            }
+
+            deserializer.deserialize_seq(VisitorState)
         }
     }
 }
