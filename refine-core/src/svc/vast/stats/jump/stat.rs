5ebcd3246c0830990521fd0b966f3fa3a7a25deb
DarkPhoenix <phoenix@mail.ru>
2026-07-21 18:37:18 +0200

    Serialize jump stats properly

--- refine-core/src/svc/vast/stats/jump/stat.rs
+++ refine-core/src/svc/vast/stats/jump/stat.rs
@@ -4,7 +4,7 @@ use crate::{
     ud::{FitId, ItemId},
 };
 
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", cfg_eval, serde_with::serde_as, derive(serde::Serialize))]
 #[derive(Clone)]
 pub struct StatJump {
     pub max_range: PValue,
@@ -16,7 +16,11 @@ pub struct StatJump {
         serde(rename = "conduit", skip_serializing_if = "Option::is_none")
     )]
     pub jump_conduit: Option<StatJumpConduit>,
-    #[cfg_attr(feature = "serde", serde(rename = "portals", skip_serializing_if = "Vec::is_empty"))]
+    #[cfg_attr(
+        feature = "serde",
+        serde_as(as = "serde_with::KeyValueMap<_>"),
+        serde(rename = "portals", skip_serializing_if = "Vec::is_empty")
+    )]
     pub jump_portals: Vec<StatJumpPortal>,
 }
 
@@ -31,15 +35,22 @@ pub struct StatJumpSelf {
 pub struct StatJumpConduit {
     pub max_passengers: Count,
     pub fuel_use_self: Count,
-    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Vec::is_empty"))]
+    #[cfg_attr(
+        feature = "serde",
+        serde(serialize_with = "custom_serde::as_map", skip_serializing_if = "Vec::is_empty")
+    )]
     pub fuel_use_passengers: Vec<StatJumpPassenger>,
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[derive(Clone)]
 pub struct StatJumpPortal {
+    #[cfg_attr(feature = "serde", serde(rename = "$key$"))]
     pub item_id: ItemId,
-    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Vec::is_empty"))]
+    #[cfg_attr(
+        feature = "serde",
+        serde(serialize_with = "custom_serde::as_map", skip_serializing_if = "Vec::is_empty")
+    )]
     pub fuel_use_passengers: Vec<StatJumpPassenger>,
 }
 
@@ -48,4 +59,25 @@ pub struct StatJumpPortal {
 pub struct StatJumpPassenger {
     pub fit_id: FitId,
     pub fuel_use: Option<Count>,
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Custom de/serialization
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#[cfg(feature = "serde")]
+mod custom_serde {
+    use serde::ser::{SerializeMap, Serializer};
+
+    use super::*;
+
+    pub(super) fn as_map<S>(items: &[StatJumpPassenger], serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(items.len()))?;
+        for item in items {
+            map.serialize_entry(&item.fit_id, &item.fuel_use)?;
+        }
+        map.end()
+    }
 }
