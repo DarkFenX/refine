abb970e0c6e595892b76a3c6c20cbc8562eb1996
DarkPhoenix <phoenix@mail.ru>
2026-07-21 20:52:04 +0200

    Remove unused tristate field conversions

--- refine-rs/src/util/tri_state.rs
+++ refine-rs/src/util/tri_state.rs
@@ -29,31 +29,6 @@ impl<T> From<Option<T>> for TriStateField<T> {
         }
     }
 }
-impl<T, E> From<Result<T, E>> for TriStateField<T> {
-    fn from(val: Result<T, E>) -> Self {
-        match val {
-            Ok(inner) => Self::Value(inner),
-            Err(_) => Self::None,
-        }
-    }
-}
-
-impl<T> From<Option<T>> for TriStateField<Vec<T>> {
-    fn from(val: Option<T>) -> Self {
-        match val {
-            Some(inner) => Self::Value(vec![inner]),
-            None => Self::None,
-        }
-    }
-}
-impl<T, E> From<Result<T, E>> for TriStateField<Vec<T>> {
-    fn from(val: Result<T, E>) -> Self {
-        match val {
-            Ok(inner) => Self::Value(vec![inner]),
-            Err(_) => Self::None,
-        }
-    }
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Custom de/serialization
