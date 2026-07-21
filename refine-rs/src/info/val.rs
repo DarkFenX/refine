870d4fe4504f469ee31ff9356850fc95e683ade1
DarkPhoenix <phoenix@mail.ru>
2026-07-21 18:40:44 +0200

    Minor style fixes

--- refine-rs/src/info/val.rs
+++ refine-rs/src/info/val.rs
@@ -3,14 +3,14 @@ use crate::val::{FitValInfoDetails, SolValInfoDetails};
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 pub struct FitValInfo {
     pub passed: bool,
-    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "skip_details_fit"))]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "custom_serde::skip_details_fit"))]
     pub details: Option<FitValInfoDetails>,
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 pub struct SolValInfo {
     pub passed: bool,
-    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "skip_details_sol"))]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "custom_serde::skip_details_sol"))]
     pub details: Option<SolValInfoDetails>,
 }
 
@@ -18,17 +18,20 @@ pub struct SolValInfo {
 // Custom de/serialization
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #[cfg(feature = "serde")]
-fn skip_details_fit(details: &Option<FitValInfoDetails>) -> bool {
-    match details {
-        Some(details) => details.all_passed(),
-        None => true,
-    }
-}
+mod custom_serde {
+    use super::*;
 
-#[cfg(feature = "serde")]
-fn skip_details_sol(details: &Option<SolValInfoDetails>) -> bool {
-    match details {
-        Some(details) => details.all_passed(),
-        None => true,
+    pub(super) fn skip_details_fit(details: &Option<FitValInfoDetails>) -> bool {
+        match details {
+            Some(details) => details.all_passed(),
+            None => true,
+        }
+    }
+
+    pub(super) fn skip_details_sol(details: &Option<SolValInfoDetails>) -> bool {
+        match details {
+            Some(details) => details.all_passed(),
+            None => true,
+        }
     }
 }
