d1ff9e19331f0e7488eb456850881eb71eebfc52
DarkPhoenix <phoenix@mail.ru>
2026-08-04 01:00:21 +0200

    Add more docs on items

--- refine-core/src/lib.rs
+++ refine-core/src/lib.rs
@@ -19,15 +19,61 @@
 //! setup consists of the following steps:
 //!
 //! - you feed an [`ed::EveDataHandler`] implementation and an optional [`ad::AdaptedDataCacher`]
-//!   implementation to the [`Src`] constructor
+//!   implementation to the [`Src`] constructor;
 //! - during [`Src`] initialization, the library attempts to use cached adapted data. If its
 //!   fingerprint (data version \+ library version at the time of cache generation) and current
 //!   fingerprint (version of currently provided data + current library version) matched, cached
 //!   data is attempted to be loaded. If cache couldn't be loaded or fingerprints mismatch, EVE data
-//!   is fetched and converted into adapted data (this process is relatively heavy on both IO and
-//!   CPU), which is then fed to adapted data cacher implementation.
-//! - you create [`SolarSystem`], and manipulate it to create fits with ships and items, and fetch
-//!   data and stats
+//!   is fetched and converted into adapted data. This process is relatively heavy on both IO and
+//!   CPU, expect it to take approximately 1 second with locally sourced data. Adapted data is then
+//!   fed to adapted data cacher implementation for caching and further converted for ease of access
+//!   by the lib;
+//! - you create [`SolarSystem`], specifying [`Src`] it should usee, and manipulate it to create
+//!   fits with ships and items, and fetch various and stats.
+//!
+//! Three main entities available via [`SolarSystem`] are fleets, fits and items. Items come in many
+//! kinds:
+//!
+//! - autocharges: charges which are automatically loaded into some item kinds by some effects, e.g.
+//!   LR fighters have their bombs set as autocharges. Unlike regular charges, they expose effect ID
+//!   which is using them, and cannot be removed;
+//! - boosters: boosters expose slot they use and have side-effect-related methods;
+//! - character: in EVE, character is an actual item which carries some attributes, and applies some
+//!   modifications upon other items. Modern EVE seems to use just type ID 1373 for characters, but
+//!   the lib does not set it automatically to every fit, you will have to add it yourself;
+//! - charges: ammo, scripts and other things loadable into modules;
+//! - drones: one of the more complex item kinds. Drones are separate "physic" objects, i.e. can
+//!   have their own coordinate and movement set. Can be mutated, can have NPC prop mode set to
+//!   control if they are using their pseudo-MWD or not (which affects their speed and signature
+//!   radius);
+//! - fighters: like drones, are "physic" objects. A fighter item represents a fighter squad, with
+//!   ability to fetch squad count data, and override count. Fighters usually have multiple active
+//!   abilities, which enable or disable effects on them;
+//! - fit-wide effects: environmental effects which are assigned to a fit, and applied to all items
+//!   of that fit;
+//! - implants: just implant, the only special is that they expose slot they use under current EVE
+//!   data;
+//! - modules: like drones, one of more complex item kinds; can be mutated, have lots of extra
+//!   methods which control how they cycle;
+//! - projected effects: environmental effects which are applied only to specific items;
+//! - rigs: ship rigs, nothing special about those;
+//! - services: citadel/structure services like cloning, the only special thing is that they have
+//!   their own set of states. FLEX structures also have services, which are usually automatically
+//!   installed. In the lib, they are not automatically installed, you will have to add them
+//!   yourself;
+//! - ships: another "physic" object. Not much else is special about it, aside that all the items it
+//!   carries (like modules) inherit its coordinates and movement;
+//! - skills: character skill, assigned to a fit in this case. Skills have levels, and a single fit
+//!   cannot have more than 2 skills with the same type ID. This is the only item which enforces
+//!   strict type ID-based restriction;
+//! - stances: t3 ship modes (e.g. t3 destroyers) are implemented as separate item, this is the item
+//!   kind for that;
+//! - subsystems: item kind to handle t3 ship subsystems; under the hood, subsystems take specific
+//!   subsystem slot, and subsystems expose that (similar to booster/implant slots);
+//! - system-wide effect: environmental effect which is applied to all items in the solar system.
+//!
+//! Most of those items are assigned directly to a fit, except for (auto)charges and system-wide/
+//! fit-wide/projected effects. Modules are the only ones which are using ordered container.
 
 pub use api::{
     Ability, AbilityId, AbilityIter, AbilityMut, AddMode, Affector, AttrId, Autocharge, AutochargeMut, Booster,
