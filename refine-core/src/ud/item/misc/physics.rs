ed12ad25d38516f25dcc1b40c52c04717ce4796a
DarkPhoenix <phoenix@mail.ru>
2026-08-05 22:41:29 +0200

    Make default UPhysics to have actual unit vector instead of zero vector, in order to expose correct direction instead of 45/45/0 when movement of drone/fighter/ship was not set

--- refine-core/src/ud/item/misc/physics.rs
+++ refine-core/src/ud/item/misc/physics.rs
@@ -1,9 +1,16 @@
-use crate::{misc::Xyz, num::PValue};
+use crate::{
+    misc::Xyz,
+    num::{PValue, Value},
+};
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Default)]
 pub(crate) struct UPhysics {
     pub(crate) coordinates: Xyz,
     // Direction as a unit vector relatively object coordinates
-    pub(crate) direction: Xyz,
+    pub(crate) direction: Xyz = Xyz {
+        x: Value::ONE,
+        y: Value::ZERO,
+        z: Value::ZERO,
+    },
     pub(crate) speed: PValue,
 }
