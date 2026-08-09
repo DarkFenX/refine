22fafc8dfc6f5112123bf6e3dc620805c97a0f42
DarkPhoenix <phoenix@mail.ru>
2026-08-09 17:33:33 +0200

    Fix duplicate wording in error chaing for adding skill

--- refine-rs/src/cmd/inner/item_skill/add.rs
+++ refine-rs/src/cmd/inner/item_skill/add.rs
@@ -80,6 +80,6 @@ impl ICmdSkillAddICtx {
 
 #[derive(thiserror::Error, Debug)]
 pub enum FitAddSkillError {
-    #[error("failed to add skill")]
+    #[error(transparent)]
     SkillAddFailed(#[from] rc::err::AddSkillError),
 }
