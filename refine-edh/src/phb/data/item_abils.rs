73b71a416655a23f38a90cc9aeaeb35fb1081c2c
DarkPhoenix <phoenix@mail.ru>
2026-08-11 17:19:26 +0200

    Simplify item ability data conversion

--- refine-edh/src/phb/data/item_abils.rs
+++ refine-edh/src/phb/data/item_abils.rs
@@ -13,27 +13,30 @@ pub(in crate::phb) struct PItemFighterAbils {
 }
 impl KeyMergeOne<rc::ed::EItemAbil> for PItemFighterAbils {
     fn key_merge(self, key: Key) -> Vec<rc::ed::EItemAbil> {
-        let mut vec = Vec::new();
-        for (slot, p_abil_data) in [self.ability_slot0, self.ability_slot1, self.ability_slot2]
-            .into_iter()
-            .enumerate()
-        {
-            let Some(p_abil_data) = p_abil_data else {
-                continue;
-            };
-            let (charge_count, charge_rearm_duration) = p_abil_data
-                .charges
-                .map_or((None, None), |v| (Some(v.charge_count), Some(v.rearm_time_seconds)));
-            vec.push(rc::ed::EItemAbil {
+        [
+            (0, self.ability_slot0),
+            (1, self.ability_slot1),
+            (2, self.ability_slot2),
+        ]
+        .into_iter()
+        .filter_map(|(slot, p_abil_data)| {
+            let p_abil_data = p_abil_data?;
+            Some(rc::ed::EItemAbil {
                 item_id: rc::ed::EItemId::from_i32(key),
                 abil_id: rc::ed::EAbilId::from_i32(p_abil_data.ability_id),
-                slot: rc::ed::EInt::from_i32(slot.saturating_cast()),
+                slot: rc::ed::EInt::from_i32(slot),
                 cooldown: p_abil_data.cooldown_seconds.map(rc::ed::EFloat::from_f64),
-                charge_count: charge_count.map(rc::ed::EInt::from_i32),
-                charge_rearm_duration: charge_rearm_duration.map(rc::ed::EFloat::from_f64),
-            });
-        }
-        vec
+                charge_count: p_abil_data
+                    .charges
+                    .as_ref()
+                    .map(|v| rc::ed::EInt::from_i32(v.charge_count)),
+                charge_rearm_duration: p_abil_data
+                    .charges
+                    .as_ref()
+                    .map(|v| rc::ed::EFloat::from_f64(v.rearm_time_seconds)),
+            })
+        })
+        .collect()
     }
 }
 
