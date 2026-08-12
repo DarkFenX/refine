ddb3d4034edf1bbbf9f6557fca0641328a624ebe
DarkPhoenix <phoenix@mail.ru>
2026-08-12 13:40:34 +0200

    Add conversion helper, similar to the one implemented in the app, to reduce code duplication

--- refine-edh/src/phb/data/item_space_comp.rs
+++ refine-edh/src/phb/data/item_space_comp.rs
@@ -20,64 +20,36 @@ impl KeyMergeOne<rc::ed::EItemBuff> for PItemSpaceComp {
     fn key_merge(self, key: Key) -> Vec<rc::ed::EItemBuff> {
         vec![rc::ed::EItemBuff {
             item_id: rc::ed::EItemId::from_i32(key),
-            system_wide_buffs: self.system_wide_effects.and_then(|v| v.global_debuffs).map(|data| {
-                rc::ed::EItemBuffData {
-                    buffs: data
-                        .dbuffs
-                        .into_iter()
-                        .map(|(id, value)| rc::ed::EItemBuffEntry {
-                            id: rc::ed::EBuffId::from_i32(id),
-                            value: rc::ed::EFloat::from_f64(value),
-                        })
-                        .collect(),
-                    item_list_filter: data.eligible_type_list_id.map(rc::ed::EItemListId::from_i32),
-                }
-            }),
-            system_emitter_buffs: self.system_dbuff_emitter.map(|data| rc::ed::EItemBuffData {
-                buffs: data
-                    .dbuff_collections
-                    .into_iter()
-                    .map(|(id, value)| rc::ed::EItemBuffEntry {
-                        id: rc::ed::EBuffId::from_i32(id),
-                        value: rc::ed::EFloat::from_f64(value),
-                    })
-                    .collect(),
-                item_list_filter: None,
-            }),
-            proxy_effect_buffs: self.applied_proximity_effects.map(|data| rc::ed::EItemBuffData {
-                buffs: data
-                    .effects
-                    .into_iter()
-                    .map(|(id, value)| rc::ed::EItemBuffEntry {
-                        id: rc::ed::EBuffId::from_i32(id),
-                        value: rc::ed::EFloat::from_f64(value),
-                    })
-                    .collect(),
-                item_list_filter: None,
-            }),
-            proxy_trigger_buffs: self.proximity_trap.map(|data| rc::ed::EItemBuffData {
-                buffs: data
-                    .dbuffs
-                    .into_iter()
-                    .map(|(id, value)| rc::ed::EItemBuffEntry {
-                        id: rc::ed::EBuffId::from_i32(id),
-                        value: rc::ed::EFloat::from_f64(value),
-                    })
-                    .collect(),
-                item_list_filter: data.trigger_filter_type_list_id.map(rc::ed::EItemListId::from_i32),
-            }),
-            ship_link_buffs: self.link_with_ship.map(|data| rc::ed::EItemBuffData {
-                buffs: data
-                    .dbuffs
-                    .into_iter()
-                    .map(|(id, value)| rc::ed::EItemBuffEntry {
-                        id: rc::ed::EBuffId::from_i32(id),
-                        value: rc::ed::EFloat::from_f64(value),
-                    })
-                    .collect(),
-                item_list_filter: data.linkable_ship_type_list_id.map(rc::ed::EItemListId::from_i32),
-            }),
+            system_wide_buffs: self
+                .system_wide_effects
+                .and_then(|v| v.global_debuffs)
+                .map(|data| into_item_buff_data(data.dbuffs, data.eligible_type_list_id)),
+            system_emitter_buffs: self
+                .system_dbuff_emitter
+                .map(|data| into_item_buff_data(data.dbuff_collections, None)),
+            proxy_effect_buffs: self
+                .applied_proximity_effects
+                .map(|data| into_item_buff_data(data.effects, None)),
+            proxy_trigger_buffs: self
+                .proximity_trap
+                .map(|data| into_item_buff_data(data.dbuffs, data.trigger_filter_type_list_id)),
+            ship_link_buffs: self
+                .link_with_ship
+                .map(|data| into_item_buff_data(data.dbuffs, data.linkable_ship_type_list_id)),
         }]
+    }
+}
+
+fn into_item_buff_data(buffs: Vec<(i32, f64)>, item_list_id: Option<i32>) -> rc::ed::EItemBuffData {
+    rc::ed::EItemBuffData {
+        buffs: buffs
+            .into_iter()
+            .map(|(id, value)| rc::ed::EItemBuffEntry {
+                id: rc::ed::EBuffId::from_i32(id),
+                value: rc::ed::EFloat::from_f64(value),
+            })
+            .collect(),
+        item_list_filter: item_list_id.map(rc::ed::EItemListId::from_i32),
     }
 }
 
