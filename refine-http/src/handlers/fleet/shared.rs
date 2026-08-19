fab8fb741e7e6998afbc36090adb2bfb333050a8
DarkPhoenix <phoenix@mail.ru>
2026-08-19 15:18:29 +0200

    Actually pass new parameters via HTTP

--- refine-http/src/handlers/fleet/shared.rs
+++ refine-http/src/handlers/fleet/shared.rs
@@ -4,9 +4,16 @@ use serde::Deserialize;
 pub(crate) struct FleetParams {
     #[serde(default)]
     fleet: rs::FleetInfoMode,
+    #[serde(default)]
+    fit: rs::FitInfoMode,
+    #[serde(default)]
+    item: rs::ItemInfoMode,
 }
 impl FleetParams {
     pub(super) fn into_cmd(self) -> rs::FleetInfoCmd {
-        rs::FleetInfoCmd::new().with_fleet(self.fleet)
+        rs::FleetInfoCmd::new()
+            .with_fleet(self.fleet)
+            .with_fit_default(self.fit)
+            .with_item_default(self.item)
     }
 }
