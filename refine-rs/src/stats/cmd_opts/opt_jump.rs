6d704fc191eef8624ddaede6e1af339d61404494
DarkPhoenix <phoenix@mail.ru>
2026-08-19 17:25:02 +0200

    Parametrize jump stat too

--- refine-rs/src/stats/cmd_opts/opt_jump.rs
+++ refine-rs/src/stats/cmd_opts/opt_jump.rs
@@ -5,13 +5,22 @@ use crate::{
     stats::{StatAffectors, StatJumpRange},
 };
 
-#[cfg_attr(feature = "serde", derive(serde::Deserialize))]
-#[derive(Clone, Default)]
-pub struct StatOptionJump {
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize),
+    serde(bound(deserialize = "F: serde::Deserialize<'de>"))
+)]
+#[derive(Clone)]
+pub struct StatOptionJump<F = FitId> {
     #[cfg_attr(feature = "serde", serde(default))]
     pub range: StatJumpRange = StatJumpRange::Max,
     #[cfg_attr(feature = "serde", serde(default))]
-    pub passenger_fit_ids: Vec<FitId> = Vec::new(),
+    pub passenger_fit_ids: Vec<F> = Vec::new(),
     #[cfg_attr(feature = "serde", serde(default))]
     pub passenger_fuel_affectors: StatAffectors = CtlAffectors::Unmodified,
+}
+impl<F> Default for StatOptionJump<F> {
+    fn default() -> Self {
+        Self { .. }
+    }
 }
