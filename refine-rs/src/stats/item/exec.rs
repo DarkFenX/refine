5e69ddc9d31e25ea2d41b443c39c3c4c56b98cd3
DarkPhoenix <phoenix@mail.ru>
2026-08-21 22:06:34 +0200

    Refactor HKT on stats inner options types not to expose trait to public interface, which shouldn't be there

--- refine-rs/src/stats/item/exec.rs
+++ refine-rs/src/stats/item/exec.rs
@@ -18,126 +18,126 @@ impl ItemStatsOptionsInt<StatOptionResolved, FitId, ItemId> {
     pub(in crate::stats) fn execute(&self, core_item: &mut rc::ItemMut) -> ItemStats {
         let mut stats = ItemStats { .. };
         // Output
-        if let Some(options) = self.dmg.as_ref() {
+        if let Some(options) = self.dmg.get() {
             stats.dmg = get_dmg_stats(core_item, options);
         }
-        if let Some(options) = self.mps.as_ref() {
+        if let Some(options) = self.mps.get() {
             stats.mps = get_mps_stats(core_item, options);
         }
-        if let Some(options) = self.outgoing_nps.as_ref() {
+        if let Some(options) = self.outgoing_nps.get() {
             stats.outgoing_nps = get_outgoing_nps_stats(core_item, options);
         }
-        if let Some(options) = self.outgoing_cps.as_ref() {
+        if let Some(options) = self.outgoing_cps.get() {
             stats.outgoing_cps = get_outgoing_cps_stats(core_item, options);
         }
-        if let Some(options) = self.outgoing_rps.as_ref() {
+        if let Some(options) = self.outgoing_rps.get() {
             stats.outgoing_rps = get_outgoing_rps_stats(core_item, options);
         }
         // Tank
-        if self.resists {
+        if self.resists.is_enabled() {
             stats.resists = StatResult::from_result_outer(core_item.get_stat_resists());
         }
-        if self.hp {
+        if self.hp.is_enabled() {
             stats.hp = StatResult::from_result_outer(core_item.get_stat_hp());
         }
-        if let Some(options) = self.ehp.as_ref() {
+        if let Some(options) = self.ehp.get() {
             stats.ehp = get_ehp_stats(core_item, options);
         }
-        if self.wc_ehp {
+        if self.wc_ehp.is_enabled() {
             stats.wc_ehp = StatResult::from_result_outer(core_item.get_stat_wc_ehp());
         }
-        if let Some(options) = self.rps.as_ref() {
+        if let Some(options) = self.rps.get() {
             stats.rps = get_rps_stats(core_item, options);
         }
-        if let Some(options) = self.erps.as_ref() {
+        if let Some(options) = self.erps.get() {
             stats.erps = get_erps_stats(core_item, options);
         }
-        if self.breach_resist {
+        if self.breach_resist.is_enabled() {
             stats.breach_resist = StatResult::from_result_outer(core_item.get_stat_breach_resist());
         }
         // Cap
-        if self.cap_amount {
+        if self.cap_amount.is_enabled() {
             stats.cap_amount = StatResult::from_result_outer(core_item.get_stat_cap_amount());
         }
-        if let Some(options) = self.cap_balance.as_ref() {
+        if let Some(options) = self.cap_balance.get() {
             stats.cap_balance = get_cap_balance_stats(core_item, options);
         }
-        if let Some(options) = self.cap_sim.as_ref() {
+        if let Some(options) = self.cap_sim.get() {
             stats.cap_sim = get_cap_sim_stats(core_item, options);
         }
-        if self.neut_resist {
+        if self.neut_resist.is_enabled() {
             stats.neut_resist = StatResult::from_result_outer(core_item.get_stat_neut_resist());
         }
         // Sensors
-        if self.locks {
+        if self.locks.is_enabled() {
             stats.locks = StatResult::from_result_outer(core_item.get_stat_locks());
         }
-        if self.lock_range {
+        if self.lock_range.is_enabled() {
             stats.lock_range = StatResult::from_result_outer(core_item.get_stat_lock_range());
         }
-        if self.scan_res {
+        if self.scan_res.is_enabled() {
             stats.scan_res = StatResult::from_result_outer(core_item.get_stat_scan_res());
         }
-        if self.sensors {
+        if self.sensors.is_enabled() {
             stats.sensors = StatResult::from_result_outer(core_item.get_stat_sensors());
         }
-        if self.dscan_range {
+        if self.dscan_range.is_enabled() {
             stats.dscan_range = StatResult::from_result_outer(core_item.get_stat_dscan_range());
         }
-        if self.probing_size {
+        if self.probing_size.is_enabled() {
             stats.probing_size = StatResult::from_result_outer(core_item.get_stat_probing_size());
         }
-        if let Some(options) = self.incoming_jam.as_ref() {
+        if let Some(options) = self.incoming_jam.get() {
             stats.incoming_jam = get_incoming_jam_stats(core_item, options);
         }
         // Mobility
-        if self.speed {
+        if self.speed.is_enabled() {
             stats.speed = StatResult::from_result_outer(core_item.get_stat_speed());
         }
-        if self.agility {
+        if self.agility.is_enabled() {
             stats.agility = StatResult::from_result_outer(core_item.get_stat_agility());
         }
-        if self.align_time {
+        if self.align_time.is_enabled() {
             stats.align_time = StatResult::from_result_outer(core_item.get_stat_align_time());
         }
-        if self.sig_radius {
+        if self.sig_radius.is_enabled() {
             stats.sig_radius = StatResult::from_result_outer(core_item.get_stat_sig_radius());
         }
-        if let Some(options) = self.mass.as_ref() {
+        if let Some(options) = self.mass.get() {
             stats.mass = get_mass_stats(core_item, options);
         }
-        if self.warp_speed {
+        if self.warp_speed.is_enabled() {
             stats.warp_speed = StatResult::from_result_outer(core_item.get_stat_warp_speed());
         }
-        if self.max_warp_range {
+        if self.max_warp_range.is_enabled() {
             stats.max_warp_range = StatResult::from_result_outer(core_item.get_stat_max_warp_range());
         }
-        if let Some(options) = self.jump.as_ref() {
+        if let Some(options) = self.jump.get() {
             stats.jump = get_jump_stats(core_item, options);
         }
         // Misc
-        if self.drone_control_range {
+        if self.drone_control_range.is_enabled() {
             stats.drone_control_range = StatResult::from_result_outer(core_item.get_stat_drone_control_range());
         }
-        if self.can_warp {
+        if self.can_warp.is_enabled() {
             stats.can_warp = StatResult::from_result_outer(core_item.get_stat_can_warp());
         }
-        if self.can_jump_gate {
+        if self.can_jump_gate.is_enabled() {
             stats.can_jump_gate = StatResult::from_result_outer(core_item.get_stat_can_jump_gate());
         }
-        if self.can_jump_wormhole {
+        if self.can_jump_wormhole.is_enabled() {
             stats.can_jump_wormhole = StatResult::from_result_outer(core_item.get_stat_can_jump_wormhole());
         }
-        if self.can_jump_drive {
+        if self.can_jump_drive.is_enabled() {
             stats.can_jump_drive = StatResult::from_result_outer(core_item.get_stat_can_jump_drive());
         }
-        if self.can_dock_station {
+        if self.can_dock_station.is_enabled() {
             stats.can_dock_station = StatResult::from_result_outer(core_item.get_stat_can_dock_station());
         }
-        if self.can_dock_citadel {
+        if self.can_dock_citadel.is_enabled() {
             stats.can_dock_citadel = StatResult::from_result_outer(core_item.get_stat_can_dock_citadel());
         }
-        if self.can_tether {
+        if self.can_tether.is_enabled() {
             stats.can_tether = StatResult::from_result_outer(core_item.get_stat_can_tether());
         }
         stats
