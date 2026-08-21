28c9a9047d9f997ac8054635be7fa1777577ceff
DarkPhoenix <phoenix@mail.ru>
2026-08-21 15:56:48 +0200

    Serialize items as a map in fit stats

--- refine-rs/src/stats/fit/result.rs
+++ refine-rs/src/stats/fit/result.rs
@@ -10,11 +10,15 @@ use crate::{
     },
 };
 
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", cfg_eval, serde_with::serde_as, derive(serde::Serialize))]
 #[derive(Clone)]
 pub struct FitStatsResp {
     pub fit: FitStats,
-    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Vec::is_empty"))]
+    #[cfg_attr(
+        feature = "serde",
+        serde_as(as = "serde_with::Map<_, _>"),
+        serde(skip_serializing_if = "Vec::is_empty")
+    )]
     pub items: Vec<(ItemId, ItemStats)>,
 }
 
