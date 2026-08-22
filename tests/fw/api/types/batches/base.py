0c6bea49a998b9f3c300f4229b8baf4cda731d62
DarkPhoenix <phoenix@mail.ru>
2026-08-22 23:34:20 +0200

    Fix an oversight

--- tests/fw/api/types/batches/base.py
+++ tests/fw/api/types/batches/base.py
@@ -70,10 +70,10 @@ class BaseCmdBatchCtx:
         self._ret_datas[index] = EntityData(kind=DataFillKind.copy, data=data)
         return Item(client=self._client, data=data, sol_id=self._sol_id)
 
-    def _make_stats[T](self, *, cls: type[T], path: str | None = None) -> T:
+    def _make_stats[T](self, *, cls: type[T]) -> T:
         index = len(self._commands) - 1
         data = {}
-        self._ret_datas[index] = EntityData(kind=DataFillKind.copy, data=data, path=path)
+        self._ret_datas[index] = EntityData(kind=DataFillKind.copy, data=data)
         return cls(data=data)
 
     def _make_val_result[T](self, *, cls: type[T]) -> T:
