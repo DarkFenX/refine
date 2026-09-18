9cee42cf8a232e50b3f644094f64f68e7b69a906
DarkPhoenix <phoenix@mail.ru>
2026-09-18 15:47:28 +0200

    Add comment, change one of checks in logging setup

--- refine-http/src/logging.rs
+++ refine-http/src/logging.rs
@@ -46,7 +46,9 @@ pub(crate) fn setup_logging(settings: SettingsLog) -> (Option<WorkerGuard>, LogB
         .with_filter(refine_targets(STDOUT_LEVEL));
     // We log into file only if we've been given path and appropriate log level
     let (file_log, file_guard, effective_max_level) = match (settings.dir, LevelFilter::from_str(&settings.level)) {
-        (Some(dir), Ok(max_level)) if max_level != LevelFilter::OFF && !settings.level.is_empty() => {
+        // Tracing parses empty string into error level for some reason; consider it as "off"
+        // instead with the additional check
+        (Some(dir), Ok(max_level)) if max_level > LevelFilter::OFF && !settings.level.is_empty() => {
             let (rotation, time_format) = match settings.rotate {
                 true => (tracing_appender::rolling::Rotation::DAILY, TIME_FORMAT_SHORT),
                 false => (tracing_appender::rolling::Rotation::NEVER, TIME_FORMAT_FULL),
